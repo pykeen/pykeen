8b71c9703e3d356e0ffc19506c8ce8373583f5a8
Mehdi Ali <ali-mehdi@live.de>
2018-06-06 18:02:27 +0200

    Implement TransE and TransH in PyTorch

---
+++ src/kg_embeddings_model/trans_e.py
@@ -0,0 +1,93 @@
+import torch
+import torch.autograd
+import torch.nn as nn
+
+from utilities.constants import EMBEDDING_DIM, MARGIN_LOSS, NUM_ENTITIES, NUM_RELATIONS
+
+
+class TransE(nn.Module):
+
+    def __init__(self, config):
+        super(TransE, self).__init__()
+        # A simple lookup table that stores embeddings of a fixed dictionary and size
+
+        num_entities = config[NUM_ENTITIES]
+        num_relations = config[NUM_RELATIONS]
+        embedding_dim = config[EMBEDDING_DIM]
+        margin_loss = config[MARGIN_LOSS]
+
+        self.entities_embeddings = nn.Embedding(num_entities, embedding_dim)
+        self.relation_embeddings = nn.Embedding(num_relations, embedding_dim)
+        self.margin_loss = margin_loss
+
+    def loss_fct(self, pos_score, neg_score):
+        """
+
+        :param pos_score:
+        :param neg_score:
+        :return:
+        """
+        criterion = nn.MarginRankingLoss(margin=self.margin_loss, size_average=False)
+        # y == -1 indicates that second input to criterion should get a larger loss
+        y = torch.Tensor([-1])
+        pos_score = pos_score.unsqueeze(0)
+        neg_score = neg_score.unsqueeze(0)
+        loss = criterion(pos_score, neg_score, y)
+
+        return loss
+
+    def calc_score(self, h_emb, r_emb, t_emb):
+        """
+
+        :param h_emb:
+        :param r_emb:
+        :param t_emb:
+        :return:
+        """
+        # TODO: - torch.abs(h_emb + r_emb - t_emb)
+        # Compute score and transform result to 1D tensor
+        score = - torch.sum(torch.abs(h_emb + r_emb - t_emb))
+
+        return score
+
+    def predict(self, head, relation, tail):
+        """
+
+        :param head:
+        :param relation:
+        :param tail:
+        :return:
+        """
+        head_emb = self.embeddings(head)
+        relation_emb = self.embeddings(relation)
+        tail_emb = self.embeddings(tail)
+
+        score = self.calc_score(h_emb=head_emb, r_emb=relation_emb, t_emb=tail_emb)
+
+        return score
+
+    def forward(self, pos_exmpl, neg_exmpl):
+        """
+
+        :param pos_exmpl:
+        :param neg_exmpl:
+        :return:
+        """
+
+        pos_h, pos_r, pos_t = pos_exmpl
+        neg_h, neg_r, neg_t, = neg_exmpl
+
+        pos_h_emb = self.entities_embeddings(pos_h)
+        pos_r_emb = self.relation_embeddings(pos_r)
+        pos_t_emb = self.entities_embeddings(pos_t)
+
+        neg_h_emb = self.entities_embeddings(neg_h)
+        neg_r_emb = self.relation_embeddings(neg_r)
+        neg_t_emb = self.entities_embeddings(neg_t)
+
+        pos_score = self.calc_score(h_emb=pos_h_emb, r_emb=pos_r_emb, t_emb=pos_t_emb)
+        neg_score = self.calc_score(h_emb=neg_h_emb, r_emb=neg_r_emb, t_emb=neg_t_emb)
+
+        loss = self.loss_fct(pos_score=pos_score, neg_score=neg_score)
+
+        return loss
---
+++ src/kg_embeddings_model/trans_h.py
@@ -0,0 +1,112 @@
+
+import torch
+import torch.nn as nn
+import torch.autograd
+
+from utilities.constants import EMBEDDING_DIM, MARGIN_LOSS, NUM_ENTITIES, NUM_RELATIONS
+
+'https://github.com/thunlp/OpenKE/blob/OpenKE-PyTorch/models/TransH.py'
+class TransH(nn.Module):
+
+    def __init__(self, config):
+        super(TransH, self).__init__()
+
+        num_entities = config[NUM_ENTITIES]
+        num_relations = config[NUM_RELATIONS]
+        embedding_dim = config[EMBEDDING_DIM]
+        margin_loss = config[MARGIN_LOSS]
+
+        # A simple lookup table that stores embeddings of a fixed dictionary and size
+        self.entities_embeddings = nn.Embedding(num_entities, embedding_dim)
+        self.relation_embeddings = nn.Embedding(num_relations, embedding_dim)
+        self.normal_vector_embeddings = nn.Embedding(num_relations, embedding_dim)
+        self.margin_loss = margin_loss
+
+    def project_entity(self, entity_emb, normal_vec_emb):
+        projection = entity_emb - (normal_vec_emb.T*entity_emb)*normal_vec_emb
+
+        return projection
+
+    def compute_score(self, h_emb, r_emb, t_emb):
+        """
+
+        :param h_emb:
+        :param r_emb:
+        :param t_emb:
+        :return:
+        """
+        score = - (torch.sum(torch.abs(h_emb + r_emb - t_emb))**2)
+
+        return score
+
+    def loss_fct(self, pos_score, neg_score):
+        """
+
+        :param pos_score:
+        :param neg_score:
+        :return:
+        """
+        criterion = nn.MarginRankingLoss(margin=self.margin_loss, size_average=False)
+        # y == -1 indicates that second input to criterion should get a larger loss
+        y = torch.Tensor([-1])
+        pos_score = pos_score.unsqueeze(0)
+        neg_score = neg_score.unsqueeze(0)
+        loss = criterion(pos_score, neg_score, y)
+
+        return loss
+
+    def predict(self, head, relation, tail):
+        """
+
+        :param head:
+        :param relation:
+        :param tail:
+        :return:
+        """
+        head_emb = self.entities_embeddings(head)
+        tail_emb = self.entities_embeddings(tail)
+        relation_emb = self.relation_embeddings(relation)
+        rel_normal_emb = self.normal_vector_embeddings(relation)
+
+        head_emb_projected = self.project_entity(entity_emb=head_emb, normal_vec_emb=rel_normal_emb)
+        tail_emb_projected = self.project_entity(entity_emb=tail_emb, normal_vec_emb=rel_normal_emb)
+
+        score = self.compute_score(h_emb=head_emb_projected, relation_emb=relation_emb, t_emb=tail_emb_projected)
+
+        return score
+
+    def forward(self, pos_exmpl, neg_exmpl):
+        """
+
+        :param pos_exmpl:
+        :param neg_exmpl:
+        :return:
+        """
+
+        pos_head, pos_rel, pos_tail = pos_exmpl
+        neg_head, neg_rel, neg_tail = neg_exmpl
+
+        pos_head_emb = self.entities_embeddings(pos_head)
+        pos_rel_emb = self.relation_embeddings(pos_rel)
+        pos_tail_emb = self.entities_embeddings(pos_tail)
+        pos_normal_emb = self.normal_vector_embeddings(pos_rel)
+
+        neg_head_emb = self.entities_embeddings(neg_head)
+        neg_rel_emb = self.relation_embeddings(neg_rel)
+        neg_tail_emb = self.entities_embeddings(neg_tail)
+        neg_normal_emb = self.normal_vector_embeddings(neg_rel)
+
+        projected_head_pos = self.project_entity(entity_emb=pos_head_emb,normal_vec_emb=pos_normal_emb)
+        projected_tail_pos = self.project_entity(entity_emb=pos_tail_emb, normal_vec_emb=pos_normal_emb)
+
+        projected_head_neg = self.project_entity(entity_emb=neg_head_emb, normal_vec_emb=neg_normal_emb)
+        projected_tail_neg = self.project_entity(entity_emb=neg_tail_emb, normal_vec_emb=neg_normal_emb)
+
+        pos_score = self.calc_score(h_emb=projected_head_pos, r_emb=pos_rel_emb, t_emb=projected_tail_pos)
+        neg_score = self.calc_score(h_emb=projected_head_pos, r_emb=neg_rel_emb, t_emb=projected_tail_neg)
+
+        loss = self.loss_fct(pos_score=pos_score, neg_score=neg_score)
+
+        return loss
+
+
