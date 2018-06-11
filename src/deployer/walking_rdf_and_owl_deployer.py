16eea3ad11d4b65073b2a7de2559382cb57209be
Mehdi Ali <ali-mehdi@live.de>
2018-06-11 11:36:10 +0200

    Extend exemplary deployer

--- src/deployer/walking_rdf_and_owl_deployer.py
+++ src/deployer/walking_rdf_and_owl_deployer.py
@@ -1,4 +1,3 @@
-
 import click
 import yaml
 
@@ -8,14 +7,17 @@ from utilities.pipeline import Pipeline
 @click.command()
 @click.option('-cfg_path', help='path to config file', required=True)
 def main(cfg_path):
-
     with open(cfg_path, 'r') as ymlfile:
         cfg = yaml.load(ymlfile)
 
     pipeline = Pipeline(config=cfg)
 
-    trained_kg_model = pipeline.start_pipeline(learning_rate=0.001, num_epochs=10, ratio_of_neg_triples=0.5,
-                                               batch_size=None)
+    trained_kg_model, eval_summary = pipeline.start_pipeline(learning_rate=0.001, num_epochs=10,
+                                                             ratio_of_neg_triples=0.5,
+                                                             batch_size=None, ratio_test_data=1/5, seed=2)
+
+    print(eval_summary)
+
 
 if __name__ == '__main__':
     main()
