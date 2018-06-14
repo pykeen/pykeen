4382de399be9d9e8e39459e5c0d8ec00a15b2075
Mehdi Ali <ali-mehdi@live.de>
2018-06-14 13:42:33 +0200

    Add new base directory

--- src/utilities/config_utils.py
+++ src/utilities/config_utils.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
-
+import getpass
 
 def create_base_dir():
-    base_dir = os.environ.get('KG_EMBEDDINGS_PIPELINE_DIRECTORY',
-                              os.path.join(os.path.expanduser('~'), '.kg_embeddings_pipeline'))
-    os.makedirs(base_dir, exist_ok=True)
+    try:
+        base_dir = os.environ.get('KG_EMBEDDINGS_PIPELINE_DIRECTORY',
+                                  os.path.join('/data', getpass.getuser(), 'kg_embeddings_pipeline'))
+        os.makedirs(base_dir, exist_ok=True)
+    except:
+        base_dir = os.environ.get('KG_EMBEDDINGS_PIPELINE_DIRECTORY',
+                                  os.path.join(os.path.expanduser('~'), '.kg_embeddings_pipeline'))
+        os.makedirs(base_dir, exist_ok=True)
+
+
     return base_dir
