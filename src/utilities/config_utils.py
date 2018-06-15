d998ab9a1e7d7397d60596b10f6ea5d985d88e8c
Mehdi Ali <ali-mehdi@live.de>
2018-06-15 11:59:16 +0200

    Change base directory

--- src/utilities/config_utils.py
+++ src/utilities/config_utils.py
@@ -5,7 +5,7 @@ import getpass
 def create_base_dir():
     try:
         base_dir = os.environ.get('KG_EMBEDDINGS_PIPELINE_DIRECTORY',
-                                  os.path.join('data', getpass.getuser(), 'project_data', 'kg_embeddings_pipeline'))
+                                  os.path.join('/','data', getpass.getuser(), 'project_data', 'kg_embeddings_pipeline'))
         os.makedirs(base_dir, exist_ok=True)
     except:
         base_dir = os.environ.get('KG_EMBEDDINGS_PIPELINE_DIRECTORY',
