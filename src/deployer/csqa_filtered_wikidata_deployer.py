c4aa0238db7c731b53647f7fba06bac1c32bb5fc
Mehdi Ali <ali-mehdi@live.de>
2018-06-16 17:09:51 +0200

    Add deployer for training CSQA Wikidata embeddings

---
+++ src/deployer/csqa_filtered_wikidata_deployer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+w_dir = os.path.dirname(os.getcwd())
+sys.path.append(w_dir)
+
+import click
+import yaml
+from utilities.pipeline import Pipeline
+
+
+@click.command()
+@click.option('-cfg_path', help='path to config file', required=True)
+def main(cfg_path):
+    with open(cfg_path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+    pipeline = Pipeline(config=cfg)
+
+    trained_kg_model, eval_summary = pipeline.start_pipeline(learning_rate=0.01, num_epochs=10,
+                                                             ratio_of_neg_triples=0.5,
+                                                             batch_size=None, ratio_test_data=1/5, seed=2)
+
+    print(eval_summary)
+
+
+if __name__ == '__main__':
+    main()
