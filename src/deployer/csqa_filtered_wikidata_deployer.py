c14324c59fa8361969b68604e430f2bf4b5bb412
Mehdi Ali <ali-mehdi@live.de>
2018-06-16 19:10:32 +0200

    Extend exemplary deployer

--- src/deployer/csqa_filtered_wikidata_deployer.py
+++ src/deployer/csqa_filtered_wikidata_deployer.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
+import pickle
 import sys
+import time
+
+from utilities.constants import KG_EMBEDDINGS_PIPELINE_DIR, ENTITY_TO_EMBEDDINGS, EVAL_RESULTS, CSQA_WIKIDATA
 
 w_dir = os.path.dirname(os.getcwd())
 sys.path.append(w_dir)
@@ -18,11 +22,30 @@ def main(cfg_path):
 
     pipeline = Pipeline(config=cfg)
 
-    trained_kg_model, eval_summary = pipeline.start_pipeline(learning_rate=0.01, num_epochs=10,
-                                                             ratio_of_neg_triples=0.5,
-                                                             batch_size=None, ratio_test_data=1/5, seed=2)
+    trained_kg_model, eval_summary, entity_to_embedding, relation_to_embedding = pipeline.start_pipeline(
+        learning_rate=0.01, num_epochs=1,
+        ratio_of_neg_triples=0.5,
+        batch_size=None,
+        ratio_test_data=1 / 5, seed=2)
 
-    print(eval_summary)
+    current_time = time.strftime("%H:%M:%S")
+    current_date = time.strftime("%d/%m/%Y").replace('/', '-')
+    entity_to_embedding_out = os.path.join(KG_EMBEDDINGS_PIPELINE_DIR, CSQA_WIKIDATA, 'output', current_date,
+                                           current_time)
+    os.makedirs(entity_to_embedding_out, exist_ok=True)
+
+    eval_results_out = os.path.join(KG_EMBEDDINGS_PIPELINE_DIR, CSQA_WIKIDATA, 'output', current_date, current_time)
+
+    os.makedirs(eval_results_out, exist_ok=True)
+
+    entity_to_embedding_out = os.path.join(entity_to_embedding_out, ENTITY_TO_EMBEDDINGS + '.pkl')
+    eval_results_out = os.path.join(eval_results_out, EVAL_RESULTS + '.pkl')
+
+    with open(entity_to_embedding_out, 'wb') as handle:
+        pickle.dump(entity_to_embedding, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    with open(eval_results_out, 'wb') as handle:
+        pickle.dump(eval_summary, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
