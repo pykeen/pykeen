80633744009838ceec42ade188763186d4388690
Mehdi Ali <ali-mehdi@live.de>
2018-06-22 10:27:41 +0200

    Set flag for CPU use

--- src/deployer/walking_rdf_and_owl_deployer.py
+++ src/deployer/walking_rdf_and_owl_deployer.py
@@ -16,9 +16,9 @@ def main(cfg_path):
     with open(cfg_path, 'r') as ymlfile:
         cfg = yaml.load(ymlfile)
 
-    pipeline = Pipeline(config=cfg)
+    pipeline = Pipeline(config=cfg, enforce_cpu_use=True)
 
-    trained_kg_model, eval_summary = pipeline.start_pipeline(learning_rate=0.001, num_epochs=10,
+    trained_kg_model, eval_summary = pipeline.start_pipeline(learning_rate=0.001, num_epochs=1,
                                                              ratio_of_neg_triples=0.5,
                                                              batch_size=None, ratio_test_data=1/5, seed=2)
 
