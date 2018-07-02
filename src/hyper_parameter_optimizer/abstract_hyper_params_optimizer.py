8bc866c24fc92acab6dd9f86561fd6310bb18402
Mehdi Ali <ali-mehdi@live.de>
2018-07-02 15:30:02 +0200

    Implement interface for hyper-parameter optimization modules

---
+++ src/hyper_parameter_optimizer/abstract_hyper_params_optimizer.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-add # -*- coding: utf-8 -*-
+import abc
+
+class AbstractHPOptimizer(object):
+
+    @abc.abstractmethod
+    def optimize_hyperparams(self, config, path_to_kg, device, seed):
+        return
