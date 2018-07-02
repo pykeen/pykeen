dedaa052ae3d26f290b0dcea168d8ead64c9a896
Mehdi Ali <ali-mehdi@live.de>
2018-07-02 15:23:58 +0200

    Exemplary script for applying hyper-parameter optimization (random search)

---
+++ src/deployer/hpo_walking_rdf_and_owl_deployer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+w_dir = os.path.dirname(os.getcwd())
+sys.path.append(w_dir)
+
+import click
+import yaml
+
+from utilities.pipeline import Pipeline
+
+
+@click.command()
+@click.option('-cfg_path', help='path to config file', required=True)
+def main(cfg_path):
+    with open(cfg_path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+
+    pipeline = Pipeline(config=cfg, seed=2)
+
+    trained_model, eval_summary, entity_to_embedding, relation_to_embedding = pipeline.start_hpo()
+
+    print(eval_summary)
+
+
+if __name__ == '__main__':
+    main()
