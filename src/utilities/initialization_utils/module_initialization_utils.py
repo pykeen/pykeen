1b51b84780274b8b0c2b9e800c4a20a7bb081ce0
Mehdi Ali <ali-mehdi@live.de>
2018-07-24 11:03:11 +0200

    Restructure the project

--- src/utilities/initialization_utils/module_initialization_utils.py
+++ src/utilities/initialization_utils/module_initialization_utils.py
@@ -2,10 +2,12 @@
 
 from corpus_reader.csqa_filtered_wikidata_reader import CSQAWikiDataReader
 from corpus_reader.walking_rdf_and_owl_reader import WROCReader
+from corpus_reader.wn18_reader import WN18Reader
 from evaluation_methods.mean_rank_evaluator import MeanRankEvaluator
 from kg_embeddings_model.trans_e import TransE
 from kg_embeddings_model.trans_h import TransH
-from utilities.constants import CLASS_NAME, WROC_READER, TRANS_E, TRANS_H, MEAN_RANK_EVALUATOR, CSQA_WIKIDATA_READER
+from utilities.constants import CLASS_NAME, WROC_READER, TRANS_E, TRANS_H, MEAN_RANK_EVALUATOR, CSQA_WIKIDATA_READER, \
+    WN18_READER
 
 
 def get_evaluator(config):
@@ -27,6 +29,8 @@ def get_reader(config):
         return WROCReader()
     if class_name == CSQA_WIKIDATA_READER:
         return CSQAWikiDataReader()
+    if class_name == WN18_READER:
+        return WN18Reader()
 
 
 def get_kg_embedding_model(config):
