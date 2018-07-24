0dfd86d40671273bfe76c551e2b3fff6858ec258
Mehdi Ali <ali-mehdi@live.de>
2018-07-24 11:36:43 +0200

    Adapt pipeline to new API

--- src/utilities/pipeline.py
+++ src/utilities/pipeline.py
@@ -6,12 +6,12 @@ import numpy as np
 import torch
 from sklearn.model_selection import train_test_split
 
-from utilities.constants import READER, KG_EMBEDDING_MODEL, NUM_ENTITIES, NUM_RELATIONS, EVALUATOR, PREFERRED_DEVICE, \
+from evaluation_methods.mean_rank_evaluator import MeanRankEvaluator
+from hyper_parameter_optimizer.random_search_optimizer import RandomSearchHPO
+from utilities.constants import KG_EMBEDDING_MODEL, NUM_ENTITIES, NUM_RELATIONS, PREFERRED_DEVICE, \
     GPU, HPO, LEARNING_RATE, NUM_EPOCHS, BATCH_SIZE
-from utilities.hpo_initialization_utils import get_hyper_parameter_optimizer
-from utilities.instance_creation_utils import create_mapped_triples, create_negative_triples
-from utilities.module_initialization_utils import get_evaluator, get_reader, \
-    get_kg_embedding_model
+from utilities.triples_creation_utils.instance_creation_utils import create_mapped_triples
+from utilities.initialization_utils.module_initialization_utils import get_kg_embedding_model
 from utilities.train_utils import train
 
 logging.basicConfig(level=logging.INFO)
@@ -40,31 +40,33 @@ class Pipeline(object):
 
         # Initialize reader
         log.info("-------------Read Corpus-------------")
-        reader_config = self.config[READER]
-        self.corpus_reader = get_reader(config=reader_config)
-        path_to_kg = self.corpus_reader.retreive_knowledge_graph()
 
-        evaluator_config = self.config[EVALUATOR]
-        evaluator = get_evaluator(config=evaluator_config)
+        # TODO: Adapt
+        evaluator = MeanRankEvaluator()  # get_evaluator(config=evaluator_config)
+        path_to_train_data = self.config['training_set_path']
 
         if is_hpo_mode:
-            hp_optimizer_config = self.config[HPO]
-            hp_optimizer = get_hyper_parameter_optimizer(hp_optimizer_config, evaluator)
+            hp_optimizer = RandomSearchHPO(evaluator=evaluator)
             trained_model, train_entity_to_id, train_rel_to_id, eval_result, metric_string, params = hp_optimizer.optimize_hyperparams(
-                self.config, path_to_kg, self.device, self.seed)
+                path_to_train_data, self.config, self.device, self.seed)
         else:
             train_params = self.config['data_params']
             ratio_test_data = train_params['ratio_test_data']
 
-            pos_triples = np.loadtxt(fname=path_to_kg, dtype=str, comments='@Comment@ Subject Predicate Object')
-            neg_triples = create_negative_triples(seed=self.seed, pos_triples=pos_triples)
-            train_pos, test_pos, train_neg, test_neg = train_test_split(pos_triples, neg_triples,
-                                                                        test_size=ratio_test_data,
-                                                                        random_state=self.seed)
+            pos_triples = np.loadtxt(fname=path_to_train_data, dtype=str, comments='@Comment@ Subject Predicate Object')
+            # neg_triples = create_negative_triples(seed=self.seed, pos_triples=pos_triples,
+            #                                       filter_neg_triples=False)
 
-            mapped_pos_tripels, train_entity_to_id, train_rel_to_id = create_mapped_triples(pos_triples)
-            mapped_neg_triples, _, _ = create_mapped_triples(pos_triples, entity_to_id=train_entity_to_id,
-                                                             rel_to_id=train_rel_to_id)
+            if 'validation_set_path' in self.config:
+                train_pos, test_pos = train_test_split(pos_triples, test_size=ratio_test_data, random_state=self.seed)
+            else:
+                train_pos = pos_triples
+                test_pos = np.loadtxt(fname=self.config['validation_set_path'], dtype=str,
+                                      comments='@Comment@ Subject Predicate Object')
+
+            mapped_pos_tripels, train_entity_to_id, train_rel_to_id = create_mapped_triples(train_pos)
+            # mapped_neg_triples, _, _ = create_mapped_triples(pos_triples, entity_to_id=train_entity_to_id,
+            #                                                  rel_to_id=train_rel_to_id)
 
             # Initialize KG embedding model
             kb_embedding_model_config = self.config[KG_EMBEDDING_MODEL]
@@ -77,11 +79,10 @@ class Pipeline(object):
             lr = kb_embedding_model_config[LEARNING_RATE]
             params = kb_embedding_model_config
 
-
             log.info("-------------Train KG Embeddings-------------")
             print(batch_size)
             trained_model = train(kg_embedding_model=kg_embedding_model, learning_rate=lr, num_epochs=num_epochs,
-                                  batch_size=batch_size, pos_triples=mapped_pos_tripels, neg_triples=mapped_neg_triples,
+                                  batch_size=batch_size, pos_triples=mapped_pos_tripels,
                                   device=self.device, seed=self.seed)
 
             log.info("-------------Start Evaluation-------------")
