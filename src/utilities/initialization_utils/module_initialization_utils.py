d035d67807c3a3d42ac1074cc4203bb87308de23
Mehdi Ali <ali-mehdi@live.de>
2018-07-24 12:55:58 +0200

    Remove unnecessary code

--- src/utilities/initialization_utils/module_initialization_utils.py
+++ src/utilities/initialization_utils/module_initialization_utils.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from corpus_reader.csqa_filtered_wikidata_reader import CSQAWikiDataReader
-from corpus_reader.walking_rdf_and_owl_reader import WROCReader
-from corpus_reader.wn18_reader import WN18Reader
 from evaluation_methods.mean_rank_evaluator import MeanRankEvaluator
 from kg_embeddings_model.trans_e import TransE
 from kg_embeddings_model.trans_h import TransH
-from utilities.constants import CLASS_NAME, WROC_READER, TRANS_E, TRANS_H, MEAN_RANK_EVALUATOR, CSQA_WIKIDATA_READER, \
-    WN18_READER
+from utilities.constants import CLASS_NAME, TRANS_E, TRANS_H, MEAN_RANK_EVALUATOR
 
 
 def get_evaluator(config):
@@ -15,22 +11,6 @@ def get_evaluator(config):
 
     if class_name == MEAN_RANK_EVALUATOR:
         return MeanRankEvaluator()
-
-
-def get_reader(config):
-    """
-
-    :param config:
-    :return:
-    """
-    class_name = config[CLASS_NAME]
-
-    if class_name == WROC_READER:
-        return WROCReader()
-    if class_name == CSQA_WIKIDATA_READER:
-        return CSQAWikiDataReader()
-    if class_name == WN18_READER:
-        return WN18Reader()
 
 
 def get_kg_embedding_model(config):
