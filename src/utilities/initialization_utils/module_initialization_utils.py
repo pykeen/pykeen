c7cd767bf50bace433472a1753df1eb9c9f189f5
Mehdi Ali <ali-mehdi@live.de>
2018-08-01 00:15:06 +0200

    Remove dependencies of old evaluation API

--- src/utilities/initialization_utils/module_initialization_utils.py
+++ src/utilities/initialization_utils/module_initialization_utils.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from evaluation_methods.mean_rank_evaluator import MeanRankEvaluator
 from kg_embeddings_model.trans_e import TransE
 from kg_embeddings_model.trans_h import TransH
-from utilities.constants import CLASS_NAME, TRANS_E, TRANS_H, MEAN_RANK_EVALUATOR, KG_EMBEDDING_MODEL
-
-
-def get_evaluator(config):
-    class_name = config[CLASS_NAME]
-
-    if class_name == MEAN_RANK_EVALUATOR:
-        return MeanRankEvaluator()
+from utilities.constants import TRANS_E, TRANS_H, KG_EMBEDDING_MODEL
 
 
 def get_kg_embedding_model(config):
