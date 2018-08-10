2184db444bedbee4d8a7dd5b8f6ad0b5e40e8da8
Mehdi Ali <ali-mehdi@live.de>
2018-08-10 10:07:53 +0200

    Extract and pass user specified 'k' for hits_at_k

--- src/hyper_parameter_optimizer/random_search_optimizer.py
+++ src/hyper_parameter_optimizer/random_search_optimizer.py
@@ -7,7 +7,7 @@ import numpy as np
 from hyper_parameter_optimizer.abstract_hyper_params_optimizer import AbstractHPOptimizer
 from utilities.constants import LEARNING_RATE, MARGIN_LOSS, EMBEDDING_DIM, BATCH_SIZE, NUM_EPOCHS, \
     KG_EMBEDDING_MODEL, NUM_ENTITIES, NUM_RELATIONS, SEED, HYPER_PARAMTER_OPTIMIZATION_PARAMS, NUM_OF_MAX_HPO_ITERS, \
-    NORMALIZATION_OF_ENTITIES, EVAL_METRICS, MEAN_RANK, HITS_AT_K
+    NORMALIZATION_OF_ENTITIES, EVAL_METRICS, MEAN_RANK, HITS_AT_K, K_FOR_HITS_AT_K
 from utilities.evaluation_utils.compute_metrics import compute_mean_rank_and_hits_at_k, compute_mean_rank, \
     compute_hits_at_k
 from utilities.initialization_utils.module_initialization_utils import get_kg_embedding_model
@@ -16,7 +16,6 @@ from utilities.triples_creation_utils.instance_creation_utils import create_mapp
 
 
 class RandomSearchHPO(AbstractHPOptimizer):
-
 
     def optimize_hyperparams(self, train_pos, test_pos, entity_to_id, rel_to_id, mapped_pos_train_tripels, config,
                              device, seed):
@@ -45,7 +44,6 @@ class RandomSearchHPO(AbstractHPOptimizer):
         eval_summary = OrderedDict()
 
         all_entities = np.array(list(entity_to_id.values()))
-
 
         for _ in range(max_iters):
             lr = random.choice(learning_rates)
@@ -77,30 +75,28 @@ class RandomSearchHPO(AbstractHPOptimizer):
             mapped_pos_test_tripels, _, _ = create_mapped_triples(test_pos, entity_to_id=entity_to_id,
                                                                   rel_to_id=rel_to_id)
 
-
             if is_mean_rank_selected and is_hits_at_k_selected:
+                k = config[K_FOR_HITS_AT_K]
                 mean_rank, hits_at_k = compute_mean_rank_and_hits_at_k(all_entities=all_entities,
                                                                        kg_embedding_model=trained_model,
-                                                                       triples=mapped_pos_test_tripels, k=10)
+                                                                       triples=mapped_pos_test_tripels, k=k)
                 eval_summary[MEAN_RANK] = mean_rank
                 eval_summary[HITS_AT_K] = hits_at_k
                 eval_results.append(mean_rank)
 
-            if is_mean_rank_selected == True and is_hits_at_k_selected==False:
+            if is_mean_rank_selected == True and is_hits_at_k_selected == False:
                 mean_rank = compute_mean_rank(all_entities=all_entities, kg_embedding_model=trained_model,
                                               triples=mapped_pos_test_tripels)
                 eval_summary[MEAN_RANK] = mean_rank
                 eval_results.append(mean_rank)
             if is_hits_at_k_selected == True and is_mean_rank_selected == False:
+                k = config[K_FOR_HITS_AT_K]
                 hits_at_k = compute_hits_at_k(all_entities=all_entities, kg_embedding_model=trained_model,
-                                              triples=mapped_pos_test_tripels, k=10)
+                                              triples=mapped_pos_test_tripels, k=k)
                 eval_summary[HITS_AT_K] = hits_at_k
                 eval_results.append(hits_at_k)
 
-
-
             trained_models.append(trained_model)
-
 
         index_of_max = np.argmax(a=eval_results)
 
