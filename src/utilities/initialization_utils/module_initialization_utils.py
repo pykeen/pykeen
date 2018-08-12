37f21cc1bff8006b6303cf1e416519d12b0509f9
Mehdi Ali <ali-mehdi@live.de>
2018-08-12 21:29:25 +0200

    Extend initialization module

--- src/utilities/initialization_utils/module_initialization_utils.py
+++ src/utilities/initialization_utils/module_initialization_utils.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-
+from kg_embeddings_model.conv_e import ConvE
+from kg_embeddings_model.trans_d import TransD
 from kg_embeddings_model.trans_e import TransE
 from kg_embeddings_model.trans_h import TransH
-from utilities.constants import TRANS_E, TRANS_H, KG_EMBEDDING_MODEL
+from kg_embeddings_model.trans_r import TransR
+from utilities.constants import TRANS_E, TRANS_H, KG_EMBEDDING_MODEL, TRANS_D, TRANS_R, CONV_E
 
 
 def get_kg_embedding_model(config):
@@ -15,5 +17,13 @@ def get_kg_embedding_model(config):
 
     if model_name == TRANS_E:
         return TransE(config=config)
-    elif model_name == TRANS_H:
+    if model_name == TRANS_H:
         return TransH(config=config)
+    if model_name == TRANS_D:
+        return TransD(config=config)
+    if model_name == TRANS_R:
+        return TransR(config=config)
+    if model_name == CONV_E:
+        return ConvE(config=config)
+
+
