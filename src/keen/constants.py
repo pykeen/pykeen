8d8625af1b3864514d46e3507790ab346c8dd7ff
Mehdi Ali <ali-mehdi@live.de>
2018-10-01 19:30:23 +0200

    Implement new CLI for TransR

--- src/keen/constants.py
+++ src/keen/constants.py
@@ -79,148 +79,152 @@ OUTPUT_DIREC = 'output_direc'
 # -----------------Command line interface messages-----------------
 
 TRAINING_FILE_PROMPT_MSG = '> Please provide here the path to your training file: '
-TRAINING_FILE_ERROR_MSG = 'An error occured, either the path is not correct or the training file doesn\'t exist.\n' \
+TRAINING_FILE_ERROR_MSG = 'An error occurred, either the path is not correct or the training file doesn\'t exist.\n' \
                           'Please try again.'
 
 TEST_FILE_PROMPT_MSG = '> Please provide here the path to your test file: '
-TEST_FILE_ERROR_MSG = 'An error occured, either the path is not correct or the test file doesn\'t exist.\n' \
+TEST_FILE_ERROR_MSG = 'An error occurred, either the path is not correct or the test file doesn\'t exist.\n' \
                           'Please try again.'
 
 EMBEDDING_DIMENSION_PRINT_MSG = 'Please type the range of preferred embedding dimensions for entities comma separated (e.g. 50,100,200):'
 EMBEDDING_DIMENSION_PROMPT_MSG = '> Please select the embedding dimensions:'
-EMBEDDING_DIMENSION_ERROR_MSG = 'Invalid input, please positice integer as embedding dimensions.'
+EMBEDDING_DIMENSION_ERROR_MSG = 'An error occurred, please positive integer as embedding dimension.'
+
+ENTITIES_EMBEDDING_DIMENSION_PRINT_MSG = 'Please provide the embedding dimension for entities (e.g. 40). '
+ENTITIES_EMBEDDING_DIMENSION_PROMPT_MSG = '> Entity embedding dimension: '
+ENTITIES_EMBEDDING_DIMENSION_ERROR_MSG = 'An error occurred, please proive and positive integer as embedding dimension (e.g. 50).'
 
 BATCH_SIZES_PRINT_MSG = 'Please type the range of preferred batch sizes comma separated (e.g. 32, 64, 128):'
 BATCH_SIZES_PROMPT_MSG = '> Please select the batch size(s):'
-BATCH_SIZES_ERROR_MSG = 'Invalid input, please select integers as batch size(s)'
+BATCH_SIZES_ERROR_MSG = 'An error occurred, please select integers as batch size(s)'
 
 EPOCHS_PRINT_MSG = 'Please type the range of preferred epochs comma separated (e.g. 1, 5, 100):'
 EPOCHS_PROMPT_MSG = '> Epochs:'
-EPOCHS_ERROR_MSG = 'Invalid input, please select integers as epoch(s)'
+EPOCHS_ERROR_MSG = 'An error occurred, please select integers as epoch(s)'
 
 LEARNING_RATES_PRINT_MSG = 'Please type the range of preferred learning rate(s) comma separated (e.g. 0.1, 0.01, 0.0001:'
 LEARNING_RATES_PROMPT_MSG = '> Please select the learning rate(s):'
-LEARNING_RATES_ERROR_MSG = 'Invalid input, please float values for the learning rate(s).'
+LEARNING_RATES_ERROR_MSG = 'An error occurred, please float values for the learning rate(s).'
 
 MARGIN_LOSSES_PRINT_MSG = 'Please type the range of preferred margin losse(s) comma separated  (e.g. 1,2,10):'
 MARGIN_LOSSES_PROMPT_MSG = '> Please select the margin losse(s):'
-MARGIN_LOSSES_ERROR_MSG = 'Invalid input, please type in positive integer as embedding dimensions.'
+MARGIN_LOSSES_ERROR_MSG = 'An error occurred, please type in positive integer as embedding dimensions.'
 
 NORMS_FOR_NORMALIZATION_OF_ENTITIES_PRINT_MSG = 'Please select L-p norm(s) for the normalization of entities comma separated (e.g. 0.5, 1, 2):'
 NORMS_FOR_NORMALIZATION_OF_ENTITIES_PROMPT_MSG = '> L-p norm(s):'
-NORMS_FOR_NORMALIZATION_OF_ENTITIES_ERROR_MSG = 'Invalid input, please provide float value(s).'
+NORMS_FOR_NORMALIZATION_OF_ENTITIES_ERROR_MSG = 'An error occurred, please provide float value(s).'
 
 NORMS_SCROING_FUNCTION_PRINT_MSG = 'Please select L-p norm(s) used as scoring function comma separated (e.g. 0.5, 1, 2):'
 NORMS_SCROING_FUNCTION_PROMPT_MSG = '> L-p norm(s):'
-NORMS_SCROING_FUNCTION_ERROR_MSG = 'Invalid input, please provide float value(s).'
+NORMS_SCROING_FUNCTION_ERROR_MSG = 'An error occurred, please provide float value(s).'
 
 MAX_HPO_ITERS_PRINT_MSG = 'Please type the maximum number of iterationns for they hyper-parameter search:'
 MAX_HPO_ITERS_PROMPT_MSG = '> Maximum number of iterations: '
-MAX_HPO_ITERS_ERROR_MSG = 'Invalid input, please type in a positive integer for the maximum number of iterations.'
+MAX_HPO_ITERS_ERROR_MSG = 'An error occurred, please type in a positive integer for the maximum number of iterations.'
 
-EMBEDDING_DIMENSION_PRINT_MSG = 'Please type the preferred embedding dimension of entities and relations, and press enter.'
+EMBEDDING_DIMENSION_PRINT_MSG = 'Please provide the embedding dimension of entities and relations, and press enter.'
 EMBEDDING_DIMENSION_PROMPT_MSG = '> Please select the embedding dimension: '
-EMBEDDING_DIMENSION_ERROR_MSG = 'Invalid input, the embedding dimension must be a positive integer such as 20.\n' \
+EMBEDDING_DIMENSION_ERROR_MSG = 'An error occurred, the embedding dimension must be a positive integer such as 20.\n' \
                                 'Please try again.'
 
-RELATION_EMBEDDING_DIMENSION_PRINT_MSG = 'Please type the preferred embedding dimension of relations:'
+RELATION_EMBEDDING_DIMENSION_PRINT_MSG = 'Please provide the embedding dimension of relations:'
 RELATION_EMBEDDING_DIMENSION_PROMPT_MSG = '> Please select the embedding dimension: '
-EMBEDDING_DIMENSION_ERROR_MSG = 'Invalid input, please type in integer as embedding dimension.'
+RELATION_EMBEDDING_DIMENSION_ERROR_MSG = 'An error occurred, please type in integer as embedding dimension such as 30.'
 
 MARGIN_LOSS_PRINT_MSG = 'Please type in the margin losses:'
 MARGIN_LOSS_PROMPT_MSG = '> Margin loss:'
-MARGIN_LOSS_ERROR_MSG = 'Invalid input, please type in a float value.'
+MARGIN_LOSS_ERROR_MSG = 'An error occurred, please type in a float value.'
 
 LEARNING_RATE_PRINT_MSG = 'Please type in the learning rate.'
 LEARNING_RATE_PROMPT_MSG = '> Learning rate:'
-LEARNING_RATE_ERROR_MSG = 'Invalid input, the learning rate should be a positive float value.\n' \
+LEARNING_RATE_ERROR_MSG = 'An error occurred, the learning rate should be a positive float value.\n' \
                           'Please try again.'
 
 BATCH_SIZE_PRINT_MSG = 'Please type the batch size comma:'
 BATCH_SIZE_PROMPT_MSG = '> Batch size:'
-BATCH_SIZE_ERROR_MSG = 'Invalid input, please select a integer.'
+BATCH_SIZE_ERROR_MSG = 'An error occurred, please select a integer.'
 
 EPOCH_PRINT_MSG = 'Please type the number of epochs:'
 EPOCH_PROMPT_MSG = '> Epochs:'
-EPOCH_ERROR_MSG = 'Invalid input, please select an integers.'
+EPOCH_ERROR_MSG = 'An error occurred, please select an integers.'
 
 ENTITIES_NORMALIZATION_PRINT_MSG = 'Please select a norm to use to normalize the entities. The norm should be a positive integer greater than 0'
 ENTITIES_NORMALIZATION_PROMPT_MSG = '> Norm to use for normalization of the entities: '
-ENTITIES_NORMALIZATION_ERROR_MSG = 'Invalid input, the norm should be an integer greater than 0, such as 1\n' \
+ENTITIES_NORMALIZATION_ERROR_MSG = 'An error occurred, the norm should be an integer greater than 0, such as 1\n' \
                              'Please try again.'
 
 SCORING_FUNCTION_PRINT_MSG = 'Please select a norm to use as a scoring function. The norm should be a positive integer greater than 0'
 SCORING_FUNCTION_PROMPT_MSG = '> Norm to use as scoring function: '
-SCORING_FUNCTION_ERROR_MSG = 'Invalid input, the norm for the scoring function should be an integer greater than 0, such as 1\n' \
+SCORING_FUNCTION_ERROR_MSG = 'An error occurred, the norm for the scoring function should be an integer greater than 0, such as 1\n' \
                              'Please try again.'
 
 SAVE_CONFIG_PRINT_MSG = 'Do you want to save the configuration file?'
 SAVE_CONFIG_PROMPT_MSG = '> \'yes\' or \'no\':'
-SAVE_CONFIG_ERROR_MSG = 'Invalid input, please type \'yes\' or \'no\'.'
+SAVE_CONFIG_ERROR_MSG = 'An error occurred, please type \'yes\' or \'no\'.'
 
 K_FOR_HITS_AT_K_PRINT_MSG = 'Please select \'k\' for hits@k'
 K_FOR_HITS_AT_K_PROMPT_MSG = '> k:'
-K_FOR_HITS_AT_K_ERROR_MSG = 'Invalid input \'k\' must be a positive integer.'
+K_FOR_HITS_AT_K_ERROR_MSG = 'An error occurred, \'k\' must be a positive integer.'
 
 CONV_E_HPO_INPUT_CHANNELS_PRINT_MSG = 'Please select (comma seperated) the number of input channels for ConvE'
 CONV_E_HPO_INPUT_CHANNELS_PROMPT_MSG = '> Input channels:'
-CONV_E_HPO_INPUT_CHANNELS_ERROR_MSG = 'Invalid input, input channels must be positive integers.'
+CONV_E_HPO_INPUT_CHANNELS_ERROR_MSG = 'An error occurred, input channels must be positive integers.'
 
 CONV_E_INPUT_CHANNEL_PRINT_MSG = 'Please select the number of input channels'
 CONV_E_INPUT_CHANNEL_PROMPT_MSG = '> Input channels:'
-CONV_E_INPUT_CHANNEL_ERROR_MSG = 'Invalid input, the number of input channels must be a positive number'
+CONV_E_INPUT_CHANNEL_ERROR_MSG = 'An error occurred, the number of input channels must be a positive number'
 
 CONV_E_HPO_OUT_CHANNELS_PRINT_MSG = 'Please select (comma seperated) the number of output channels for ConvE'
 CONV_E_HPO_OUT_CHANNELS_PROMPT_MSG = '> Output channels:'
-CONV_E_HPO_OUT_CHANNELS_ERROR_MSG = 'Invalid input, output channels must be positive integers.'
+CONV_E_HPO_OUT_CHANNELS_ERROR_MSG = 'An error occurred, output channels must be positive integers.'
 
 CONV_E_OUT_CHANNEL_PRINT_MSG = 'Please select the number of output channels.'
 CONV_E_OUT_CHANNEL_PROMPT_MSG = '> Output channels:'
-CONV_E_OUT_CHANNEL_ERROR_MSG = 'Invalid input, the number of output channels must be a positive number.'
+CONV_E_OUT_CHANNEL_ERROR_MSG = 'An error occurred, the number of output channels must be a positive number.'
 
 CONV_E_HPO_KERNEL_HEIGHTS_PRINT_MSG = 'Please select the kernel heights for ConvE'
 CONV_E_HPO_KERNEL_HEIGHTS_PROMPT_MSG = '> Kernel height for defined height %d:'
-CONV_E_HPO_KERNEL_HEIGHTS_ERROR_MSG = 'Invalid input, kernel heights must be positive integers and <= than %d (defined height).'
+CONV_E_HPO_KERNEL_HEIGHTS_ERROR_MSG = 'An error occurred, kernel heights must be positive integers and <= than %d (defined height).'
 
 CONV_E_KERNEL_HEIGHT_PRINT_MSG = 'Please select the kernel height for ConvE.'
 CONV_E_KERNEL_HEIGHT_PROMPT_MSG = '> Kernel height for defined height %d:'
-CONV_E_KERNEL_HEIGHT_ERROR_MSG = 'Invalid input, the kernel height must be a positive integer and <= than %d (defined height).'
+CONV_E_KERNEL_HEIGHT_ERROR_MSG = 'An error occurred, the kernel height must be a positive integer and <= than %d (defined height).'
 
 CONV_E_HPO_KERNEL_WIDTHS_PRINT_MSG = 'Please select the kernel widths for ConvE'
 CONV_E_HPO_KERNEL_WIDTHS_PROMPT_MSG = '> Kernel width for defined width %d:'
-CONV_E_HPO_KERNEL_WIDTHS_ERROR_MSG = 'Invalid input, kernel widths mus be positive integers and <= than %d (defined width).'
+CONV_E_HPO_KERNEL_WIDTHS_ERROR_MSG = 'An error occurred, kernel widths mus be positive integers and <= than %d (defined width).'
 
 CONV_E_KERNEL_WIDTH_PRINT_MSG = 'Please select the kernel width for ConvE'
 CONV_E_KERNEL_WIDTH_PROMPT_MSG = '> Kernel width for defined width %d:'
-CONV_E_KERNEL_WIDTH_ERROR_MSG = 'Invalid input, kernel width mus be a positive integer and <= than %d (defined width).'
+CONV_E_KERNEL_WIDTH_ERROR_MSG = 'An error occurred, kernel width mus be a positive integer and <= than %d (defined width).'
 
 TRAINING_SET_PRINT_MSG = 'Please provide the path to the training file.'
-TRAINING_FILE_ERROR_MSG = 'An error occured, either the path is not correct or the training file doesn\'t exist.\n' \
+TRAINING_FILE_ERROR_MSG = 'An error occurred, either the path is not correct or the training file doesn\'t exist.\n' \
                           'Please try again.'
 
 CONFIG_FILE_PROMPT_MSG = '> Please provide the path to your existing configuration file: '
-CONFIG_FILE_ERROR_MSG = 'An error occured, please make sure that the file exists, and that it is JSON file.\n' \
+CONFIG_FILE_ERROR_MSG = 'An error occurred, please make sure that the file exists, and that it is JSON file.\n' \
                         'Please try again.'
 
 CONV_E_HPO_INPUT_DROPOUTS_PRINT_MSG = 'Please select (comma separated) the input dropout value(s)'
 CONV_E_HPO_INPUT_DROPOUTS_PROMPT_MSG = '> Input dropout value(s):'
-CONV_E_HPO_INPUT_DROPOUTS_ERROR_MSG = 'Invalid input, input must be positive float values.'
+CONV_E_HPO_INPUT_DROPOUTS_ERROR_MSG = 'An error occurred, input must be positive float values.'
 
 CONV_E_INPUT_DROPOUT_PRINT_MSG = 'Please select the input dropout value'
 CONV_E_INPUT_DROPOUT_PROMPT_MSG = '> Input dropout value:'
-CONV_E_INPUT_DROPOUT_ERROR_MSG = 'Invalid input, input dropout must be a positive float value.'
+CONV_E_INPUT_DROPOUT_ERROR_MSG = 'An error occurred, input dropout must be a positive float value.'
 
 CONV_E_HPO_OUTPUT_DROPOUT_PRINT_MSG = 'Please select (comma separated) the output dropout value(s)'
 CONV_E_HPO_OUTPUT_DROPOUT_PROMPT_MSG = '> Output dropout value(s):'
-CONV_E_HPO_OUTPUT_DROPOUT_ERROR_MSG = 'Invalid output, input must be positive float values.'
+CONV_E_HPO_OUTPUT_DROPOUT_ERROR_MSG = 'An error occurred, input must be positive float values.'
 
 CONV_E_OUTPUT_DROPOUT_PRINT_MSG = 'Please select the output dropout value'
 CONV_E_OUTPUT_DROPOUT_PROMPT_MSG = '> Output dropout value:'
-CONV_E_OUTPUT_DROPOUT_ERROR_MSG = 'Invalid output, input must be positive float values.'
+CONV_E_OUTPUT_DROPOUT_ERROR_MSG = 'An error occurred, input must be positive float values.'
 
 CONV_E_HPO_FEATURE_MAP_DROPOUT_PRINT_MSG = 'Please select (comma separated) the feature map dropout value(s)'
 CONV_E_HPO_FEATURE_MAP_DROPOUT_PROMPT_MSG = '> Feature map dropout value(s):'
-CONV_E_HPO_FEATURE_MAP_DROPOUT_ERROR_MSG = 'Invalid input, input must be positive float values.'
+CONV_E_HPO_FEATURE_MAP_DROPOUT_ERROR_MSG = 'An error occurred, input must be positive float values.'
 
 CONV_E_FEATURE_MAP_DROPOUT_PRINT_MSG = 'Please select the feature map dropout value'
 CONV_E__FEATURE_MAP_DROPOUT_PROMPT_MSG = '> Feature map dropout value:'
@@ -228,11 +232,11 @@ CONV_E_FEATURE_MAP_DROPOUT_ERROR_MSG = 'Invalid output, input must be a positive
 
 WEIGHTS_SOFT_CONSTRAINT_TRANS_H_PRINT_MSG = 'Please provide the value for weighting the soft constraints of the loss function'
 WEIGHTS_SOFT_CONSTRAINT_TRANS_H_PROMPT_MSG = '> Weight value for soft constraints: '
-WEIGHTS_SOFT_CONSTRAINT_TRANS_H_ERROR_MSG = 'Invalid input, the weight value must be a float value such as 0.5'
+WEIGHTS_SOFT_CONSTRAINT_TRANS_H_ERROR_MSG = 'An error occurred, the weight value must be a float value such as 0.5'
 
 WEIGHT_SOFT_CONSTRAINT_TRANS_H_PRINT_MSG = 'Please select the weight value for the soft constraints of the loss function'
 WEIGHT_SOFT_CONSTRAINT_TRANS_H_PROMPT_MSG = '> Weight value for soft constraints: '
-WEIGHT_SOFT_CONSTRAINT_TRANS_H_ERROR_MSG = 'Invalid input, input must be positive a float value.'
+WEIGHT_SOFT_CONSTRAINT_TRANS_H_ERROR_MSG = 'An error occurred, input must be positive a float value.'
 
 # ----------------------------------
 
---
+++ src/keen/utilities/cli_utils/trans_r_cli.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+'''Implementation the command line interface needed for TransR.'''
+
+from collections import OrderedDict
+
+from keen.constants import MARGIN_LOSS_PRINT_MSG, MARGIN_LOSS_PROMPT_MSG, MARGIN_LOSS_ERROR_MSG, \
+    SCORING_FUNCTION_PRINT_MSG, \
+    SCORING_FUNCTION_PROMPT_MSG, SCORING_FUNCTION_ERROR_MSG, LEARNING_RATE_PRINT_MSG, \
+    LEARNING_RATE_PROMPT_MSG, LEARNING_RATE_ERROR_MSG, BATCH_SIZE_PRINT_MSG, BATCH_SIZE_PROMPT_MSG, \
+    BATCH_SIZE_ERROR_MSG, EPOCH_PRINT_MSG, EPOCH_PROMPT_MSG, EPOCH_ERROR_MSG, EMBEDDING_DIM, SCORING_FUNCTION_NORM, \
+    LEARNING_RATE, BATCH_SIZE, NUM_EPOCHS, KG_EMBEDDING_MODEL_NAME, MARGIN_LOSS, \
+    ENTITIES_EMBEDDING_DIMENSION_PRINT_MSG, ENTITIES_EMBEDDING_DIMENSION_PROMPT_MSG, \
+    ENTITIES_EMBEDDING_DIMENSION_ERROR_MSG, RELATION_EMBEDDING_DIMENSION_PRINT_MSG, \
+    RELATION_EMBEDDING_DIMENSION_PROMPT_MSG, RELATION_EMBEDDING_DIMENSION_ERROR_MSG, RELATION_EMBEDDING_DIM
+from keen.utilities.cli_utils.cli_print_msg_helper import print_training_margin_loss_message, print_scoring_fct_message, \
+    print_section_divider, print_learning_rate_message, print_batch_size_message, \
+    print_number_epochs_message, print_entities_embedding_dimension_message, print_relations_embedding_dimension_message
+from keen.utilities.cli_utils.cli_training_query_helper import select_integer_value, select_float_value
+
+
+def configure_trans_r_training_pipeline(model_name):
+    """
+
+    :return:
+    """
+    config = OrderedDict()
+    config[KG_EMBEDDING_MODEL_NAME] = model_name
+
+    # Step 1: Query embedding dimension for entities
+    print_entities_embedding_dimension_message()
+    embedding_dimension = select_integer_value(print_msg=ENTITIES_EMBEDDING_DIMENSION_PRINT_MSG,
+                                               prompt_msg=ENTITIES_EMBEDDING_DIMENSION_PROMPT_MSG,
+                                               error_msg=ENTITIES_EMBEDDING_DIMENSION_ERROR_MSG)
+    config[EMBEDDING_DIM] = embedding_dimension
+    print_section_divider()
+
+    # Step 2: Query embedding dimension for relations
+    print_relations_embedding_dimension_message()
+    relation_embedding_dimension = select_integer_value(print_msg=RELATION_EMBEDDING_DIMENSION_PRINT_MSG,
+                                               prompt_msg=RELATION_EMBEDDING_DIMENSION_PROMPT_MSG,
+                                               error_msg=RELATION_EMBEDDING_DIMENSION_ERROR_MSG)
+    config[RELATION_EMBEDDING_DIM] = relation_embedding_dimension
+    print_section_divider()
+
+    # Step 2: Query margin loss
+    print_training_margin_loss_message()
+    magin_loss = select_float_value(print_msg=MARGIN_LOSS_PRINT_MSG,
+                                    prompt_msg=MARGIN_LOSS_PROMPT_MSG,
+                                    error_msg=MARGIN_LOSS_ERROR_MSG)
+    config[MARGIN_LOSS] = magin_loss
+    print_section_divider()
+
+    # Step 3: Query L_p norm as scoring function
+    print_scoring_fct_message()
+    scoring_fct_norm = select_integer_value(print_msg=SCORING_FUNCTION_PRINT_MSG,
+                                            prompt_msg=SCORING_FUNCTION_PROMPT_MSG,
+                                            error_msg=SCORING_FUNCTION_ERROR_MSG)
+    config[SCORING_FUNCTION_NORM] = scoring_fct_norm
+    print_section_divider()
+
+    # Step 5: Query learning rate
+    print_learning_rate_message()
+    learning_rate = select_float_value(print_msg=LEARNING_RATE_PRINT_MSG,
+                                       prompt_msg=LEARNING_RATE_PROMPT_MSG,
+                                       error_msg=LEARNING_RATE_ERROR_MSG)
+    config[LEARNING_RATE] = learning_rate
+    print_section_divider()
+
+    # Step 6: Query batch size
+    print_batch_size_message()
+    batch_size = select_integer_value(print_msg=BATCH_SIZE_PRINT_MSG,
+                                      prompt_msg=BATCH_SIZE_PROMPT_MSG,
+                                      error_msg=BATCH_SIZE_ERROR_MSG)
+    config[BATCH_SIZE] = batch_size
+    print_section_divider()
+
+    # Step 7: Query number of epochs
+    print_number_epochs_message()
+    number_epochs = select_integer_value(print_msg=EPOCH_PRINT_MSG,
+                                         prompt_msg=EPOCH_PROMPT_MSG,
+                                         error_msg=EPOCH_ERROR_MSG)
+    config[NUM_EPOCHS] = number_epochs
+    print_section_divider()
+
+    return config
+
+
+def configure_trans_r_hpo_pipeline():
+    pass
