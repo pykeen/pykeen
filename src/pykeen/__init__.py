5813f31e471775d582d769f8eec66799cd8255e1
Mehdi Ali <ali-mehdi@live.de>
2018-10-12 14:29:57 +0200

    Bump version: 0.0.4-dev → 0.0.5-dev

--- src/pykeen/__init__.py
+++ src/pykeen/__init__.py
@@ -15,7 +15,7 @@ Clone the repository from `GitHub <https://github.com/SmartDataAnalytics/pykeen.
 
 from .run import run
 
-__version__ = '0.0.4-dev'
+__version__ = '0.0.5-dev'
 
 __title__ = 'pykeen'
 __description__ = "A package for training and evaluating knowledge graph embeddings"
