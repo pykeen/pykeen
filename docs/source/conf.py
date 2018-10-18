6a4e66e0d895f6f84490dda74cef224a3ff0f2c6
Mehdi Ali <ali-mehdi@live.de>
2018-10-18 11:46:25 +0200

    Revert version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2018, Mehdi Ali'
 author = 'Mehdi Ali'
 
 # The full version, including alpha/beta/rc tags.
-release = '0.0.5'
+release = '0.0.5-dev'
 
 # The short X.Y version.
 parsed_version = re.match(
--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -4,7 +4,7 @@
 
 from collections import OrderedDict
 
-VERSION = '0.0.5'
+VERSION = '0.0.5-dev'
 
 # KG embedding model
 KG_EMBEDDING_MODEL = 'kg_embedding_model'
