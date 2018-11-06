76cb12bf34c88eda1e3cf641b4fdcbb3d41e37b8
Mehdi Ali <ali-mehdi@live.de>
2018-11-06 16:50:59 +0100

    New version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2018, Mehdi Ali'
 author = 'Mehdi Ali'
 
 # The full version, including alpha/beta/rc tags.
-release = '0.0.9-dev'
+release = '0.0.9'
 
 # The short X.Y version.
 parsed_version = re.match(
