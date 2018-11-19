e6b3a5c46fd9765e1488146272751d67c21a0023
Mehdi Ali <ali-mehdi@live.de>
2018-11-19 13:01:07 +0100

    Bump version: 0.0.12 → 0.0.13-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ copyright = '2018, Mehdi Ali'
 author = 'Mehdi Ali'
 
 # The full version, including alpha/beta/rc tags.
-release = '0.0.12'
+release = '0.0.13-dev'
 
 # The short X.Y version.
 parsed_version = re.match(
--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -4,7 +4,7 @@
 
 from collections import OrderedDict
 
-VERSION = '0.0.12'
+VERSION = '0.0.13-dev'
 
 PYKEEN = 'PyKEEN'
 
