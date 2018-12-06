3e4224e6271aeca867cb9dee517f35e6be162eef
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-12-06 11:51:54 +0100

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,7 @@
 
 import unittest
 
+from pykeen.constants import *
 from pykeen.kge_models import TransE
 
 
@@ -13,6 +14,13 @@ class TestModelInstantiation(unittest.TestCase):
     def test_instantiate_trans_e(self):
         """Test that TransE can be instantiated."""
         trans_e = TransE(config={
+            NUM_ENTITIES: 5,
+            NUM_RELATIONS: 5,
+            EMBEDDING_DIM: 5,
+            NORM_FOR_NORMALIZATION_OF_ENTITIES: 2,
+            SCORING_FUNCTION_NORM: 1,
+            MARGIN_LOSS: 4,
         })
+        # TODO @mehdi type checking for parameters
+        # TODO @mehdi @cthoyt refactor reused code for TransXXX models to base class
         self.assertIsNotNone(trans_e)
-        # TODO
