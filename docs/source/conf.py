49ed1a6ce63ec1393749ca3ed1c1e2fd6f077208
Mehdi Ali <ali-mehdi@live.de>
2019-01-18 10:31:45 +0100

    Create new release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ copyright = '2018, Mehdi Ali'
 author = 'Mehdi Ali'
 
 # The full version, including alpha/beta/rc tags.
-release = '0.0.17-dev'
+release = '0.0.17'
 
 # The short X.Y version.
 parsed_version = re.match(
--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -4,7 +4,7 @@
 
 from collections import OrderedDict
 
-VERSION = '0.0.17-dev'
+VERSION = '0.0.17'
 
 
 def get_version() -> str:
