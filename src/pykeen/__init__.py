b4504456b33a23c9aff36fc2bec6ab8ec4b9208c
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-01-31 15:03:06 +0100

    Switch to declarative setup

--- src/pykeen/__init__.py
+++ src/pykeen/__init__.py
@@ -15,13 +15,3 @@ Clone the repository from `GitHub <https://github.com/SmartDataAnalytics/pykeen.
 
 from .constants import get_version  # noqa: F401
 from .run import run  # noqa: F401
-
-__version__ = '0.0.19'
-
-__title__ = 'pykeen'
-__description__ = "A package for training and evaluating knowledge graph embeddings"
-__url__ = 'https://github.com/SmartDataAnalytics/PyKEEN.git'
-__author__ = 'Mehdi Ali'
-__email__ = 'mehdi.ali@cs.uni-bonn.de'
-__license__ = 'MIT License'
-__copyright__ = 'Copyright (c) 2018 Mehdi Ali'
