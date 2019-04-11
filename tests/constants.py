315542bd295bd8f750220e2b21b8e859bf74ef1a
Mehdi Ali <ali-mehdi@live.de>
2019-04-11 15:38:26 +0200

    Refactor tests

--- tests/constants.py
+++ tests/constants.py
@@ -3,6 +3,11 @@
 """Testing constants for PyKEEN."""
 
 import os
+import tempfile
+import unittest
+import numpy as np
+import pykeen
+import pykeen.constants as pkc
 
 __all__ = [
     'RESOURCES_DIRECTORY',
@@ -10,3 +15,55 @@ __all__ = [
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 RESOURCES_DIRECTORY = os.path.join(HERE, 'resources')
+
+
+class BaseTestTrainingMode(unittest.TestCase):
+    """Base class for testing the training mode."""
+    config = dict()
+    config[pkc.TRAINING_SET_PATH] = os.path.join(RESOURCES_DIRECTORY, 'data', 'rdf.nt'),
+    config[pkc.EXECUTION_MODE] = pkc.TRAINING_MODE
+    config[pkc.SEED] = 0
+    config[pkc.LEARNING_RATE] = 0.01
+    config[pkc.NUM_EPOCHS] = 20
+    config[pkc.BATCH_SIZE] = 64
+    config[pkc.PREFERRED_DEVICE] = pkc.CPU
+
+    def setUp(self):
+        self.dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.dir.cleanup()
+
+    def start_training(self, config):
+        """Test that ConvE is trained correctly in training mode."""
+        results = pykeen.run(
+            config=config,
+            output_directory=self.dir.name,
+        )
+
+        return results
+
+    def check_basic_results(self, results):
+        """Test basic functionalities that are always called when a model is trained in training model."""
+        self.assertIsNotNone(results)
+        self.assertIsNotNone(results.results[pkc.TRAINED_MODEL])
+        self.assertIsNotNone(results.results[pkc.LOSSES])
+        self.assertIsNotNone(results.results[pkc.ENTITY_TO_EMBEDDING])
+        self.assertIsNotNone(results.results[pkc.ENTITY_TO_ID])
+        self.assertIsNotNone(results.results[pkc.RELATION_TO_ID])
+        self.assertIsNotNone(results.results[pkc.FINAL_CONFIGURATION])
+
+    def check_that_model_has_not_been_evalauted(self, results):
+        """Test that model has not been evaluated."""
+        self.assertNotIn(pkc.EVAL_SUMMARY, results.results)
+
+    def check_evaluation_results(self, results):
+        """Test evaluation specific functionalities."""
+        self.assertIn(pkc.MEAN_RANK, results.results[pkc.EVAL_SUMMARY])
+        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.MEAN_RANK]), float)
+        self.assertIn(pkc.HITS_AT_K, results.results[pkc.EVAL_SUMMARY])
+        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.HITS_AT_K][1]), np.float64)
+        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.HITS_AT_K][3]), np.float64)
+        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.HITS_AT_K][5]), np.float64)
+        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.HITS_AT_K][10]), np.float64)
+
--- tests/test_training_mode_for_conv_e.py
+++ tests/test_training_mode_for_conv_e.py
@@ -3,34 +3,15 @@
 """Test training mode for ConvE."""
 
 import logging
-import os
-import tempfile
-import unittest
 
-import numpy as np
-
-import pykeen
 import pykeen.constants as pkc
-from tests.constants import RESOURCES_DIRECTORY
+from tests.constants import BaseTestTrainingMode
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('pykeen').setLevel(logging.INFO)
 
 CONFIG = dict(
-    training_set_path=os.path.join(RESOURCES_DIRECTORY, 'data', 'rdf.nt'),
-    execution_mode=pkc.TRAINING_MODE,
-    random_seed=0,
-    kg_embedding_model_name=pkc.CONV_E_NAME,
-    embedding_dim=50,
-    ConvE_input_channels=1,
-    ConvE_output_channels=3,
-    ConvE_height=5,
-    ConvE_width=10,
-    ConvE_kernel_height=5,
-    ConvE_kernel_width=3,
-    conv_e_input_dropout=0.2,
-    conv_e_feature_map_dropout=0.5,
-    conv_e_output_dropout=0.5,
+
     margin_loss=1,
     learning_rate=0.01,
     num_epochs=20,
@@ -39,55 +20,36 @@ CONFIG = dict(
 )
 
 
-class TestTrainingModeForConvE(unittest.TestCase):
+class TestTrainingModeForConvE(BaseTestTrainingMode):
     """Test that ConvE can be trained and evaluated correctly in training mode."""
 
-    def setUp(self):
-        self.dir = tempfile.TemporaryDirectory()
-
-    def tearDown(self):
-        self.dir.cleanup()
+    config = BaseTestTrainingMode.config
+    config[pkc.KG_EMBEDDING_MODEL_NAME] = pkc.CONV_E_NAME
+    config[pkc.EMBEDDING_DIM] = 50
+    config[pkc.CONV_E_INPUT_CHANNELS] = 1
+    config[pkc.CONV_E_OUTPUT_CHANNELS] = 3
+    config[pkc.CONV_E_HEIGHT] = 5
+    config[pkc.CONV_E_WIDTH] = 10
+    config[pkc.CONV_E_KERNEL_HEIGHT] = 5
+    config[pkc.CONV_E_KERNEL_WIDTH] = 3
+    config[pkc.CONV_E_INPUT_DROPOUT] = 0.2
+    config[pkc.CONV_E_FEATURE_MAP_DROPOUT] = 0.5
+    config[pkc.CONV_E_OUTPUT_DROPOUT] = 0.5
+    config[pkc.MARGIN_LOSS] = 1
 
     def test_training(self):
         """Test that ConvE is trained correctly in training mode."""
-        results = pykeen.run(
-            config=CONFIG,
-            output_directory=self.dir.name,
-        )
-
-        self.assertIsNotNone(results)
-        self.assertIsNotNone(results.results[pkc.TRAINED_MODEL])
-        self.assertIsNotNone(results.results[pkc.LOSSES])
-        self.assertIsNotNone(results.results[pkc.ENTITY_TO_EMBEDDING])
-        self.assertNotIn(pkc.EVAL_SUMMARY, results.results)
-        self.assertIsNotNone(results.results[pkc.ENTITY_TO_ID])
-        self.assertIsNotNone(results.results[pkc.RELATION_TO_ID])
-        self.assertIsNotNone(results.results[pkc.FINAL_CONFIGURATION])
+        results = self.start_training(config=self.config)
+        self.check_basic_results(results=results)
+        self.check_that_model_has_not_been_evalauted(results=results)
 
     def test_evaluation(self):
         """Test that ConvE is trained and evaluated correctly in training mode. """
         # 10 % of training set will be used as a test set
-        config = CONFIG.copy()
+        config = self.config.copy()
         config[pkc.TEST_SET_RATIO] = 0.1
         config[pkc.FILTER_NEG_TRIPLES] = True
 
-        results = pykeen.run(
-            config=config,
-            output_directory=self.dir.name,
-        )
-
-        self.assertIsNotNone(results)
-        self.assertIsNotNone(results.results[pkc.TRAINED_MODEL])
-        self.assertIsNotNone(results.results[pkc.LOSSES])
-        self.assertIsNotNone(results.results[pkc.ENTITY_TO_EMBEDDING])
-        self.assertIn(pkc.EVAL_SUMMARY, results.results)
-        self.assertIn(pkc.MEAN_RANK, results.results[pkc.EVAL_SUMMARY])
-        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.MEAN_RANK]), float)
-        self.assertIn(pkc.HITS_AT_K, results.results[pkc.EVAL_SUMMARY])
-        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.HITS_AT_K][1]), np.float64)
-        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.HITS_AT_K][3]), np.float64)
-        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.HITS_AT_K][5]), np.float64)
-        self.assertEqual(type(results.results[pkc.EVAL_SUMMARY][pkc.HITS_AT_K][10]), np.float64)
-        self.assertIsNotNone(results.results[pkc.ENTITY_TO_ID])
-        self.assertIsNotNone(results.results[pkc.RELATION_TO_ID])
-        self.assertIsNotNone(results.results[pkc.FINAL_CONFIGURATION])
+        results = self.start_training(config=config)
+        self.check_basic_results(results=results)
+        self.check_evaluation_results(results=results)
