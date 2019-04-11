0e1305a2644ef1795800cfc94d58e0eeea3b310c
Mehdi Ali <ali-mehdi@live.de>
2019-04-11 18:56:39 +0200

    Bump version: 0.0.24-dev → 0.0.24

--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -7,7 +7,7 @@ from typing import Callable, Dict
 import numpy as np
 from pkg_resources import iter_entry_points
 
-VERSION = '0.0.24-dev'
+VERSION = '0.0.24'
 
 #: Functions for specifying exotic resources with a given prefix
 IMPORTERS: Dict[str, Callable[[str], np.ndarray]] = {
