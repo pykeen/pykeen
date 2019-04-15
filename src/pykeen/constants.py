112cac901a4d3fb78e3a6d5ef650089b7bfcff73
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-04-15 09:09:51 +0200

    Update constants.py

--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -127,29 +127,33 @@ ENTITIES_EMBEDDING_DIMENSION_PRINT_MSG = 'Please provide the embedding dimension
 ENTITIES_EMBEDDING_DIMENSION_PROMPT_MSG = '> Entity embedding dimension: '
 ENTITIES_EMBEDDING_DIMENSION_ERROR_MSG = 'An error occurred, please proive and positive integer as embedding dimension (e.g. 50).'
 
-ENTITIES_EMBEDDING_DIMENSIONS_PRINT_MSG = 'Please provide (comma separated) the embedding dimension(s) for entities (e.g. 50,100). '
+ENTITIES_EMBEDDING_DIMENSIONS_PRINT_MSG = 'Please provide (comma separated) the embedding dimension(s) for entities (e.g. 50, 100). '
 ENTITIES_EMBEDDING_DIMENSIONS_PROMPT_MSG = '> Entity embedding dimensions: '
 ENTITIES_EMBEDDING_DIMENSIONS_ERROR_MSG = 'An error occurred, the embedding dimensions must be positive integers and separated by a comma.\n' \
                                           'Please try again.'
 
 BATCH_SIZES_PRINT_MSG = 'Please type (comma separated) the batch size(s) (e.g. 32, 64, 128):'
-BATCH_SIZES_PROMPT_MSG = '> Batch size(s):'
+BATCH_SIZES_PROMPT_MSG = '> Batch size(s): '
 BATCH_SIZES_ERROR_MSG = 'An error occurred, the batch sizes must be positive integers and separated by a comma.\n' \
                         'Please try again.'
 
 EPOCHS_PRINT_MSG = 'Please type (comma separated) the number of epochs (e.g. 50,100,500).'
-EPOCHS_PROMPT_MSG = '> Epochs:'
+EPOCHS_PROMPT_MSG = '> Epochs: '
 EPOCHS_ERROR_MSG = 'An error occurred, the number of epochs must be positive integers and separated by a comma.\n' \
                    'Please try again.'
 
 LEARNING_RATES_PRINT_MSG = 'Please type (comma separated) the learning rate(s) (e.g. 0.1, 0.01, 0.0001).'
-LEARNING_RATES_PROMPT_MSG = '> Learning rate(s):'
+LEARNING_RATES_PROMPT_MSG = '> Learning rate(s): '
 LEARNING_RATES_ERROR_MSG = 'An error occurred, the learning rates must be float values and separated by a comma.\n' \
                            'Please try again.'
 
-MARGIN_LOSSES_PRINT_MSG = 'Please type (comma separated) the margin losses(s) comma separated  (e.g. 1,2,10), and press enter-'
-MARGIN_LOSSES_PROMPT_MSG = '> Margin losse(s):'
-MARGIN_LOSSES_ERROR_MSG = 'An error occurred, the margin losses must be positive float values and separated by a comma (e.g. 0.5,3)'
+MARGIN_LOSSES_PRINT_MSG = 'Please type (comma separated) the margin losses(s) comma separated  (e.g. 1, 2, 10), and press enter-'
+MARGIN_LOSSES_PROMPT_MSG = '> Margin losse(s): '
+MARGIN_LOSSES_ERROR_MSG = 'An error occurred, the margin losses must be positive float values and separated by a comma (e.g. 0.5, 3)'
+
+MARGIN_LOSS_PRINT_MSG = 'Please type in the margin losses:'
+MARGIN_LOSS_PROMPT_MSG = '> Margin loss: '
+MARGIN_LOSS_ERROR_MSG = 'An error occurred, please type in a float value.'
 
 HPO_ITERS_PRINT_MSG = 'Please type (comma separated) the number of iterations of hyper-parameter search (e.g. 5)'
 HPO_ITERS_PROMPT_MSG = '> Number of iterations: '
@@ -172,15 +176,11 @@ RELATION_EMBEDDING_DIMENSION_ERROR_MSG = 'An error occurred, please type in an i
 RELATION_EMBEDDING_DIMENSIONS_PRINT_MSG = 'Please provide (comma separated) the embedding dimensions of relations:'
 RELATION_EMBEDDING_DIMENSIONS_PROMPT_MSG = '> Relation embedding dimensions: '
 RELATION_EMBEDDING_DIMENSIONS_ERROR_MSG = 'An error occurred, the relation embedding dimensions must be positive integers and separated\n' \
-                                          'by a comma e.g. 50,100,200' \
+                                          'by a comma e.g. 50, 100, 200' \
                                           'Please try again. \n'
 
-MARGIN_LOSS_PRINT_MSG = 'Please type in the margin losses:'
-MARGIN_LOSS_PROMPT_MSG = '> Margin loss:'
-MARGIN_LOSS_ERROR_MSG = 'An error occurred, please type in a float value.'
-
 LEARNING_RATE_PRINT_MSG = 'Please type in the learning rate.'
-LEARNING_RATE_PROMPT_MSG = '> Learning rate:'
+LEARNING_RATE_PROMPT_MSG = '> Learning rate: '
 LEARNING_RATE_ERROR_MSG = 'An error occurred, the learning rate should be a positive float value.\n' \
                           'Please try again.'
 
@@ -204,7 +204,7 @@ ENTITIES_NORMALIZATION_ERROR_MSG = 'An error occurred, the norm should be an int
 NORMS_FOR_NORMALIZATION_OF_ENTITIES_PRINT_MSG = 'Please select (comma separated) a list of norms to use for normalizing the entities.'
 NORMS_FOR_NORMALIZATION_OF_ENTITIES_PROMPT_MSG = '> Norms: '
 NORMS_FOR_NORMALIZATION_OF_ENTITIES_ERROR_MSG = 'An error occurred, the normalization norms should integers, greater than 0, \n' \
-                                                ' and separated by a comma (e.g. 1,2,3). Please try again.'
+                                                ' and separated by a comma (e.g. 1, 2, 3). Please try again.'
 
 NORM_SCORING_FUNCTION_PRINT_MSG = 'Please select a norm to use as a scoring function. The norm should be a positive integer greater than 0'
 NORM_SCORING_FUNCTION_PROMPT_MSG = '> Norm to use as scoring function: '
@@ -214,7 +214,7 @@ NORM_SCORING_FUNCTION_ERROR_MSG = 'An error occurred, the norm for the scoring f
 NORMS_SCORING_FUNCTION_PRINT_MSG = 'Please select (comma separated) a list of norms to use as a scoring functions. The norms should be a positive integers greater than 0'
 NORMS_SCORING_FUNCTION_PROMPT_MSG = '> Norms to use as scoring function: '
 NORMS_SCORING_FUNCTION_ERROR_MSG = 'An error occurred, the norms for the scoring functions should integers, greater than 0, \n' \
-                                   ' and separated by a comma (e.g. 1,2,3). Please try again.'
+                                   ' and separated by a comma (e.g. 1, 2, 3). Please try again.'
 
 SAVE_CONFIG_PRINT_MSG = 'Do you want to save the configuration file?'
 SAVE_CONFIG_PROMPT_MSG = '> \'yes\' or \'no\':'
@@ -224,7 +224,7 @@ K_FOR_HITS_AT_K_PRINT_MSG = 'Please select \'k\' for hits@k'
 K_FOR_HITS_AT_K_PROMPT_MSG = '> k:'
 K_FOR_HITS_AT_K_ERROR_MSG = 'An error occurred, \'k\' must be a positive integer.'
 
-CONV_E_HPO_INPUT_CHANNELS_PRINT_MSG = 'Please select (comma seperated) the number of input channels for ConvE'
+CONV_E_HPO_INPUT_CHANNELS_PRINT_MSG = 'Please select (comma separated) the number of input channels for ConvE'
 CONV_E_HPO_INPUT_CHANNELS_PROMPT_MSG = '> Input channels:'
 CONV_E_HPO_INPUT_CHANNELS_ERROR_MSG = 'An error occurred, input channels must be positive integers.'
 
@@ -233,7 +233,7 @@ CONV_E_INPUT_CHANNEL_PROMPT_MSG = '> Number of input channels:'
 CONV_E_INPUT_CHANNEL_ERROR_MSG = 'An error occurred, the number of input channels must be a positive integer.\n' \
                                  'Please try again.'
 
-CONV_E_HPO_OUT_CHANNELS_PRINT_MSG = 'Please select (comma seperated) the number of output channels for ConvE'
+CONV_E_HPO_OUT_CHANNELS_PRINT_MSG = 'Please select (comma separated) the number of output channels for ConvE'
 CONV_E_HPO_OUT_CHANNELS_PROMPT_MSG = '> Output channels:'
 CONV_E_HPO_OUT_CHANNELS_ERROR_MSG = 'An error occurred, output channels must be positive integers.'
 
@@ -266,7 +266,7 @@ CONFIG_FILE_ERROR_MSG = 'An error occurred, please make sure that the file exist
                         'Please try again.'
 
 CONV_E_HPO_INPUT_DROPOUTS_PRINT_MSG = 'Please select (comma separated) the input dropout value(s)'
-CONV_E_HPO_INPUT_DROPOUTS_PROMPT_MSG = '> Input dropout value(s):'
+CONV_E_HPO_INPUT_DROPOUTS_PROMPT_MSG = '> Input dropout value(s): '
 CONV_E_HPO_INPUT_DROPOUTS_ERROR_MSG = 'An error occurred, input must be positive float values.'
 
 CONV_E_INPUT_DROPOUT_PRINT_MSG = 'Please select the dropout rate for the input layer.'
@@ -278,22 +278,25 @@ CONV_E_HPO_OUTPUT_DROPOUT_PROMPT_MSG = '> Output dropout value(s):'
 CONV_E_HPO_OUTPUT_DROPOUT_ERROR_MSG = 'An error occurred, input must be positive float values.'
 
 CONV_E_OUTPUT_DROPOUT_PRINT_MSG = 'Please select the output dropout value'
-CONV_E_OUTPUT_DROPOUT_PROMPT_MSG = '> Output dropout value:'
+CONV_E_OUTPUT_DROPOUT_PROMPT_MSG = '> Output dropout value: '
 CONV_E_OUTPUT_DROPOUT_ERROR_MSG = 'An error occurred, input must be positive float values.'
 
 CONV_E_HPO_FEATURE_MAP_DROPOUT_PRINT_MSG = 'Please select (comma separated) the feature map dropout value(s)'
-CONV_E_HPO_FEATURE_MAP_DROPOUT_PROMPT_MSG = '> Feature map dropout value(s):'
+CONV_E_HPO_FEATURE_MAP_DROPOUT_PROMPT_MSG = '> Feature map dropout value(s): '
 CONV_E_HPO_FEATURE_MAP_DROPOUT_ERROR_MSG = 'An error occurred, input must be positive float values.'
 
 CONV_E_FEATURE_MAP_DROPOUT_PRINT_MSG = 'Please select the feature map dropout value'
 CONV_E__FEATURE_MAP_DROPOUT_PROMPT_MSG = '> Feature map dropout value:'
 CONV_E_FEATURE_MAP_DROPOUT_ERROR_MSG = 'Invalid output, input must be a positive float value.'
 
-WEIGHT_SOFT_CONSTRAINT_TRANS_H_PRINT_MSG = 'Please select the weight value for the soft constraints of the loss function'
+WEIGHT_SOFT_CONSTRAINT_TRANS_H_PRINT_MSG = 'Please select the weight value for the soft constraints of' \
+                                           ' the loss function'
 WEIGHT_SOFT_CONSTRAINT_TRANS_H_PROMPT_MSG = '> Weight value for soft constraints: '
 WEIGHT_SOFT_CONSTRAINT_TRANS_H_ERROR_MSG = 'An error occurred, input must be positive a float value.'
 
-WEIGHTS_SOFT_CONSTRAINT_TRANS_H_PRINT_MSG = 'Please provide (comma separated) the weight values for weighting the soft constraints of the loss function'
+WEIGHTS_SOFT_CONSTRAINT_TRANS_H_PRINT_MSG = 'Please provide (comma separated) the weight values for weighting the' \
+                                            ' soft constraints of the loss function'
 WEIGHTS_SOFT_CONSTRAINT_TRANS_H_PROMPT_MSG = '> Weight values for soft constraints: '
-WEIGHTS_SOFT_CONSTRAINT_TRANS_H_ERROR_MSG = 'An error occurred, the weight values for the soft constraint must be float values, \n' \
-                                            ' and separated by a comma (e.g. 0.1,0.5). Please try again.'
+WEIGHTS_SOFT_CONSTRAINT_TRANS_H_ERROR_MSG = 'An error occurred, the weight values for the soft constraint must be' \
+                                            ' float values, \n and separated by a comma (e.g. 0.1, 0.5).' \
+                                            ' Please try again.'
