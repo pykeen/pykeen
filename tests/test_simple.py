a61b6082b6a5807c0dbb819585230b7844c76727
mali <ali-mehdi@live.de>
2019-04-25 14:39:08 +0200

    Create simple test

---
+++ tests/test_simple.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Test utils for processing numeric literals."""
+
+import unittest
+
+class NumericLiteralsUtilsTests(unittest.TestCase):
+    """Class for testing utils for processing numeric literals."""
+
+    def test_create_matix_of_literals(self):
+        """."""
+        self.assertEqual(1,1)
