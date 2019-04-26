603c2a688ea22d99e12cb97defe282a7e894c0ef
mali <ali-mehdi@live.de>
2019-04-26 14:53:32 +0200

    Create constants

---
+++ src/poem/constants.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Constants defined for XXX."""
+
+# Device related constants
+PREFERRED_DEVICE = 'preferred_device'
+GPU = 'gpu'
+CPU = 'cpu'
+
+MARGIN_LOSS = 'margin_mrl'
+NUM_ENTITIES = 'number_entities'
+NUM_RELATIONS = 'num_relations'
+EMBEDDING_DIM = 'embedding_dim'
+LEARNING_RATE = 'learning_rate'
+
+# Model names
+DISTMULT_LITERAL_NAME_OWA = 'DistMultLiteral_OWA'
+DISTMULT_LITERAL_NAME_CWA = 'DistMultLiteral_CWA'
+
+# DistMult related constants
+DISTMULT_INPUT_DROPOUT = 'input_dropout'
