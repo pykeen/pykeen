736cd6689f11db3834a5b06460d87bda20a5dd39
mali <ali-mehdi@live.de>
2019-04-28 14:06:17 +0200

    Extend triple factory

--- src/poem/instance_creation_factories/triples_factory.py
+++ src/poem/instance_creation_factories/triples_factory.py
@@ -4,20 +4,32 @@
 
 from dataclasses import dataclass
 import numpy as np
+from sklearn.model_selection import train_test_split
+
+from kupp.triples_preprocessing_utils.basic_triple_utils import load_triples, create_entity_and_relation_mappings, \
+    map_triples_elements_to_ids
+from poem.basic_utils import is_evaluation_requested, is_cwa
+from poem.constants import TRAINING_SET_PATH, TEST_SET_PATH, TEST_SET_RATIO, OWA, CWA
+from typing import Tuple
+
 
 @dataclass
 class Instances():
     """."""
-    triples: np.array
+    training_triples: np.array
+    test_triples = None
+    has_test_instances = False
+    has_validation_instances = False
+    kg_assumption: str
 
-@dataclass
-class OWAInstances(Instances):
-    """."""
 
 @dataclass
 class CWAInstances(Instances):
     """."""
-    labels: np.array
+    training_labels: np.array
+    test_labels: np.array
+    kg_assumption = CWA
+
 
 class TriplesFactory():
     """."""
@@ -25,6 +37,70 @@ class TriplesFactory():
     def __int__(self, config):
         """."""
         self.config = config
+        self.entity_to_id = None
+        self.relation_to_id = None
+        self.train_triples = None
+        self.test_triples = None
+        self.all_triples = None
+
+    def map_triples(self, train_triples, test_triples=None, validation_triples = None) -> Tuple[np.ndarray, np.ndarray]:
+        """."""
+        all_triples: np.ndarray = np.concatenate([train_triples, test_triples], axis=0)
+        # Map each entity/relation to a unique id
+        self.entity_to_id, self.relation_to_id = create_entity_and_relation_mappings(triples=all_triples)
+
+        # Map triple elements to their ids
+        mapped_train_triples = map_triples_elements_to_ids(triples=train_triples,
+                                                           entity_to_id=self.entity_to_id,
+                                                           rel_to_id=self.relation_to_id)
+
+        if test_triples is None:
+            return mapped_train_triples, None
+
+        mapped_test_triples = map_triples_elements_to_ids(triples=test_triples,
+                                                          entity_to_id=self.entity_to_id,
+                                                          rel_to_id=self.relation_to_id)
+
+        return mapped_train_triples, mapped_test_triples
+
+    def get_test_triples(self, train_triples):
+        """."""
+        if TEST_SET_PATH in self.config:
+            test_triples = load_triples(self.config[TEST_SET_PATH])
+        else:
+            train_triples, test_triples = train_test_split(
+                train_triples,
+                test_size=self.config.get(TEST_SET_RATIO, 0.1),
+                random_state=self.seed,
+            )
+
+        return train_triples, test_triples
 
     def create_instances(self) -> Instances:
         """"""
+
+        # Step 1: Load training triples
+        training_triples = load_triples(path=self.config[TRAINING_SET_PATH], delimiter='\t')
+        self.train_triples = training_triples
+        self.all_triples = training_triples
+
+        # Step 2: Create test triples if requested
+        if is_evaluation_requested:
+            train_triples, test_triples = self.get_test_triples(train_triples=training_triples)
+            self.test_triples = test_triples
+            self.all_triples = np.concatenate([train_triples, test_triples], axis=0)
+
+        training_triples, test_triples = self.map_triples(train_triples=self.train_triples,
+                                                          test_triples=self.test_triples)
+
+        return Instances(training_triples=training_triples,
+                         test_triples=test_triples,
+                         has_test_instances=True,
+                         kg_assumption=OWA)
+
+
+        # TODO
+        #if is_cwa(self.config):
+         #   # Create multi-labels
+          #  pass
+
