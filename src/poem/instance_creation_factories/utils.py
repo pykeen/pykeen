0b064bdb2b1a971c63e62133b3e62ad1424d1e5b
Mehdi Ali <ali-mehdi@live.de>
2019-04-29 16:44:09 +0200

    Refactor

--- src/poem/instance_creation_factories/utils.py
+++ src/poem/instance_creation_factories/utils.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 
 """Implementation of basic utils for creating instances."""
-from poem.constants import TRIPLES_FACTORY, NUMERIC_LITERALS_FACTORY, FACTORY_NAME
+from poem.constants import DISTMULT_LITERAL_NAME_OWA, \
+    KG_EMBEDDING_MODEL_NAME
 from poem.instance_creation_factories.triples_factory import TriplesFactory
 from poem.instance_creation_factories.triples_numeric_literals_factory import TriplesNumericLiteralsFactory
 
-FACTORIES = {
-    TRIPLES_FACTORY: TriplesFactory,
-    NUMERIC_LITERALS_FACTORY: TriplesNumericLiteralsFactory
+KGE_MODEL_NAME_TO_FACTORY = {
+    DISTMULT_LITERAL_NAME_OWA: TriplesNumericLiteralsFactory
 }
 
 
 def get_factory(config) -> TriplesFactory:
     """."""
 
-    factory_name = config[FACTORY_NAME]
-    factory = FACTORIES.get(factory_name)
+    kge_model_name = config[KG_EMBEDDING_MODEL_NAME]
+    factory = KGE_MODEL_NAME_TO_FACTORY.get(kge_model_name)
 
     if factory is None:
-        raise ValueError(f'invalid factory name: {factory_name}')
+        raise ValueError(f'invalid factory name: {kge_model_name}')
 
     return factory(config)
-
