1aee836b5be500f4c3d397e1df77e0de83e6310a
Mehdi Ali <ali-mehdi@live.de>
2019-04-29 16:58:03 +0200

    Set seed for random generators

--- src/poem/pipeline.py
+++ src/poem/pipeline.py
@@ -11,12 +11,13 @@ import torch.nn as nn
 
 from poem import model_config
 from poem.basic_utils import is_evaluation_requested
-from poem.constants import EXECUTION_MODE, TRAINING_MODE, HPO_MODE
+from poem.constants import EXECUTION_MODE, TRAINING_MODE, HPO_MODE, KG_EMBEDDING_MODEL_NAME, DISTMULT_LITERAL_NAME_OWA, \
+    PATH_TO_NUMERIC_LITERALS, SEED
 from poem.instance_creation_factories.triples_factory import TriplesFactory, Instances
 from poem.instance_creation_factories.utils import get_factory
 from poem.kge_models.utils import get_kge_model, get_training_loop
 from poem.model_config import ModelConfig
-from poem.training_loops.basic_training_loop import TrainingLoop
+import torch
 
 log = logging.getLogger(__name__)
 
@@ -49,13 +50,16 @@ class ExperimentalArtifactsContainingEvalResults(ExperimentalArtifacts):
 class Pipeline():
     """."""
 
-    def __init__(self, config, instances: Optional[Instances]):
-        self.config: Dict = config
+    def __init__(self, config: Dict, instances: Optional[Instances]):
+        self.config = config
         self.model_config: ModelConfig = None
         self.instance_factory: TriplesFactory = None
-        self.instances: Instances = instances
+        self.instances = instances
         self.has_preprocessed_instances = self.instances is True
 
+        # Set random generators
+        torch.manual_seed(config[SEED])
+        np.random.seed(config[SEED])
 
     def run(self):
         """."""
@@ -80,7 +84,6 @@ class Pipeline():
         elif exec_mode == HPO_MODE:
             self.perform_hpo()
 
-
     def preprocess(self):
         """Create instances."""
 
@@ -93,8 +96,8 @@ class Pipeline():
     def train(self):
         """."""
         self.model_config = ModelConfig(config=self.config,
-                                   multimodal_data=self.instances.multimodal_data,
-                                   has_multimodal_data= self.instances.has_multimodal_data )
+                                        multimodal_data=self.instances.multimodal_data,
+                                        has_multimodal_data=self.instances.has_multimodal_data)
         kge_model = get_kge_model(model_config=model_config)
         train_loop = get_training_loop(model_config=self.model_config, kge_model=kge_model, instances=self.instances)
         # Train the model based on the defined training loop
@@ -109,3 +112,13 @@ class Pipeline():
         """."""
 
 
+if __name__ == '__main__':
+    config = {
+        KG_EMBEDDING_MODEL_NAME: DISTMULT_LITERAL_NAME_OWA,
+        PATH_TO_NUMERIC_LITERALS: ''
+
+    }
+
+    # preprocess
+    pipeline = Pipeline(config=config)
+    instances = pipeline.preprocess()
