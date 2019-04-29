b91d2ec71a4262d648114597c8a53a84f741a1a8
Mehdi Ali <ali-mehdi@live.de>
2019-04-29 11:03:00 +0200

    Implement Pipeline's preprocessing function

--- src/poem/pipeline.py
+++ src/poem/pipeline.py
@@ -3,11 +3,14 @@
 """Implementation of the basic pipeline."""
 
 import logging
+from dataclasses import dataclass
 from typing import Dict, Mapping
 
 import numpy as np
 import torch.nn as nn
-from dataclasses import dataclass
+
+from poem.constants import EXECUTION_MODE, TRAINING_MODE, HPO_MODE
+from poem.utils import get_factory
 
 log = logging.getLogger(__name__)
 
@@ -36,14 +39,39 @@ class ExperimentalArtifactsContainingEvalResults(ExperimentalArtifacts):
     """."""
     eval_results: EvalResults
 
+
 class Pipeline():
     """."""
+
+    def __init__(self, config):
+        self.config = config
+        self.instance_factory = None
+        self.instances = None
 
     def run(self):
         """."""
 
+        # Step 1: Create instances
+        self.instances = self.preprocess()
+
+        if EXECUTION_MODE not in self.config:
+            raise KeyError()
+
+        exec_mode = self.config[EXECUTION_MODE]
+
+        if exec_mode != TRAINING_MODE and exec_mode != HPO_MODE:
+            raise ValueError()
+
+        # Step 2: Train in training or HPO mode
+        if exec_mode == TRAINING_MODE:
+            self.train()
+        elif exec_mode == HPO_MODE:
+            self.perform_hpo()
+
     def preprocess(self):
-        """."""
+        """Create instances."""
+        self.instance_factory = get_factory(self.config)
+        return self.instance_factory.create_instances()
 
     def train(self):
         """."""
@@ -53,6 +81,8 @@ class Pipeline():
 
     def evaluate(self):
         """."""
+
+
 def run(config: Dict) -> ExperimentalArtifacts:
     """."""
 
