672c095c714c6a42ab97766a3b78e74c7d37a6f6
mali <ali-mehdi@live.de>
2019-05-02 22:37:24 +0200

    Integrate negative sampler into training loop

--- src/poem/training_loops/owa_training_loop.py
+++ src/poem/training_loops/owa_training_loop.py
@@ -13,6 +13,7 @@ from tqdm import trange
 from poem.constants import BATCH_SIZE, NUM_EPOCHS
 from poem.instance_creation_factories.triples_factory import Instances
 from poem.model_config import ModelConfig
+from poem.negative_sampling.basic_negative_sampler import BasicNegativeSampler
 from poem.training_loops.basic_training_loop import TrainingLoop
 from poem.training_loops.utils import split_list_in_batches
 
@@ -24,14 +25,16 @@ class OWATrainingLoop(TrainingLoop):
 
     def __init__(self, model_config: ModelConfig, kge_model: nn.Module, instances: Instances):
         super().__init__(model_config=model_config, kge_model=kge_model, instances=instances)
+        # Later, different negative sampling algorithms can be set
+        self.negative_sampler = BasicNegativeSampler(all_entities=self.all_entities)
 
     def train(self):
         """."""
         self.kge_model = self.kge_model.to(self.kge_model.device)
 
         optimizer = self.get_optimizer(config=self.config, kge_model=self.kge_model)
-        num_pos_triples = self.instances.training_instances.shape[0]
-        num_entities = self.all_entities.shape[0]
+        pos_triples = self.instances.instances
+        num_pos_triples = pos_triples.shape[0]
 
         start_training = timeit.default_timer()
 
@@ -48,27 +51,8 @@ class OWATrainingLoop(TrainingLoop):
 
             for i, pos_batch in enumerate(pos_batches):
                 current_batch_size = len(pos_batch)
-                batch_subjs = pos_batch[:, 0:1]
-                batch_relations = pos_batch[:, 1:2]
-                batch_objs = pos_batch[:, 2:3]
-
-                num_subj_corrupt = len(pos_batch) // 2
-                num_obj_corrupt = len(pos_batch) - num_subj_corrupt
+                neg_batch = self.negative_sampler.sample(pos_batch=pos_batch)
                 pos_batch = torch.tensor(pos_batch, dtype=torch.long, device=self.kge_model.device)
-
-                corrupted_subj_indices = np.random.choice(np.arange(0, num_entities), size=num_subj_corrupt)
-                corrupted_subjects = np.reshape(self.all_entities[corrupted_subj_indices], newshape=(-1, 1))
-                subject_based_corrupted_triples = np.concatenate(
-                    [corrupted_subjects, batch_relations[:num_subj_corrupt], batch_objs[:num_subj_corrupt]], axis=1)
-
-                corrupted_obj_indices = np.random.choice(np.arange(0, num_entities), size=num_obj_corrupt)
-                corrupted_objects = np.reshape(self.all_entities[corrupted_obj_indices], newshape=(-1, 1))
-
-                object_based_corrupted_triples = np.concatenate(
-                    [batch_subjs[num_subj_corrupt:], batch_relations[num_subj_corrupt:], corrupted_objects], axis=1)
-
-                neg_batch = np.concatenate([subject_based_corrupted_triples, object_based_corrupted_triples], axis=0)
-
                 neg_batch = torch.tensor(neg_batch, dtype=torch.long, device=self.kge_model.device)
 
                 # Recall that torch *accumulates* gradients. Before passing in a
