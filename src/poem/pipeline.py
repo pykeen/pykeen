ea470b8de9282d6defdb1b21c6f6c758c3f0cedd
mali <ali-mehdi@live.de>
2019-05-06 11:30:20 +0200

    Refactor API of pipeline

--- src/poem/pipeline.py
+++ src/poem/pipeline.py
@@ -13,13 +13,14 @@ import torch.nn as nn
 
 from poem.basic_utils import is_evaluation_requested
 from poem.constants import EXECUTION_MODE, TRAINING_MODE, HPO_MODE, SEED, CWA, OWA, TEST_SET_PATH, TEST_SET_RATIO, \
-    EVALUATOR, RANK_BASED_EVALUATOR
-from poem.evaluation.abstract_evaluator import AbstractEvalutor
+    EVALUATOR, RANK_BASED_EVALUATOR, KG_EMBEDDING_MODEL_NAME, DISTMULT_LITERAL_NAME_OWA, TRAINING_SET_PATH, \
+    PATH_TO_NUMERIC_LITERALS, KG_ASSUMPTION, MARGIN_LOSS
+from poem.evaluation.abstract_evaluator import AbstractEvalutor, EvaluatorConfig
 from poem.evaluation.ranked_based_evaluator import RankBasedEvaluator
 from poem.instance_creation_factories.instances import MultimodalInstances
 from poem.instance_creation_factories.triples_factory import TriplesFactory, Instances
-from poem.instance_creation_factories.utils import get_factory
-from poem.kge_models.utils import get_kge_model
+from poem.instance_creation_factories.triples_numeric_literals_factory import TriplesNumericLiteralsFactory
+from poem.kge_models.kge_models_using_numerical_literals.distmult_literal_e_owa import DistMultLiteral
 from poem.model_config import ModelConfig
 from poem.training_loops.basic_training_loop import TrainingLoop
 from poem.training_loops.owa_training_loop import OWATrainingLoop
@@ -52,18 +53,12 @@ class ExperimentalArtifactsContainingEvalResults(ExperimentalArtifacts):
     eval_results: EvalResults
 
 
-@dataclass
-class EvaluatorConfig:
-    """."""
-    config: Dict
-    kge_model: nn.Module
-    entity_to_id: Dict[str, int]
-    relation_to_id: Dict[str, int]
-    training_triples: np.ndarray = None
-
-
 class Pipeline():
     """."""
+
+    KGE_MODELS = {
+        DistMultLiteral.model_name: DistMultLiteral,
+    }
 
     KG_ASSUMPTION_TO_TRAINING_LOOP = {
         CWA: None,
@@ -74,11 +69,17 @@ class Pipeline():
         RANK_BASED_EVALUATOR: RankBasedEvaluator,
     }
 
+    KGE_MODEL_NAME_TO_FACTORY = {
+        DISTMULT_LITERAL_NAME_OWA: TriplesNumericLiteralsFactory
+    }
+
     def __init__(self, config: Dict, training_instances: Optional[Instances] = None,
                  test_instances: Optional[Instances] = None):
         self.config = config
         self.model_config: ModelConfig = None
         self.instance_factory: TriplesFactory = None
+        self.training_loop = None
+        self.evaluator = None
         self.training_instances = training_instances
         self.test_instances = test_instances
         self.has_preprocessed_instances = self.training_instances is True
@@ -91,28 +92,74 @@ class Pipeline():
     def _is_evaluation_requested(self):
         return TEST_SET_PATH in self.config or TEST_SET_RATIO in self.config
 
-    def _create_model_config(self) -> ModelConfig:
+    # ---------------------Get pipeline components---------------------#
+    @staticmethod
+    def get_evaluator(kge_model, config, entity_to_id, relation_to_id, training_triples) -> AbstractEvalutor:
+        """."""
+        evaluator: AbstractEvalutor = config.get(EVALUATOR)
+        eval_config = EvaluatorConfig(config=config,
+                                      entity_to_id=entity_to_id,
+                                      relation_to_id=relation_to_id,
+                                      kge_model=kge_model,
+                                      training_triples=training_triples)
+
+        return evaluator(evaluator_config=eval_config)
+
+    @staticmethod
+    def get_training_loop(model_config: ModelConfig, kge_model: nn.Module, instances: Instances) -> TrainingLoop:
+        """Get training loop."""
+        training_loop = Pipeline.KG_ASSUMPTION_TO_TRAINING_LOOP[kge_model.kg_assumption]
+
+        return training_loop(model_config=model_config, kge_model=kge_model, instances=instances)
+
+    @staticmethod
+    def get_kge_model(model_config: ModelConfig) -> nn.Module:
+        """Get an instance of a knowledge graph embedding model with the given configuration."""
+        kge_model_name = model_config.config[KG_EMBEDDING_MODEL_NAME]
+        kge_model_cls = Pipeline.KGE_MODELS.get(kge_model_name)
+
+        if kge_model_cls is None:
+            raise ValueError(f'Invalid KGE model name: {kge_model_name}')
+
+        return kge_model_cls(model_config=model_config)
+
+    @staticmethod
+    def create_model_config(config, instances: Instances) -> ModelConfig:
         """"""
 
         multimodal_data = None
 
-        if isinstance(self.training_instances, MultimodalInstances):
-            multimodal_data = self.training_instances.multimodal_data
+        if isinstance(instances, MultimodalInstances):
+            multimodal_data = instances.multimodal_data
 
-        model_config = ModelConfig(config=self.config, multimodal_data=multimodal_data)
+        model_config = ModelConfig(config=config, multimodal_data=multimodal_data)
         return model_config
 
-    def _preprocess_train_triples(self) -> Instances:
+    @staticmethod
+    def get_factory(config) -> TriplesFactory:
+        """."""
+        kge_model_name = config[KG_EMBEDDING_MODEL_NAME]
+        factory = Pipeline.KGE_MODEL_NAME_TO_FACTORY.get(kge_model_name)
+
+        if factory is None:
+            raise ValueError(f'invalid factory name: {kge_model_name}')
+
+        return factory(config)
+
+    @staticmethod
+    def preprocess_train_triples(config) -> Instances:
         """"""
 
-        self.instance_factory = get_factory(self.config)
-        return self.instance_factory.create_instances()
+        instance_factory = Pipeline.get_factory(config)
+        return instance_factory.create_instances()
 
-    def _proprcess_train_and_test_triples(self) -> (Instances, Instances):
+    @staticmethod
+    def proprcess_train_and_test_triples(config) -> (Instances, Instances):
         """"""
-        self.instance_factory = get_factory(self.config)
-        return self.instance_factory.create_train_and_test_instances()
+        instance_factory = Pipeline.get_factory(config)
+        return instance_factory.create_train_and_test_instances()
 
+    # -------- Pipeline's instance methods--------#
     def preprocess(self) -> Union[Tuple[Instances, Instances], Instances]:
         """Create instances."""
 
@@ -120,31 +167,34 @@ class Pipeline():
             raise Warning("Instances will be created, although already provided")
 
         if self._is_evaluation_requested:
-            return self._proprcess_train_and_test_triples()
+            return self.proprcess_train_and_test_triples(config=self.config)
         else:
-            return self._preprocess_train_triples()
-
-    def _get_evaluator(self, kge_model) -> AbstractEvalutor:
-        """."""
-        evaluator: AbstractEvalutor = self.config.get(EVALUATOR)
-        eval_config = EvaluatorConfig(config=self.config,
-                                      entity_to_id=self.training_instances.entity_to_id,
-                                      relation_to_id=self.training_instances.relation_to_id,
-                                      kge_model=kge_model,
-                                      training_triples=self.training_instances.instances)
-
-        return evaluator(evaluator_config=eval_config)
-
-    def _get_training_loop(self, model_config: ModelConfig, kge_model: nn.Module, instances: Instances) -> TrainingLoop:
-        """Get training loop."""
-        training_loop = self.KG_ASSUMPTION_TO_TRAINING_LOOP[kge_model.kg_assumption]
-
-        return training_loop(model_config=model_config, kge_model=kge_model, instances=instances)
+            return self.preprocess_train_triples(config=self.config)
 
     def _perform_only_training(self):
         """"""
         if self.has_preprocessed_instances is False:
             self.training_instances = self.preprocess()
+
+    def train(self, kge_model, training_instances):
+        """."""
+        self.training_loop = self.get_training_loop(model_config=kge_model.model_config,
+                                                    kge_model=kge_model,
+                                                    instances=training_instances)
+        # Train the model based on the defined training loop
+        fitted_kge_model, losses_per_epochs = self.training_loop.train()
+
+        return fitted_kge_model, losses_per_epochs
+
+    def perform_hpo(self):
+        """."""
+
+    def evaluate(self, kge_model, test_triples):
+        """."""
+        self.evaluator = self.get_evaluator(kge_model=kge_model)
+        metric_results = self.evaluator.evaluate(test_triples=test_triples)
+
+        return metric_results
 
     def run(self):
         """."""
@@ -160,49 +210,37 @@ class Pipeline():
         if self.has_preprocessed_instances is False:
             self.training_instances = self.preprocess()
         if exec_mode == TRAINING_MODE:
-            kge_model, losses_per_epochs = self.train()
+            self.model_config = self.create_model_config(config=self.config, instances=self.training_instances)
+            kge_model = self.get_kge_model(model_config=self.model_config)
+            fitted_kge_model, losses_per_epochs = self.train(kge_model=kge_model,
+                                                             training_instances=self.training_instances)
 
             if is_evaluation_requested(config=self.config):
                 # eval
-                metric_results = self.evaluate(kge_model=kge_model, test_triples=self.test_instances.instances)
+                metric_results = self.evaluate(kge_model=fitted_kge_model, test_triples=self.test_instances.instances)
 
         elif exec_mode == HPO_MODE:
             self.perform_hpo()
 
-    def train(self):
-        """."""
-        self.model_config = self._create_model_config()
-        kge_model = get_kge_model(model_config=self.model_config)
-        train_loop = self._get_training_loop(model_config=self.model_config,
-                                             kge_model=kge_model,
-                                             instances=self.training_instances)
-        # Train the model based on the defined training loop
-        kge_model, losses_per_epochs = train_loop.train()
 
-        return kge_model, losses_per_epochs
+if __name__ == '__main__':
+    p = '/Users/mali/PycharmProjects/LiteralE/data/FB15k/literals/numerical_literals.txt'
+    t = '/Users/mali/PycharmProjects/LiteralE/data/FB15k/test.txt'
+    config = {
+        KG_EMBEDDING_MODEL_NAME: DISTMULT_LITERAL_NAME_OWA,
+        TRAINING_SET_PATH: t,
+        PATH_TO_NUMERIC_LITERALS: p,
+        KG_ASSUMPTION: OWA,
+        MARGIN_LOSS: 1,
 
-    def perform_hpo(self):
-        """."""
+        SEED: 2
 
-    def evaluate(self, kge_model, test_triples):
-        """."""
-        evaluator = self._get_evaluator(kge_model=kge_model)
-        metric_results = evaluator.evaluate(test_triples=test_triples)
-
-        return metric_results
-
-# if __name__ == '__main__':
-#     p = '/Users/mali/PycharmProjects/LiteralE/data/FB15k/literals/numerical_literals.tsv.txt'
-#     t = '/Users/mali/PycharmProjects/LiteralE/data/FB15k/test.txt'
-#     config = {
-#         KG_EMBEDDING_MODEL_NAME: DISTMULT_LITERAL_NAME_OWA,
-#         TRAINING_SET_PATH: t,
-#         PATH_TO_NUMERIC_LITERALS: p,
-#         KG_ASSUMPTION: OWA,
-#         SEED: 2
-#
-#     }
-#     # preprocess
-#     pipeline = Pipeline(config=config)
-#     instances = pipeline.preprocess()
-#     print(instances)
+    }
+    # preprocess
+    pipeline = Pipeline(config=config)
+    instances = pipeline.preprocess()
+    model_config = pipeline.create_model_config(config=config, instances=instances)
+    kge_model = pipeline.get_kge_model(model_config=model_config)
+    fitted_kge_model, losses = pipeline.train()
+    print(fitted_kge_model)
+    print(losses)
