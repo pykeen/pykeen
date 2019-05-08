34f658354e16e840d507bc59697a7cafd3c13f4b
mali <ali-mehdi@live.de>
2019-05-08 14:45:17 +0200

    Refactor API of instance factories

--- src/poem/instance_creation_factories/triples_factory.py
+++ src/poem/instance_creation_factories/triples_factory.py
@@ -2,132 +2,48 @@
 
 """Implementation of basic instance factory which creates just instances based on standard KG triples."""
 
-from typing import Optional, Tuple, Union
-
 import numpy as np
-from sklearn.model_selection import train_test_split
-
-
-from poem.constants import TRAINING_SET_PATH, TEST_SET_PATH, TEST_SET_RATIO, OWA, SEED, KG_ASSUMPTION
-from poem.instance_creation_factories.instances import Instances, OWAInstances
-from poem.preprocessing.triples_preprocessing_utils.basic_triple_utils import map_triples_elements_to_ids, load_triples, \
-    create_entity_and_relation_mappings
+from poem.instance_creation_factories.instances import Instances, OWAInstances, CWAInstances
+from poem.preprocessing.instance_creation_utils.utils import create_multi_label_objects_instance
+from poem.preprocessing.triples_preprocessing_utils.basic_triple_utils import map_triples_elements_to_ids, \
+    get_unique_subject_relation_pairs
 
 
 class TriplesFactory:
     """."""
 
-    def __init__(self, config):
+    def __init__(self, entity_to_id, relation_to_id):
         """."""
-        self.config = config
-        self.entity_to_id = None
-        self.relation_to_id = None
-        self.train_triples = None
-        self.test_triples = None
-        self.all_triples = None
+        self.entity_to_id = entity_to_id
+        self.relation_to_id = relation_to_id
 
-    def map_triples(
-            self,
-            triples,
-    ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
-        """."""
-        # all_triples: np.ndarray = np.concatenate([train_triples, test_triples], axis=0)
-        # Map each entity/relation to a unique id
-
-
-        # Map triple elements to their ids
-        mapped_train_triples = map_triples_elements_to_ids(
-            triples=triples,
-            entity_to_id=self.entity_to_id,
-            rel_to_id=self.relation_to_id,
-        )
-
-        return mapped_train_triples
-
-    def get_test_triples(self, train_triples):
-        """."""
-        if TEST_SET_PATH in self.config:
-            test_triples = load_triples(self.config[TEST_SET_PATH])
-        else:
-            train_triples, test_triples = train_test_split(
-                train_triples,
-                test_size=self.config.get(TEST_SET_RATIO, 0.1),
-                random_state=self.config[SEED],
-            )
-
-        return train_triples, test_triples
-
-    @property
-    def is_OWA(self):
-        """."""
-        return self.config[KG_ASSUMPTION] == OWA
-
-    def _create_cwa_instances(self):
-        pass
-
-    def _create_owa_instance_object(self, instances, entity_to_id, relation_to_id) -> Instances:
+    def create_owa_instances(self, triples) -> OWAInstances:
         """"""
-        return OWAInstances(instances=instances,
-                            entity_to_id=entity_to_id,
-                            relation_to_id=relation_to_id)
+        mapped_triples = map_triples_elements_to_ids(triples=triples,
+                                                     entity_to_id=self.entity_to_id,
+                                                     rel_to_id=self.relation_to_id)
+        return OWAInstances(instances=mapped_triples,
+                            entity_to_id=self.entity_to_id,
+                            relation_to_id=self.relation_to_id)
 
-    def _create_owa_train_and_test_instances(self, training_triples, test_triples):
+    def create_cwa_instances(self, triples):
         """."""
-        training_instances = self._create_owa_instance_object(instances=training_triples,
-                                                              entity_to_id=self.entity_to_id,
-                                                              relation_to_id=self.relation_to_id)
+        mapped_triples = map_triples_elements_to_ids(triples=triples,
+                                                     entity_to_id=self.entity_to_id,
+                                                     rel_to_id=self.relation_to_id)
 
-        test_instances = self._create_owa_instance_object(instances=test_triples,
-                                                          entity_to_id=self.entity_to_id,
-                                                          relation_to_id=self.relation_to_id)
+        # Step 1: Extract all unique subject relation pairs from the triples
+        subject_relation_pairs = get_unique_subject_relation_pairs(triples=mapped_triples)
 
-        return training_instances, test_instances
+        # Step 2: Create for each (s,r) pair the multi object label
+        s_r_to_mulit_objects = create_multi_label_objects_instance(unique_s_r_pairs=subject_relation_pairs,
+                                                                   triples=mapped_triples,
+                                                                   num_entities=len(self.entity_to_id))
 
-    def _create_entity_and_relations_mappings(self):
-        """."""
-        self.entity_to_id, self.relation_to_id = create_entity_and_relation_mappings(triples=self.all_triples)
+        subject_relation_pairs = np.array(list(s_r_to_mulit_objects.keys()), dtype=np.float)
+        labels = np.array(list(s_r_to_mulit_objects.values()), dtype=np.float)
 
-    def create_train_and_test_instances(self) -> (Instances, Instances):
-        """."""
-
-        # Step 1: Load training triples
-        training_triples = load_triples(path=self.config[TRAINING_SET_PATH], delimiter='\t')
-        self.train_triples = training_triples
-        self.all_triples = training_triples
-
-        # Step 2: Create test triples if requested
-        train_triples, test_triples = self.get_test_triples(train_triples=training_triples)
-        self.test_triples = test_triples
-        self.all_triples = np.concatenate([train_triples, test_triples], axis=0)
-        self._create_entity_and_relations_mappings()
-
-
-        training_triples = self.map_triples(triples=self.train_triples)
-
-        test_triples = self.map_triples(triples=self.test_triples)
-
-        if self.is_OWA:
-            return self._create_owa_train_and_test_instances(training_triples=training_triples,
-                                                             test_triples=test_triples)
-        else:
-            # TODO: ADD CWA logic
-            pass
-
-    def create_instances(self) -> Instances:
-        """"""
-
-        # Step 1: Load training triples
-        training_triples = load_triples(path=self.config[TRAINING_SET_PATH], delimiter='\t')
-        self.train_triples = training_triples
-        self.all_triples = training_triples
-        self._create_entity_and_relations_mappings()
-        training_triples = self.map_triples(triples=self.train_triples)
-
-        if self.is_OWA:
-            return self._create_owa_instance_object(instances=training_triples,
-                                                    entity_to_id=self.entity_to_id,
-                                                    relation_to_id=self.relation_to_id)
-
-        else:
-            # TODO: ADD CWA logic
-            pass
+        return CWAInstances(instances=subject_relation_pairs,
+                            entity_to_id=self.entity_to_id,
+                            relation_to_id=self.relation_to_id,
+                            labels=labels)
--- src/poem/instance_creation_factories/triples_numeric_literals_factory.py
+++ src/poem/instance_creation_factories/triples_numeric_literals_factory.py
@@ -6,60 +6,52 @@ from typing import Tuple
 
 import numpy as np
 from typing import Dict
-from poem.constants import PATH_TO_NUMERIC_LITERALS, NUMERIC_LITERALS, OWA, CWA
-from poem.instance_creation_factories.instances import MultimodalInstances, MultimodalOWAInstances, \
+from poem.constants import PATH_TO_NUMERIC_LITERALS, NUMERIC_LITERALS
+from poem.instance_creation_factories.instances import MultimodalOWAInstances, \
     MultimodalCWAInstances
-from poem.instance_creation_factories.triples_factory import TriplesFactory, Instances
+from poem.instance_creation_factories.triples_factory import TriplesFactory
 from poem.preprocessing.numeric_literals_preprocessing_utils.basic_utils import create_matix_of_literals
-from poem.preprocessing.triples_preprocessing_utils.basic_triple_utils import load_triples
 
 
 class TriplesNumericLiteralsFactory(TriplesFactory):
     """."""
 
-    def __init__(self, config: Dict):
-        super().__init__(config)
+    def __init__(self, entity_to_id, relation_to_id, numeric_triples):
+        super().__init__(entity_to_id, relation_to_id)
+        self.numeric_triples = numeric_triples
+        self.numeric_literals = None
+        self.multimodal_data = None
 
     def _create_numeric_literals(self) -> np.ndarray:
         """"""
-        numeric_triples = load_triples(path=self.config[PATH_TO_NUMERIC_LITERALS], delimiter='\t')
-        numeric_literals = create_matix_of_literals(numeric_triples=numeric_triples, entity_to_id=self.entity_to_id)
-        return numeric_literals
-
-    def _create_multimodal_instances(self, instances: Instances, numeric_literals) -> MultimodalInstances:
-        """"""
-
-        multimodal_data = {
-            NUMERIC_LITERALS: numeric_literals
+        self.numeric_literals = create_matix_of_literals(numeric_triples=self.numeric_triples,
+                                                         entity_to_id=self.entity_to_id)
+        self.multimodal_data = {
+            NUMERIC_LITERALS: self.numeric_literals
         }
 
-        if instances.kg_assumption == OWA:
-            return MultimodalOWAInstances(instances=instances.instances,
-                                          entity_to_id=instances.entity_to_id,
-                                          relation_to_id=instances.relation_to_id,
-                                          kg_assumption=instances.kg_assumption,
-                                          multimodal_data=multimodal_data)
-        elif instances.kg_assumption == CWA:
-            return MultimodalCWAInstances(instances=instances.instances,
-                                          entity_to_id=instances.entity_to_id,
-                                          relation_to_id=instances.relation_to_id,
-                                          kg_assumption=instances.kg_assumption,
-                                          multimodal_data=multimodal_data)
-
-    def create_train_and_test_instances(self) -> Tuple[Instances, Instances]:
+    def create_owa_instances(self, triples) -> MultimodalOWAInstances:
         """"""
-        train_instances, test_instances = super().create_train_and_test_instances()
-        numeric_literals = self._create_numeric_literals()
+        owa_instances = super().create_owa_instances(triples=triples)
 
-        train_instances = self._create_multimodal_instances(instances=train_instances,
-                                                            numeric_literals=numeric_literals)
-        test_instances = self._create_multimodal_instances(instances=test_instances,
-                                                           numeric_literals=numeric_literals)
+        if self.multimodal_data is None:
+            self._create_numeric_literals()
 
-        return train_instances, test_instances
+        return MultimodalOWAInstances(instances=owa_instances.instances,
+                                      entity_to_id=owa_instances.entity_to_id,
+                                      relation_to_id=owa_instances.relation_to_id,
+                                      kg_assumption=owa_instances.kg_assumption,
+                                      multimodal_data=self.multimodal_data)
 
-    def create_instances(self) -> Instances:
-        """"""
-        triple_instances = super().create_instances()
-        numeric_literals = self._create_numeric_literals()
-        return self._create_multimodal_instances(instances=triple_instances, numeric_literals=numeric_literals)
+    def create_cwa_instances(self, triples) -> MultimodalCWAInstances:
+        """."""
+        cwa_instances = super().create_cwa_instances(triples=triples)
+
+        if self.multimodal_data is None:
+            self._create_numeric_literals()
+
+        return MultimodalCWAInstances(instances=cwa_instances.instances,
+                                      entity_to_id=cwa_instances.entity_to_id,
+                                      relation_to_id=cwa_instances.relation_to_id,
+                                      kg_assumption=cwa_instances.kg_assumption,
+                                      multimodal_data=self.multimodal_data)
