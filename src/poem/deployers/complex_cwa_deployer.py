075ff45525f392fe7a03c80c3628de535bf0b334
mali <ali-mehdi@live.de>
2019-05-09 15:20:03 +0200

    Implement exemplary script to train ComplexCWA

--- src/poem/deployers/complex_cwa_deployer.py
+++ src/poem/deployers/complex_cwa_deployer.py
@@ -1,15 +1,16 @@
-import torch
-
-from poem.constants import KG_ASSUMPTION, EMBEDDING_DIM, CWA, NUM_ENTITIES, NUM_RELATIONS
+from poem.constants import KG_ASSUMPTION, EMBEDDING_DIM, CWA, NUM_ENTITIES, NUM_RELATIONS, INPUT_DROPOUT
 from poem.instance_creation_factories.triples_factory import TriplesFactory
 from poem.kge_models.unimodal_kge_models.complex_cwa import ComplexCWA
 from poem.model_config import ModelConfig
 from poem.preprocessing.triples_preprocessing_utils.basic_triple_utils import create_entity_and_relation_mappings, \
     load_triples
+from torch import optim
 import numpy as np
 
+from poem.training_loops.cwa_training_loop import CWATrainingLoop
+
 if __name__ == '__main__':
-    path_to_training_data = '/Users/mali/PycharmProjects/POEM/tests/resources/test.txt'
+    path_to_training_data = '../../../tests/resources/test.txt'
 
     # Step 1: Create instances
     training_triples = load_triples(path=path_to_training_data)
@@ -22,8 +23,21 @@ if __name__ == '__main__':
         KG_ASSUMPTION: CWA,
         EMBEDDING_DIM: 200,
         NUM_ENTITIES: len(entity_to_id),
-        NUM_RELATIONS: len(relation_to_id)
+        NUM_RELATIONS: len(relation_to_id),
+        INPUT_DROPOUT: 0.2
     }
 
+    # Configure KGE model
     model_config = ModelConfig(config=config, multimodal_data=None)
     kge_model = ComplexCWA(model_config=model_config)
+    parameters = filter(lambda p: p.requires_grad, kge_model.parameters())
+    optimizer = optim.Adam(params=parameters)
+
+    all_entities = np.array(list(entity_to_id.values()))
+
+    # Train
+    cwa_training_loop = CWATrainingLoop(kge_model=kge_model, optimizer=optimizer, all_entities=all_entities)
+
+    fitted_kge_model, losses = cwa_training_loop.train(training_instances=instances,
+                                                       num_epochs=2,
+                                                       batch_size=128)
