1d18a93622478bcd01dcf43e1b22642553c9ca29
mali <ali-mehdi@live.de>
2019-05-09 17:05:49 +0200

    Implement ComplexLiteral

--- src/poem/kge_models/kge_models_using_numerical_literals/complex_literal_cwa.py
+++ src/poem/kge_models/kge_models_using_numerical_literals/complex_literal_cwa.py
@@ -7,11 +7,11 @@ import torch.nn as nn
 from torch.nn.init import xavier_normal_
 
 from poem.constants import INPUT_DROPOUT, COMPLEX_CWA_NAME, NUM_ENTITIES, \
-    NUM_RELATIONS, EMBEDDING_DIM, PREFERRED_DEVICE
+    NUM_RELATIONS, EMBEDDING_DIM, PREFERRED_DEVICE, GPU, NUMERIC_LITERALS
 from poem.model_config import ModelConfig
 
 
-class ComplexLiteral(torch.nn.Module):
+class ComplexLiteralCWA(torch.nn.Module):
     """
         An implementation of ComplexLiteral [agustinus2018] based on the closed world assumption (CWA).
 
@@ -19,69 +19,102 @@ class ComplexLiteral(torch.nn.Module):
                            arXiv preprint arXiv:1802.00934 (2018).
         """
 
-    def __init__(self, num_entities, num_relations, numerical_literals):
-        super(ComplexLiteral, self).__init__()
+    def __init__(self, num_entities, num_relations, multimodal_data, embedding_dim=50, input_dropout=0.2,
+                 preferred_device=GPU):
+        super(ComplexLiteralCWA, self).__init__()
+        self.device = torch.device(
+            'cuda:0' if torch.cuda.is_available() and preferred_device else 'cpu')
+        # Entity dimensions
+        #: The number of entities in the knowledge graph
+        self.num_entities = num_entities
+        #: The number of unique relation types in the knowledge graph
+        self.num_relations = num_relations
+        #: The dimension of the embeddings to generate
+        self.embedding_dim = embedding_dim
 
-        self.emb_dim = Config.embedding_dim
-
-        self.emb_e_real = torch.nn.Embedding(num_entities, self.emb_dim, padding_idx=0)
-        self.emb_e_img = torch.nn.Embedding(num_entities, self.emb_dim, padding_idx=0)
-        self.emb_rel_real = torch.nn.Embedding(num_relations, self.emb_dim, padding_idx=0)
-        self.emb_rel_img = torch.nn.Embedding(num_relations, self.emb_dim, padding_idx=0)
+        self.entity_embs_real = nn.Embedding(self.num_entities, self.embedding_dim, padding_idx=0)
+        self.entity_embs_img = nn.Embedding(self.num_entities, self.embedding_dim, padding_idx=0)
+        self.relation_embs_real = nn.Embedding(self.num_relations, self.embedding_dim, padding_idx=0)
+        self.relation_embs_img = nn.Embedding(self.num_relations, self.embedding_dim, padding_idx=0)
 
         # Literal
-        # num_ent x n_num_lit
-        self.numerical_literals = Variable(torch.from_numpy(numerical_literals)).cuda()
-        self.n_num_lit = self.numerical_literals.size(1)
+        # num_ent x num_lit
+        numeric_literals = multimodal_data.get(NUMERIC_LITERALS)
+        self.numeric_literals = nn.Embedding.from_pretrained(
+            torch.tensor(numeric_literals, dtype=torch.float, device=self.device), freeze=True)
+        # Number of columns corresponds to number of literals
+        self.num_of_literals = self.numeric_literals.weight.data.shape[1]
 
-        self.emb_num_lit_real = torch.nn.Sequential(
-            torch.nn.Linear(self.emb_dim+self.n_num_lit, self.emb_dim),
+        self.real_non_lin_transf = torch.nn.Sequential(
+            nn.Linear(self.embedding_dim + self.num_of_literals, self.embedding_dim),
             torch.nn.Tanh()
         )
 
-        self.emb_num_lit_img = torch.nn.Sequential(
-            torch.nn.Linear(self.emb_dim+self.n_num_lit, self.emb_dim),
+        self.img_non_lin_transf = torch.nn.Sequential(
+            nn.Linear(self.embedding_dim + self.num_of_literals, self.embedding_dim),
             torch.nn.Tanh()
         )
 
-        # Dropout + loss
-        self.inp_drop = torch.nn.Dropout(Config.input_dropout)
-        self.loss = torch.nn.BCELoss()
+        self.inp_drop = torch.nn.Dropout(input_dropout)
+        self.criterion = torch.nn.BCELoss()
 
     def init(self):
-        xavier_normal_(self.emb_e_real.weight.data)
-        xavier_normal_(self.emb_e_img.weight.data)
-        xavier_normal_(self.emb_rel_real.weight.data)
-        xavier_normal_(self.emb_rel_img.weight.data)
+        """."""
+        xavier_normal_(self.entity_embs_real.weight.data)
+        xavier_normal_(self.entity_embs_img.weight.data)
+        xavier_normal_(self.relation_embs_real.weight.data)
+        xavier_normal_(self.relation_embs_img.weight.data)
 
-    def forward(self, e1, rel):
-        e1_emb_real = self.emb_e_real(e1).view(Config.batch_size, -1)
-        rel_emb_real = self.emb_rel_real(rel).view(Config.batch_size, -1)
-        e1_emb_img = self.emb_e_img(e1).view(Config.batch_size, -1)
-        rel_emb_img = self.emb_rel_img(rel).view(Config.batch_size, -1)
+    def _apply_g_function(self, real_embs, img_embs, literals):
+        """"""
+        real = self.real_non_lin_transf(torch.cat([real_embs, literals], 1))
+        img = self.img_non_lin_transf(torch.cat([img_embs, literals], 1))
 
-        # Begin literals
+        return real, img
 
-        e1_num_lit = self.numerical_literals[e1.view(-1)]
-        e1_emb_real = self.emb_num_lit_real(torch.cat([e1_emb_real, e1_num_lit], 1))
-        e1_emb_img = self.emb_num_lit_img(torch.cat([e1_emb_img, e1_num_lit], 1))
+    def _compute_loss(self, predictions, labels):
+        """"""
+        loss = self.criterion(predictions, labels)
+        return loss
 
-        e2_multi_emb_real = self.emb_num_lit_real(torch.cat([self.emb_e_real.weight, self.numerical_literals], 1))
-        e2_multi_emb_img = self.emb_num_lit_img(torch.cat([self.emb_e_img.weight, self.numerical_literals], 1))
+    def forward(self, batch, labels):
+        """"""
+        batch_heads = batch[:, 0:1]
+        batch_relations = batch[:, 1:2]
+
+        heads_embedded_real = self.inp_drop(self.entity_embs_real(batch_heads)).view(-1, self.embedding_dim)
+        rels_embedded_real = self.inp_drop(self.relation_embs_real(batch_relations)).view(-1,
+                                                                                          self.embedding_dim)
+        heads_embedded_img = self.inp_drop(self.entity_embs_img(batch_heads)).view(-1, self.embedding_dim)
+        relations_embedded_img = self.inp_drop(self.relation_embs_img(batch_relations)).view(-1,
+                                                                                             self.embedding_dim)
+        # Literals
+        head_literals = self.numeric_literals(batch_heads).view(-1, self.num_of_literals)
+        heads_embedded_real, heads_embedded_img = self._apply_g_function(
+            real_embs=heads_embedded_real,
+            img_embs=heads_embedded_img,
+            literals=head_literals
+        )
+
+        e2_multi_emb_real = self.real_non_lin_transf(
+            torch.cat([self.entity_embs_real.weight, self.numeric_literals.weight], 1))
+        e2_multi_emb_img = self.img_non_lin_transf(
+            torch.cat([self.entity_embs_img.weight, self.numeric_literals.weight], 1))
 
         # End literals
 
-        e1_emb_real = self.inp_drop(e1_emb_real)
-        rel_emb_real = self.inp_drop(rel_emb_real)
-        e1_emb_img = self.inp_drop(e1_emb_img)
-        rel_emb_img = self.inp_drop(rel_emb_img)
+        heads_embedded_real = self.inp_drop(heads_embedded_real)
+        rels_embedded_real = self.inp_drop(rels_embedded_real)
+        heads_embedded_img = self.inp_drop(heads_embedded_img)
+        relations_embedded_img = self.inp_drop(relations_embedded_img)
 
-        realrealreal = torch.mm(e1_emb_real*rel_emb_real, e2_multi_emb_real.t())
-        realimgimg = torch.mm(e1_emb_real*rel_emb_img, e2_multi_emb_img.t())
-        imgrealimg = torch.mm(e1_emb_img*rel_emb_real, e2_multi_emb_img.t())
-        imgimgreal = torch.mm(e1_emb_img*rel_emb_img, e2_multi_emb_real.t())
+        real_real_real = torch.mm(heads_embedded_real * rels_embedded_real, e2_multi_emb_real.t())
+        real_img_img = torch.mm(heads_embedded_real * relations_embedded_img, e2_multi_emb_img.t())
+        img_real_img = torch.mm(heads_embedded_img * heads_embedded_real, e2_multi_emb_img.t())
+        img_img_real = torch.mm(heads_embedded_img * relations_embedded_img, e2_multi_emb_real.t())
 
-        pred = realrealreal + realimgimg + imgrealimg - imgimgreal
-        pred = F.sigmoid(pred)
+        predictions = real_real_real + real_img_img + img_real_img - img_img_real
+        predictions = torch.sigmoid(predictions)
+        loss = self._compute_loss(predictions=predictions, labels=labels)
 
-        return pred
+        return loss
