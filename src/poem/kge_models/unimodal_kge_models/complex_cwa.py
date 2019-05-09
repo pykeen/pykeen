fdc4c441fcc4758a870c324bfb5d25d27e00457c
mali <ali-mehdi@live.de>
2019-05-09 15:19:06 +0200

    Refactor Complex

--- src/poem/kge_models/unimodal_kge_models/complex_cwa.py
+++ src/poem/kge_models/unimodal_kge_models/complex_cwa.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-"""Implementation of the Complex model."""
+"""Implementation of the Complex model based on the closed world assumption (CWA)."""
 
 import torch
 import torch.nn as nn
 from torch.nn.init import xavier_normal_
 
 from poem.constants import INPUT_DROPOUT, COMPLEX_CWA_NAME, NUM_ENTITIES, \
-    NUM_RELATIONS, EMBEDDING_DIM
-from torch.nn import functional as F
+    NUM_RELATIONS, EMBEDDING_DIM, PREFERRED_DEVICE
 from poem.model_config import ModelConfig
 
 
@@ -23,7 +22,8 @@ class ComplexCWA(torch.nn.Module):
 
     def __init__(self, model_config: ModelConfig):
         super(ComplexCWA, self).__init__()
-
+        self.device = torch.device(
+            'cuda:0' if torch.cuda.is_available() and model_config.config(PREFERRED_DEVICE) else 'cpu')
         # Entity dimensions
         #: The number of entities in the knowledge graph
         self.config = model_config.config
@@ -42,12 +42,12 @@ class ComplexCWA(torch.nn.Module):
         self.init()
 
         self.inp_drop = torch.nn.Dropout(self.config[INPUT_DROPOUT])
-        self.loss = torch.nn.BCELoss()
+        self.criterion = torch.nn.BCELoss()
 
     def init(self):
         """."""
         xavier_normal_(self.entity_embeddings_real.weight.data)
-        xavier_normal_(self.entity_embeddings_img.data)
+        xavier_normal_(self.entity_embeddings_img.weight.data)
         xavier_normal_(self.relation_embeddings_real.weight.data)
         xavier_normal_(self.relation_embeddings_img.weight.data)
 
@@ -58,13 +58,15 @@ class ComplexCWA(torch.nn.Module):
 
     def forward(self, batch, labels):
         """"""
-        batch_subjects = batch[:,0:1]
-        batch_relations = batch[:,1:2]
+        batch_subjects = batch[:, 0:1]
+        batch_relations = batch[:, 1:2]
 
-        subjects_embedded_real = self.inp_drop(self.emb_e_real(batch_subjects)).view(-1, self.embedding_dim)
-        relations_embedded_real = self.inp_drop(self.emb_rel_real(batch_relations)).view(-1, self.embedding_dim)
-        subjects_embedded_img = self.inp_drop(self.emb_e_img(batch_subjects)).view(-1, self.embedding_dim)
-        relations_embedded_img = self.inp_drop(self.emb_rel_img(batch_relations)).view(-1, self.embedding_dim)
+        subjects_embedded_real = self.inp_drop(self.entity_embeddings_real(batch_subjects)).view(-1, self.embedding_dim)
+        relations_embedded_real = self.inp_drop(self.relation_embeddings_real(batch_relations)).view(-1,
+                                                                                                     self.embedding_dim)
+        subjects_embedded_img = self.inp_drop(self.entity_embeddings_img(batch_subjects)).view(-1, self.embedding_dim)
+        relations_embedded_img = self.inp_drop(self.relation_embeddings_img(batch_relations)).view(-1,
+                                                                                                   self.embedding_dim)
 
         # Apply dropout
         subjects_embedded_real = self.inp_drop(subjects_embedded_real)
@@ -84,6 +86,6 @@ class ComplexCWA(torch.nn.Module):
                                 self.entity_embeddings_real.weight.transpose(1, 0))
 
         predictions = real_real_real + real_img_img + img_real_img - img_img_real
-        predictions = F.sigmoid(predictions)
+        predictions = torch.sigmoid(predictions)
         loss = self._compute_loss(predictions=predictions, labels=labels)
         return loss
