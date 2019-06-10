42536752335a3a13c872294aff2117c677b85f4e
mali <ali-mehdi@live.de>
2019-06-10 11:43:29 +0200

    Implement script for computing nearest neighbours

---
+++ src/poem/postprocessing/distances.py
@@ -0,0 +1,20 @@
+import torch
+
+
+def comute_nearest_neighbours(x, neighbours, k=1):
+    """
+
+    :param x: tensor of shape 1 x D
+    :param neighbours: tensor of shape N x D
+    :param k:
+    :return:
+    """
+    dist = torch.nn.PairwiseDistance(p=2.0)
+    x = x.expand(neighbours.shape[0], neighbours.shape[1])
+
+    distances = dist(x, neighbours).view(-1)
+    sorted_indices = torch.argsort(distances)
+
+    k_nearest = distances[0:k]
+
+    return sorted_indices[0:k], k_nearest
