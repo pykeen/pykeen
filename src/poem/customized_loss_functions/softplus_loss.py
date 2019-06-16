cb227b27d2fb16a4ca74b8ea31f94d8111e23d85
mali <ali-mehdi@live.de>
2019-06-16 21:47:48 +0200

    Implement softplus loss

--- src/poem/customized_loss_functions/softplus_loss.py
+++ src/poem/customized_loss_functions/softplus_loss.py
@@ -1,4 +1,3 @@
-
 import torch
 from torch._jit_internal import weak_script_method
 from torch.nn.modules.loss import _Loss
@@ -6,10 +5,18 @@ from torch.nn.modules.loss import _Loss
 
 class SoftplusLoss(_Loss):
 
-    def __init__(self,reduction='mean'):
+    def __init__(self, reduction='mean'):
         super(SoftplusLoss, self).__init__(reduction=reduction)
 
     @weak_script_method
     def forward(self, scores, labels):
-        torch.exponential_()
+        """."""
+        labels = labels * (-1)
+        scores = labels * scores
+        scores = torch.exp(scores)
+        scores += 1.
 
+        scores = torch.log(scores)
+        loss = torch.mean(scores) if self.reduction == 'mean' else torch.sum(scores)
+
+        return loss
