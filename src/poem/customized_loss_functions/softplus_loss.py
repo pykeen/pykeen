4481303e488cce1d75862796882eb2e409170f15
mali <ali-mehdi@live.de>
2019-06-18 16:58:22 +0200

    Refactor SoftplusLoss

--- src/poem/customized_loss_functions/softplus_loss.py
+++ src/poem/customized_loss_functions/softplus_loss.py
@@ -7,16 +7,15 @@ class SoftplusLoss(_Loss):
 
     def __init__(self, reduction='mean'):
         super(SoftplusLoss, self).__init__(reduction=reduction)
+        self.softplus = torch.nn.Softplus(beta=1, threshold=20)
 
     @weak_script_method
     def forward(self, scores, labels):
         """."""
         labels = labels * (-1)
         scores = labels * scores
-        scores = torch.exp(scores)
-        scores += 1.
 
-        scores = torch.log(scores)
-        loss = torch.mean(scores) if self.reduction == 'mean' else torch.sum(scores)
+        loss = self.softplus(labels * scores)
+        loss = torch.mean(loss) if self.reduction == 'mean' else torch.sum(scores)
 
         return loss
