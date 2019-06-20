fe14fc606f302c4add46c4eb5fcc226414b4ff01
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-06-20 11:05:03 +0200

    Remove conditional logic from Softplus loss class

--- src/poem/customized_loss_functions/softplus_loss.py
+++ src/poem/customized_loss_functions/softplus_loss.py
@@ -8,14 +8,17 @@ class SoftplusLoss(_Loss):
     def __init__(self, reduction='mean'):
         super(SoftplusLoss, self).__init__(reduction=reduction)
         self.softplus = torch.nn.Softplus(beta=1, threshold=20)
+        if self.reduction == 'mean':
+            self._reduction_method = torch.mean
+        else:
+            self._reduction_method = torch.sum
 
     @weak_script_method
     def forward(self, scores, labels):
         """."""
         labels = labels * (-1)
         scores = labels * scores
-
+        # FIXME Why not just do loss = self.softplus(labels * labels * scores * -1)?
         loss = self.softplus(labels * scores)
-        loss = torch.mean(loss) if self.reduction == 'mean' else torch.sum(scores)
-
+        loss = self._reduction_method(loss)
         return loss
