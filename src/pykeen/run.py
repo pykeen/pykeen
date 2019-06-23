4ac1e61ca1945f09f5bdce1b86913cdd1b5ece58
mali <ali-mehdi@live.de>
2019-06-23 14:06:58 +0200

    Fix handling of output directory

--- src/pykeen/run.py
+++ src/pykeen/run.py
@@ -5,9 +5,10 @@
 import json
 import os
 import pickle
+import time
 from dataclasses import dataclass
 from typing import Dict, Mapping, Optional
-import time
+
 import numpy as np
 import torch
 
@@ -134,9 +135,9 @@ def run(
     if output_directory is None:
         if OUTPUT_DIREC not in config:
             raise Exception('No output directory defined.')
-
         output_directory = os.path.join(config[OUTPUT_DIREC], time.strftime("%Y-%m-%d-%H-%M-%S"))
-        os.makedirs(output_directory, exist_ok=True)
+
+    os.makedirs(output_directory, exist_ok=True)
 
     config['pykeen-version'] = VERSION
 
