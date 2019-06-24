5237245336c96fda609073699fb8fb6f61d299b7
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-06-24 12:33:15 +0200

    Add iterator implementation of splitting list in batches
    There are a lot of things that can be updated to stream instead of load in memory

--- src/poem/training_loops/utils.py
+++ src/poem/training_loops/utils.py
@@ -2,16 +2,24 @@
 
 """Utilities for training KGE models."""
 
-from typing import List
+from typing import Iterable, List, TypeVar
 
 __all__ = [
+    'split_list_in_batches_iter',
     'split_list_in_batches',
 ]
 
+X = TypeVar('X')
 
-def split_list_in_batches(input_list, batch_size: int) -> List:
+
+def split_list_in_batches_iter(input_list: List[X], batch_size: int) -> Iterable[List[X]]:
     """Split a list of instances in batches of size batch_size."""
-    return [
+    return (
         input_list[i:i + batch_size]
         for i in range(0, len(input_list), batch_size)
-    ]
+    )
+
+
+def split_list_in_batches(input_list: List[X], batch_size: int) -> List[List[X]]:
+    """Split a list of instances in batches of size batch_size."""
+    return list(split_list_in_batches_iter(input_list=input_list, batch_size=batch_size))
