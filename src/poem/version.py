d061b896292a63119dee51f53eed95055ef24997
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-06-24 17:06:36 +0200

    Add version getter

--- src/poem/version.py
+++ src/poem/version.py
@@ -2,6 +2,14 @@
 
 """Version information for POEM."""
 
-__all__ = ['VERSION']
+__all__ = [
+    'VERSION',
+    'get_version',
+]
 
 VERSION = '0.0.1'
+
+
+def get_version() -> str:
+    """Get the software version of POEM."""
+    return VERSION
