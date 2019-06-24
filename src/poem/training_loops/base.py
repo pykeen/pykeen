4ac514759056e131a4f6edcdaa7e547d46f6f7f0
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-06-24 17:08:11 +0200

    Add embeddingdb uploader to training loop
    @mali-git I need your help implementing _get_entity_to_vector_dict() and also getting the dimensions and other configuration JSON stuff. Then we can automatically upload the results to the embeddingdb (which will stay separate from pykeen/poem since it's more general for entity embeddings coming from other types of representation learning as well, e.g., RatVec, chemical autoencoders, etc.)

--- src/poem/training_loops/base.py
+++ src/poem/training_loops/base.py
@@ -4,12 +4,14 @@
 
 import logging
 from abc import ABC, abstractmethod
-from typing import List, Tuple
+from typing import List, Mapping, Tuple
 
 import numpy as np
 import torch.nn as nn
+from tqdm import tqdm
 
 from ..instance_creation_factories.instances import Instances
+from ..version import get_version
 
 __all__ = [
     'TrainingLoop',
@@ -45,3 +47,40 @@ class TrainingLoop(ABC):
 
         :return: A pair of the KGE model and the losses per epoch.
         """
+
+    def _get_entity_to_vector_dict(self) -> Mapping:
+        raise NotImplementedError
+
+    def to_embeddingdb(self, session=None, use_tqdm: bool = False):
+        """Upload to the embedding database.
+
+        :param session: Optional SQLAlchemy session
+        :param use_tqdm: Use :mod:`tqdm` progress bar?
+        :rtype: embeddingdb.sql.models.Collection
+        """
+        from embeddingdb.sql.models import Embedding, Collection
+
+        if session is None:
+            from embeddingdb.sql.models import get_session
+            session = get_session()
+
+        collection = Collection(
+            package_name='poem',
+            package_version=get_version(),
+            dimensions=...,
+            extras=...,
+        )
+
+        it = self._get_entity_to_vector_dict().items()
+        if use_tqdm:
+            it = tqdm(it, desc='Building SQLAlchemy models')
+        for curie, vector in it:
+            embedding = Embedding(
+                collection=collection,
+                curie=curie,
+                vector=vector,
+            )
+            session.add(embedding)
+        session.add(collection)
+        session.commit()
+        return collection
