8d9cfad7172de0a1a0072bc9adfc255188425fc4
mali <ali-mehdi@live.de>
2019-07-16 14:54:12 +0200

    Add FIXME

--- src/poem/hyper_parameter_optimization/random_search.py
+++ src/poem/hyper_parameter_optimization/random_search.py
@@ -77,6 +77,7 @@ class RandomSearch(HPOptimizer):
             model = type(model_class.__name__, (), **params_to_values)
 
             # Train model
+            #FIXME: Pass model to training loop
             trained_model, losses_per_epochs = training_loop.train(
                 training_instances=training_instances,
                 **current_params_to_values,
