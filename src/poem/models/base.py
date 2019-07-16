4901fa0e4b0d373bae5b1c58d12584f6da03a3e9
mali <ali-mehdi@live.de>
2019-07-16 14:44:38 +0200

    Fix formatting

--- src/poem/models/base.py
+++ src/poem/models/base.py
@@ -70,6 +70,7 @@ class BaseModule(nn.Module):
         # Marker to check wether the forward constraints of a models has been applied before starting loss calculation
         self.forward_constraint_applied = False
 
+
     def _init_embeddings(self):
         self.entity_embeddings = nn.Embedding(
             self.num_entities,
