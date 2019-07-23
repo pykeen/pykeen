7ba05ebb36e811f610d2af175507e1fe5dfeb75a
mali <ali-mehdi@live.de>
2019-07-23 11:50:55 +0200

    Prepate basic tests to check that models can be executed

---
+++ tests/test_models.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+"""Test that models can be executed."""
+import unittest
+
+
+class TestModels(unittest.TestCase):
+    """Test that models can be executed."""
+
+    def __init__(self):
+        """."""
+        factory = None
+        path_to_training_file = None
+
+    def test_um(self):
+        """Tests that Unstructured Model can be executed."""
+        pass
+
+    def test_se(self):
+        """Tests that Structured Embedding can be executed."""
+        pass
+
+    def test_trans_e(self):
+        """Tests that TransE can be executed."""
+        pass
+
+    def test_trans_h(self):
+        """Tests that TransH can be executed."""
+        pass
+
+    def test_trans_r(self):
+        """Tests that TransR can be executed."""
+        pass
+
+    def test_trans_d(self):
+        """Tests that TransD can be executed."""
+        pass
+
+    def test_rescale(self):
+        """Tests that RESCALE can be executed."""
+        pass
+
+    def test_distmult(self):
+        """Tests that DISTMULT can be executed."""
+        pass
+
+    def test_complex(self):
+        """Tests that COMPLEX can be executed."""
+        pass
+
+    def test_rotate(self):
+        """Tests that Rotate can be executed."""
+        pass
+
+    def test_hole(self):
+        """Tests that HolE can be executed."""
+        pass
+
+    def test_conv_kb(self):
+        """Tests that ConvKB can be executed."""
+        pass
