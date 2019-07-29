38c19fea53da0268c27476d0d1d40b29073d5a16
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2019-07-29 16:34:55 +0200

    Update unittest
    * Cleanup
    * Add non-positivity checking for TransX model family

--- tests/test_models.py
+++ tests/test_models.py
@@ -92,233 +92,32 @@ class TestCaseTransD(ModelTestCase):
 class TestCaseTransE(ModelTestCase):
     model_cls = TransE
 
+    def check_scores(self, batch, scores):
+        super(TestCaseTransE, self).check_scores(batch=batch, scores=scores)
+
+        # Distance-based model
+        assert (scores <= 0.0).all()
+
+
+class TestCaseTransH(ModelTestCase):
+    model_cls = TransH
+
+    def check_scores(self, batch, scores):
+        super(TestCaseTransH, self).check_scores(batch=batch, scores=scores)
+
+        # Distance-based model
+        assert (scores <= 0.0).all()
+
 
 class TestCaseTransR(ModelTestCase):
     model_cls = TransR
 
+    def check_scores(self, batch, scores):
+        super(TestCaseTransR, self).check_scores(batch=batch, scores=scores)
+
+        # Distance-based model
+        assert (scores <= 0.0).all()
+
 
 class TestCaseUM(ModelTestCase):
     model_cls = UnstructuredModel
-
-# class TestModels(unittest.TestCase):
-#     """Test that models can be executed."""
-#
-#     path_to_training_data = os.path.join(RESOURCES_DIRECTORY, 'test.txt')
-#     factory = TriplesFactory(path=path_to_training_data)
-#
-#     def _common_test(self, constructor):
-#         model = constructor(triples_factory=self.factory)
-#
-#     def test_um(self):
-#         """Tests that Unstructured Model can be executed."""
-#         um = UnstructuredModel(triples_factory=self.factory)
-#         self.assertIsNotNone(um)
-#
-#     def test_se(self):
-#         """Tests that Structured Embedding can be executed."""
-#         model = StructuredEmbedding(triples_factory=self.factory, embedding_dim=8)
-#         self.assertIsNotNone(model)
-#
-#         # Dummy forward passes
-#         # TODO: Use triple factory
-#         batch_size = 16
-#         triples = torch.zeros(batch_size, 3, dtype=torch.long)
-#
-#         # TODO: Refactor common tests for all models, e.g. shape checking
-#         # Test forward_owa
-#         scores = model.forward_owa(triples)
-#         # Check shape
-#         assert scores.shape == (batch_size, 1)
-#
-#         # Test forward_cwa
-#         scores = model.forward_cwa(triples[:, :2])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#         # Test forward_inverse_cwa
-#         scores = model.forward_inverse_cwa(triples[:, 1:])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#     def test_trans_e(self):
-#         """Tests that TransE can be executed."""
-#         trans_e = TransE(triples_factory=self.factory)
-#         self.assertIsNotNone(trans_e)
-#
-#     def test_trans_h(self):
-#         """Tests that TransH can be executed."""
-#         trans_h = TransH(triples_factory=self.factory)
-#         self.assertIsNotNone(trans_h)
-#
-#     def test_trans_r(self):
-#         """Tests that TransR can be executed."""
-#         trans_r = TransR(triples_factory=self.factory)
-#         self.assertIsNotNone(trans_r)
-#
-#     def test_trans_d(self):
-#         """Tests that TransD can be executed."""
-#         trans_d = TransD(triples_factory=self.factory)
-#         self.assertIsNotNone(trans_d)
-#
-#     def test_rescal(self):
-#         """Tests that RESCAL can be executed."""
-#         model = RESCAL(triples_factory=self.factory)
-#         self.assertIsNotNone(model)
-#
-#         # Dummy forward passes
-#         # TODO: Use triple factory
-#         batch_size = 16
-#         triples = torch.zeros(batch_size, 3, dtype=torch.long)
-#
-#         # TODO: Refactor common tests for all models, e.g. shape checking
-#         # Test forward_owa
-#         scores = model.forward_owa(triples)
-#         # Check shape
-#         assert scores.shape == (batch_size, 1)
-#
-#         # Test forward_cwa
-#         scores = model.forward_cwa(triples[:, :2])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#         # Test forward_inverse_cwa
-#         scores = model.forward_inverse_cwa(triples[:, 1:])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#     def test_distmult(self):
-#         """Tests that DISTMULT can be executed."""
-#         model = DistMult(triples_factory=self.factory)
-#         self.assertIsNotNone(model)
-#
-#         # Dummy forward passes
-#         # TODO: Use triple factory
-#         batch_size = 16
-#         triples = torch.zeros(batch_size, 3, dtype=torch.long)
-#
-#         # TODO: Refactor common tests for all models, e.g. shape checking
-#         # Test forward_owa
-#         scores = model.forward_owa(triples)
-#         # Check shape
-#         assert scores.shape == (batch_size, 1)
-#
-#         # Test forward_cwa
-#         scores = model.forward_cwa(triples[:, :2])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#         # Test forward_inverse_cwa
-#         scores = model.forward_inverse_cwa(triples[:, 1:])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#     def test_complex(self):
-#         """Tests that COMPLEX can be executed."""
-#         model = ComplEx(triples_factory=self.factory)
-#         self.assertIsNotNone(model)
-#
-#         # Dummy forward passes
-#         # TODO: Use triple factory
-#         batch_size = 16
-#         triples = torch.zeros(batch_size, 3, dtype=torch.long)
-#
-#         # TODO: Refactor common tests for all models, e.g. shape checking
-#         # Test forward_owa
-#         scores = model.forward_owa(triples)
-#         # Check shape
-#         assert scores.shape == (batch_size, 1)
-#
-#         # Test forward_cwa
-#         scores = model.forward_cwa(triples[:, :2])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#         # Test forward_inverse_cwa
-#         scores = model.forward_inverse_cwa(triples[:, 1:])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#     def test_rotate(self):
-#         """Tests that Rotate can be executed."""
-#         model = RotatE(triples_factory=self.factory)
-#         self.assertIsNotNone(model)
-#
-#         # Dummy forward passes
-#         # TODO: Use triple factory
-#         batch_size = 16
-#         triples = torch.zeros(batch_size, 3, dtype=torch.long)
-#
-#         # TODO: Refactor common tests for all models, e.g. shape checking
-#         # Test forward_owa
-#         scores = model.forward_owa(triples)
-#         # Check shape
-#         assert scores.shape == (batch_size, 1)
-#         # Scores are negative distance -> non-positive
-#         assert scores.max() <= 0.
-#
-#         # Test forward_cwa
-#         scores = model.forward_cwa(triples[:, :2])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#         # Scores are negative distance -> non-positive
-#         assert scores.max() <= 0.
-#
-#         # Test forward_inverse_cwa
-#         scores = model.forward_inverse_cwa(triples[:, 1:])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#         # Scores are negative distance -> non-positive
-#         assert scores.max() <= 0.
-#
-#     def test_hole(self):
-#         """Tests that HolE can be executed."""
-#         model = HolE(triples_factory=self.factory, embedding_dim=8)
-#         self.assertIsNotNone(model)
-#
-#         # Dummy forward passes
-#         # TODO: Use triple factory
-#         batch_size = 16
-#         triples = torch.zeros(batch_size, 3, dtype=torch.long)
-#
-#         # TODO: Refactor common tests for all models, e.g. shape checking
-#         # Test forward_owa
-#         scores = model.forward_owa(triples)
-#         # Check shape
-#         assert scores.shape == (batch_size, 1)
-#
-#         # Test forward_cwa
-#         scores = model.forward_cwa(triples[:, :2])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#         # Test forward_inverse_cwa
-#         scores = model.forward_inverse_cwa(triples[:, 1:])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#     # TODO
-#     def test_conv_kb(self):
-#         """Tests that ConvKB can be executed."""
-#         model = ConvKB(triples_factory=self.factory, embedding_dim=4, num_filters=8)
-#         self.assertIsNotNone(model)
-#
-#         # Dummy forward passes
-#         # TODO: Use triple factory
-#         batch_size = 16
-#         triples = torch.zeros(batch_size, 3, dtype=torch.long)
-#
-#         # TODO: Refactor common tests for all models, e.g. shape checking
-#         # Test forward_owa
-#         scores = model.forward_owa(triples)
-#         # Check shape
-#         assert scores.shape == (batch_size, 1)
-#
-#         # Test forward_cwa
-#         scores = model.forward_cwa(triples[:, :2])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
-#
-#         # Test forward_inverse_cwa
-#         scores = model.forward_inverse_cwa(triples[:, 1:])
-#         # Check shape
-#         assert scores.shape == (batch_size, model.num_entities)
