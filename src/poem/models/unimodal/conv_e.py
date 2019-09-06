4a7de3bbccaabba07ceca22d512eb57c7ca5536e
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-09-06 08:44:42 +0200

    Improve ConvE example and documentation (#185)
    - Remove old notebook
    - Add example into class docstring for ConvE

--- src/poem/models/unimodal/conv_e.py
+++ src/poem/models/unimodal/conv_e.py
@@ -27,6 +27,38 @@ class ConvE(BaseModule):
     .. seealso::
 
         - Official Implementation: https://github.com/TimDettmers/ConvE/blob/master/model.py
+
+    Example usage:
+
+    >>> # Step 1: Get triples
+    >>> from poem.datasets import Nations
+    >>> dataset = Nations(create_inverse_triples=True)
+    >>> # Step 2: Configure the model
+    >>> from poem.models import ConvE
+    >>> model = ConvE(
+    ...     embedding_dim       = 200,
+    ...     input_channels      = 1,
+    ...     output_channels     = 32,
+    ...     embedding_height    = 10,
+    ...     embedding_width     = 20,
+    ...     kernel_height       = 3,
+    ...     kernel_width        = 3,
+    ...     input_dropout       = 0.2,
+    ...     feature_map_dropout = 0.2,
+    ...     output_dropout      = 0.3,
+    ...     preferred_device    = 'gpu',
+    ... )
+    >>> # Step 3: Configure the loop
+    >>> from torch.optim import Adam
+    >>> optimizer = Adam(params=model.get_grad_params())
+    >>> from poem.training import CWATrainingLoop
+    >>> training_loop = CWATrainingLoop(model=model, optimizer=optimizer)
+    >>> # Step 4: Train
+    >>> losses = training_loop.train(num_epochs=5, batch_size=256)
+    >>> # Step 5: Evaluate the model
+    >>> from poem.evaluation import RankBasedEvaluator
+    >>> evaluator = RankBasedEvaluator(model, filter_neg_triples=False)
+    >>> metric_result = evaluator.evaluate(dataset.testing, batch_size=8192)
     """
 
     def __init__(
