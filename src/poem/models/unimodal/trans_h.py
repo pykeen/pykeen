0a66da00b6a3516fc1c91e4f2fbada47cfbc8337
mali <ali-mehdi@live.de>
2019-12-03 09:37:58 +0100

    Revert "Change default value of epsilon"
    This reverts commit 0f7d1f21

--- src/poem/models/unimodal/trans_h.py
+++ src/poem/models/unimodal/trans_h.py
@@ -70,7 +70,7 @@ class TransH(BaseModule):
     #: The settings used by [wang2014]_ for TransH
     regularizer_default_kwargs = dict(
         weight=0.05,
-        epsilon=1.0e-06,
+        epsilon=0.00001,
     )
 
     def __init__(
