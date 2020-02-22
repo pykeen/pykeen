d8a7eab26f36faede9d1a0bb6298d906549ce702
mali <ali-mehdi@live.de>
2020-02-22 03:49:09 +0300

    Bump version: 0.0.1 → 0.0.2-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ copyright = '2019, Mehdi Ali'
 author = 'Mehdi Ali'
 
 # The full version, including alpha/beta/rc tags.
-release = '0.0.1'
+release = '0.0.2-dev'
 
 # The short X.Y version.
 parsed_version = re.match(
