1579c4ce500acee2b8b49eb11ccec71c6c02f3ff
mali <ali-mehdi@live.de>
2020-02-24 01:46:42 +0300

    Revert "Bump version: 0.2.0-dev → 0.2.1-dev"
    This reverts commit 55720964

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ copyright = '2019, Mehdi Ali'
 author = 'Mehdi Ali'
 
 # The full version, including alpha/beta/rc tags.
-release = '0.2.1-dev'
+release = '0.2.0-dev'
 
 # The short X.Y version.
 parsed_version = re.match(
