63e95bf9984fc606a39a20ed084771f4fcf6bf7e
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-06-23 09:02:55 +0200

    Add score_h / score_t methods to R-GCN

--- src/pykeen/models/unimodal/rgcn.py
+++ src/pykeen/models/unimodal/rgcn.py
@@ -749,3 +749,13 @@ class RGCN(Model):
         # Enrich embeddings
         self.base_model.entity_embeddings.weight.data = self._enrich_embeddings(batch=None)
         return self.base_model.score_hrt(hrt_batch=hrt_batch)
+
+    def score_h(self, rt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
+        # Enrich embeddings
+        self.base_model.entity_embeddings.weight.data = self._enrich_embeddings(batch=None)
+        return self.base_model.score_h(rt_batch=rt_batch)
+
+    def score_t(self, hr_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
+        # Enrich embeddings
+        self.base_model.entity_embeddings.weight.data = self._enrich_embeddings(batch=None)
+        return self.base_model.score_t(hr_batch=hr_batch)
