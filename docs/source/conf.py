f459fb48f90506369c26f32545793fd996133e86
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-06-25 14:57:59 +0200

    Bump version: 1.0.0-dev → 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ copyright = '2020, PyKEEN Project Team'
 author = 'PyKEEN Project Team'
 
 # The full version, including alpha/beta/rc tags.
-release = '1.0.0-dev'
+release = '1.0.0'
 
 # The short X.Y version.
 parsed_version = re.match(
--- src/pykeen/version.py
+++ src/pykeen/version.py
@@ -11,7 +11,7 @@ __all__ = [
     'get_git_hash',
 ]
 
-VERSION = '1.0.0-dev'
+VERSION = '1.0.0'
 
 
 def get_git_hash() -> str:
