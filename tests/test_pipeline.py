ae9f78c75e8243554497404974a7d3b43620e9fe
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-07-15 23:34:56 +0200

    Add tests for prediction

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -4,6 +4,7 @@
 
 import unittest
 
+from pykeen.datasets import Nations
 from pykeen.models import TransE
 from pykeen.models.base import Model
 from pykeen.pipeline import PipelineResult, pipeline
@@ -22,6 +23,22 @@ class TestPipeline(unittest.TestCase):
         self.assertIsInstance(pipeline_result, PipelineResult)
         self.assertIsInstance(pipeline_result.model, Model)
         self.assertIsInstance(pipeline_result.model.regularizer, NoRegularizer)
+
+        model = pipeline_result.model
+        nations = Nations()
+        testing_mapped_triples = nations.testing.mapped_triples.to(model.device)
+
+        tails_df = model.predict_tails('brazil', 'intergovorgs', testing=testing_mapped_triples)
+        training_tails = set(tails_df.loc[tails_df['in_training'], 'tail_label'])
+        self.assertEqual({'usa', 'uk', 'netherlands', 'egypt', 'india', 'israel', 'indonesia'}, training_tails)
+        testing_tails = set(tails_df.loc[tails_df['in_testing'], 'tail_label'])
+        self.assertEqual({'poland', 'cuba'}, testing_tails)
+
+        heads_df = model.predict_heads('conferences', 'brazil', testing=testing_mapped_triples)
+        training_heads = set(heads_df.loc[heads_df['in_training'], 'head_label'])
+        self.assertEqual({'usa', 'india', 'ussr', 'poland', 'cuba'}, training_heads)
+        testing_heads = set(heads_df.loc[heads_df['in_testing'], 'head_label'])
+        self.assertEqual(set(), testing_heads)
 
     def test_specify_regularizer(self):
         """Test a pipeline that uses a regularizer."""
