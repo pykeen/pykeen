550044239005d6e19f32277b567ab7c983528219
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-07-21 16:39:35 +0200

    Fix _prepare_cleanup

--- src/pykeen/triples/triples_factory.py
+++ src/pykeen/triples/triples_factory.py
@@ -540,7 +540,7 @@ def _tf_cleanup_all(
 
 def _tf_cleanup_deterministic(training: np.ndarray, testing: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """Cleanup a triples array (testing) with respect to another (training)."""
-    training_entities, testing_entities, to_move, move_id_mask = _prepare_cleanup(training, testing)
+    move_id_mask = _prepare_cleanup(training, testing)
 
     training = np.concatenate([training, testing[move_id_mask]])
     testing = testing[~move_id_mask]
@@ -565,7 +565,7 @@ def _tf_cleanup_randomized(
     if isinstance(random_state, int):
         random_state = np.random.RandomState(random_state)
 
-    training_entities, testing_entities, to_move, move_id_mask = _prepare_cleanup(training, testing)
+    move_id_mask = _prepare_cleanup(training, testing)
 
     # While there are still triples that should be moved to the training set
     while move_id_mask.any():
@@ -579,18 +579,22 @@ def _tf_cleanup_randomized(
         testing = testing[testing_mask]
 
         # Recalculate the training entities, testing entities, to_move, and move_id_mask
-        training_entities, testing_entities, to_move, move_id_mask = _prepare_cleanup(training, testing)
+        move_id_mask = _prepare_cleanup(training, testing)
 
     return training, testing
 
 
-def _prepare_cleanup(training: np.ndarray, testing: np.ndarray):
-    training_entities = _get_unique(training)
-    testing_entities = _get_unique(testing)
-    to_move = testing_entities[~np.isin(testing_entities, training_entities)]
-    move_id_mask = np.isin(testing[:, [0, 2]], to_move).any(axis=1)
-    return training_entities, testing_entities, to_move, move_id_mask
+def _prepare_cleanup(training: np.ndarray, testing: np.ndarray) -> np.ndarray:
+    to_move_mask = None
+    for col in [[0, 2], 1]:
+        training_ids, test_ids = [np.unique(triples[:, col]) for triples in [training, testing]]
+        to_move = test_ids[~np.isin(test_ids, training_ids)]
+        this_to_move_mask = np.isin(testing[:, col], to_move)
+        if this_to_move_mask.ndim > 1:
+            this_to_move_mask = this_to_move_mask.any(axis=1)
+        if to_move_mask is None:
+            to_move_mask = this_to_move_mask
+        else:
+            to_move_mask = this_to_move_mask | to_move_mask
 
-
-def _get_unique(x):
-    return np.unique(x[:, [0, 2]])
+    return to_move_mask
