572051bdd3252fb05552ad51be17e8579bd80139
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-07-27 13:42:00 +0200

    Add dictionary inversion utility

--- src/pykeen/utils.py
+++ src/pykeen/utils.py
@@ -443,3 +443,23 @@ def ensure_ftp_directory(*, ftp: ftplib.FTP, directory: str) -> None:
         ftp.mkd(directory)
     except ftplib.error_perm:
         pass  # its fine...
+
+
+def invert_mapping(mapping: Mapping[str, int]) -> Mapping[int, str]:
+    """
+    Invert a mapping.
+
+    :param mapping:
+        The mapping, key -> value.
+
+    :return:
+        The inverse mapping, value -> key.
+    """
+    num_unique_values = len(set(mapping.values()))
+    num_keys = len(mapping)
+    if num_unique_values < num_keys:
+        raise ValueError(f'Mapping is not bijective! Only {num_unique_values}/{num_keys} are unique.')
+    return {
+        value: key
+        for key, value in mapping.items()
+    }
