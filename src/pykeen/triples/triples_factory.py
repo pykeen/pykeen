4135794eb27084c0609585933fd4c3334f8fee07
Laurent Vermue <lvermue@users.noreply.github.com>
2020-10-08 14:35:14 +0200

    Adjust deprecated implicit dtype object ndarray creation

--- src/pykeen/triples/triples_factory.py
+++ src/pykeen/triples/triples_factory.py
@@ -359,7 +359,7 @@ class TriplesFactory:
         )
         sp, multi_o = zip(*s_p_to_multi_tails.items())
         mapped_triples: torch.LongTensor = torch.tensor(sp, dtype=torch.long)
-        labels = np.array([np.array(item) for item in multi_o])
+        labels = np.array([np.array(item) for item in multi_o], dtype=object)
 
         return LCWAInstances(
             mapped_triples=mapped_triples,
