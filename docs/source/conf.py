8ea31b57be356b7c121c345e47e97b85c21c564e
Laurent Vermue <lvermue@users.noreply.github.com>
2020-10-11 10:17:20 +0200

    Remove LaTeX build options from makefile

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -164,36 +164,36 @@ htmlhelp_basename = 'PyKEENdoc'
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
+# latex_elements = {
+#     The paper size ('letterpaper' or 'a4paper').
+#
+#     'papersize': 'letterpaper',
+#
+#     The font size ('10pt', '11pt' or '12pt').
+#
+#     'pointsize': '10pt',
+#
+#     Additional stuff for the LaTeX preamble.
+#
+#     'preamble': '',
+#
+#     Latex figure (float) alignment
+#
+#     'figure_align': 'htbp',
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        'pykeen.tex',
-        'PyKEEN Documentation',
-        author,
-        'manual',
-    ),
-]
+# latex_documents = [
+#     (
+#         master_doc,
+#         'pykeen.tex',
+#         'PyKEEN Documentation',
+#         author,
+#         'manual',
+#     ),
+# ]
 
 # -- Options for manual page output ------------------------------------------
 
