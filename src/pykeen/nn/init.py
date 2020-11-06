4b8aa586ea2a24b4241b219ae6c9d221b5dcf91d
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-11-06 16:28:52 +0100

    Fix docs

--- src/pykeen/nn/init.py
+++ src/pykeen/nn/init.py
@@ -14,8 +14,8 @@ from .emb import Embedding
 __all__ = [
     'xavier_uniform_',
     'xavier_uniform_normed_',
-    'embedding_xavier_uniform_',
     'xavier_normal_',
+    'embedding_xavier_uniform_',
     'embedding_xavier_normal_',
 ]
 
@@ -27,16 +27,8 @@ def xavier_uniform_(tensor, gain: float = 1.):
     return tensor
 
 
-def xavier_uniform_normed_(tensor, gain: float = 1.):
-    """Initialize weights of the tensor similarly to Glorot/Xavier initialization the normalize."""
-    x = xavier_uniform_(tensor, gain=gain)
-    functional.normalize(x.data, out=x.data)
-    return x
-
-
-# TODO delete this function and move documentation
-def embedding_xavier_uniform_(embedding: Union[torch.nn.Embedding, Embedding], gain: float = 1.):
-    r"""Initialize weights of embedding similarly to Glorot/Xavier initialization.
+def xavier_uniform_normed_(tensor: torch.Tensor, gain: float = 1.) -> torch.Tensor:
+    r"""Initialize weights of the tensor similarly to Glorot/Xavier initialization the normalize.
 
     Proceed as if it was a linear layer with fan_in of zero and Xavier uniform
     initialization is used, i.e. fill the weight of input `embedding` with values values
@@ -45,6 +37,19 @@ def embedding_xavier_uniform_(embedding: Union[torch.nn.Embedding, Embedding], g
     .. math::
 
         a = \text{gain} \times \sqrt{\frac{6}{\text{embedding_dim}}}
+
+    :param tensor: A tensor
+    :param gain: An optional scaling factor, defaults to 1.0.
+    :return: Embedding with weights by the Xavier uniform initializer.
+    """
+    x = xavier_uniform_(tensor, gain=gain)
+    functional.normalize(x.data, out=x.data)
+    return x
+
+
+# TODO delete this function and move documentation
+def embedding_xavier_uniform_(embedding: Union[torch.nn.Embedding, Embedding], gain: float = 1.):
+    """Initialize weights of embedding similarly to Glorot/Xavier initialization.
 
     :param embedding: An embedding
     :param gain: An optional scaling factor, defaults to 1.0.
@@ -68,20 +73,8 @@ def embedding_xavier_uniform_(embedding: Union[torch.nn.Embedding, Embedding], g
         raise TypeError
 
 
-def xavier_normal_(tensor, gain: float = 1.0):
-    """Initialize weights of the tensor similarly to Glorot/Xavier initialization."""
-    std = gain * 2 / math.sqrt(tensor.shape[1])  # TODO @mberr is that index right?
-    torch.nn.init.normal_(tensor, mean=0., std=std)
-    return tensor
-
-
-# TODO delete this function and move documentation
-def embedding_xavier_normal_(embedding: Union[torch.nn.Embedding, Embedding], gain: float = 1.):
-    r"""Initialize weights of embedding similarly to Glorot/Xavier initialization.
-
-    :param embedding: An embedding
-    :param gain: An optional scaling factor, defaults to 1.0.
-    :return: Embedding with weights by the Xavier normal initializer.
+def xavier_normal_(tensor: torch.Tensor, gain: float = 1.0) -> torch.Tensor:
+    r"""Initialize weights of the tensor similarly to Glorot/Xavier initialization.
 
     Proceed as if it was a linear layer with fan_in of zero and Xavier normal
     initialization is used. Fill the weight of input `embedding` with values values
@@ -90,6 +83,19 @@ def embedding_xavier_normal_(embedding: Union[torch.nn.Embedding, Embedding], ga
     .. math::
 
         a = \text{gain} \times \sqrt{\frac{2}{\text{embedding_dim}}}
+
+    :param tensor: A tensor
+    :param gain: An optional scaling factor, defaults to 1.0.
+    :return: Embedding with weights by the Xavier normal initializer.
+    """
+    std = gain * 2 / math.sqrt(tensor.shape[1])  # TODO @mberr is that index right?
+    torch.nn.init.normal_(tensor, mean=0., std=std)
+    return tensor
+
+
+# TODO delete this function and move documentation
+def embedding_xavier_normal_(embedding: Union[torch.nn.Embedding, Embedding], gain: float = 1.):
+    """Initialize weights of embedding similarly to Glorot/Xavier initialization.
 
     In the following example, an embedding is initialized using the suggested gain for the rectified
     linear unit (ReLu).
