688060c8ef1673e8d7b5e46254a85819f8d0b71e
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-11-07 18:51:02 +0100

    Post merge-fixes for RGCN

--- src/pykeen/models/unimodal/rgcn/model.py
+++ src/pykeen/models/unimodal/rgcn/model.py
@@ -15,8 +15,9 @@ from .decompositions import (
 )
 from .weightings import EdgeWeighting, edge_weightings, get_edge_weighting
 from .. import ComplEx, DistMult, ERMLP
-from ... import EntityRelationEmbeddingModel
+from ... import Model
 from ....losses import Loss
+from ....nn import Embedding, RepresentationModule
 from ....triples import TriplesFactory
 
 __all__ = [
@@ -24,37 +25,6 @@ __all__ = [
 ]
 
 logger = logging.getLogger(name=path.basename(__file__))
-
-
-def _get_neighborhood(
-    start_nodes: torch.LongTensor,
-    sources: torch.LongTensor,
-    targets: torch.LongTensor,
-    k: int,
-    num_nodes: int,
-    undirected: bool = False,
-) -> torch.BoolTensor:
-    # Construct node neighbourhood mask
-    node_mask = torch.zeros(num_nodes, device=start_nodes.device, dtype=torch.bool)
-
-    # Set nodes in batch to true
-    node_mask[start_nodes] = True
-
-    # Compute k-neighbourhood
-    for _ in range(k):
-        # if the target node needs an embeddings, so does the source node
-        node_mask[sources] |= node_mask[targets]
-
-        if undirected:
-            node_mask[targets] |= node_mask[sources]
-
-    # Create edge mask
-    edge_mask = node_mask[targets]
-
-    if undirected:
-        edge_mask |= node_mask[sources]
-
-    return edge_mask
 
 
 class Bias(nn.Module):
@@ -87,7 +57,183 @@ class Bias(nn.Module):
         return x + self.bias.unsqueeze(dim=0)
 
 
-class RGCN(EntityRelationEmbeddingModel):
+class RGCNRepresentations(RepresentationModule):
+    """Entity representations enriched by R-GCN."""
+
+    def __init__(
+        self,
+        triples_factory: TriplesFactory,
+        embedding_dim: int = 500,
+        num_bases: Optional[int] = None,
+        num_blocks: Optional[int] = None,
+        num_layers: int = 2,
+        use_bias: bool = True,
+        use_batch_norm: bool = False,
+        activation_cls: Optional[Type[nn.Module]] = None,
+        activation_kwargs: Optional[Mapping[str, Any]] = None,
+        sparse_messages_slcwa: bool = True,
+        edge_dropout: float = 0.4,
+        self_loop_dropout: float = 0.2,
+        edge_weighting: Union[None, str, EdgeWeighting] = 'inverse_indegree',
+        _decomposition: Union[None, str, Type[RelationSpecificMessagePassing]] = None,
+        buffer_messages: bool = True,
+        memory_intense: bool = False,
+        base_representations: Optional[RepresentationModule] = None,
+    ):
+        super().__init__()
+
+        # normalize representations
+        if base_representations is None:
+            base_representations = Embedding(
+                num_embeddings=triples_factory.num_entities,
+                embedding_dim=embedding_dim,
+                # https://github.com/MichSchli/RelationPrediction/blob/c77b094fe5c17685ed138dae9ae49b304e0d8d89/code/encoders/affine_transform.py#L24-L28
+                initializer=nn.init.xavier_uniform_,
+            )
+        self.base_embeddings = base_representations
+
+        # buffering of messages
+        self.buffer_messages = buffer_messages
+        self.enriched_embeddings = None
+
+        # if edge weighting is None, keep it as none. otherwise look it up
+        self.edge_weighting = edge_weighting and get_edge_weighting(edge_weighting)
+        self.edge_dropout = edge_dropout
+        if self_loop_dropout is None:
+            self_loop_dropout = edge_dropout
+        self.self_loop_dropout = self_loop_dropout
+        self.use_batch_norm = use_batch_norm
+        if activation_cls is None:
+            activation_cls = nn.ReLU
+        self.activation_cls = activation_cls
+        self.activation_kwargs = activation_kwargs
+        if use_batch_norm:
+            if use_bias:
+                logger.warning('Disabling bias because batch normalization was used.')
+            use_bias = False
+        self.use_bias = use_bias
+        self.num_layers = num_layers
+        self.sparse_messages_slcwa = sparse_messages_slcwa
+
+        # Save graph using buffers, such that the tensors are moved together with the model
+        h, r, t = self.triples_factory.mapped_triples.t()
+        self.register_buffer('sources', h)
+        self.register_buffer('targets', t)
+        self.register_buffer('edge_types', r)
+
+        decomposition_cls: Type[RelationSpecificMessagePassing] = get_decomposition_cls(_decomposition)
+
+        message_passing_kwargs = {}
+        if decomposition_cls is BasesDecomposition:
+            message_passing_kwargs['num_bases'] = num_bases
+            message_passing_kwargs['memory_intense'] = memory_intense
+        elif decomposition_cls is BlockDecomposition:
+            message_passing_kwargs['num_blocks'] = num_blocks
+
+        layers = []
+        for _ in range(num_layers):
+            layers.append(decomposition_cls(
+                input_dim=self.embedding_dim,
+                num_relations=self.num_relations,
+                **message_passing_kwargs,
+            ))
+            if self.use_bias:
+                layers.append(Bias(dim=self.embedding_dim))
+            if self.use_batch_norm:
+                layers.append(nn.BatchNorm1d(num_features=self.embedding_dim))
+            layers.append(self.activation_cls(**(self.activation_kwargs or {})))
+        self.layers = nn.ModuleList(layers)
+
+    def post_parameter_update(self) -> None:  # noqa: D102
+        super().post_parameter_update()
+
+        # invalidate enriched embeddings
+        self.enriched_embeddings = None
+
+    def reset_parameters(self):
+        self.base_embeddings.reset_parameters()
+
+        for m in self.layers:
+            if hasattr(m, 'reset_parameters'):
+                m.reset_parameters()
+            elif any(p.requires_grad for p in m.parameters()):
+                logger.warning('Layers %s has parameters, but no reset_parameters.', m)
+
+    def forward(
+        self,
+        indices: Optional[torch.LongTensor] = None,
+    ) -> torch.FloatTensor:
+        """Enrich the entity embeddings of the decoder using R-GCN message propagation."""
+        # TODO: Start of commented out for debugging.
+        # # use buffered messages if applicable
+        # if self.enriched_embeddings is not None:
+        #     return self.enriched_embeddings
+        #
+        # # clear cached embeddings as soon as possible to avoid unnecessary memory consumption
+        # self.enriched_embeddings = None
+        # TODO: End of commented out for debugging
+
+        # Bind fields
+        # shape: (num_entities, embedding_dim)
+        x = self.entity_embeddings(indices=None)
+        sources = self.sources
+        targets = self.targets
+        edge_types = self.edge_types
+
+        # Edge dropout: drop the same edges on all layers (only in training mode)
+        if self.training and self.edge_dropout is not None:
+            # Get random dropout mask
+            edge_keep_mask = torch.rand(self.sources.shape[0], device=x.device) > self.edge_dropout
+
+            # Apply to edges
+            sources = sources[edge_keep_mask]
+            targets = targets[edge_keep_mask]
+            edge_types = edge_types[edge_keep_mask]
+
+        # Different dropout for self-loops (only in training mode)
+        if self.training and self.self_loop_dropout is not None:
+            node_keep_mask = torch.rand(self.num_entities, device=x.device) > self.self_loop_dropout
+        else:
+            node_keep_mask = None
+
+        # fixed edges -> pre-compute weights
+        if self.edge_weighting is not None:
+            edge_weights = torch.empty_like(sources, dtype=torch.float32)
+            for r in range(self.num_relations):
+                mask = edge_types == r
+                if mask.any():
+                    edge_weights[mask] = self.edge_weighting(sources[mask], targets[mask])
+        else:
+            edge_weights = None
+
+        for layer in self.layers:
+            if isinstance(layer, RelationSpecificMessagePassing):
+                kwargs = dict(
+                    node_keep_mask=node_keep_mask,
+                    source=sources,
+                    target=targets,
+                    edge_type=edge_types,
+                    edge_weights=edge_weights,
+                )
+            else:
+                kwargs = dict()
+            x = layer(x, **kwargs)
+
+        # TODO: Start of commented out for debugging.
+        # # Cache enriched representations
+        # self.enriched_embeddings = x
+        # TODO: End of commented out for debugging
+
+        return x
+
+
+class Decoder(nn.Module):
+    # TODO: Replace this by interaction function, once https://github.com/pykeen/pykeen/pull/107 is merged.
+    def forward(self, h, r, t):
+        return (h * r * t).sum(dim=-1)
+
+
+class RGCN(Model):
     """An implementation of R-GCN from [schlichtkrull2018]_.
 
     This model uses graph convolutions with relation-specific weights.
@@ -196,9 +342,11 @@ class RGCN(EntityRelationEmbeddingModel):
             Enable memory-intense forward pass which may be faster, in particular if the number of different relations
             is small.
         """
+        if self.triples_factory.create_inverse_triples:
+            raise ValueError('R-GCN handles edges in an undirected manner.')
+
         super().__init__(
             triples_factory=triples_factory,
-            embedding_dim=embedding_dim,
             loss=loss,
             predict_with_sigmoid=predict_with_sigmoid,
             automatic_memory_optimization=automatic_memory_optimization,
@@ -206,161 +354,47 @@ class RGCN(EntityRelationEmbeddingModel):
             random_seed=random_seed,
         )
 
-        if self.triples_factory.create_inverse_triples:
-            raise ValueError('R-GCN handles edges in an undirected manner.')
+        self.entity_representations = RGCNRepresentations(
+            triples_factory=triples_factory,
+            embedding_dim=embedding_dim,
+            num_bases=num_bases,
+            num_blocks=num_blocks,
+            num_layers=num_layers,
+            use_bias=use_bias,
+            use_batch_norm=use_batch_norm,
+            activation_cls=activation_cls,
+            activation_kwargs=activation_kwargs,
+            sparse_messages_slcwa=sparse_messages_slcwa,
+            edge_dropout=edge_dropout,
+            self_loop_dropout=self_loop_dropout,
+            edge_weighting=edge_weighting,
+            _decomposition=_decomposition,
+            buffer_messages=buffer_messages,
+            memory_intense=memory_intense,
+        )
+        self.relation_embeddings = Embedding(
+            num_embeddings=triples_factory.num_relations,
+            embedding_dim=embedding_dim,
+        )
+        # TODO: Dummy
+        self.decoder = Decoder()
 
-        self.embedding_dim = embedding_dim
-
-        # buffering of messages
-        self.buffer_messages = buffer_messages
-        self.enriched_embeddings = None
-
-        # if edge weighting is None, keep it as none. otherwise look it up
-        self.edge_weighting = edge_weighting and get_edge_weighting(edge_weighting)
-        self.edge_dropout = edge_dropout
-        if self_loop_dropout is None:
-            self_loop_dropout = edge_dropout
-        self.self_loop_dropout = self_loop_dropout
-        self.use_batch_norm = use_batch_norm
-        if activation_cls is None:
-            activation_cls = nn.ReLU
-        self.activation_cls = activation_cls
-        self.activation_kwargs = activation_kwargs
-        if use_batch_norm:
-            if use_bias:
-                logger.warning('Disabling bias because batch normalization was used.')
-            use_bias = False
-        self.use_bias = use_bias
-        self.num_layers = num_layers
-        self.sparse_messages_slcwa = sparse_messages_slcwa
-
-        # Save graph using buffers, such that the tensors are moved together with the model
-        h, r, t = self.triples_factory.mapped_triples.t()
-        self.register_buffer('sources', h)
-        self.register_buffer('targets', t)
-        self.register_buffer('edge_types', r)
-
-        decomposition_cls: Type[RelationSpecificMessagePassing] = get_decomposition_cls(_decomposition)
-
-        message_passing_kwargs = {}
-        if decomposition_cls is BasesDecomposition:
-            message_passing_kwargs['num_bases'] = num_bases
-            message_passing_kwargs['memory_intense'] = memory_intense
-        elif decomposition_cls is BlockDecomposition:
-            message_passing_kwargs['num_blocks'] = num_blocks
-
-        layers = []
-        for _ in range(num_layers):
-            layers.append(decomposition_cls(
-                input_dim=self.embedding_dim,
-                num_relations=self.num_relations,
-                **message_passing_kwargs,
-            ))
-            if self.use_bias:
-                layers.append(Bias(dim=self.embedding_dim))
-            if self.use_batch_norm:
-                layers.append(nn.BatchNorm1d(num_features=self.embedding_dim))
-            layers.append(self.activation_cls(**(self.activation_kwargs or {})))
-        self.layers = nn.ModuleList(layers)
-
-        # Finalize initialization
+        # Finalize initialization, needs to be done manually instead of with
+        # a post-init hook because this model is very special :)
         self.reset_parameters_()
 
     def post_parameter_update(self) -> None:  # noqa: D102
         super().post_parameter_update()
+        self.entity_representations.post_parameter_update()
+        self.relation_embeddings.post_parameter_update()
 
-        # invalidate enriched embeddings
-        self.enriched_embeddings = None
-
-    def _reset_parameters_(self):  # noqa: D102
-        # https://github.com/MichSchli/RelationPrediction/blob/c77b094fe5c17685ed138dae9ae49b304e0d8d89/code/encoders/affine_transform.py#L24-L28
-        nn.init.xavier_uniform_(self.entity_embeddings.weight)
-        nn.init.xavier_uniform_(self.relation_embeddings.weight)
-
-        for m in self.layers:
-            if hasattr(m, 'reset_parameters'):
-                m.reset_parameters()
-            elif any(p.requires_grad for p in m.parameters()):
-                logger.warning('Layers %s has parameters, but no reset_parameters.', m)
-
-    def _enrich_embeddings_(self) -> torch.FloatTensor:
-        """Enrich the entity embeddings of the decoder using R-GCN message propagation."""
-        # TODO: Start of commented out for debugging.
-        # # use buffered messages if applicable
-        # if self.enriched_embeddings is not None:
-        #     return self.enriched_embeddings
-        #
-        # # clear cached embeddings as soon as possible to avoid unnecessary memory consumption
-        # self.enriched_embeddings = None
-        # TODO: End of commented out for debugging
-
-        # Bind fields
-        # shape: (num_entities, embedding_dim)
-        x = self.entity_embeddings.weight
-        sources = self.sources
-        targets = self.targets
-        edge_types = self.edge_types
-
-        # Edge dropout: drop the same edges on all layers (only in training mode)
-        if self.training and self.edge_dropout is not None:
-            # Get random dropout mask
-            edge_keep_mask = torch.rand(self.sources.shape[0], device=x.device) > self.edge_dropout
-
-            # Apply to edges
-            sources = sources[edge_keep_mask]
-            targets = targets[edge_keep_mask]
-            edge_types = edge_types[edge_keep_mask]
-
-        # Different dropout for self-loops (only in training mode)
-        if self.training and self.self_loop_dropout is not None:
-            node_keep_mask = torch.rand(self.num_entities, device=x.device) > self.self_loop_dropout
-        else:
-            node_keep_mask = None
-
-        # fixed edges -> pre-compute weights
-        if self.edge_weighting is not None:
-            edge_weights = torch.empty_like(sources, dtype=torch.float32)
-            for r in range(self.num_relations):
-                mask = edge_types == r
-                if mask.any():
-                    edge_weights[mask] = self.edge_weighting(sources[mask], targets[mask])
-        else:
-            edge_weights = None
-
-        for layer in self.layers:
-            if isinstance(layer, RelationSpecificMessagePassing):
-                kwargs = dict(
-                    node_keep_mask=node_keep_mask,
-                    source=sources,
-                    target=targets,
-                    edge_type=edge_types,
-                    edge_weights=edge_weights,
-                )
-            else:
-                kwargs = dict()
-            x = layer(x, **kwargs)
-
-        # TODO: Start of commented out for debugging.
-        # # Cache enriched representations
-        # self.enriched_embeddings = x
-        # TODO: End of commented out for debugging
-
-        return x
+    def _reset_parameters_(self):
+        self.entity_representations.reset_parameters()
+        self.relation_embeddings.reset_parameters()
 
     def score_hrt(self, hrt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
         # Enrich embeddings
-        x = self._enrich_embeddings_()
-        h, r, t = hrt_batch.t()
-        return torch.einsum('bd,bd,bd->b', x[h], self.relation_embeddings(r), x[t]).unsqueeze(dim=-1)
-
-    def score_h(self, rt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        # Enrich embeddings
-        x = self._enrich_embeddings_()
-        r, t = rt_batch.t()
-        return torch.einsum('ed,bd,bd->be', x, self.relation_embeddings(r), x[t])
-
-    def score_t(self, hr_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        # Enrich embeddings
-        x = self._enrich_embeddings_()
-        h, r = hr_batch.t()
-        return torch.einsum('bd,bd,ed->be', x[h], self.relation_embeddings(r), x)
+        h = self.entity_representations(indices=hrt_batch[:, 0])
+        t = self.entity_representations(indices=hrt_batch[:, 2])
+        r = self.relation_embeddings(indices=hrt_batch[:, 1])
+        return self.decoder(h, r, t).unsqueeze(dim=-1)
