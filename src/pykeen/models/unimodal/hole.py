3f35c86651d7de1e297b681d08754c9f8a03a2f0
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-11-07 19:22:30 +0100

    Upgrade HolE

--- src/pykeen/models/unimodal/hole.py
+++ src/pykeen/models/unimodal/hole.py
@@ -6,8 +6,9 @@ from typing import Optional
 
 import torch
 import torch.autograd
+from torch.fft import irfft, rfft
 
-from ..base import EntityRelationEmbeddingModel
+from ..base import InteractionFunction, SimpleVectorEntityRelationEmbeddingModel
 from ...losses import Loss
 from ...nn.init import xavier_uniform_
 from ...regularizers import Regularizer
@@ -19,7 +20,35 @@ __all__ = [
 ]
 
 
-class HolE(EntityRelationEmbeddingModel):
+class HolEInteractionFunction(InteractionFunction):
+    """Interaction function for HolE."""
+
+    def forward(
+        self,
+        h: torch.FloatTensor,
+        r: torch.FloatTensor,
+        t: torch.FloatTensor,
+    ) -> torch.FloatTensor:  # noqa: D102
+        # Circular correlation of entity embeddings
+        a_fft = rfft(h, signal_ndim=1, onesided=True)
+        b_fft = rfft(t, signal_ndim=1, onesided=True)
+
+        # complex conjugate, a_fft.shape = (batch_size, num_entities, d', 2)
+        a_fft[:, :, :, 1] *= -1
+
+        # Hadamard product in frequency domain
+        p_fft = a_fft * b_fft
+
+        # inverse real FFT, shape: (batch_size, num_entities, d)
+        composite = irfft(p_fft, signal_ndim=1, onesided=True, signal_sizes=(h.shape[-1],))
+
+        # inner product with relation embedding
+        scores = torch.sum(r * composite, dim=-1, keepdim=False)
+
+        return scores
+
+
+class HolE(SimpleVectorEntityRelationEmbeddingModel):
     r"""An implementation of HolE [nickel2016]_.
 
     Holographic embeddings (HolE) make use of the circular correlation operator to compute interactions between
@@ -63,10 +92,13 @@ class HolE(EntityRelationEmbeddingModel):
         regularizer: Optional[Regularizer] = None,
     ) -> None:
         """Initialize the model."""
+        interaction_function = HolEInteractionFunction()
+
         super().__init__(
             triples_factory=triples_factory,
             embedding_dim=embedding_dim,
             loss=loss,
+            interaction_function=interaction_function,
             automatic_memory_optimization=automatic_memory_optimization,
             preferred_device=preferred_device,
             random_seed=random_seed,
@@ -77,77 +109,3 @@ class HolE(EntityRelationEmbeddingModel):
             entity_constrainer=clamp_norm,
             entity_constrainer_kwargs=dict(maxnorm=1., p=2, dim=-1),
         )
-
-    @staticmethod
-    def interaction_function(
-        h: torch.FloatTensor,
-        r: torch.FloatTensor,
-        t: torch.FloatTensor,
-    ) -> torch.FloatTensor:
-        """Evaluate the interaction function for given embeddings.
-
-        The embeddings have to be in a broadcastable shape.
-
-        :param h: shape: (batch_size, num_entities, d)
-            Head embeddings.
-        :param r: shape: (batch_size, num_entities, d)
-            Relation embeddings.
-        :param t: shape: (batch_size, num_entities, d)
-            Tail embeddings.
-
-        :return: shape: (batch_size, num_entities)
-            The scores.
-        """
-        # Circular correlation of entity embeddings
-        a_fft = torch.rfft(h, signal_ndim=1, onesided=True)
-        b_fft = torch.rfft(t, signal_ndim=1, onesided=True)
-
-        # complex conjugate, a_fft.shape = (batch_size, num_entities, d', 2)
-        a_fft[:, :, :, 1] *= -1
-
-        # Hadamard product in frequency domain
-        p_fft = a_fft * b_fft
-
-        # inverse real FFT, shape: (batch_size, num_entities, d)
-        composite = torch.irfft(p_fft, signal_ndim=1, onesided=True, signal_sizes=(h.shape[-1],))
-
-        # inner product with relation embedding
-        scores = torch.sum(r * composite, dim=-1, keepdim=False)
-
-        return scores
-
-    def score_hrt(self, hrt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        h = self.entity_embeddings(indices=hrt_batch[:, 0]).unsqueeze(dim=1)
-        r = self.relation_embeddings(indices=hrt_batch[:, 1]).unsqueeze(dim=1)
-        t = self.entity_embeddings(indices=hrt_batch[:, 2]).unsqueeze(dim=1)
-
-        # Embedding Regularization
-        self.regularize_if_necessary(h, r, t)
-
-        scores = self.interaction_function(h=h, r=r, t=t).view(-1, 1)
-
-        return scores
-
-    def score_t(self, hr_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        h = self.entity_embeddings(indices=hr_batch[:, 0]).unsqueeze(dim=1)
-        r = self.relation_embeddings(indices=hr_batch[:, 1]).unsqueeze(dim=1)
-        t = self.entity_embeddings(indices=None).unsqueeze(dim=0)
-
-        # Embedding Regularization
-        self.regularize_if_necessary(h, r, t)
-
-        scores = self.interaction_function(h=h, r=r, t=t)
-
-        return scores
-
-    def score_h(self, rt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        h = self.entity_embeddings(indices=None).unsqueeze(dim=0)
-        r = self.relation_embeddings(indices=rt_batch[:, 0]).unsqueeze(dim=1)
-        t = self.entity_embeddings(indices=rt_batch[:, 1]).unsqueeze(dim=1)
-
-        # Embedding Regularization
-        self.regularize_if_necessary(h, r, t)
-
-        scores = self.interaction_function(h=h, r=r, t=t)
-
-        return scores
