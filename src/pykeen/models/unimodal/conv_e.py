3f794bb670fcea9afeab640bb12bed7194f376aa
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-11-09 13:08:53 +0100

    Extract ConvE interaction function

--- src/pykeen/models/unimodal/conv_e.py
+++ src/pykeen/models/unimodal/conv_e.py
@@ -4,12 +4,10 @@
 
 import logging
 import math
-import sys
 from typing import Optional, Tuple, Type
 
 import torch
 from torch import nn
-from torch.nn import functional as F  # noqa: N812
 
 from ..base import EntityRelationEmbeddingModel, InteractionFunction
 from ...losses import BCEAfterSigmoidLoss, Loss
@@ -75,8 +73,167 @@ def _calculate_missing_shape_information(
     return input_channels, width, height
 
 
+def _add_cuda_warning(func):
+    def wrapped(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except RuntimeError as e:
+            if not is_cudnn_error(e):
+                raise e
+            raise RuntimeError(
+                '\nThis code crash might have been caused by a CUDA bug, see '
+                'https://github.com/allenai/allennlp/issues/2888, '
+                'which causes the code to crash during evaluation mode.\n'
+                'To avoid this error, the batch size has to be reduced.'
+            ) from e
+
+    return wrapped
+
+
 class ConvEInteractionFunction(InteractionFunction):
     """ConvE interaction function."""
+
+    #: If batch normalization is enabled, this is: num_features – C from an expected input of size (N,C,L)
+    bn0: Optional[torch.nn.BatchNorm2d]
+    #: If batch normalization is enabled, this is: num_features – C from an expected input of size (N,C,H,W)
+    bn1: Optional[torch.nn.BatchNorm2d]
+    bn2: Optional[torch.nn.BatchNorm2d]
+
+    def __init__(
+        self,
+        input_channels: Optional[int] = None,
+        output_channels: int = 32,
+        embedding_height: Optional[int] = None,
+        embedding_width: Optional[int] = None,
+        kernel_height: int = 3,
+        kernel_width: int = 3,
+        input_dropout: float = 0.2,
+        output_dropout: float = 0.3,
+        feature_map_dropout: float = 0.2,
+        embedding_dim: int = 200,
+        apply_batch_normalization: bool = True,
+    ):
+        super().__init__()
+
+        # Automatic calculation of remaining dimensions
+        logger.info(f'Resolving {input_channels} * {embedding_width} * {embedding_height} = {embedding_dim}.')
+        if embedding_dim is None:
+            embedding_dim = input_channels * embedding_width * embedding_height
+
+        # Parameter need to fulfil:
+        #   input_channels * embedding_height * embedding_width = embedding_dim
+        input_channels, embedding_width, embedding_height = _calculate_missing_shape_information(
+            embedding_dim=embedding_dim,
+            input_channels=input_channels,
+            width=embedding_width,
+            height=embedding_height,
+        )
+        logger.info(f'Resolved to {input_channels} * {embedding_width} * {embedding_height} = {embedding_dim}.')
+        self.embedding_height = embedding_height
+        self.embedding_width = embedding_width
+        self.input_channels = input_channels
+
+        if self.input_channels * self.embedding_height * self.embedding_width != self.embedding_dim:
+            raise ValueError(
+                f'Product of input channels ({self.input_channels}), height ({self.embedding_height}), and width '
+                f'({self.embedding_width}) does not equal target embedding dimension ({self.embedding_dim})',
+            )
+
+        self.inp_drop = nn.Dropout(input_dropout)
+        self.hidden_drop = nn.Dropout(output_dropout)
+        self.feature_map_drop = nn.Dropout2d(feature_map_dropout)
+
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=self.input_channels,
+            out_channels=output_channels,
+            kernel_size=(kernel_height, kernel_width),
+            stride=1,
+            padding=0,
+            bias=True,
+        )
+
+        self.apply_batch_normalization = apply_batch_normalization
+        if self.apply_batch_normalization:
+            self.bn0 = nn.BatchNorm2d(self.input_channels)
+            self.bn1 = nn.BatchNorm2d(output_channels)
+            self.bn2 = nn.BatchNorm1d(self.embedding_dim)
+        else:
+            self.bn0 = None
+            self.bn1 = None
+            self.bn2 = None
+        num_in_features = (
+            output_channels
+            * (2 * self.embedding_height - kernel_height + 1)
+            * (self.embedding_width - kernel_width + 1)
+        )
+        self.fc = nn.Linear(num_in_features, self.embedding_dim)
+        self.activation = nn.ReLU()
+
+    @_add_cuda_warning
+    def forward(
+        self,
+        h: torch.FloatTensor,
+        r: torch.FloatTensor,
+        t: torch.FloatTensor,
+        **kwargs,
+    ) -> torch.FloatTensor:  # noqa: D102
+        # get tail bias term
+        if "t_bias" not in kwargs:
+            raise TypeError(f"{self.__class__.__name__}.forward expects keyword argument 't_bias'.")
+        t_bias: torch.FloatTensor = kwargs.pop("t_bias")
+        self._check_for_empty_kwargs(kwargs)
+
+        # bind sizes
+        batch_size, num_heads = h.shape[:2]
+        num_relations = r.shape[1]
+
+        # repeat if necessary
+        h = h.unsqueeze(dim=2).repeat(1, 1, num_relations, 1)
+        r = r.unsqueeze(dim=1).repeat(1, num_heads, 1, 1)
+
+        # resize and concat head and relation, batch_size', num_input_channels, 2*height, width
+        # with batch_size' = batch_size * num_heads * num_relations
+        x = torch.cat([
+            h.view(-1, self.input_channels, self.embedding_height, self.embedding_width),
+            r.view(-1, self.input_channels, self.embedding_height, self.embedding_width),
+        ], dim=2)
+
+        # batch_size, num_input_channels, 2*height, width
+        if self.apply_batch_normalization:
+            x = self.bn0(x)
+
+        # batch_size, num_input_channels, 2*height, width
+        x = self.inp_drop(x)
+
+        # (N,C_out,H_out,W_out)
+        x = self.conv1(x)
+
+        if self.apply_batch_normalization:
+            x = self.bn1(x)
+
+        x = self.activation(x)
+        x = self.feature_map_drop(x)
+
+        # batch_size', num_output_channels * (2 * height - kernel_height + 1) * (width - kernel_width + 1)
+        x = x.view(batch_size, -1)
+        x = self.fc(x)
+        x = self.hidden_drop(x)
+
+        if self.apply_batch_normalization:
+            x = self.bn2(x)
+        x = self.activation(x)
+
+        # reshape: (batch_size', )
+        x = x.view(batch_size, num_heads, num_relations, 1, self.embedding_dim)
+
+        # For efficient calculation, each of the convolved [h, r] rows has only to be multiplied with one t row
+        # output_shape: (batch_size, num_heads, num_relations, num_tails)
+        x = x @ t.view(batch_size, 1, 1, -1, self.embedding_dim).transpose(-1, -2)
+
+        # add bias term
+        x = x + t_bias[:, None, None, :]
+
+        return x
 
 
 class ConvE(EntityRelationEmbeddingModel):
@@ -161,12 +318,6 @@ class ConvE(EntityRelationEmbeddingModel):
     #: The default parameters for the default loss function class
     loss_default_kwargs = {}
 
-    #: If batch normalization is enabled, this is: num_features – C from an expected input of size (N,C,L)
-    bn0: Optional[torch.nn.BatchNorm2d]
-    #: If batch normalization is enabled, this is: num_features – C from an expected input of size (N,C,H,W)
-    bn1: Optional[torch.nn.BatchNorm2d]
-    bn2: Optional[torch.nn.BatchNorm2d]
-
     def __init__(
         self,
         triples_factory: TriplesFactory,
@@ -216,219 +367,45 @@ class ConvE(EntityRelationEmbeddingModel):
             initializer=nn.init.zeros_,
         )
 
-        # Automatic calculation of remaining dimensions
-        logger.info(f'Resolving {input_channels} * {embedding_width} * {embedding_height} = {embedding_dim}.')
-        if embedding_dim is None:
-            embedding_dim = input_channels * embedding_width * embedding_height
-
-        # Parameter need to fulfil:
-        #   input_channels * embedding_height * embedding_width = embedding_dim
-        input_channels, embedding_width, embedding_height = _calculate_missing_shape_information(
-            embedding_dim=embedding_dim,
+        self.interaction_function = ConvEInteractionFunction(
             input_channels=input_channels,
-            width=embedding_width,
-            height=embedding_height,
+            output_channels=output_channels,
+            embedding_height=embedding_height,
+            embedding_width=embedding_width,
+            kernel_height=kernel_height,
+            kernel_width=kernel_width,
+            input_dropout=input_dropout,
+            output_dropout=output_dropout,
+            feature_map_dropout=feature_map_dropout,
+            embedding_dim=embedding_dim,
+            apply_batch_normalization=apply_batch_normalization,
         )
-        logger.info(f'Resolved to {input_channels} * {embedding_width} * {embedding_height} = {embedding_dim}.')
-        self.embedding_height = embedding_height
-        self.embedding_width = embedding_width
-        self.input_channels = input_channels
-
-        if self.input_channels * self.embedding_height * self.embedding_width != self.embedding_dim:
-            raise ValueError(
-                f'Product of input channels ({self.input_channels}), height ({self.embedding_height}), and width '
-                f'({self.embedding_width}) does not equal target embedding dimension ({self.embedding_dim})',
-            )
-
-        self.inp_drop = nn.Dropout(input_dropout)
-        self.hidden_drop = nn.Dropout(output_dropout)
-        self.feature_map_drop = nn.Dropout2d(feature_map_dropout)
-
-        self.conv1 = torch.nn.Conv2d(
-            in_channels=self.input_channels,
-            out_channels=output_channels,
-            kernel_size=(kernel_height, kernel_width),
-            stride=1,
-            padding=0,
-            bias=True,
-        )
-
-        self.apply_batch_normalization = apply_batch_normalization
-        if self.apply_batch_normalization:
-            self.bn0 = nn.BatchNorm2d(self.input_channels)
-            self.bn1 = nn.BatchNorm2d(output_channels)
-            self.bn2 = nn.BatchNorm1d(self.embedding_dim)
-        else:
-            self.bn0 = None
-            self.bn1 = None
-            self.bn2 = None
-        num_in_features = (
-            output_channels
-            * (2 * self.embedding_height - kernel_height + 1)
-            * (self.embedding_width - kernel_width + 1)
-        )
-        self.fc = nn.Linear(num_in_features, self.embedding_dim)
 
     def _reset_parameters_(self):  # noqa: D102
         super()._reset_parameters_()
-
         self.bias_term.reset_parameters()
-
-        # weights
-        for module in [
-            self.conv1,
-            self.bn0,
-            self.bn1,
-            self.bn2,
-            self.fc,
-        ]:
-            if module is None:
-                continue
-            module.reset_parameters()
-
-    def _convolve_entity_relation(self, h: torch.LongTensor, r: torch.LongTensor) -> torch.FloatTensor:
-        """Perform the main calculations of the ConvE model."""
-        batch_size = h.shape[0]
-
-        # batch_size, num_input_channels, 2*height, width
-        x = torch.cat([h, r], dim=2)
-
-        try:
-            # batch_size, num_input_channels, 2*height, width
-            if self.apply_batch_normalization:
-                x = self.bn0(x)
-
-            # batch_size, num_input_channels, 2*height, width
-            x = self.inp_drop(x)
-            # (N,C_out,H_out,W_out)
-            x = self.conv1(x)
-
-            if self.apply_batch_normalization:
-                x = self.bn1(x)
-            x = F.relu(x)
-            x = self.feature_map_drop(x)
-            # batch_size, num_output_channels * (2 * height - kernel_height + 1) * (width - kernel_width + 1)
-            x = x.view(batch_size, -1)
-            x = self.fc(x)
-            x = self.hidden_drop(x)
-
-            if self.apply_batch_normalization:
-                x = self.bn2(x)
-            x = F.relu(x)
-        except RuntimeError as e:
-            if not is_cudnn_error(e):
-                raise e
-            logger.warning(
-                '\nThis code crash might have been caused by a CUDA bug, see '
-                'https://github.com/allenai/allennlp/issues/2888, '
-                'which causes the code to crash during evaluation mode.\n'
-                'To avoid this error, the batch size has to be reduced.\n'
-                f'The original error message: \n{e.args[0]}',
-            )
-            sys.exit(1)
-
-        return x
+        self.interaction_function.reset_parameters()
 
     def score_hrt(self, hrt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        h = self.entity_embeddings(indices=hrt_batch[:, 0]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-        r = self.relation_embeddings(indices=hrt_batch[:, 1]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
+        h = self.entity_embeddings(indices=hrt_batch[:, 0])
+        r = self.relation_embeddings(indices=hrt_batch[:, 1])
         t = self.entity_embeddings(indices=hrt_batch[:, 2])
-
-        # Embedding Regularization
+        t_bias = self.bias_term(indices=hrt_batch[:, 2])
         self.regularize_if_necessary(h, r, t)
-
-        x = self._convolve_entity_relation(h, r)
-
-        # For efficient calculation, each of the convolved [h, r] rows has only to be multiplied with one t row
-        x = (x.view(-1, self.embedding_dim) * t).sum(dim=1, keepdim=True)
-
-        """
-        In ConvE the bias term add the end is added for each tail item. In the sLCWA training approach we only have
-        one tail item for each head and relation. Accordingly the relevant bias for each tail item and triple has to be
-        looked up.
-        """
-        x = x + self.bias_term(indices=hrt_batch[:, 2])
-        # The application of the sigmoid during training is automatically handled by the default loss.
-
-        return x
+        return self.interaction_function.score_hrt(h=h, r=r, t=t, t_bias=t_bias)
 
     def score_t(self, hr_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        h = self.entity_embeddings(indices=hr_batch[:, 0]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-        r = self.relation_embeddings(indices=hr_batch[:, 1]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-        t = self.entity_embeddings(indices=None).transpose(1, 0)
-
-        # Embedding Regularization
-        self.regularize_if_necessary(h, r, t)
-
-        x = self._convolve_entity_relation(h, r)
-
-        x = x @ t
-        x = x + self.bias_term(indices=None).t()
-        # The application of the sigmoid during training is automatically handled by the default loss.
-
-        return x
+        h = self.entity_embeddings(indices=hr_batch[:, 0])
+        r = self.relation_embeddings(indices=hr_batch[:, 1])
+        all_entities = self.entity_embeddings(indices=None)
+        t_bias = self.bias_term(indices=None)
+        self.regularize_if_necessary(h, r, all_entities)
+        return self.interaction_function.score_t(h=h, r=r, all_entities=all_entities, t_bias=t_bias)
 
     def score_h(self, rt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        rt_batch_size = rt_batch.shape[0]
-        h = self.entity_embeddings(indices=None)
-        r = self.relation_embeddings(indices=rt_batch[:, 0]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
+        all_entities = self.entity_embeddings(indices=None)
+        r = self.relation_embeddings(indices=rt_batch[:, 0])
         t = self.entity_embeddings(indices=rt_batch[:, 1])
-
-        # Embedding Regularization
-        self.regularize_if_necessary(h, r, t)
-
-        '''
-        Every head has to be convolved with every relation in the rt_batch. Hence we repeat the
-        relation _num_entities_ times and the head _rt_batch_size_ times.
-        '''
-        r = r.repeat(h.shape[0], 1, 1, 1)
-        # Code to repeat each item successively instead of the entire tensor
-        h = h.unsqueeze(1).repeat(1, rt_batch_size, 1).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-
-        x = self._convolve_entity_relation(h, r)
-
-        '''
-        For efficient computation, each convolved [h, r] pair has only to be multiplied with the corresponding t
-        embedding found in the rt_batch with [r, t] pairs.
-        '''
-        x = (x.view(self.num_entities, rt_batch_size, self.embedding_dim) * t[None, :, :]).sum(2).transpose(1, 0)
-
-        """
-        In ConvE the bias term at the end is added for each tail item. In the score_h function, each row holds
-        the same tail for many different heads, meaning that these items have to be looked up for each tail of each row
-        and only then can be added correctly.
-        """
-        x = x + self.bias_term(indices=rt_batch[:, 1])
-        # The application of the sigmoid during training is automatically handled by the default loss.
-
-        return x
+        t_bias = self.bias_term(indices=rt_batch[:, 1])
+        self.regularize_if_necessary(all_entities, r, t)
+        return self.interaction_function.score_h(all_entities=all_entities, r=r, t=t, t_bias=t_bias)
