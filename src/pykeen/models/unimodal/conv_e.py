ee3f48b59f59cd0891a85beac77f2519fe937be4
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-11-12 11:38:21 +0100

    Add functional conve interaction function

--- src/pykeen/models/unimodal/conv_e.py
+++ src/pykeen/models/unimodal/conv_e.py
@@ -11,12 +11,11 @@ from torch import nn
 
 from ..base import EntityRelationEmbeddingModel, InteractionFunction
 from ...losses import BCEAfterSigmoidLoss, Loss
-from ...nn import Embedding
+from ...nn import Embedding, functional as pykeen_functional
 from ...nn.init import xavier_normal_
 from ...regularizers import Regularizer
 from ...triples import TriplesFactory
 from ...typing import DeviceHint
-from ...utils import is_cudnn_error
 
 __all__ = [
     'ConvE',
@@ -72,23 +71,6 @@ def _calculate_missing_shape_information(
         raise ValueError(f'Could not resolve {original} to a valid factorization of {embedding_dim}.')
 
     return input_channels, width, height
-
-
-def _add_cuda_warning(func):
-    def wrapped(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except RuntimeError as e:
-            if not is_cudnn_error(e):
-                raise e
-            raise RuntimeError(
-                '\nThis code crash might have been caused by a CUDA bug, see '
-                'https://github.com/allenai/allennlp/issues/2888, '
-                'which causes the code to crash during evaluation mode.\n'
-                'To avoid this error, the batch size has to be reduced.',
-            ) from e
-
-    return wrapped
 
 
 class ConvEInteractionFunction(InteractionFunction):
@@ -171,7 +153,6 @@ class ConvEInteractionFunction(InteractionFunction):
         self.fc = nn.Linear(self.num_in_features, self.embedding_dim)
         self.activation = nn.ReLU()
 
-    @_add_cuda_warning
     def forward(
         self,
         h: torch.FloatTensor,
@@ -184,61 +165,26 @@ class ConvEInteractionFunction(InteractionFunction):
             raise TypeError(f"{self.__class__.__name__}.forward expects keyword argument 't_bias'.")
         t_bias: torch.FloatTensor = kwargs.pop("t_bias")
         self._check_for_empty_kwargs(kwargs)
-
-        # bind sizes
-        batch_size = max(x.shape[0] for x in (h, r, t))
-        num_heads = h.shape[1]
-        num_relations = r.shape[1]
-        num_tails = t.shape[1]
-
-        # repeat if necessary
-        h = h.unsqueeze(dim=2).repeat(1 if h.shape[0] == batch_size else batch_size, 1, num_relations, 1)
-        r = r.unsqueeze(dim=1).repeat(1 if r.shape[0] == batch_size else batch_size, num_heads, 1, 1)
-
-        # resize and concat head and relation, batch_size', num_input_channels, 2*height, width
-        # with batch_size' = batch_size * num_heads * num_relations
-        x = torch.cat([
-            h.view(-1, self.input_channels, self.embedding_height, self.embedding_width),
-            r.view(-1, self.input_channels, self.embedding_height, self.embedding_width),
-        ], dim=2)
-
-        # batch_size, num_input_channels, 2*height, width
-        if self.apply_batch_normalization:
-            x = self.bn0(x)
-
-        # batch_size, num_input_channels, 2*height, width
-        x = self.inp_drop(x)
-
-        # (N,C_out,H_out,W_out)
-        x = self.conv1(x)
-
-        if self.apply_batch_normalization:
-            x = self.bn1(x)
-
-        x = self.activation(x)
-        x = self.feature_map_drop(x)
-
-        # batch_size', num_output_channels * (2 * height - kernel_height + 1) * (width - kernel_width + 1)
-        x = x.view(-1, self.num_in_features)
-        x = self.fc(x)
-        x = self.hidden_drop(x)
-
-        if self.apply_batch_normalization:
-            x = self.bn2(x)
-        x = self.activation(x)
-
-        # reshape: (batch_size', embedding_dim)
-        x = x.view(batch_size, num_heads, num_relations, 1, self.embedding_dim)
-
-        # For efficient calculation, each of the convolved [h, r] rows has only to be multiplied with one t row
-        # output_shape: (batch_size, num_heads, num_relations, num_tails)
-        t = t.view(t.shape[0], 1, 1, num_tails, self.embedding_dim).transpose(-1, -2)
-        x = (x @ t).squeeze(dim=-2)
-
-        # add bias term
-        x = x + t_bias.view(t.shape[0], 1, 1, num_tails)
-
-        return x
+        return pykeen_functional.conve_interaction(
+            h=h,
+            r=r,
+            t=t,
+            t_bias=t_bias,
+            input_channels=self.input_channels,
+            embedding_height=self.embedding_height,
+            embedding_width=self.embedding_width,
+            num_in_features=self.num_in_features,
+            embedding_dim=self.embedding_dim,
+            bn0=self.bn0,
+            bn1=self.bn1,
+            bn2=self.bn2,
+            inp_drop=self.inp_drop,
+            feature_map_drop=self.feature_map_drop,
+            hidden_drop=self.hidden_drop,
+            conv1=self.conv1,
+            activation=self.activation,
+            fc=self.fc,
+        )
 
 
 class ConvE(EntityRelationEmbeddingModel):
--- src/pykeen/nn/functional.py
+++ src/pykeen/nn/functional.py
@@ -1,12 +1,14 @@
 """Functional forms of interaction methods."""
-from typing import Tuple
+from typing import Optional, Tuple
 
 import torch
+from torch import nn
 
-from ..utils import normalize_for_einsum, split_complex
+from ..utils import is_cudnn_error, normalize_for_einsum, split_complex
 
 __all__ = [
     "complex_interaction",
+    "conve_interaction",
     "distmult_interaction",
 ]
 
@@ -21,6 +23,100 @@ def _normalize_terms_for_einsum(
     r_term, r = normalize_for_einsum(x=r, batch_size=batch_size, symbol='r')
     t_term, t = normalize_for_einsum(x=t, batch_size=batch_size, symbol='t')
     return h, h_term, r, r_term, t, t_term
+
+
+def _add_cuda_warning(func):
+    def wrapped(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except RuntimeError as e:
+            if not is_cudnn_error(e):
+                raise e
+            raise RuntimeError(
+                '\nThis code crash might have been caused by a CUDA bug, see '
+                'https://github.com/allenai/allennlp/issues/2888, '
+                'which causes the code to crash during evaluation mode.\n'
+                'To avoid this error, the batch size has to be reduced.',
+            ) from e
+
+    return wrapped
+
+
+@_add_cuda_warning
+def conve_interaction(
+    h: torch.FloatTensor,
+    r: torch.FloatTensor,
+    t: torch.FloatTensor,
+    t_bias: torch.FloatTensor,
+    input_channels: int,
+    embedding_height: int,
+    embedding_width: int,
+    num_in_features: int,
+    embedding_dim: int,
+    bn0: Optional[nn.BatchNorm1d],
+    bn1: Optional[nn.BatchNorm1d],
+    bn2: Optional[nn.BatchNorm1d],
+    inp_drop: nn.Dropout,
+    feature_map_drop: nn.Dropout2d,
+    hidden_drop: nn.Dropout,
+    conv1: nn.Conv2d,
+    activation: nn.Module,
+    fc: nn.Linear,
+) -> torch.FloatTensor:
+    # bind sizes
+    batch_size = max(x.shape[0] for x in (h, r, t))
+    num_heads = h.shape[1]
+    num_relations = r.shape[1]
+    num_tails = t.shape[1]
+
+    # repeat if necessary
+    h = h.unsqueeze(dim=2).repeat(1 if h.shape[0] == batch_size else batch_size, 1, num_relations, 1)
+    r = r.unsqueeze(dim=1).repeat(1 if r.shape[0] == batch_size else batch_size, num_heads, 1, 1)
+
+    # resize and concat head and relation, batch_size', num_input_channels, 2*height, width
+    # with batch_size' = batch_size * num_heads * num_relations
+    x = torch.cat([
+        h.view(-1, input_channels, embedding_height, embedding_width),
+        r.view(-1, input_channels, embedding_height, embedding_width),
+    ], dim=2)
+
+    # batch_size, num_input_channels, 2*height, width
+    if bn0 is not None:
+        x = bn0(x)
+
+    # batch_size, num_input_channels, 2*height, width
+    x = inp_drop(x)
+
+    # (N,C_out,H_out,W_out)
+    x = conv1(x)
+
+    if bn1 is not None:
+        x = bn1(x)
+
+    x = activation(x)
+    x = feature_map_drop(x)
+
+    # batch_size', num_output_channels * (2 * height - kernel_height + 1) * (width - kernel_width + 1)
+    x = x.view(-1, num_in_features)
+    x = fc(x)
+    x = hidden_drop(x)
+
+    if bn2 is not None:
+        x = bn2(x)
+    x = activation(x)
+
+    # reshape: (batch_size', embedding_dim)
+    x = x.view(batch_size, num_heads, num_relations, 1, embedding_dim)
+
+    # For efficient calculation, each of the convolved [h, r] rows has only to be multiplied with one t row
+    # output_shape: (batch_size, num_heads, num_relations, num_tails)
+    t = t.view(t.shape[0], 1, 1, num_tails, embedding_dim).transpose(-1, -2)
+    x = (x @ t).squeeze(dim=-2)
+
+    # add bias term
+    x = x + t_bias.view(t.shape[0], 1, 1, num_tails)
+
+    return x
 
 
 def distmult_interaction(
