Fix KL divergence

src/pykeen/nn/sim.py
@@ -71,21 +71,24 @@ def kullback_leibler_similarity(
     epsilon: float = 1.0e-10,
     exact: bool = True,
 ) -> torch.FloatTensor:
-    r"""Compute the similarity based on KL divergence.
+    r"""Compute the negative KL divergence.
 
     This is done between two Gaussian distributions given by mean `mu_*` and diagonal covariance matrix `sigma_*`.
-
+    
     .. math::
+    
+        D((\mu_0, \Sigma_0), (\mu_1, \Sigma_1)) = 0.5 * (
+          tr(\Sigma_1^-1 \Sigma_0)
+          + (\mu_1 - \mu_0) * \Sigma_1^-1 (\mu_1 - \mu_0)
+          - k
+          + ln (det(\Sigma_1) / det(\Sigma_0))
+        )
 
-        D((\mu_e, \Sigma_e), (\mu_r, \Sigma_r)))
-        = \frac{1}{2} \left(
-            tr(\Sigma_r^{-1}\Sigma_e)
-            + (\mu_r - \mu_e)^T\Sigma_r^{-1}(\mu_r - \mu_e)
-            - \log \frac{det(\Sigma_e)}{det(\Sigma_r)} - k_e
-        \right)
+    .. note ::
+        This methods assumes diagonal covariance matrices :math:`\Sigma`.
 
-    Note: The sign of the function has been flipped as opposed to the description in the paper, as the
-          Kullback Leibler divergence is large if the distributions are dissimilar.
+    .. seealso ::
+        https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Kullback%E2%80%93Leibler_divergence
 
     :param e: shape: (batch_size, num_heads, num_tails, d)
         The entity Gaussian distributions, as mean/diagonal covariance pairs.
@@ -99,33 +102,51 @@ def kullback_leibler_similarity(
     :return: torch.Tensor, shape: (s_1, ..., s_k)
         The similarity.
     """
-    # invert covariance, shape: (batch_size, num_relations, d)
-    safe_sigma_r = torch.clamp_min(r.diagonal_covariance, min=epsilon)
-    sigma_r_inv = torch.reciprocal(safe_sigma_r)
 
-    #: a = tr(\Sigma_r^{-1}\Sigma_e), (batch_size, num_heads, num_relations, num_tails)
-    # [(b, h, t, d), (b, r, d) -> (b, 1, r, d) -> (b, 1, d, r)] -> (b, h, t, r) -> (b, h, r, t)
-    sim = (e.diagonal_covariance @ sigma_r_inv.unsqueeze(dim=1).transpose(-2, -1)).transpose(-2, -1)
+    assert (e.diagonal_covariance > 0).all() and (r.diagonal_covariance > 0).all()
 
-    #: b = (\mu_r - \mu_e)^T\Sigma_r^{-1}(\mu_r - \mu_e)
-    r_shape = r.mean.shape
-    # mu.shape: (b, h, r, t, d)
-    mu = r.mean.view(r_shape[0], 1, r_shape[1], 1, r_shape[2]) - e.mean.unsqueeze(dim=2)
-    sim = sim + (mu ** 2 @ sigma_r_inv.view(r_shape[0], 1, r_shape[1], r_shape[2], 1)).squeeze(dim=-1)
+    # broadcast shapes to (batch_size, num_heads, num_relations, num_tails, dim)
+    e_shape = e.mean.shape  # (batch_size, num_heads, num_tails, dim)
+    e_mean = e.mean.view(e_shape[0], e_shape[1], 1, e_shape[2], e_shape[3])
+    e_var = e.diagonal_covariance.view(e_shape[0], e_shape[1], 1, e_shape[2], e_shape[3])
 
-    #: c = \log \frac{det(\Sigma_e)}{det(\Sigma_r)}
-    # = sum log (sigma_e)_i - sum log (sigma_r)_i
-    # ce.shape: (b, h, t)
-    ce = e.diagonal_covariance.clamp_min(min=epsilon).log().sum(dim=-1)
-    # cr.shape: (b, r)
-    cr = safe_sigma_r.log().sum(dim=-1)
-    sim = sim + ce.unsqueeze(dim=2) - cr.view(r_shape[0], 1, r_shape[1], 1)
+    r_shape = r.mean.shape  # (batch_size, num_relations, dim)
+    r_mean = r.mean.view(r_shape[0], 1, r_shape[1], 1, r_shape[2])
+    r_var: torch.FloatTensor = r.diagonal_covariance.view(r_shape[0], 1, r_shape[1], 1, r_shape[2])
+
+    # 1. Component
+    # tr(sigma_1^-1 sigma_0) = sum (sigma_1^-1 sigma_0)[i, i]
+    # since sigma_0, sigma_1 are diagonal matrices:
+    # = sum (sigma_1^-1[i] sigma_0[i]) = sum (sigma_0[i] / sigma_1[i])
+    r_var_safe = r_var.clamp_min(min=epsilon)
+    result = (e_var / r_var_safe).sum(dim=-1)
+
+    # 2. Component
+    # (mu_1 - mu_0) * Sigma_1^-1 (mu_1 - mu_0)
+    # with mu = (mu_1 - mu_0)
+    # = mu * Sigma_1^-1 mu
+    # since Sigma_1 is diagonal
+    # = mu**2 / sigma_1
+    mu = r_mean - e_mean
+    result = result + (mu.pow(2) / r_var_safe).sum(dim=-1)
+
+    # 3. Component
+    if exact:
+        result = result - e_shape[-1]
+
+    # 4. Component
+    # ln (det(\Sigma_1) / det(\Sigma_0))
+    # = ln det Sigma_1 - ln det Sigma_0
+    # since Sigma is diagonal, we have det Sigma = prod Sigma[ii]
+    # = ln prod Sigma_1[ii] - ln prod Sigma_0[ii]
+    # = sum ln Sigma_1[ii] - sum ln Sigma_0[ii]
+    e_var_safe = e_var.clamp_min(min=epsilon)
+    result = result + r_var_safe.log().sum(dim=-1) - e_var_safe.log().sum(dim=-1)
 
     if exact:
-        sim = sim - e.mean.shape[-1]
-        sim = 0.5 * sim
+        result = 0.5 * result
 
-    return sim
+    return -result
 
 
 KG2E_SIMILARITIES = {

tests/test_nn.py
@@ -76,9 +76,9 @@ class KullbackLeiblerTests(unittest.TestCase):
 
     def setUp(self) -> None:
         self.e_mean = torch.rand(self.d)
-        self.e_var = torch.rand(self.d)
+        self.e_var = torch.rand(self.d).exp()
         self.r_mean = torch.rand(self.d)
-        self.r_var = torch.rand(self.d)
+        self.r_var = torch.rand(self.d).exp()
 
     def get_e(self, pre_shape=(1, 1, 1)):
         return GaussianDistribution(
@@ -102,12 +102,14 @@ class KullbackLeiblerTests(unittest.TestCase):
 
         p = torch.distributions.MultivariateNormal(loc=self.e_mean, covariance_matrix=torch.diag(self.e_var))
         q = torch.distributions.MultivariateNormal(loc=self.r_mean, covariance_matrix=torch.diag(self.r_var))
-        sim2 = torch.distributions.kl_divergence(p=p, q=q).view(-1)
+        sim2 = -torch.distributions.kl_divergence(p=p, q=q).view(-1)
         assert torch.allclose(sim, sim2)
 
     def test_self_similarity(self):
         """Check value of similarity to self."""
         # e: (batch_size, num_heads, num_tails, d)
+        # https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence#Properties
+        # divergence = 0 => similarity = -divergence = 0
         e = self.get_e()
         r = self.get_e(pre_shape=(1, 1))
         sim = kullback_leibler_similarity(e=e, r=r, exact=True)
@@ -115,6 +117,8 @@ class KullbackLeiblerTests(unittest.TestCase):
 
     def test_value_range(self):
         """Check the value range."""
+        # https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence#Properties
+        # divergence >= 0 => similarity = -divergence <= 0
         e = self.get_e()
         r = self.get_r()
         sim = kullback_leibler_similarity(e=e, r=r, exact=True)