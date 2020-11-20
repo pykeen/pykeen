99090eff400185ae3314edc0179b5e4ca234d3b6
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-11-20 12:01:53 +0100

    asaf

--- src/pykeen/nn/functional.py
+++ src/pykeen/nn/functional.py
@@ -138,11 +138,11 @@ def complex_interaction(
     return tensor_sum(*(
         factor * extended_einsum("bhd,brd,btd->bhrt", hh, rr, tt)
         for factor, hh, rr, tt in [
-        (+1, h_re, r_re, t_re),
-        (+1, h_re, r_im, t_im),
-        (+1, h_im, r_re, t_im),
-        (-1, h_im, r_im, t_re),
-    ]
+            (+1, h_re, r_re, t_re),
+            (+1, h_re, r_im, t_im),
+            (+1, h_im, r_re, t_im),
+            (-1, h_im, r_im, t_re),
+        ]
     ))
 
 
