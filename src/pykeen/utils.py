10798604bceb3d8357d3a8169fcb3fe423849140
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-11-25 18:41:45 +0100

    Add tensor_sum / tensor_product to __all__

--- src/pykeen/utils.py
+++ src/pykeen/utils.py
@@ -51,6 +51,8 @@ __all__ = [
     'set_random_seed',
     'split_complex',
     'split_list_in_batches_iter',
+    'tensor_sum',
+    'tensor_product',
     'upgrade_to_sequence',
     'view_complex',
     'NoRandomSeedNecessary',
