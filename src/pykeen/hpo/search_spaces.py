5b02762901b419f8d3a0dbbd75513a49b544be1b
mali <ali-mehdi@live.de>
2020-12-02 14:48:32 +0100

    Define default HPO embedding dimension range

--- src/pykeen/hpo/search_spaces.py
+++ src/pykeen/hpo/search_spaces.py
@@ -3,3 +3,4 @@
 """Default Hyper-parameter search spaces in PyKEEN that are used package-wide."""
 
 DEFAULT_DROPOUT_HPO_RANGE = dict(type=float, low=0.0, high=0.5, q=0.1)
+DEFAULT_EMBEDDING_HPO_EMBEDDING_DIM_RANGE = dict(type=int, low=5, high=8, scale='power_two')
