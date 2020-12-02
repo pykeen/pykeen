b4d3b5f379edd7551b8da371f246ba1baa003b78
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-02 20:39:21 +0100

    Update benchmark script

--- benchmarking/benchmark_splitting.py
+++ benchmarking/benchmark_splitting.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Benchmark the speed for generating new datasets by remixing old ones."""
+
 import itertools as itt
 import logging
 import os
@@ -15,10 +19,16 @@ from pykeen.datasets import get_dataset
 from pykeen.triples.splitting import split
 
 HERE = os.path.abspath(os.path.dirname(__file__))
+RESULTS = os.path.join(HERE, 'results')
+os.makedirs(RESULTS, exist_ok=True)
+
+tsv_path = os.path.join(RESULTS, 'split_benchmark.tsv')
+png_path = os.path.join(RESULTS, 'split_benchmark.png')
+scatter_png_path = os.path.join(RESULTS, 'split_benchmark_scatter.png')
 
 
 @click.command()
-@click.option('-r', '--replicates', type=int, default=5, show_default=True)
+@click.option('-r', '--replicates', type=int, default=3, show_default=True)
 def main(replicates: int):
     import pykeen.triples.splitting
     pykeen.triples.splitting.logger.setLevel(logging.ERROR)
@@ -27,7 +37,7 @@ def main(replicates: int):
     import pykeen.utils
     pykeen.utils.logger.setLevel(logging.ERROR)
 
-    methods = ['new', 'old']
+    methods = ['old', 'new']
     ratios = [0.8]
     datasets = [
         'nations',
@@ -40,27 +50,30 @@ def main(replicates: int):
         'FB15k237',
         'wn18',
         'fb15k',
-        'hetionet',
         'YAGO310',
         'OGBBioKG',
+        'hetionet',
         'OGBWikiKG',
+        'openbiolink'
         'drkg',
     ]
 
     rows = []
     outer_it = tqdm(datasets, desc='Dataset')
-    for _dataset in outer_it:
-        dataset = get_dataset(dataset=_dataset)
+    for dataset in outer_it:
+        dataset = get_dataset(dataset=dataset)
+        dataset_name = dataset.__class__.__name__
         triples = np.concatenate([
             dataset.training.triples,
             dataset.testing.triples,
             dataset.validation.triples,
         ])
-        inner_it = itt.product(methods, ratios, range(replicates))
+        del dataset
+        inner_it = itt.product(methods, ratios, range(1, 1 + replicates))
         inner_it = tqdm(
             inner_it,
             total=len(methods) * len(ratios) * replicates,
-            desc=f'{dataset.__class__.__name__} ({intword(triples.shape[0])})',
+            desc=f'{dataset_name} ({intword(triples.shape[0])})',
         )
         for method, ratio, replicate in inner_it:
             t = time.time()
@@ -72,7 +85,7 @@ def main(replicates: int):
             )
             total = time.time() - t
             rows.append((
-                dataset.__class__.__name__,
+                dataset_name,
                 triples.shape[0],
                 method,
                 ratio,
@@ -82,6 +95,7 @@ def main(replicates: int):
                 results[1].shape[0],
                 results[2].shape[0],
             ))
+            del results
 
     df = pd.DataFrame(rows, columns=[
         'dataset',
@@ -94,16 +108,44 @@ def main(replicates: int):
         'testing_size',
         'validation_size',
     ])
-    tsv_path = os.path.join(HERE, 'split_benchmark.tsv')
-    df.to_csv(tsv_path, sep='\t', index=False)
 
-    sns.barplot(data=df, y='dataset', x='time', hue='method')
-    png_path = os.path.join(HERE, 'split_benchmark.png')
-    plt.xscale('log')
-    plt.xlabel('Time (s)')
-    plt.ylabel('')
-    plt.tight_layout()
-    plt.savefig(png_path, dpi=300)
+    df.to_csv(tsv_path, sep='\t', index=False)
+    _make_1(df)
+    _make_2(df)
+
+
+def _make_1(df):
+    """Make the chart comparing the dataset times by method."""
+    fig, ax = plt.subplots(1, 1)
+    sns.barplot(data=df, y='dataset', x='time', hue='method', ax=ax)
+    ax.set_xscale('log')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('')
+    fig.tight_layout()
+    fig.savefig(png_path, dpi=300)
+    plt.close(fig)
+
+
+def _make_2(df):
+    """Make chart comparing dataset sizes to times."""
+    fig, axes = plt.subplots(1, 3, figsize=(14, 6), sharey='all')
+    xs = [
+        'dataset_size',
+        'training_size',
+        'testing_size',
+    ]
+    for x, ax in zip(xs, axes.ravel()):
+        sns.scatterplot(
+            data=df, y='time', x=x, hue='dataset', style='method', ax=ax, x_jitter=.1,
+            legend=None,
+        )
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_ylabel('Time (s)')
+
+    fig.tight_layout()
+    fig.savefig(scatter_png_path, dpi=300)
+    plt.close(fig)
 
 
 if __name__ == '__main__':
