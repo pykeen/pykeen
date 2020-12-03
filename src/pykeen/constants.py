6d754d7633633a43b20d81ed94d7ea6a91ff1233
mali <ali-mehdi@live.de>
2020-12-03 07:27:04 +0100

    Move default HPO settings

--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -9,3 +9,5 @@ __all__ = [
 ]
 
 PYKEEN_HOME = os.environ.get('PYKEEN_HOME') or os.path.join(os.path.expanduser('~'), '.pykeen')
+DEFAULT_DROPOUT_HPO_RANGE = dict(type=float, low=0.0, high=0.5, q=0.1)
+DEFAULT_EMBEDDING_HPO_EMBEDDING_DIM_RANGE = dict(type=int, low=16, high=256, q=16)
