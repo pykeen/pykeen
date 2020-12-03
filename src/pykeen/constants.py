2fc7a092be30558a0f7fe44b3759484e35664009
mali <ali-mehdi@live.de>
2020-12-03 13:32:15 +0100

    Add reference explaining computational benefits of specific tensor shapes

--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -10,4 +10,6 @@ __all__ = [
 
 PYKEEN_HOME = os.environ.get('PYKEEN_HOME') or os.path.join(os.path.expanduser('~'), '.pykeen')
 DEFAULT_DROPOUT_HPO_RANGE = dict(type=float, low=0.0, high=0.5, q=0.1)
+# We define the embedding dimensions as a multiple of 16 because it is computational beneficial (on a GPU)
+# see: https://docs.nvidia.com/deeplearning/performance/index.html#optimizing-performance
 DEFAULT_EMBEDDING_HPO_EMBEDDING_DIM_RANGE = dict(type=int, low=16, high=256, q=16)
