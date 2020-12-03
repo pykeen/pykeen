e72e7a555cb8e6b9277d58cafda5799409ad3dce
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-03 20:04:00 +0100

    Add more default directories
    Trigger CI

--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -7,12 +7,18 @@ import pystow
 __all__ = [
     'PYKEEN_HOME',
     'PYKEEN_DATASETS',
+    'PYKEEN_BENCHMARKS',
+    'PYKEEN_EXPERIMENTS',
+    'PYKEEN_CHECKPOINTS',
 ]
 
 #: Can be modified by setting environment variable ``PYKEEN_HOME``
 #: For more information, see https://github.com/cthoyt/pystow
 PYKEEN_HOME = pystow.get('pykeen')
 PYKEEN_DATASETS = pystow.get('pykeen', 'datasets')
+PYKEEN_BENCHMARKS = pystow.get('pykeen', 'benchmarks')
+PYKEEN_EXPERIMENTS = pystow.get('pykeen', 'experiments')
+PYKEEN_CHECKPOINTS = pystow.get('pykeen', 'checkpoints')
 
 DEFAULT_DROPOUT_HPO_RANGE = dict(type=float, low=0.0, high=0.5, q=0.1)
 # We define the embedding dimensions as a multiple of 16 because it is computational beneficial (on a GPU)
