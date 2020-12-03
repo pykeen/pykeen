515a3a8bbbbee477c2f2fab30c6225d5db67fa47
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-03 19:46:35 +0100

    Add pystow datasets

--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -6,10 +6,12 @@ import pystow
 
 __all__ = [
     'PYKEEN_HOME',
+    'PYKEEN_DATASETS',
     'PYKEEN_BENCHMARK_HOME',
 ]
 
 PYKEEN_HOME = pystow.get('pykeen')
+PYKEEN_DATASETS = pystow.get('pykeen', 'datasets')
 PYKEEN_BENCHMARK_HOME = pystow.get('pykeen', 'benchmarking')
 DEFAULT_DROPOUT_HPO_RANGE = dict(type=float, low=0.0, high=0.5, q=0.1)
 # We define the embedding dimensions as a multiple of 16 because it is computational beneficial (on a GPU)
