deeaf67afacd610a171aacee2694af327f7359e9
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-03 16:34:13 +0100

    Use PyStow for path management

--- src/pykeen/constants.py
+++ src/pykeen/constants.py
@@ -2,17 +2,12 @@
 
 """Constants for PyKEEN."""
 
-import os
-import pathlib
+import pystow
 
 __all__ = [
     'PYKEEN_HOME',
     'PYKEEN_BENCHMARK_HOME',
 ]
 
-PYKEEN_DEFAULT_PATH = pathlib.Path.home() / '.pykeen'
-PYKEEN_HOME = pathlib.Path(os.environ.get('PYKEEN_HOME') or PYKEEN_DEFAULT_PATH)
-PYKEEN_HOME.mkdir(exist_ok=True, parents=True)
-
-PYKEEN_BENCHMARK_HOME = PYKEEN_HOME / 'benchmarking'
-PYKEEN_BENCHMARK_HOME.mkdir(exist_ok=True, parents=True)
+PYKEEN_HOME = pystow.get('pykeen')
+PYKEEN_BENCHMARK_HOME = pystow.get('pykeen', 'benchmarking')
