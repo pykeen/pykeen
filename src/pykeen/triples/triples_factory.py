1546c3f0a6649979d5aca580a1c9abc2cdcb7623
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-12-04 12:52:36 +0100

    Improve efficiency of word-cloud by pre-filtering ID-based

--- src/pykeen/triples/triples_factory.py
+++ src/pykeen/triples/triples_factory.py
@@ -3,6 +3,7 @@
 """Implementation of basic instance factory which creates just instances based on standard KG triples."""
 
 import dataclasses
+import itertools
 import logging
 import os
 import re
@@ -705,8 +706,7 @@ class TriplesFactory:
             install it automatically, or install it yourself with
             ``pip install git+https://github.com/kavgan/word_cloud.git``.
         """
-        text = [" ".join(ht) for ht in self._vectorized_entity_labeler(self.mapped_triples[:, [0, 2]].numpy()).T]
-        return self._word_cloud(text=text, top=top or 100)
+        return self._word_cloud(ids=self.mapped_triples[:, [0, 2]], id_to_label=self.entity_id_to_label, top=top or 100)
 
     def relation_word_cloud(self, top: Optional[int] = None):
         """Make a word cloud based on the frequency of occurrence of each relation in a Jupyter notebook.
@@ -719,10 +719,9 @@ class TriplesFactory:
             install it automatically, or install it yourself with
             ``pip install git+https://github.com/kavgan/word_cloud.git``.
         """
-        text = self._vectorized_relation_labeler(self.mapped_triples[:, 1].numpy()).tolist()
-        return self._word_cloud(text=text, top=top or 100)
+        return self._word_cloud(ids=self.mapped_triples[:, 1], id_to_label=self.relation_id_to_label, top=top or 100)
 
-    def _word_cloud(self, *, text: List[str], top: int):
+    def _word_cloud(self, *, ids: torch.LongTensor, id_to_label: Mapping[int, str], top: int):
         try:
             from word_cloud.word_cloud_generator import WordCloud
         except ImportError:
@@ -731,6 +730,16 @@ class TriplesFactory:
                 'Try installing it with `pip install git+https://github.com/kavgan/word_cloud.git`',
             )
             return
+
+        # pre-filter to keep only topk
+        uniq, counts = ids.view(-1).unique(return_counts=True)
+        top_counts, top_ids = counts.topk(k=top, largest=True)
+
+        # generate text
+        text = list(itertools.chain(*(
+            itertools.repeat(id_to_label[e_id], count)
+            for e_id, count in zip(top_ids.tolist(), top_counts.tolist())
+        )))
 
         from IPython.core.display import HTML
         word_cloud = WordCloud()
