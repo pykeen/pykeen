235709f1bbb3a514f97c069928bf80ca09a18f2c
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-07 15:26:16 +0100

    Update generate_literals.py

--- src/pykeen/datasets/generate_literals.py
+++ src/pykeen/datasets/generate_literals.py
@@ -1,27 +1,35 @@
 # -*- coding: utf-8 -*-
 
-"""Generate the Fake Nations Literal Dataset."""
+"""Generate a fake literals dataset to accompany a real dataset.."""
+
+from typing import Any, List, TextIO, Tuple, Type, Union
 
 import click
 import torch
 
 import pykeen.nn
+from pykeen.datasets.base import Dataset
 from pykeen.models.base import EntityRelationEmbeddingModel
 from pykeen.pipeline import PipelineResult, pipeline
 
 
 @click.command()
+@click.option('--dataset', required=True)
 @click.option('--features', type=int, default=2, show_default=True)
 @click.option('--seed', type=int, default=2, show_default=True)
 @click.option('--output', type=click.File('w'))
-@click.option('--dataset', required=True)
-def generate(features: int, seed: int, output):
+def main(dataset, features: int, seed: int, output: TextIO):
     """Generate random literals for the Nations dataset."""
-    for h, r, t in generate_literals(features=features, seed=seed):
+    for h, r, t in generate_literals(dataset=dataset, features=features, seed=seed):
         print(h, r, t, sep='\t', file=output)
 
 
-def generate_literals(dataset: str, features: int, seed: int, model: str = 'rotate'):
+def generate_literals(
+    dataset: Union[None, str, Dataset, Type[Dataset]],
+    features: int,
+    seed: int,
+    model: str = 'rotate',
+) -> List[Tuple[str, str, Any]]:
     """Generate literals or the given dataset."""
     generator = torch.Generator(seed)
 
@@ -49,3 +57,7 @@ def generate_literals(dataset: str, features: int, seed: int, model: str = 'rota
         for (_, label), value in zip(sorted(tf.entity_id_to_label.items()), feature):
             rows.append((label, f'feature{i}', value.item()))
     return rows
+
+
+if __name__ == '__main__':
+    main()
