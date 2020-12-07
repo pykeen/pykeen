2b1a198bb0cc5bda021d436e29c763a20ea0ad05
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-12-07 13:51:26 +0100

    Remove obsolete parameter

--- src/pykeen/triples/triples_factory.py
+++ src/pykeen/triples/triples_factory.py
@@ -482,7 +482,7 @@ class TriplesFactory:
         """Get the inverse relation identifier for the given relation."""
         if not self.create_inverse_triples:
             raise ValueError('Can not get inverse triple, they have not been created.')
-        relation = next(iter(self.relations_to_ids(relations=[relation], include_inverses=False)))
+        relation = next(iter(self.relations_to_ids(relations=[relation])))
         return self._get_inverse_relation_id(relation)
 
     @staticmethod
