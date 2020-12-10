2654fed46b30a02507509f1692499ea0aa1fc875
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-10 16:47:58 +0100

    Update benchmark
    Trigger CI

--- benchmarking/benchmark_splitting.py
+++ benchmarking/benchmark_splitting.py
@@ -2,21 +2,21 @@
 
 """Benchmark the speed for generating new datasets by remixing old ones."""
 
+import itertools as itt
 import logging
 import os
+import time
 from datetime import datetime
 
 import click
-import itertools as itt
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import seaborn as sns
-import time
+import torch
 from humanize import intword
 from tqdm import tqdm
 
-from pykeen.datasets import get_dataset
+from pykeen.datasets import datasets, get_dataset
 from pykeen.triples.splitting import split
 from pykeen.utils import get_benchmark
 from pykeen.version import get_git_hash
@@ -32,10 +32,12 @@ columns = [
     'hash',
     'dataset',
     'dataset_size',
+    'dataset_load_time',
+    'dataset_cat_time',
     'method',
     'ratio',
     'replicate',
-    'time',
+    'split_time',
     'training_size',
     'testing_size',
     'validation_size',
@@ -47,7 +49,7 @@ def _log(s):
 
 
 @click.command()
-@click.option('-r', '--replicates', type=int, default=2, show_default=True)
+@click.option('-r', '--replicates', type=int, default=5, show_default=True)
 @click.option('-f', '--force', is_flag=True)
 def main(replicates: int, force: bool):
     import pykeen.triples.splitting
@@ -60,45 +62,35 @@ def main(replicates: int, force: bool):
     git_hash = get_git_hash()
     methods = ['cleanup', 'coverage']
     ratios = [0.8]
-    datasets = [
-        'nations',
-        'kinships',
-        'umls',
-        'codexsmall',
-        'codexmedium',
-        'codexlarge',
-        'wn18rr',
-        'fb15k237',
-        'wn18',
-        'fb15k',
-        'yago310',
-        'ogbbiokg',
-        # 'hetionet',
-        'ogbwikikg',
-        'openbiolink',
-        'drkg',
-    ]
 
+    click.echo(f'output directory: {SPLITTING_DIRECTORY.as_posix()}')
     rows = []
-    outer_it = tqdm(datasets, desc='Dataset')
+    outer_it = tqdm(sorted(datasets), desc='Dataset')
     for dataset in outer_it:
         dataset_path = RESULTS_DIRECTORY / f'{dataset}.tsv'
         if dataset_path.exists() and not force:
-            _log(f'loading pre-calculated {dataset}')
+            _log(f'loading pre-calculated {dataset} from {dataset_path}')
             df = pd.read_csv(dataset_path, sep='\t')
             rows.extend(df.values)
             continue
 
         _log(f'loading {dataset}')
+        t = time.time()
         dataset = get_dataset(dataset=dataset)
         dataset_name = dataset.__class__.__name__
-        _log(f'concatenating {dataset_name}')
-        triples = np.concatenate([
+        ccl = [
             dataset.training.mapped_triples,
             dataset.testing.mapped_triples,
             dataset.validation.mapped_triples,
-        ])
-        _log(f'done concatenating {dataset_name}')
+        ]
+        load_time = time.time() - t
+        _log(f'done loading {dataset_name} after {load_time:.3f} seconds')
+        _log(f'concatenating {dataset_name}')
+        t = time.time()
+        mapped_triples: torch.LongTensor = torch.cat(ccl, dim=0)
+        concat_time = time.time() - t
+        _log(f'done concatenating {dataset_name} after {concat_time:.3f} seconds')
+        _log(f'deleting {dataset_name}')
         del dataset
         _log(f'done deleting {dataset_name}')
 
@@ -107,53 +99,57 @@ def main(replicates: int, force: bool):
         inner_it = tqdm(
             inner_it,
             total=len(methods) * len(ratios) * replicates,
-            desc=f'{dataset_name} ({intword(triples.shape[0])})',
+            desc=f'{dataset_name} ({intword(mapped_triples.shape[0])})',
         )
         for method, ratio, replicate in inner_it:
             t = time.time()
             results = split(
-                triples=triples,
+                mapped_triples,
                 ratios=[ratio, (1 - ratio) / 2],
                 method=method,
                 random_state=replicate,
             )
-            total = time.time() - t
+            split_time = time.time() - t
             dataset_rows.append((
                 git_hash,
                 dataset_name,
-                triples.shape[0],
+                mapped_triples.shape[0],
+                load_time,
+                concat_time,
                 method,
                 ratio,
                 replicate,
-                total,
+                split_time,
                 results[0].shape[0],
                 results[1].shape[0],
                 results[2].shape[0],
             ))
             del results
 
+        _log(f'writing to {dataset_path}')
         pd.DataFrame(dataset_rows, columns=columns).to_csv(dataset_path, sep='\t', index=False)
         rows.extend(dataset_rows)
 
     df = pd.DataFrame(rows, columns=columns)
     df.to_csv(tsv_path, sep='\t', index=False)
-    _make_1(df)
-    _make_2(df)
+    _make_1(df, git_hash)
+    _make_2(df, git_hash)
 
 
-def _make_1(df):
+def _make_1(df, git_hash):
     """Make the chart comparing the dataset times by method."""
     fig, ax = plt.subplots(1, 1)
-    sns.barplot(data=df, y='dataset', x='time', hue='method', ax=ax)
+    sns.barplot(data=df, y='dataset', x='split_time', hue='method', ax=ax)
     ax.set_xscale('log')
-    ax.set_xlabel('Time (s)')
+    ax.set_xlabel('Split Time (s)')
     ax.set_ylabel('')
+    ax.set_title(git_hash)
     fig.tight_layout()
     fig.savefig(png_path, dpi=300)
     plt.close(fig)
 
 
-def _make_2(df):
+def _make_2(df, git_hash):
     """Make chart comparing dataset sizes to times."""
     fig, axes = plt.subplots(1, 3, figsize=(14, 6), sharey='all')
     xs = [
@@ -163,12 +159,13 @@ def _make_2(df):
     ]
     for x, ax in zip(xs, axes.ravel()):
         sns.scatterplot(
-            data=df, y='time', x=x, hue='dataset', style='method', ax=ax, x_jitter=.1,
+            data=df, y='split_time', x=x, hue='dataset', style='method', ax=ax, x_jitter=.1,
             legend=None,
         )
         ax.set_xscale('log')
         ax.set_yscale('log')
-        ax.set_ylabel('Time (s)')
+        ax.set_ylabel('Split Time (s)')
+        ax.set_title(git_hash)
 
     fig.tight_layout()
     fig.savefig(scatter_png_path, dpi=300)
