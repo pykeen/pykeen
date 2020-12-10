d551b1e62b673ce4083f731efc6991bd4974f9c3
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-12-10 17:43:15 +0100

    Fix ComplEx constructor merge

--- src/pykeen/models/unimodal/complex.py
+++ src/pykeen/models/unimodal/complex.py
@@ -116,7 +116,4 @@ class ComplEx(ERModel):
             loss=loss,
             preferred_device=preferred_device,
             random_seed=random_seed,
-            regularizer=regularizer,
-            # initialize with entity and relation embeddings with standard normal distribution, cf.
-            # https://github.com/ttrouill/complex/blob/dc4eb93408d9a5288c986695b58488ac80b1cc17/efe/models.py#L481-L487
         )
