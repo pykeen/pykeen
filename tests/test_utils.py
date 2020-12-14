edfe56aaf5f07438b94671a45869feeedfb28bb0
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-14 22:13:15 +0100

    Add test for empty()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Unittest for for global utilities."""
+
 import itertools
 import string
 import unittest
@@ -10,7 +11,8 @@ import torch
 
 from pykeen.nn import Embedding
 from pykeen.utils import (
-    clamp_norm, compact_mapping, compose, flatten_dictionary, get_until_first_blank, l2_regularization, torch_is_in_1d,
+    clamp_norm, compact_mapping, compose, empty, flatten_dictionary, get_until_first_blank, l2_regularization,
+    torch_is_in_1d,
 )
 
 
@@ -268,3 +270,12 @@ def test_torch_is_in_1d():
                 invert=invert,
             )
             assert (result == expected_result).all()
+
+
+class TestOther(unittest.TestCase):
+    """Test remaining functions."""
+
+    def test_empty(self):
+        """Test the :func:`empty` function."""
+        self.assertTrue(empty(iter([])))
+        self.assertFalse(empty(iter([1])))
