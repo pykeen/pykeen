c9b240e89f666400c0ce4dfbc125a8980013887b
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-12-17 08:39:38 +0100

    extract common code

--- src/pykeen/nn/compute_kernel.py
+++ src/pykeen/nn/compute_kernel.py
@@ -130,16 +130,21 @@ def _complex_select(
     return h_re @ t_re.transpose(-2, -1) - h_im @ t_im.transpose(-2, -1)
 
 
+def _complex_to_stacked(h, r, t):
+    (r_re, r_im), (t_re, t_im) = [split_complex(x=x) for x in (r, t)]
+    h = torch.cat([h, h], dim=-1)  # re im re im
+    r = torch.cat([r_re, r_re, r_im, r_im], dim=-1)  # re re im im
+    t = torch.cat([t_re, t_im, t_im, t_re], dim=-1)  # re im im re
+    return h, r, t
+
+
 def _complex_stacked(
     h: torch.FloatTensor,
     r: torch.FloatTensor,
     t: torch.FloatTensor,
 ) -> torch.FloatTensor:
     """Stack vectors."""
-    (r_re, r_im), (t_re, t_im) = [split_complex(x=x) for x in (r, t)]
-    h = torch.cat([h, h], dim=-1)  # re im re im
-    r = torch.cat([r_re, r_re, r_im, r_im], dim=-1)  # re re im im
-    t = torch.cat([t_re, t_im, t_im, t_re], dim=-1)  # re im im re
+    h, r, t = _complex_to_stacked(h, r, t)
     return (h * r * t).sum(dim=-1)
 
 
@@ -149,10 +154,7 @@ def _complex_stacked_select(
     t: torch.FloatTensor,
 ) -> torch.FloatTensor:
     """Stack vectors and select order."""
-    (r_re, r_im), (t_re, t_im) = [split_complex(x=x) for x in (r, t)]
-    h = torch.cat([h, h], dim=-1)  # re im re im
-    r = torch.cat([r_re, r_re, r_im, r_im], dim=-1)  # re re im im
-    t = torch.cat([t_re, t_im, t_im, t_re], dim=-1)  # re im im re
+    h, r, t = _complex_to_stacked(h, r, t)
     hr_cost = numpy.prod([max(hs, rs) for hs, rs in zip(h.shape, r.shape)])
     rt_cost = numpy.prod([max(ts, rs) for ts, rs in zip(t.shape, r.shape)])
     if hr_cost < rt_cost:
