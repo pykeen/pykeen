fb7483c37fdb41e0e1621d6c80e0aa80e84b25ee
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-01-25 18:17:08 +0100

    add more functionality to CSVResultTracker

--- src/pykeen/trackers/file.py
+++ src/pykeen/trackers/file.py
@@ -1,8 +1,11 @@
 """Tracking results in local files."""
-
+import csv
+import datetime
 import logging
 import pathlib
 from typing import Any, Mapping, Optional, TextIO, Union
+
+import pystow
 
 from .base import ResultTracker
 
@@ -11,6 +14,13 @@ __all__ = [
 ]
 
 logger = logging.getLogger(__name__)
+
+
+def _format_key(key: str, prefix: Optional[str] = None) -> str:
+    """Prepend prefix is necessary."""
+    if prefix is None:
+        return key
+    return f"{prefix}.{key}"
 
 
 class CSVResultTracker(ResultTracker):
@@ -23,20 +33,34 @@ class CSVResultTracker(ResultTracker):
         tail -f results.txt | grep "hits_at_10"
     """
 
+    #: The column names
+    HEADER = ("type", "step", "key", "value")
+
     #: The file where the results are written to.
     file: TextIO
 
-    def __init__(self, path: Union[None, str, pathlib.Path] = None):
+    def __init__(
+        self,
+        path: Union[None, str, pathlib.Path] = None,
+        **kwargs,
+    ):
+        """
+        Initialize the tracker.
+
+        :param path:
+            The path of the log file.
+        :param kwargs:
+            Additional keyword based arguments forwarded to csv.writer.
+        """
         if path is None:
-            path = "results.txt"
-        self.path = pathlib.Path(path)
-        self.path.parent.mkdir(exist_ok=True, parents=True)
-        self.file = self.path.open(mode="w")
+            path = pystow.get("logs", f"{datetime.datetime.now().isoformat()}.csv")
+        logger.info(f"Logging to {path.as_uri()}.")
+        path.parent.mkdir(exist_ok=True, parents=True)
+        self.file = path.open("w", newline="", encoding="utf8")
+        self.csv_writer = csv.writer(self.file, **kwargs)
 
     def start_run(self, run_name: Optional[str] = None) -> None:  # noqa: D102
-        logger.info(f"Logging results for {run_name} to {self.path.as_uri()}.")
-        # write header
-        self.file.writelines(["type,step,key,value\n"])
+        self.csv_writer.writerow(self.HEADER)
 
     def end_run(self) -> None:  # noqa: D102
         self.file.close()
@@ -46,12 +70,10 @@ class CSVResultTracker(ResultTracker):
         params: Mapping[str, Any],
         prefix: Optional[str] = None,
     ) -> None:  # noqa: D102
-        lines = []
-        for key, value in params.items():
-            if prefix is not None:
-                key = f"{prefix}.{key}"
-            lines.append(",".join(("parameter", 0, key, value)) + "\n")
-        self.file.writelines(lines)
+        self.csv_writer.writerows(
+            ("parameter", 0, _format_key(key=key, prefix=prefix), value)
+            for key, value in params.items()
+        )
         self.file.flush()
 
     def log_metrics(
@@ -60,10 +82,8 @@ class CSVResultTracker(ResultTracker):
         step: Optional[int] = None,
         prefix: Optional[str] = None,
     ) -> None:  # noqa: D102
-        lines = []
-        for key, value in metrics.items():
-            if prefix is not None:
-                key = f"{prefix}.{key}"
-            lines.append(",".join(("metric", step, key, value)) + "\n")
-        self.file.writelines(lines)
+        self.csv_writer.writerows(
+            ("metric", step, _format_key(key=key, prefix=prefix), value)
+            for key, value in metrics.items()
+        )
         self.file.flush()
