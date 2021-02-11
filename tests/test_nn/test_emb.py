480aa4f93ddfbe31ebadd51636256693fc86294f
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-02-11 17:40:41 +0100

    Add test for consistency for backwards compatible attributes

--- tests/test_nn/test_emb.py
+++ tests/test_nn/test_emb.py
@@ -11,3 +11,8 @@ class EmbeddingTests(RepresentationTestCase):
         num_embeddings=7,
         embedding_dim=13,
     )
+
+    def test_backwards_compatibility(self):
+        """Test shape and num_embeddings."""
+        assert self.instance.max_id == self.instance.num_embeddings
+        assert self.instance.shape == (self.instance.embedding_dim,)
