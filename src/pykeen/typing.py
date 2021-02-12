9910025e18624276f973a6205455a8362baed533
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-12 20:25:51 +0100

    Update typing.py
    Trigger CI
    
    Co-Authored-By: Stephen Bonner <10208489+sbonner0@users.noreply.github.com>

--- src/pykeen/typing.py
+++ src/pykeen/typing.py
@@ -12,21 +12,20 @@ __all__ = [
     'Hint',
     'Mutation',
     'OneOrSequence',
-    # Others
+    # Triples
     'LabeledTriples',
     'MappedTriples',
     'EntityMapping',
     'RelationMapping',
+    # Others
+    'DeviceHint',
+    'TorchRandomHint',
     # Tensor Functions
     'Initializer',
     'Normalizer',
     'Constrainer',
     'cast_constrainer',
-    'InteractionFunction',
-    'DeviceHint',
-    'TorchRandomHint',
     # Tensors
-    'TensorType',
     'HeadRepresentation',
     'RelationRepresentation',
     'TailRepresentation',
@@ -46,18 +45,12 @@ MappedTriples = torch.LongTensor
 EntityMapping = Mapping[str, int]
 RelationMapping = Mapping[str, int]
 
-#: A type variable bound by :class:`torch.Tensor` or :class:`torch.FloatTensor`.
-TensorType = TypeVar("TensorType", torch.Tensor, torch.FloatTensor)
-#: An interaction function takes in head tensor(s), relation tensor(s), and tail tensor(s)
-#: then returns a score
-InteractionFunction = Callable[[TensorType, TensorType, TensorType], TensorType]
-
 #: A function that can be applied to a tensor to initialize it
-Initializer = Mutation[TensorType]
+Initializer = Mutation[torch.FloatTensor]
 #: A function that can be applied to a tensor to normalize it
-Normalizer = Mutation[TensorType]
+Normalizer = Mutation[torch.FloatTensor]
 #: A function that can be applied to a tensor to constrain it
-Constrainer = Mutation[TensorType]
+Constrainer = Mutation[torch.FloatTensor]
 
 
 def cast_constrainer(f) -> Constrainer:
