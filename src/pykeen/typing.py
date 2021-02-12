1d28264ec37ba5e6fb260391506549c4cbb053ac
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-12 20:18:17 +0100

    Update typing.py
    Co-Authored-By: Stephen Bonner <10208489+sbonner0@users.noreply.github.com>

--- src/pykeen/typing.py
+++ src/pykeen/typing.py
@@ -17,6 +17,7 @@ __all__ = [
     'MappedTriples',
     'EntityMapping',
     'RelationMapping',
+    # Tensor Functions
     'Initializer',
     'Normalizer',
     'Constrainer',
@@ -24,6 +25,8 @@ __all__ = [
     'InteractionFunction',
     'DeviceHint',
     'TorchRandomHint',
+    # Tensors
+    'TensorType',
     'HeadRepresentation',
     'RelationRepresentation',
     'TailRepresentation',
@@ -34,6 +37,7 @@ __all__ = [
 
 X = TypeVar('X')
 Hint = Union[None, str, X]
+#: A function that mutates the input and returns a new object of the same type as output
 Mutation = Callable[[X], X]
 OneOrSequence = Union[X, Sequence[X]]
 
@@ -42,12 +46,17 @@ MappedTriples = torch.LongTensor
 EntityMapping = Mapping[str, int]
 RelationMapping = Mapping[str, int]
 
-# comment: TypeVar expects none, or at least two super-classes
+#: A type variable bound by :class:`torch.Tensor` or :class:`torch.FloatTensor`.
 TensorType = TypeVar("TensorType", torch.Tensor, torch.FloatTensor)
+#: An interaction function takes in head tensor(s), relation tensor(s), and tail tensor(s)
+#: then returns a score
 InteractionFunction = Callable[[TensorType, TensorType, TensorType], TensorType]
 
+#: A function that can be applied to a tensor to initialize it
 Initializer = Mutation[TensorType]
+#: A function that can be applied to a tensor to normalize it
 Normalizer = Mutation[TensorType]
+#: A function that can be applied to a tensor to constrain it
 Constrainer = Mutation[TensorType]
 
 
@@ -64,7 +73,11 @@ TorchRandomHint = Hint[torch.Generator]
 #: A type variable for head representations used in :class:`pykeen.models.Model`,
 #: :class:`pykeen.nn.modules.Interaction`, etc.
 HeadRepresentation = TypeVar("HeadRepresentation", bound=OneOrSequence[torch.FloatTensor])
+#: A type variable for relation representations used in :class:`pykeen.models.Model`,
+#: :class:`pykeen.nn.modules.Interaction`, etc.
 RelationRepresentation = TypeVar("RelationRepresentation", bound=OneOrSequence[torch.FloatTensor])
+#: A type variable for tail representations used in :class:`pykeen.models.Model`,
+#: :class:`pykeen.nn.modules.Interaction`, etc.
 TailRepresentation = TypeVar("TailRepresentation", bound=OneOrSequence[torch.FloatTensor])
 
 
