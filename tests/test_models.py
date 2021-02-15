22e32c0ae64945ba043b71f4397599a469d8e556
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-15 16:14:05 +0100

    Update test_models.py
    Trigger CI

--- tests/test_models.py
+++ tests/test_models.py
@@ -14,7 +14,7 @@ import pykeen.experiments
 import pykeen.models
 from pykeen.models import (
     EntityEmbeddingModel, EntityRelationEmbeddingModel, Model, MultimodalModel, _MODELS,
-    _OldAbstractModel,
+    _OldAbstractModel, _NewAbstractModel, ERModel
 )
 from pykeen.models.predict import get_novelty_mask, predict
 from pykeen.models.unimodal.rgcn import (
@@ -31,10 +31,12 @@ from tests.constants import EPSILON
 SKIP_MODULES = {
     Model.__name__,
     _OldAbstractModel.__name__,
+    _NewAbstractModel.__name__,
     'DummyModel',
     MultimodalModel.__name__,
     EntityEmbeddingModel.__name__,
     EntityRelationEmbeddingModel.__name__,
+    ERModel.__name__,
     'MockModel',
     'models',
     'get_model_cls',
