8a87840d598eee041cde86c6e8f53e89edd3d155
mali <ali-mehdi@live.de>
2021-02-23 10:45:09 +0100

    Encapsulate logic into own function

--- src/pykeen/experiments/cli.py
+++ src/pykeen/experiments/cli.py
@@ -12,6 +12,7 @@ from uuid import uuid4
 
 import click
 from pykeen.ablation import prepare_ablation_from_path
+from pykeen.ablation.ablation import _run_ablation_experiments, _create_path_with_id
 
 __all__ = [
     'experiments',
@@ -196,31 +197,16 @@ def ablation(
 
     A sample file can be run with ``pykeen experiment ablation tests/resources/hpo_complex_nations.json``.
     """
-    datetime = time.strftime('%Y-%m-%d-%H-%M')
-    directory = os.path.join(directory, f'{datetime}_{uuid4()}')
+    directory = _create_path_with_id(directory=directory)
     directories = prepare_ablation_from_path(path=path, directory=directory, save_artifacts=save_artifacts)
 
-    if dry_run:
-        return
-
-    from pykeen.hpo import hpo_pipeline_from_path
-
-    for output_directory, rv_config_path in directories:
-        hpo_pipeline_result = hpo_pipeline_from_path(rv_config_path)
-        hpo_pipeline_result.save_to_directory(output_directory)
-
-        if not best_replicates:
-            continue
-
-        best_pipeline_dir = os.path.join(output_directory, 'best_pipeline')
-        os.makedirs(best_pipeline_dir, exist_ok=True)
-        logger.info('Re-training best pipeline and saving artifacts in %s', best_pipeline_dir)
-        hpo_pipeline_result.replicate_best_pipeline(
-            replicates=best_replicates,
-            move_to_cpu=move_to_cpu,
-            save_replicates=not discard_replicates,
-            directory=best_pipeline_dir,
-        )
+    _run_ablation_experiments(
+        directories=directories,
+        best_replicates=best_replicates,
+        dry_run=dry_run,
+        move_to_cpu=move_to_cpu,
+        discard_replicates=discard_replicates
+    )
 
 
 if __name__ == '__main__':
