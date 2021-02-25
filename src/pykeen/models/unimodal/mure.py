6a73e1ac94aa0db424a5ef52defba540ad9f7014
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-02-25 10:21:36 +0100

    Add settings from github.com/ibalazevic/multirelational-poincare

--- src/pykeen/models/unimodal/mure.py
+++ src/pykeen/models/unimodal/mure.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Implementation of MuRE."""
-
+import functools
 from typing import Any, ClassVar, Mapping
+
+import torch
 
 from ..nbase import ERModel
 from ...constants import DEFAULT_EMBEDDING_HPO_EMBEDDING_DIM_RANGE
@@ -10,6 +12,7 @@ from ...nn import EmbeddingSpecification
 from ...nn.init import xavier_normal_
 from ...nn.modules import MuREInteraction
 from ...typing import Hint, Initializer
+from ...utils import compose
 
 __all__ = [
     'MuRE',
@@ -47,26 +50,41 @@ class MuRE(ERModel):
         :param p: The $l_p$ norm. Defaults to 2.
         :param power_norm: Should the power norm be used? Defaults to true.
         """
+        # comment:
+        # https://github.com/ibalazevic/multirelational-poincare/blob/34523a61ca7867591fd645bfb0c0807246c08660/model.py#L52
+        # uses float64
         super().__init__(
             interaction=MuREInteraction(p=p, power_norm=power_norm),
             entity_representations=[
                 EmbeddingSpecification(
                     embedding_dim=embedding_dim,
-                    initializer=entity_initializer,
+                    initializer=compose(
+                        torch.nn.init.normal_,
+                        lambda x: 1.0e-03 * x,
+                    ),
                 ),
+                # entity bias for head
                 EmbeddingSpecification(
                     embedding_dim=1,
-                    initializer=entity_initializer,
+                    initializer=torch.nn.init.zeros_,
+                ),
+                # entity bias for tail
+                EmbeddingSpecification(
+                    embedding_dim=1,
+                    initializer=torch.nn.init.zeros_,
                 ),
             ],
             relation_representations=[
                 EmbeddingSpecification(
                     embedding_dim=embedding_dim,
-                    initializer=relation_initializer,
+                    initializer=compose(
+                        torch.nn.init.normal_,
+                        lambda x: 1.0e-03 * x,
+                    ),
                 ),
                 EmbeddingSpecification(
                     shape=(embedding_dim, embedding_dim),
-                    initializer=relation_initializer,
+                    initializer=functools.partial(torch.nn.init.uniform_, a=-1, b=1),
                 ),
             ],
             **kwargs,
