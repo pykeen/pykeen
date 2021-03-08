70c02642db4f5dfb368cf524d7cf59ad3613eb28
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-03-08 23:52:24 +0100

    Add unittest for broadcast_cat

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,7 +16,7 @@ import pytest
 import torch
 
 from pykeen.utils import (
-    calculate_broadcasted_elementwise_result_shape, clamp_norm, combine_complex, compact_mapping, compose,
+    broadcast_cat, calculate_broadcasted_elementwise_result_shape, clamp_norm, combine_complex, compact_mapping, compose,
     estimate_cost_of_sequence, flatten_dictionary, get_optimal_sequence, get_until_first_blank, project_entity,
     set_random_seed, split_complex, tensor_product, tensor_sum, torch_is_in_1d,
 )
@@ -355,3 +355,17 @@ class TestUtils(unittest.TestCase):
 
             # compare result to sequential addition
             assert torch.allclose(result, functools.reduce(operator.mul, tensors[1:], tensors[0]))
+
+    def test_broadcast_cat(self):
+        """Test broadcast_cat."""
+        generator = set_random_seed(seed=42)[1]
+        for shapes in _generate_shapes(generator=generator):
+            tensors = [torch.rand(*shape) for shape in shapes]
+
+            for dim in range(len(tensors[0].shape)):
+                result = broadcast_cat(tensors, dim=dim)
+                # check result shape
+                assert result.shape == tuple(
+                    sum(dims) if i == dim else max(dims)
+                    for i, dims in enumerate(zip(*shapes))
+                )
