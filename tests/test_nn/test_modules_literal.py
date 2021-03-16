9606a584d33105690753e29b63db8aada24b755a
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-03-16 15:00:04 +0100

    some fixes to testing

--- tests/test_nn/test_modules_literal.py
+++ tests/test_nn/test_modules_literal.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Tests for literal interaction functions."""
+from typing import Any, MutableMapping
 
 import torch
 from torch import nn
@@ -10,34 +11,26 @@ from pykeen.nn.modules import ComplExInteraction, LiteralInteraction
 from pykeen.triples.triples_numeric_literals_factory import TriplesNumericLiteralsFactory
 from tests import cases
 
-nations_literal = NationsLiteral()
-
-
-class MockLiteralInteraction(LiteralInteraction):
-    """A mock literal interaction around ComplEx."""
-
-    def __init__(self, embedding_dim: int, dropout: float = 0.1):
-        base = ComplExInteraction()
-
-        # TODO this doesnt make sense
-        triples_factory: TriplesNumericLiteralsFactory = nations_literal.training
-        combination = nn.Sequential(
-            nn.Linear(embedding_dim + triples_factory.numeric_literals.shape[1], embedding_dim),
-            nn.Dropout(dropout),
-        )
-        super().__init__(
-            base=base,
-            combination=combination,
-        )
-
 
 class LiteralTests(cases.InteractionTestCase):
     """Tests for LiteralInteraction function."""
 
-    cls = MockLiteralInteraction
-    kwargs = {
-        'embedding_dim': cases.InteractionTestCase.dim,
-    }
+    cls = LiteralInteraction
+    kwargs = dict(
+        base=ComplExInteraction(),
+    )
+
+    def _pre_instantiation_hook(self, kwargs: MutableMapping[str, Any]) -> MutableMapping[str, Any]:  # noqa: D102
+        kwargs = super()._pre_instantiation_hook(kwargs=kwargs)
+        # TODO this doesnt make sense
+        triples_factory: TriplesNumericLiteralsFactory = NationsLiteral().training
+        extra_dim = triples_factory.numeric_literals.shape[1]
+        kwargs["combination"] = nn.Sequential(
+            nn.Linear(self.dim + extra_dim, self.dim),
+            nn.Dropout(0.1),
+        )
+        self.shape_kwargs["e"] = extra_dim
+        return kwargs
 
     def _exp_score(self, h, r, t) -> torch.FloatTensor:  # noqa: D102
         return self.instance.base(
