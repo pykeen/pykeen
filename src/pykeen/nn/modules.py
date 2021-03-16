7e0d5c61cc2f80115e5ab1e5d8707bb464085e65
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-03-16 18:53:27 +0100

    adjust MuRPInteraction's relation_shape

--- src/pykeen/nn/modules.py
+++ src/pykeen/nn/modules.py
@@ -1117,7 +1117,7 @@ class MuRPInteraction(
 
     # there are separate biases for entities in head and tail position
     entity_shape = ("d", "", "")
-    relation_shape = ("d", "dd")
+    relation_shape = ("d", "d")
     func = pkf.murp_interaction
 
     @staticmethod
