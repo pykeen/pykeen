33d8dbcc47dff04f66afff379d9ff1b10e8b217a
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-16 17:30:22 +0100

    Refactor redundant code in calculation of clamped square norms

--- src/pykeen/utils.py
+++ src/pykeen/utils.py
@@ -1002,18 +1002,22 @@ def p_log_map(v):
 
 def full_p_exp_map(x, v):
     normv = torch.clamp(torch.norm(v, 2, dim=-1, keepdim=True), min=1e-10)
-    sqxnorm = torch.clamp(torch.sum(x * x, dim=-1, keepdim=True), 0, 1 - 1e-5)
+    sqxnorm = _sqnorm(x)
     y = torch.tanh(normv / (1 - sqxnorm)) * v / normv
     return p_sum(x, y)
 
 
 def p_sum(x, y):
-    sqxnorm = torch.clamp(torch.sum(x * x, dim=-1, keepdim=True), 0, 1 - 1e-5)
-    sqynorm = torch.clamp(torch.sum(y * y, dim=-1, keepdim=True), 0, 1 - 1e-5)
+    sqxnorm = _sqnorm(x)
+    sqynorm = _sqnorm(y)
     dotxy = torch.sum(x * y, dim=-1, keepdim=True)
     numerator = (1 + 2 * dotxy + sqynorm) * x + (1 - sqxnorm) * y
     denominator = 1 + 2 * dotxy + sqxnorm * sqynorm
     return numerator / denominator
+
+
+def _sqnorm(x: torch.FloatTensor) -> torch.FloatTensor:
+    return x.pow(2).sum(dim=-1, keepdim=True).clamp_max(1 - 1e-5)
 
 
 if __name__ == '__main__':
