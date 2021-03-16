428cb1cc2fefebb574b802330e0633501ba3d820
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-16 15:10:00 +0100

    Update combinations.py

--- src/pykeen/models/multimodal/combinations.py
+++ src/pykeen/models/multimodal/combinations.py
@@ -27,26 +27,18 @@ class DistMultCombination(nn.Sequential):
         super().__init__(linear, dropout)
 
 
-class ComplExLiteralCombination(nn.Module):
-    """Separately transform real and imaginary part."""
+class ComplexCombination(nn.Module):
+    """A generalized combination for models using complex tensors."""
 
     def __init__(
         self,
+        real: nn.Module,
+        imag: nn.Module,
         embedding_dim: int,
-        num_of_literals: int,
-        input_dropout: float = 0.0,
     ):
         super().__init__()
-        self.real = nn.Sequential(
-            nn.Dropout(input_dropout),
-            nn.Linear(embedding_dim + num_of_literals, embedding_dim),
-            torch.nn.Tanh(),
-        )
-        self.imag = nn.Sequential(
-            nn.Dropout(input_dropout),
-            nn.Linear(embedding_dim + num_of_literals, embedding_dim),
-            torch.nn.Tanh(),
-        )
+        self.real = real
+        self.imag = imag
         # TODO: Determine this automatically
         self.embedding_dim = 2 * embedding_dim
 
@@ -60,3 +52,25 @@ class ComplExLiteralCombination(nn.Module):
         x_re = self.real(torch.cat([x_re, literal], dim=-1))
         x_im = self.imag(torch.cat([x_im, literal], dim=-1))
         return combine_complex(x_re=x_re, x_im=x_im)
+
+
+class ComplExLiteralCombination(ComplexCombination):
+    """Separately transform real and imaginary part."""
+
+    def __init__(
+        self,
+        embedding_dim: int,
+        num_of_literals: int,
+        input_dropout: float = 0.0,
+    ):
+        real = nn.Sequential(
+            nn.Dropout(input_dropout),
+            nn.Linear(embedding_dim + num_of_literals, embedding_dim),
+            torch.nn.Tanh(),
+        )
+        imag = nn.Sequential(
+            nn.Dropout(input_dropout),
+            nn.Linear(embedding_dim + num_of_literals, embedding_dim),
+            torch.nn.Tanh(),
+        )
+        super().__init__(real=real, imag=imag, embedding_dim=embedding_dim)
