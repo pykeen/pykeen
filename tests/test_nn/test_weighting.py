5474a0e4ada6c4ada16f734e5efd58326dec9ee0
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-22 22:12:13 +0100

    Fix weighting test
    Trigger CI

--- tests/test_nn/test_weighting.py
+++ tests/test_nn/test_weighting.py
@@ -1,22 +1,24 @@
+# -*- coding: utf-8 -*-
+
 """Tests for edge weightings."""
 
 import pykeen.nn.weighting
-from tests.cases import EdgeWeightingTestCase
+from tests import cases
 
 
-class InverseInDegreeEdgeWeightingTests(EdgeWeightingTestCase):
+class InverseInDegreeEdgeWeightingTests(cases.EdgeWeightingTestCase):
     """Tests for inverse in-degree weighting."""
 
     cls = pykeen.nn.weighting.InverseInDegreeEdgeWeighting
 
 
-class InverseOutDegreeEdgeWeightingTests(EdgeWeightingTestCase):
+class InverseOutDegreeEdgeWeightingTests(cases.EdgeWeightingTestCase):
     """Tests for inverse out-degree weighting."""
 
     cls = pykeen.nn.weighting.InverseOutDegreeEdgeWeighting
 
 
-class SymmetricEdgeWeightingTests(EdgeWeightingTestCase):
+class SymmetricEdgeWeightingTests(cases.EdgeWeightingTestCase):
     """Tests for symmetric weighting."""
 
     cls = pykeen.nn.weighting.SymmetricEdgeWeighting
