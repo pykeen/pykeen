4a1245728325cdcb6cdb68919144317a8ef77d5e
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-04-11 23:20:37 +0200

    Make the version table generator more reusable

--- src/pykeen/version.py
+++ src/pykeen/version.py
@@ -3,6 +3,7 @@
 """Version information for PyKEEN."""
 
 import os
+import sys
 from subprocess import CalledProcessError, check_output  # noqa: S404
 
 __all__ = [
@@ -42,6 +43,25 @@ def get_version(with_git_hash: bool = False) -> str:
     :return: The PyKEEN version as well as the git hash, if the parameter with_git_hash was set to true.
     """
     return f'{VERSION}-{get_git_hash()}' if with_git_hash else VERSION
+
+
+def env_table(tablefmt='github', headers=('Key', 'Value')) -> str:
+    """Generate a table describing the environment in which PyKEEN is being run."""
+    import torch
+    import platform
+    from tabulate import tabulate
+    t1 = [
+        ('`os.name`', os.name),
+        ('`platform.system()`', platform.system()),
+        ('`platform.release()`', platform.release()),
+        ('python', f'{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}'),
+        ('pykeen', get_version(with_git_hash=True)),
+        ('torch', torch.__version__),
+        ('cuda available', str(torch.cuda.is_available()).lower()),
+        ('cuda', torch.version.cuda),
+        ('cudnn', torch.backends.cudnn.version()),
+    ]
+    return tabulate(t1, tablefmt=tablefmt, headers=headers)
 
 
 if __name__ == '__main__':
