c3dcf34f8e554604db3110e1cade9cd76ac06f86
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-04-21 13:24:38 +0200

    Add another note

--- src/pykeen/datasets/analysis.py
+++ src/pykeen/datasets/analysis.py
@@ -616,6 +616,11 @@ def relation_cardinality_classification(
         .. math ::
             1 = \sum_{type} conf(relation, type)
 
+    .. note ::
+       These relation types are also mentioned in TransH [wang2014]_. However, the paper does not provide any details on
+       their definition, nor is any code provided. Thus, their exact procedure is unknown and may not coincide with this
+       implementation.
+
     :param dataset:
         The dataset to investigate.
     :param parts:
