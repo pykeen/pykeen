e77415225784ef11da9d2eabd6587c99d70ffdb1
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-04-26 19:23:26 +0200

    ✂️ 🏭 Excise the triples factory from the model (#405)
    * Start excising training triples factory from model
    
    Trigger CI
    
    Suggested by @migalkin
    
    * Update base.py
    
    Trigger CI
    
    * Finish removing triples factory from model
    
    Trigger CI
    
    * Try to pass fast tests
    
    Trigger CI
    
    * Update triples_factory.py
    
    * Update test_utils.py
    
    Trigger CI
    
    * Deal with mlflow testing issue
    
    * Add missing kwargs
    
    * Get entity and relation ids
    
    Trigger CI
    
    * Update test_model_mode.py
    
    Trigger CI
    
    * Update test_model_mode.py
    
    Trigger CI
    
    * Fix doctests
    
    Trigger CI
    
    * Update making_predictions.rst
    
    * Update making_predictions.rst
    
    * Rename attribute
    
    * Rename and retype
    
    * Remove redundant properties
    
    Trigger CI
    
    * Update name
    
    Trigger CI
    
    Related to #398
    
    * Update making_predictions.rst
    
    Trigger CI
    
    * Change exception to warning
    
    Trigger CI

--- src/pykeen/evaluation/evaluator.py
+++ src/pykeen/evaluation/evaluator.py
@@ -131,7 +131,7 @@ class Evaluator(ABC):
     def evaluate(
         self,
         model: Model,
-        mapped_triples: Optional[MappedTriples] = None,
+        mapped_triples: MappedTriples,
         batch_size: Optional[int] = None,
         slice_size: Optional[int] = None,
         device: Optional[torch.device] = None,
@@ -139,11 +139,9 @@ class Evaluator(ABC):
         tqdm_kwargs: Optional[Mapping[str, str]] = None,
         restrict_entities_to: Optional[torch.LongTensor] = None,
         do_time_consuming_checks: bool = True,
+        additional_filter_triples: Optional[MappedTriples] = None,
     ) -> MetricResults:
         """Run :func:`pykeen.evaluation.evaluate` with this evaluator."""
-        if mapped_triples is None:
-            mapped_triples = model.triples_factory.mapped_triples
-
         if batch_size is None and self.automatic_memory_optimization:
             # Using automatic memory optimization on CPU may result in undocumented crashes due to OS' OOM killer.
             if model.device.type == 'cpu':
@@ -155,6 +153,7 @@ class Evaluator(ABC):
                 batch_size, slice_size = self.batch_and_slice(
                     model=model,
                     mapped_triples=mapped_triples,
+                    additional_filter_triples=additional_filter_triples,
                     batch_size=batch_size,
                     device=device,
                     use_tqdm=False,
@@ -170,6 +169,7 @@ class Evaluator(ABC):
 
         rv = evaluate(
             model=model,
+            additional_filtered_triples=additional_filter_triples,
             mapped_triples=mapped_triples,
             evaluators=self,
             batch_size=batch_size,
@@ -193,6 +193,7 @@ class Evaluator(ABC):
         use_tqdm: bool = False,
         restrict_entities_to: Optional[torch.LongTensor] = None,
         do_time_consuming_checks: bool = True,
+        additional_filter_triples: Optional[MappedTriples] = None,
     ) -> Tuple[int, Optional[int]]:
         """Find the maximum possible batch_size and slice_size for evaluation with the current setting.
 
@@ -216,6 +217,8 @@ class Evaluator(ABC):
             Should a progress bar be displayed?
         :param restrict_entities_to:
             Whether to restrict the evaluation to certain entities of interest.
+        :param additional_filter_triples:
+            Only needed if the evaluator is in filtered mode
 
         :return:
             Maximum possible batch size and, if necessary, the slice_size, which defaults to None.
@@ -227,6 +230,7 @@ class Evaluator(ABC):
             key='batch_size',
             start_value=batch_size,
             model=model,
+            additional_filter_triples=additional_filter_triples,
             mapped_triples=mapped_triples,
             device=device,
             use_tqdm=use_tqdm,
@@ -244,6 +248,7 @@ class Evaluator(ABC):
             # must have failed to start slice_size search, we start with trying half the entities.
             start_value=ceil(model.num_entities / 2),
             model=model,
+            additional_filter_triples=additional_filter_triples,
             mapped_triples=mapped_triples,
             device=device,
             use_tqdm=use_tqdm,
@@ -265,6 +270,7 @@ class Evaluator(ABC):
         use_tqdm: bool = False,
         restrict_entities_to: Optional[torch.LongTensor] = None,
         do_time_consuming_checks: bool = True,
+        additional_filter_triples: Optional[MappedTriples] = None,
     ) -> Tuple[int, bool]:
         values_dict = {}
         maximum_triples = mapped_triples.shape[0]
@@ -295,6 +301,7 @@ class Evaluator(ABC):
                 torch.cuda.empty_cache()
                 evaluate(
                     model=model,
+                    additional_filtered_triples=additional_filter_triples,
                     mapped_triples=mapped_triples,
                     evaluators=self,
                     only_size_probing=True,
@@ -350,7 +357,7 @@ class Evaluator(ABC):
     @staticmethod
     def _check_slicing_availability(model: Model, batch_size: int) -> None:
         # Test if slicing is implemented for the required functions of this model
-        if model.triples_factory.create_inverse_triples:
+        if model.use_inverse_triples:
             if not model.can_slice_t:
                 raise MemoryError(f"The current model can't be evaluated on this hardware with these parameters, as "
                                   f"evaluation batch_size={batch_size} is too big and slicing is not implemented for "
@@ -473,6 +480,7 @@ def evaluate(
     tqdm_kwargs: Optional[Mapping[str, str]] = None,
     restrict_entities_to: Optional[torch.LongTensor] = None,
     do_time_consuming_checks: bool = True,
+    additional_filtered_triples: Optional[MappedTriples] = None,
 ) -> Union[MetricResults, List[MetricResults]]:
     """Evaluate metrics for model on mapped triples.
 
@@ -512,6 +520,8 @@ def evaluate(
         Whether to perform some time consuming checks on the provided arguments. Currently, this encompasses:
         - If restrict_entities_to is not None, check whether the triples have been filtered.
         Disabling this option can accelerate the method.
+    :param additional_filtered_triples:
+        Only needed if the evaluator is in filtered mode
     """
     if isinstance(evaluators, Evaluator):  # upgrade a single evaluator to a list
         evaluators = [evaluators]
@@ -547,7 +557,14 @@ def evaluate(
 
     # Prepare for result filtering
     if filtering_necessary or positive_masks_required:
-        all_pos_triples = torch.cat([model.triples_factory.mapped_triples, mapped_triples], dim=0)
+        if additional_filtered_triples is None:
+            logger.warning(
+                'filtered setting was enabled, but there were no `additional_filtered_triples`.'
+                ' This means you probably forgot to pass the training triples.',
+            )
+            all_pos_triples = mapped_triples
+        else:
+            all_pos_triples = torch.cat([additional_filtered_triples, mapped_triples], dim=0)
         all_pos_triples = all_pos_triples.to(device=device)
     else:
         all_pos_triples = None
@@ -586,7 +603,7 @@ def evaluate(
             relation_filter = None
             for column in (0, 2):
                 relation_filter = _evaluate_batch(
-                    batch=batch,
+                    batch=batch,  # TODO fix typing
                     model=model,
                     column=column,
                     filtered_evaluators=filtered_evaluators,
