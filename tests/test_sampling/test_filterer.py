f43fc4999f96613963a5711419c6e053a896132a
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-05-04 10:54:46 +0200

    Update tests for filterer

--- tests/test_sampling/test_filterer.py
+++ tests/test_sampling/test_filterer.py
@@ -26,10 +26,9 @@ class FiltererTest(unittest_templates.GenericTestCase[Filterer]):
         return kwargs
 
     def post_instantiation_hook(self) -> None:  # noqa: D102
-        self.slcwa_instances = self.triples_factory.create_slcwa_instances()
-        self.positive_batch = self.slcwa_instances.mapped_triples[torch.randint(
+        self.positive_batch = self.triples_factory.mapped_triples[torch.randint(
             low=0,
-            high=len(self.slcwa_instances),
+            high=self.triples_factory.num_triples,
             size=(self.batch_size,),
             generator=self.generator,
         )]
@@ -38,11 +37,11 @@ class FiltererTest(unittest_templates.GenericTestCase[Filterer]):
         """Test the filter method."""
         # Check whether filtering works correctly
         # First giving an example where all triples have to be filtered
-        _, batch_filter = self.instance(negative_batch=self.positive_batch)
+        batch_filter = self.instance(negative_batch=self.positive_batch)
         # The filter should remove all triples
         assert batch_filter.sum() == 0
         # Create an example where no triples will be filtered
-        _, batch_filter = self.instance(
+        batch_filter = self.instance(
             negative_batch=(self.positive_batch + self.triples_factory.num_entities),
         )
         # The filter should not remove any triple
