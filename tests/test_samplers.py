8909f85984d1804e24267a375988479f56700abb
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-11 10:02:24 -0400

    Reorganize into generic test case

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -3,10 +3,11 @@
 """Test that samplers can be executed."""
 
 import unittest
-from typing import ClassVar, Type
+from typing import Any, MutableMapping
 
 import numpy
 import torch
+import unittest_templates
 
 from pykeen.datasets import Nations
 from pykeen.sampling import BasicNegativeSampler, BernoulliNegativeSampler, NegativeSampler
@@ -23,51 +24,49 @@ def _array_check_bounds(
     return (low <= array).all() and (array < high).all()
 
 
-class _NegativeSamplingTestCase:
+class _NegativeSamplingTestCase(unittest_templates.GenericTestCase[NegativeSampler]):
     """A test case for quickly defining common tests for samplers."""
 
     #: The batch size
-    batch_size: int
+    batch_size: int = 16
     #: The random seed
-    seed: int
+    seed: int = 42
+
+    kwargs = {
+        'num_negs_per_pos': 10
+    }
+
     #: The triples factory
     triples_factory: TriplesFactory
     #: The instances
     training_instances: Instances
-    #: Class of negative sampling to test
-    cls: ClassVar[Type[NegativeSampler]]
-    #: The negative sampler instance, initialized in setUp
-    instance: NegativeSampler
     #: A positive batch
     positive_batch: torch.LongTensor
 
-    def setUp(self) -> None:
-        """Set up the test case with a triples factory and model."""
-        self.batch_size = 16
-        self.seed = 42
-        self.num_negs_per_pos = 10
+    def pre_setup_hook(self) -> None:
         self.triples_factory = Nations().training
         self.training_instances = self.triples_factory.create_slcwa_instances()
-        self.instance = self.cls(
-            triples_factory=self.triples_factory,
-            num_negs_per_pos=self.num_negs_per_pos,
-        )
-        random = numpy.random.RandomState(seed=self.seed)
-        batch_indices = random.randint(low=0, high=len(self.training_instances), size=(self.batch_size,))
+        random_state = numpy.random.RandomState(seed=self.seed)
+        batch_indices = random_state.randint(low=0, high=len(self.training_instances), size=(self.batch_size,))
         self.positive_batch = self.training_instances.mapped_triples[batch_indices]
+
+    def _pre_instantiation_hook(self, kwargs: MutableMapping[str, Any]) -> MutableMapping[str, Any]:  # noqa: D102
+        kwargs = super()._pre_instantiation_hook(kwargs=kwargs)
+        kwargs['triples_factory'] = self.triples_factory
+        return kwargs
 
     def test_sample(self) -> None:
         # Generate negative sample
         negative_batch, batch_filter = self.instance.sample(positive_batch=self.positive_batch)
         if self.instance.filterer is not None:
             assert batch_filter is not None
-            assert batch_filter.shape == (self.batch_size * self.num_negs_per_pos,)
+            assert batch_filter.shape == (self.batch_size * self.instance.num_negs_per_pos,)
             assert batch_filter.dtype == torch.bool
         else:
             assert batch_filter is None
 
         # check shape
-        assert negative_batch.shape[0] == self.positive_batch.shape[0] * self.num_negs_per_pos
+        assert negative_batch.shape[0] == self.positive_batch.shape[0] * self.instance.num_negs_per_pos
         assert negative_batch.shape[1] == self.positive_batch.shape[1]
 
         # check bounds: heads
@@ -85,8 +84,8 @@ class _NegativeSamplingTestCase:
 
     def _update_positive_batch(self, positive_batch, batch_filter):
         # cf. slcwa training loop, mr loss helper
-        if self.num_negs_per_pos > 1:
-            positive_batch = positive_batch.repeat(self.num_negs_per_pos, 1)
+        if self.instance.num_negs_per_pos > 1:
+            positive_batch = positive_batch.repeat(self.instance.num_negs_per_pos, 1)
 
         if batch_filter is not None:
             positive_batch = positive_batch[batch_filter]
@@ -97,7 +96,7 @@ class _NegativeSamplingTestCase:
         self.instance.sample(positive_batch=self.positive_batch[:1])
 
 
-class BasicNegativeSamplerTest(_NegativeSamplingTestCase, unittest.TestCase):
+class BasicNegativeSamplerTest(_NegativeSamplingTestCase):
     """Test the basic negative sampler."""
 
     cls = BasicNegativeSampler
@@ -125,7 +124,7 @@ class BasicNegativeSamplerTest(_NegativeSamplingTestCase, unittest.TestCase):
         assert half_size - 1 <= num_subj_corrupted
 
 
-class BernoulliNegativeSamplerTest(_NegativeSamplingTestCase, unittest.TestCase):
+class BernoulliNegativeSamplerTest(_NegativeSamplingTestCase):
     """Test the Bernoulli negative sampler."""
 
     cls = BernoulliNegativeSampler
