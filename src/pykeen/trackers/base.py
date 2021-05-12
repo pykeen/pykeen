11cdbc04e60b7229ae5cdc11c33a9b7fd597d451
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-12 09:45:28 -0400

    Add configurable writer

--- src/pykeen/trackers/base.py
+++ src/pykeen/trackers/base.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """Utilities and base classes for PyKEEN tracker adapters."""
+
+import logging
 import re
 from typing import Any, Mapping, Optional, Pattern, Union
 
@@ -48,49 +50,59 @@ class ConsoleResultTracker(ResultTracker):
     def __init__(
         self,
         *,
-        print_parameters: bool = True,
+        track_parameters: bool = True,
         parameter_filter: Union[None, str, Pattern[str]] = None,
-        print_metrics: bool = True,
+        track_metrics: bool = True,
         metric_filter: Union[None, str, Pattern[str]] = None,
         start_end_run: bool = False,
+        writer: str = 'tqdm',
     ):
         """
         Initialize the tracker.
 
-        :param print_parameters:
+        :param track_parameters:
             Whether to print parameters.
         :param parameter_filter:
             A regular expression to filter parameters. If None, print all parameters.
-        :param print_metrics:
+        :param track_metrics:
             Whether to print metrics.
         :param metric_filter:
             A regular expression to filter metrics. If None, print all parameters.
         :param start_end_run:
             Whether to print start/end run messages.
+        :param writer: 
+            The writer to use - one of "tqdm", "builtin", or "logger".
         """
         self.start_end_run = start_end_run
 
-        self.print_parameters = print_parameters
+        self.track_parameters = track_parameters
         if isinstance(parameter_filter, str):
             parameter_filter = re.compile(parameter_filter)
         self.parameter_filter = parameter_filter
 
-        self.print_metrics = print_metrics
+        self.track_metrics = track_metrics
         if isinstance(metric_filter, str):
             metric_filter = re.compile(metric_filter)
         self.metric_filter = metric_filter
 
+        if writer == 'tqdm':
+            self.write = tqdm.write
+        elif writer == 'builtin':
+            self.write = print
+        elif writer == 'logging':
+            self.write = logging.getLogger('pykeen').info
+
     def start_run(self, run_name: Optional[str] = None) -> None:  # noqa: D102
         if run_name is not None and self.start_end_run:
-            tqdm.write(f"Starting run: {run_name}")
+            self.write(f"Starting run: {run_name}")
 
     def log_params(self, params: Mapping[str, Any], prefix: Optional[str] = None) -> None:  # noqa: D102
-        if not self.print_metrics:
+        if not self.track_parameters:
             return
 
         for key, value in flatten_dictionary(dictionary=params).items():
             if not self.parameter_filter or self.parameter_filter.match(key):
-                tqdm.write(f"Parameter: {key} = {value}")
+                self.write(f"Parameter: {key} = {value}")
 
     def log_metrics(
         self,
@@ -98,14 +110,14 @@ class ConsoleResultTracker(ResultTracker):
         step: Optional[int] = None,
         prefix: Optional[str] = None,
     ) -> None:  # noqa: D102
-        if not self.print_metrics:
+        if not self.track_metrics:
             return
 
-        tqdm.write(f"Step: {step}")
+        self.write(f"Step: {step}")
         for key, value in flatten_dictionary(dictionary=metrics, prefix=prefix).items():
             if not self.metric_filter or self.metric_filter.match(key):
-                tqdm.write(f"Parameter: {key} = {value}")
+                self.write(f"Parameter: {key} = {value}")
 
     def end_run(self) -> None:  # noqa: D102
         if self.start_end_run:
-            tqdm.write("Finished run.")
+            self.write("Finished run.")
