578e672bc01126f9153ce467c9a60fb9db66f211
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-12 08:18:20 -0400

    🔋 📉 Update negative sampling (#427)
    * Add negative sampler to SLCWA instances
    
    * update slcwa instances
    
    * Add batch type annotation
    
    * Add batch type annotation to training loop
    
    * Add batch type annotation to lcwa training loop
    
    * update slcwa training loop and instance generation
    
    * Update negative sampler class name normalization
    
    Replace negative sampler specific implementation of class name normalization by the utility from class-resolver
    
    * Update docstring of NegativeSampler.sample
    
    * move filtering to base class
    
    * rename method
    
    * update filtering API
    
    Return filter mask, but do not filter negatives. this is required for sub-batching filtered negatives
    
    * Update tests for filterer
    
    * Add collate to NegativeSampler
    
    * Return negative batch in 3d shape
    
    * fix sub-batching for filtered negative samples
    
    * Move negative sampler to batch collator
    
    * update type annotation of collate
    
    * Configurable collator in training loop
    
    * Use mapped triples instead of triples factory in filterer
    
    * update training loop type annotation
    
    * "fix" cyclic import
    
    * actually fix the cyclic import
    
    * fix create instances for SLCWA training loop
    
    * Fix NaNTrainingLoop batch type
    
    * update create_slcwa_instances for numeric triples factory
    
    * Add comment
    
    Trigger CI
    
    * Update triples_numeric_literals_factory.py
    
    * Update tests
    
    * Update typing
    
    * Trigger CI
    
    * Update negative_sampler.py
    
    * Update typing
    
    * Update negative_sampler.py
    
    * Add abstract method for getitem
    
    * Update instances.py
    
    * Update tox.ini
    
    * Update variable names and types for test sampler case
    
    * Unify interface for creating training instances
    
    * Unify triples creation
    
    * Update test_samplers.py
    
    * Update hpo.py
    
    * Remove unnecessary code
    
    * Revert negative sampler
    
    * Revert collator stuff
    
    * Revert
    
    * Update filtering.py
    
    * Revert slcwa
    
    * Other reverts
    
    * Move type vars
    
    * One more move
    
    * Add explicit lcwa sample type
    
    * Check for the batch filter
    
    * Begin switching to unittest-template style
    
    * update test with code from slcwa
    
    * add more batch_filter checks
    
    * Reuse positive batch processing
    
    * Reorganize into generic test case
    
    * Reorganize sampler tests
    
    * Update mocks.py
    
    Trigger CI
    
    * Update test_filterer.py
    
    * Reorganize redundant code
    
    * Update test_negative_samplers.py
    
    * Trigger CI
    
    * Update tests/test_sampling/cases.py
    
    Trigger CI
    
    Co-authored-by: Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
    
    Co-authored-by: Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
    Co-authored-by: PyKEEN_bot <pykeen2019@gmail.com>

---
+++ tests/test_sampling/cases.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+"""Test cases for sampling."""
+
+from typing import Any, MutableMapping
+
+import numpy
+import torch
+import unittest_templates
+
+from pykeen.datasets import Nations
+from pykeen.sampling import NegativeSampler
+from pykeen.triples import Instances, TriplesFactory
+
+__all__ = [
+    'NegativeSamplerGenericTestCase',
+]
+
+
+def _array_check_bounds(
+    array: torch.LongTensor,
+    low: int,
+    high: int,
+) -> bool:
+    """Check if all elements lie in bounds."""
+    return (low <= array).all() and (array < high).all()
+
+
+class NegativeSamplerGenericTestCase(unittest_templates.GenericTestCase[NegativeSampler]):
+    """A test case for quickly defining common tests for samplers."""
+
+    #: The batch size
+    batch_size: int = 16
+    #: The random seed
+    seed: int = 42
+    #: The triples factory
+    triples_factory: TriplesFactory
+    #: The instances
+    training_instances: Instances
+    #: A positive batch
+    positive_batch: torch.LongTensor
+    #: Kwargs
+    kwargs = {
+        'num_negs_per_pos': 10,
+    }
+
+    def pre_setup_hook(self) -> None:
+        """Set up the test case with a triples factory, training instances, and a default positive batch."""
+        self.triples_factory = Nations().training
+        self.training_instances = self.triples_factory.create_slcwa_instances()
+        random_state = numpy.random.RandomState(seed=self.seed)
+        batch_indices = random_state.randint(low=0, high=len(self.training_instances), size=(self.batch_size,))
+        self.positive_batch = self.training_instances.mapped_triples[batch_indices]
+
+    def _pre_instantiation_hook(self, kwargs: MutableMapping[str, Any]) -> MutableMapping[str, Any]:  # noqa: D102
+        kwargs = super()._pre_instantiation_hook(kwargs=kwargs)
+        kwargs['triples_factory'] = self.triples_factory
+        return kwargs
+
+    def test_sample(self) -> None:
+        """Test generating a negative sample."""
+        # Generate negative sample
+        negative_batch, batch_filter = self.instance.sample(positive_batch=self.positive_batch)
+        if self.instance.filterer is not None:
+            assert batch_filter is not None
+            assert batch_filter.shape == (self.batch_size * self.instance.num_negs_per_pos,)
+            assert batch_filter.dtype == torch.bool
+        else:
+            assert batch_filter is None
+
+        # check shape
+        assert negative_batch.shape[0] == self.positive_batch.shape[0] * self.instance.num_negs_per_pos
+        assert negative_batch.shape[1] == self.positive_batch.shape[1]
+
+        # check bounds: heads
+        assert _array_check_bounds(negative_batch[:, 0], low=0, high=self.triples_factory.num_entities)
+
+        # check bounds: relations
+        assert _array_check_bounds(negative_batch[:, 1], low=0, high=self.triples_factory.num_relations)
+
+        # check bounds: tails
+        assert _array_check_bounds(negative_batch[:, 2], low=0, high=self.triples_factory.num_entities)
+
+        positive_batch = self._update_positive_batch(self.positive_batch, batch_filter)
+        # test that the relations were not changed by the negative sampler
+        assert (positive_batch[:, 1] == negative_batch[:, 1]).all()
+
+        assert (negative_batch != positive_batch).any(dim=1).all()
+
+    def _update_positive_batch(self, positive_batch, batch_filter):
+        # cf. slcwa training loop, mr loss helper
+        if self.instance.num_negs_per_pos > 1:
+            positive_batch = positive_batch.repeat(self.instance.num_negs_per_pos, 1)
+
+        if batch_filter is not None:
+            positive_batch = positive_batch[batch_filter]
+        return positive_batch
+
+    def test_small_batch(self):
+        """Test on a small batch."""
+        self.instance.sample(positive_batch=self.positive_batch[:1])
