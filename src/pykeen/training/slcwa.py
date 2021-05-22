35dcdaeadbcb999f2ee2386c6a705ee818fc088a
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-05-22 18:14:20 -0400

    Properly slice positive filter based on subbatch
    Trigger CI

--- src/pykeen/training/slcwa.py
+++ src/pykeen/training/slcwa.py
@@ -91,7 +91,8 @@ class SLCWATrainingLoop(TrainingLoop[SLCWASampleType, SLCWABatchType]):
         positive_batch = positive_batch[start:stop].to(device=self.device)
         negative_batch = negative_batch[start:stop]
         if positive_filter is not None:
-            negative_batch = negative_batch[positive_filter[start:stop]]
+            positive_filter = positive_filter[start:stop]
+            negative_batch = negative_batch[positive_filter]
         # Make it negative batch broadcastable (required for num_negs_per_pos > 1).
         negative_batch = negative_batch.view(-1, 3)
 
