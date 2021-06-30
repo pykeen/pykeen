e66ad00eeaf32db00f8d1d3f3fc567af6247a794
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-06-30 14:33:10 +0200

    Add significantly worse entity co-occurrence baseline (ignoring relations)

--- src/pykeen/models/baseline.py
+++ src/pykeen/models/baseline.py
@@ -12,6 +12,11 @@ from pykeen.models import Model
 from pykeen.triples import CoreTriplesFactory
 
 
+def _get_max_id(triples_factory: CoreTriplesFactory, index: int) -> int:
+    """Get the number of entities or relations, depending on the selected column index."""
+    return triples_factory.num_relations if index == 1 else triples_factory.num_entities
+
+
 def _get_csr_matrix(
     triples_factory: CoreTriplesFactory,
     col_index: int,
@@ -20,9 +25,11 @@ def _get_csr_matrix(
 ) -> scipy.sparse.csr_matrix:
     """Create a co-occurrence matrix from triples."""
     row, col = triples_factory.mapped_triples.T[[row_index, col_index]]
+    num_rows = _get_max_id(triples_factory=triples_factory, index=row_index)
+    num_columns = _get_max_id(triples_factory=triples_factory, index=col_index)
     matrix = scipy.sparse.coo_matrix(
         (numpy.ones(triples_factory.num_triples), (row, col)),
-        shape=(triples_factory.num_relations, triples_factory.num_entities),
+        shape=(num_rows, num_columns),
     ).tocsr()
     if normalize:
         matrix = sklearn_normalize(matrix, norm="l1", axis=1)
@@ -49,6 +56,44 @@ class PseudoTypeBaseline(Model):
     def score_h(self, rt_batch: torch.LongTensor) -> torch.FloatTensor:
         r = rt_batch[:, 0].cpu().numpy()
         return torch.from_numpy(self.head_per_relation[r].todense())
+
+    def _reset_parameters_(self):
+        # TODO: this is not needed for non-parametric models!
+        raise NotImplementedError
+
+    def score_hrt(self, hrt_batch: torch.LongTensor) -> torch.FloatTensor:
+        # TODO: this is not needed for evaluation
+        raise NotImplementedError
+
+    def score_r(self, ht_batch: torch.LongTensor) -> torch.FloatTensor:
+        # TODO: this is not needed for evaluation
+        raise NotImplementedError
+
+    def collect_regularization_term(self) -> torch.FloatTensor:
+        # TODO: this is not needed for non-parametric models!
+        raise NotImplementedError
+
+
+class EntityCoOccurrenceBaseline(Model):
+    """Score based on entity-entity co-occurrence."""
+
+    def __init__(
+        self,
+        triples_factory: CoreTriplesFactory,
+        normalize: bool = False,
+    ):
+        super().__init__(triples_factory=triples_factory)
+        self.head_per_tail = _get_csr_matrix(triples_factory=triples_factory, row_index=2, col_index=0)
+        self.tail_per_head = _get_csr_matrix(triples_factory=triples_factory, row_index=0, col_index=2)
+        self.normalize = normalize
+
+    def score_t(self, hr_batch: torch.LongTensor) -> torch.FloatTensor:
+        h = hr_batch[:, 0].cpu().numpy()
+        return torch.from_numpy(self.tail_per_head[h].todense())
+
+    def score_h(self, rt_batch: torch.LongTensor) -> torch.FloatTensor:
+        t = rt_batch[:, 1].cpu().numpy()
+        return torch.from_numpy(self.head_per_tail[t].todense())
 
     def _reset_parameters_(self):
         # TODO: this is not needed for non-parametric models!
