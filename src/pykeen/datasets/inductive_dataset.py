f81d00865723dfba6310f35ce3ec7e954d1a4c7c
mali <ali-mehdi@live.de>
2021-08-05 12:31:21 +0200

    Fix lazy loading

--- src/pykeen/datasets/inductive_dataset.py
+++ src/pykeen/datasets/inductive_dataset.py
@@ -7,22 +7,22 @@ from typing import Optional, Mapping
 from pykeen.triples import CoreTriplesFactory, TriplesFactory
 
 
-class InductiveDataset():
+class InductiveDataset:
     #: A factory wrapping the transductive training triples
-    transductive_training: CoreTriplesFactory
+    _transductive_training: CoreTriplesFactory = None
     #: A factory wrapping the transductive testing triples, that share indices with the training triples
-    transductive_testing: Optional[CoreTriplesFactory]
+    _transductive_testing: Optional[CoreTriplesFactory] = None
     #: A factory wrapping the transductive validation triples, that share indices with the training triples
-    transductive_validation: Optional[CoreTriplesFactory]
+    _transductive_validation: Optional[CoreTriplesFactory] = None
     #: All datasets should take care of inverse triple creation
     create_inverse_triples: bool
 
     #: A factory wrapping the inductive inference triples
-    inductive_inference: CoreTriplesFactory
+    _inductive_inference: CoreTriplesFactory = None
     #: A factory wrapping the inductive testing triples
-    inductive_testing: CoreTriplesFactory
+    _inductive_testing: CoreTriplesFactory = None
     #: #: A factory wrapping the inductive validation inference triples
-    inductive_validation: Optional[CoreTriplesFactory]
+    _inductive_validation: Optional[CoreTriplesFactory] = None
 
     @property
     def factory_dict(self) -> Mapping[str, CoreTriplesFactory]:
@@ -45,8 +45,8 @@ class InductiveDataset():
         """The transductive training triples factory."""
         if not self._loaded:
             self._load()
-        assert self.transductive_training is not None
-        return self.transductive_training
+        assert self._transductive_training is not None
+        return self._transductive_training
 
     @property
     def transductive_testing(self) -> TriplesFactory:  # type:ignore # noqa: D401
@@ -55,7 +55,7 @@ class InductiveDataset():
             self._load()
         if not self._loaded_transductive_testing:
             self._load_transductive_testing()
-        return self.transductive_testing
+        return self._transductive_testing
 
     @property
     def transductive_validation(self) -> TriplesFactory:  # type:ignore # noqa: D401
@@ -64,51 +64,51 @@ class InductiveDataset():
             self._load()
         if not self._loaded_transductive_validation:
             self._load_transductive_validation()
-        return self.transductive_validation
+        return self._transductive_validation
 
     @property
     def inductive_inference(self) -> TriplesFactory:  # type:ignore # noqa: D401
         """The inductive inference triples factory."""
         if not self._loaded:
             self._load()
-        assert self.inductive_inference is not None
-        return self.inductive_inferenceg
+        assert self._inductive_inference is not None
+        return self._inductive_inference
 
     @property
     def inductive_validation(self) -> TriplesFactory:  # type:ignore # noqa: D401
-        """The inductive testing triples factory."""
+        """The testing triples factory that shares indices with the inductive inference triples factory."""
         if not self._loaded:
             self._load()
         if not self._loaded_inductive_validation:
             self._load_inductive_validation()
-        return self.inductive_validation
+        return self._inductive_validation
 
     @property
     def inductive_testing(self) -> TriplesFactory:  # type:ignore # noqa: D401
-        """The inductive testing triples factory."""
+        """The testing triples factory that shares indices with the inductiv inference triples factory."""
         if not self._loaded:
             self._load()
-        assert self.inductive_testing is not None
-        return self.inductive_testing
+        assert self._inductive_testing is not None
+        return self._inductive_testing
 
     @property
     def _loaded(self) -> bool:
-        is_transductive_training = self.transductive_training is not None
-        is_inductive_inference = self.inductive_inference is not None
-        is_inductive_testing = self.inductive_testing is not None
+        is_transductive_training = self._transductive_training is not None
+        is_inductive_inference = self._inductive_inference is not None
+        is_inductive_testing = self._inductive_testing is not None
         return is_transductive_training and is_inductive_inference and is_inductive_testing
 
     @property
     def _loaded_transductive_testing(self) -> bool:
-        return self.transductive_training is not None
+        return self._transductive_testing is not None
 
     @property
     def _loaded_transductive_validation(self) -> bool:
-        return self.transductive_validation is not None
+        return self._transductive_validation is not None
 
     @property
     def _loaded_inductive_validation(self) -> bool:
-        return self.inductive_validation is not None
+        return self._inductive_validation is not None
 
     def _load(self) -> None:
         NotImplementedError
