f32dd33d51d7b10f89a35a4bac182391fae5286d
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-13 17:36:25 +0200

    Update datasets to use generics

--- src/pykeen/triples/instances.py
+++ src/pykeen/triples/instances.py
@@ -3,7 +3,6 @@
 """Implementation of basic instance factory which creates just instances based on standard KG triples."""
 
 from abc import ABC
-from dataclasses import dataclass
 from typing import Generic, Mapping, Optional, Tuple, TypeVar
 
 import numpy as np
@@ -12,7 +11,6 @@ import torch
 from torch.utils import data
 
 from ..typing import MappedTriples
-from ..utils import fix_dataclass_init_docs
 
 __all__ = [
     'Instances',
@@ -30,16 +28,11 @@ SLCWASampleType = TypeVar('SLCWASampleType', bound=MappedTriples)
 SLCWABatchType = Tuple[MappedTriples, MappedTriples, Optional[torch.BoolTensor]]
 
 
-@fix_dataclass_init_docs
-@dataclass
-class Instances(data.Dataset, Generic[BatchType], ABC):
+class Instances(data.Dataset[BatchType], Generic[BatchType], ABC):
     """Triples and mappings to their indices."""
 
     def __len__(self):  # noqa:D401
         """The number of instances."""
-        raise NotImplementedError
-
-    def __getitem__(self, item: int) -> BatchType:  # noqa: D105
         raise NotImplementedError
 
     @classmethod
@@ -56,15 +49,16 @@ class Instances(data.Dataset, Generic[BatchType], ABC):
         raise NotImplementedError
 
 
-@fix_dataclass_init_docs
-@dataclass
 class SLCWAInstances(Instances[MappedTriples]):
     """Triples and mappings to their indices for sLCWA."""
 
     #: The mapped triples, shape: (num_triples, 3)
     mapped_triples: MappedTriples
 
-    def __len__(self):  # noqa: D105
+    def __init__(self, *, mapped_triples: MappedTriples):
+        self.mapped_triples = mapped_triples
+
+    def __len__(self) -> int:  # noqa: D105
         return self.mapped_triples.shape[0]
 
     def __getitem__(self, item: int) -> MappedTriples:  # noqa: D105
@@ -75,8 +69,6 @@ class SLCWAInstances(Instances[MappedTriples]):
         return cls(mapped_triples=mapped_triples)
 
 
-@fix_dataclass_init_docs
-@dataclass
 class LCWAInstances(Instances[LCWABatchType]):
     """Triples and mappings to their indices for LCWA."""
 
@@ -85,6 +77,10 @@ class LCWAInstances(Instances[LCWABatchType]):
 
     #: The compressed triples in CSR format
     compressed: scipy.sparse.csr_matrix
+
+    def __init__(self, *, pairs, compressed) -> None:
+        self.pairs = pairs
+        self.compressed = compressed
 
     @classmethod
     def from_triples(cls, mapped_triples: MappedTriples, num_entities: int) -> Instances:
@@ -118,23 +114,29 @@ class LCWAInstances(Instances[LCWABatchType]):
         return self.pairs[item], np.asarray(self.compressed[item, :].todense())[0, :]
 
 
-@fix_dataclass_init_docs
-@dataclass
-class MultimodalInstances(Instances):
+class MultimodalInstances:
     """Triples and mappings to their indices as well as multimodal data."""
 
     #: TODO: do we need these?
     numeric_literals: Mapping[str, np.ndarray]
     literals_to_id: Mapping[str, int]
 
+    def __init__(self, *, numeric_literals, literals_to_id):
+        self.numeric_literals = numeric_literals
+        self.literals_to_id = literals_to_id
 
-@fix_dataclass_init_docs
-@dataclass
-class MultimodalSLCWAInstances(SLCWAInstances, MultimodalInstances):
+
+class MultimodalSLCWAInstances(MultimodalInstances, SLCWAInstances):
     """Triples and mappings to their indices as well as multimodal data for sLCWA."""
 
+    def __init__(self, *, mapped_triples: MappedTriples, numeric_literals, literals_to_id):
+        SLCWAInstances.__init__(self, mapped_triples=mapped_triples)
+        MultimodalInstances.__init__(self, numeric_literals=numeric_literals, literals_to_id=literals_to_id)
 
-@fix_dataclass_init_docs
-@dataclass
-class MultimodalLCWAInstances(LCWAInstances, MultimodalInstances):
+
+class MultimodalLCWAInstances(MultimodalInstances, LCWAInstances):
     """Triples and mappings to their indices as well as multimodal data for LCWA."""
+
+    def __init__(self, *, pairs, compressed, numeric_literals, literals_to_id):
+        LCWAInstances.__init__(self, pairs=pairs, compressed=compressed)
+        MultimodalInstances.__init__(self, numeric_literals=numeric_literals, literals_to_id=literals_to_id)
