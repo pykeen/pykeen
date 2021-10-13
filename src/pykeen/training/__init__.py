5d111ee7f4fb618b3dbb99391f9a669998df341c
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-10-13 16:36:05 +0200

    automatically create training loop resolver from subclasses

--- src/pykeen/training/__init__.py
+++ src/pykeen/training/__init__.py
@@ -12,12 +12,10 @@ slcwa   :class:`pykeen.training.SLCWATrainingLoop`
 .. note:: This table can be re-generated with ``pykeen ls trainers -f rst``
 """
 
-from typing import Set, Type
-
 from class_resolver import Resolver
 
 from .callbacks import TrainingCallback  # noqa: F401
-from .lcwa import LCWATrainingLoop  # noqa: F401
+from .lcwa import LCWATrainingLoop, _LCWATrainingLoop  # noqa: F401
 from .slcwa import SLCWATrainingLoop  # noqa: F401
 from .training_loop import NonFiniteLossError, TrainingLoop  # noqa: F401
 
@@ -30,14 +28,10 @@ __all__ = [
     'TrainingCallback',
 ]
 
-_TRAINING_LOOP_SUFFIX = 'TrainingLoop'
-_TRAINING_LOOPS: Set[Type[TrainingLoop]] = {
-    LCWATrainingLoop,
-    SLCWATrainingLoop,
-}
-training_loop_resolver = Resolver(
-    _TRAINING_LOOPS,
+training_loop_resolver = Resolver.from_subclasses(
     base=TrainingLoop,  # type: ignore
     default=SLCWATrainingLoop,
-    suffix=_TRAINING_LOOP_SUFFIX,
+    skip={
+        _LCWATrainingLoop,
+    },
 )
