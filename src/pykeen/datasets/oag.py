da8440702b3d71bd754c00fbf9c2d0aa3b682f9d
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-05 21:55:19 +0100

    Update oag.py

--- src/pykeen/datasets/oag.py
+++ src/pykeen/datasets/oag.py
@@ -11,45 +11,48 @@ import zipfile
 from pathlib import Path
 from typing import Iterable, Mapping, Tuple, Union
 
+import click
 import pandas as pd
 import pystow
+from more_click import verbose_option
 from more_itertools import roundrobin
 from pystow.utils import name_from_url
 
 from pykeen.datasets.base import TabbedDataset
 
 __all__ = [
-    'OAG',
+    "OAG",
 ]
 
-BASE_URL = 'https://academicgraphv2.blob.core.windows.net/oag'
+MODULE = pystow.module("pykeen", "datasets", "oag")
+BASE_URL = "https://academicgraphv2.blob.core.windows.net/oag"
 
 # Links between the MAG (Microsoft Academic Graph) and AMiner (ArnetMiner) datasets
-VENUE_LINKING_URL = f'{BASE_URL}/linkage/venue_linking_pairs.zip'
-PAPER_LINKING_URL = f'{BASE_URL}/linkage/paper_linking_pairs.zip'
-AUTHOR_LINKING_URL = f'{BASE_URL}/linkage/author_linking_pairs.zip'
+VENUE_LINKING_URL = f"{BASE_URL}/linkage/venue_linking_pairs.zip"
+PAPER_LINKING_URL = f"{BASE_URL}/linkage/paper_linking_pairs.zip"
+AUTHOR_LINKING_URL = f"{BASE_URL}/linkage/author_linking_pairs.zip"
 
 # Venue downloads
-AMINER_VENUES_URL = f'{BASE_URL}/aminer/venue/aminer_venues.zip'
-MAG_VENUES_URL = f'{BASE_URL}/mag/venue/mag_venues.zip'
+AMINER_VENUES_URL = f"{BASE_URL}/aminer/venue/aminer_venues.zip"
+MAG_VENUES_URL = f"{BASE_URL}/mag/venue/mag_venues.zip"
 
 # Paper downloads
-MAG_PAPERS_0_URL = f'{BASE_URL}/mag/paper/mag_papers_0.zip'
-MAG_PAPERS_1_URL = f'{BASE_URL}/mag/paper/mag_papers_1.zip'
-MAG_PAPERS_2_URL = f'{BASE_URL}/mag/paper/mag_papers_2.zip'
-AMINER_PAPERS_0_URL = f'{BASE_URL}/aminer/paper/aminer_papers_0.zip'
-AMINER_PAPERS_1_URL = f'{BASE_URL}/aminer/paper/aminer_papers_1.zip'
-AMINER_PAPERS_2_URL = f'{BASE_URL}/aminer/paper/aminer_papers_2.zip'
-AMINER_PAPERS_3_URL = f'{BASE_URL}/aminer/paper/aminer_papers_3.zip'
+MAG_PAPERS_0_URL = f"{BASE_URL}/mag/paper/mag_papers_0.zip"
+MAG_PAPERS_1_URL = f"{BASE_URL}/mag/paper/mag_papers_1.zip"
+MAG_PAPERS_2_URL = f"{BASE_URL}/mag/paper/mag_papers_2.zip"
+AMINER_PAPERS_0_URL = f"{BASE_URL}/aminer/paper/aminer_papers_0.zip"
+AMINER_PAPERS_1_URL = f"{BASE_URL}/aminer/paper/aminer_papers_1.zip"
+AMINER_PAPERS_2_URL = f"{BASE_URL}/aminer/paper/aminer_papers_2.zip"
+AMINER_PAPERS_3_URL = f"{BASE_URL}/aminer/paper/aminer_papers_3.zip"
 
 # Author downloads
-MAG_AUTHORS_0_URL = f'{BASE_URL}/mag/author/mag_authors_0.zip'
-MAG_AUTHORS_1_URL = f'{BASE_URL}/mag/author/mag_authors_1.zip'
-MAG_AUTHORS_2_URL = f'{BASE_URL}/mag/author/mag_authors_2.zip'
-AMINER_AUTHORS_0_URL = f'{BASE_URL}/aminer/author/aminer_authors_0.zip'
-AMINER_AUTHORS_1_URL = f'{BASE_URL}/aminer/author/aminer_authors_1.zip'
-AMINER_AUTHORS_2_URL = f'{BASE_URL}/aminer/author/aminer_authors_2.zip'
-AMINER_AUTHORS_3_URL = f'{BASE_URL}/aminer/author/aminer_authors_3.zip'
+MAG_AUTHORS_0_URL = f"{BASE_URL}/mag/author/mag_authors_0.zip"
+MAG_AUTHORS_1_URL = f"{BASE_URL}/mag/author/mag_authors_1.zip"
+MAG_AUTHORS_2_URL = f"{BASE_URL}/mag/author/mag_authors_2.zip"
+AMINER_AUTHORS_0_URL = f"{BASE_URL}/aminer/author/aminer_authors_0.zip"
+AMINER_AUTHORS_1_URL = f"{BASE_URL}/aminer/author/aminer_authors_1.zip"
+AMINER_AUTHORS_2_URL = f"{BASE_URL}/aminer/author/aminer_authors_2.zip"
+AMINER_AUTHORS_3_URL = f"{BASE_URL}/aminer/author/aminer_authors_3.zip"
 
 
 class OAG(TabbedDataset):
@@ -61,12 +64,19 @@ class OAG(TabbedDataset):
 
 def _iterables():
     return [
-        _iter_triples(VENUE_LINKING_URL, 'mid', 'aid', 'venue'),
-        _iter_triples(AUTHOR_LINKING_URL, 'mid', 'aid', 'author'),
+        _iter_triples(VENUE_LINKING_URL, "mid", "aid", "sameVenue"),
+        _iter_triples(AUTHOR_LINKING_URL, "mid", "aid", "sameAuthor"),
     ]
 
 
+@click.command()
+@verbose_option
 def main():
+    for key, url in globals().items():
+        if key.endswith("_URL") and not key.startswith("BASE"):
+            print(key, url)
+            MODULE.ensure(url=url)
+    return
     i = 0
     for line in roundrobin(_iterables()):
         print(line)
@@ -83,7 +93,7 @@ def _iter_triples(url: str, s_key: str, o_key: str, rel: str) -> Iterable[Tuple[
 def _iter_zip_jsonl_linking(url: str) -> Iterable[Mapping[str, str]]:
     name = name_from_url(url)
     inner_path = f'{name[:-len(".zip")]}.txt'
-    path = pystow.ensure('pykeen', 'datasets', 'oag', url=url)
+    path = pystow.ensure("pykeen", "datasets", "oag", url=url)
     yield from _iter_zipped_jsonl(path, inner_path)
 
 
@@ -94,5 +104,5 @@ def _iter_zipped_jsonl(path: Union[str, Path], inner_path: str) -> Iterable[Mapp
                 yield json.loads(line)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
