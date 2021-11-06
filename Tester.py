0807e2c1901aa73f78e5e4ad01452e6bb9527b0e
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-06 12:34:55 +0100

    Apply black

--- Tester.py
+++ Tester.py
@@ -12,19 +12,31 @@ import torch
 
 if __name__ == "__main__":
     embedding_dim = 500
-    unif_init_bound = 2*np.sqrt(embedding_dim)
-    init_kw = dict(a=-1/unif_init_bound, b=1/unif_init_bound)
+    unif_init_bound = 2 * np.sqrt(embedding_dim)
+    init_kw = dict(a=-1 / unif_init_bound, b=1 / unif_init_bound)
     size_init_kw = dict(a=-1, b=1)
     triples_factory = WN18RR().training
-    BoxE = BoxEKG(triples_factory=triples_factory, embedding_dim=500, norm_order=2, tanh_map=True,
-                  entity_initializer_kwargs=init_kw, relation_initializer_kwargs=init_kw,
-                  relation_size_initializer_kwargs=size_init_kw)
+    BoxE = BoxEKG(
+        triples_factory=triples_factory,
+        embedding_dim=500,
+        norm_order=2,
+        tanh_map=True,
+        entity_initializer_kwargs=init_kw,
+        relation_initializer_kwargs=init_kw,
+        relation_size_initializer_kwargs=size_init_kw,
+    )
 
-    results = pipeline(random_seed=1000000, dataset="WN18RR", model=BoxE,
-                       training_kwargs=dict(num_epochs=300, batch_size=512, checkpoint_name='trial.pt',
-                                             checkpoint_frequency=100)
-                       , loss=pykeen.losses.NSSALoss(margin=3, adversarial_temperature=2.0, reduction='sum')
-                       , training_loop='sLCWA', negative_sampler='basic',  negative_sampler_kwargs=dict(num_negs_per_pos=150),
-                       result_tracker='json', result_tracker_kwargs=dict(name='test.json'),
-                       evaluation_kwargs=dict(batch_size=16), optimizer=torch.optim.Adam)
-
+    results = pipeline(
+        random_seed=1000000,
+        dataset="WN18RR",
+        model=BoxE,
+        training_kwargs=dict(num_epochs=300, batch_size=512, checkpoint_name="trial.pt", checkpoint_frequency=100),
+        loss=pykeen.losses.NSSALoss(margin=3, adversarial_temperature=2.0, reduction="sum"),
+        training_loop="sLCWA",
+        negative_sampler="basic",
+        negative_sampler_kwargs=dict(num_negs_per_pos=150),
+        result_tracker="json",
+        result_tracker_kwargs=dict(name="test.json"),
+        evaluation_kwargs=dict(batch_size=16),
+        optimizer=torch.optim.Adam,
+    )
