7a643b2896b3682810e649082774d7f00b940192
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-11-08 14:57:16 +0100

    fix epoch loss for loss reduction != "mean"

--- src/pykeen/training/training_loop.py
+++ src/pykeen/training/training_loop.py
@@ -661,7 +661,10 @@ class TrainingLoop(Generic[SampleType, BatchType], ABC):
                     self.lr_scheduler.step(epoch=epoch)
 
                 # Track epoch loss
-                epoch_loss = current_epoch_loss / num_training_instances
+                if self.model.loss.reduction == "mean":
+                    epoch_loss = current_epoch_loss / num_training_instances
+                else:
+                    epoch_loss = current_epoch_loss / len(train_data_loader)
                 self.losses_per_epochs.append(epoch_loss)
 
                 # Print loss information to console
