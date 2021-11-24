8b78014cb89fbcf7e58e890ce4d619e6808ce6b2
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-24 14:04:27 -0500

    Update test_cache.py
    Trigger CI

--- tests/test_datasets/test_cache.py
+++ tests/test_datasets/test_cache.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 
-"""Test caching.
-
-This module relies on :func:`pystow.utils.mock_home`, which needs to be used
-in a funny way to make sure that things are actually mocked properly.
-"""
+"""Test caching."""
 
 import unittest
 
-from pystow.utils import mock_home
+from pykeen.constants import PYKEEN_DATASETS
+from pykeen.datasets import Nations, _cached_get_dataset
 
 
 class TestDatasetCaching(unittest.TestCase):
     """Test caching."""
 
-    def test_it(self):
+    def test_caching(self):
         """Test dataset caching."""
         digest = "e3b0c44298fc1c149afbf4c8996fb924"
-        # The mock_home() is done before importing pykeen so the modules are mocked
-        # Don't add more tests in this module.
-        with mock_home():
-            from pykeen.constants import PYKEEN_DATASETS
-            from pykeen.datasets import Nations, _cached_get_dataset
+        directory = PYKEEN_DATASETS.joinpath(Nations.get_normalized_name(), "cache", digest)
+        if directory.is_dir():
+            for name in ("training", "testing", "validation"):
+                path = directory.joinpath(name).with_suffix(".pt")
+                if path.is_file():
+                    path.unlink()
+            directory.rmdir()
 
-            path = PYKEEN_DATASETS.joinpath(Nations.get_normalized_name(), "cache", digest)
-            self.assertFalse(path.is_dir())
-            _ = _cached_get_dataset("nations", {})
-            self.assertTrue(path.is_dir())
+        self.assertFalse(directory.is_dir())
+        _ = _cached_get_dataset("nations", {})
+        self.assertTrue(directory.is_dir())
