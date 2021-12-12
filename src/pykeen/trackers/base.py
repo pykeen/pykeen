fa7d2f58b908dc8e15af1a172158c253818455be
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-12-12 15:20:32 +0100

    add multi tracker

--- src/pykeen/trackers/base.py
+++ src/pykeen/trackers/base.py
@@ -4,7 +4,7 @@
 
 import logging
 import re
-from typing import Any, Mapping, Optional, Pattern, Union
+from typing import Any, Collection, Mapping, Optional, Pattern, Union
 
 from tqdm.auto import tqdm
 
@@ -127,3 +127,38 @@ class ConsoleResultTracker(ResultTracker):
             self.write("Run failed.")
         if self.start_end_run:
             self.write("Finished run.")
+
+
+class MultiTracker(ResultTracker):
+    """A result tracker which delegates to multiple different result trackers."""
+
+    def __init__(self, trackers: Collection[ResultTracker]) -> None:
+        """
+        Initialize the tracker.
+
+        :param trackers:
+            the base trackers.
+        """
+        super().__init__()
+        self.trackers = trackers
+
+    def start_run(self, run_name: Optional[str] = None) -> None:  # noqa: D102
+        for tracker in self.trackers:
+            tracker.start_run(run_name=run_name)
+
+    def log_params(self, params: Mapping[str, Any], prefix: Optional[str] = None) -> None:  # noqa: D102
+        for tracker in self.trackers:
+            tracker.log_params(params=params, prefix=prefix)
+
+    def log_metrics(
+        self,
+        metrics: Mapping[str, float],
+        step: Optional[int] = None,
+        prefix: Optional[str] = None,
+    ) -> None:  # noqa: D102
+        for tracker in self.trackers:
+            tracker.log_metrics(metrics=metrics, step=step, prefix=prefix)
+
+    def end_run(self, success: bool = True) -> None:  # noqa: D102
+        for tracker in self.trackers:
+            tracker.end_run(success=success)
