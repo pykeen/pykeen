3deeb0de18bb2cedcf5839202cb3f0fc0bdd32d4
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-12-17 13:44:33 +0100

    update MockModel

--- src/pykeen/models/mocks.py
+++ src/pykeen/models/mocks.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Mock models that return random results.
+"""Mock models that return fixed scores.
 
 These are useful for baselines.
 """
@@ -8,7 +8,6 @@ These are useful for baselines.
 import torch
 
 from .base import EntityRelationEmbeddingModel, Model
-from ..nn import EmbeddingSpecification
 from ..triples import CoreTriplesFactory
 
 __all__ = [
@@ -21,31 +20,40 @@ class MockModel(EntityRelationEmbeddingModel):
 
     hpo_default = {}
 
-    def __init__(self, *, triples_factory: CoreTriplesFactory, embedding_dim: int = 50, **_kwargs):
+    def __init__(self, *, triples_factory: CoreTriplesFactory, **_kwargs):
         super().__init__(
             triples_factory=triples_factory,
-            entity_representations=EmbeddingSpecification(embedding_dim=embedding_dim),
-            relation_representations=EmbeddingSpecification(embedding_dim=embedding_dim),
+            entity_representations=[],
+            relation_representations=[],
         )
-        num_entities = self.num_entities
-        self.scores = torch.arange(num_entities, dtype=torch.float, requires_grad=True)
-        self.num_backward_propagations = 0
+        self.num_entities = triples_factory.num_entities
+        self.num_relations = triples_factory.num_relations
 
-    def _generate_fake_scores(self, batch: torch.LongTensor) -> torch.FloatTensor:
-        """Generate fake scores s[b, i] = i of size (batch_size, num_entities)."""
-        batch_size = batch.shape[0]
-        batch_scores = self.scores.view(1, -1).repeat(batch_size, 1)
-        assert batch_scores.shape == (batch_size, self.num_entities)
-        return batch_scores
+    def _generate_fake_scores(
+        self,
+        h: torch.FloatTensor,
+        r: torch.FloatTensor,
+        t: torch.FloatTensor,
+    ) -> torch.FloatTensor:
+        """Generate fake scores."""
+        return (h * (self.num_entities * self.num_relations) + r * self.num_entities + t).requires_grad_(True)
 
     def score_hrt(self, hrt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        return self.scores[torch.randint(high=self.num_entities, size=hrt_batch.shape[:-1])].view(-1, 1)
+        return self._generate_fake_scores(*hrt_batch.t()).unsqueeze(dim=-1)
 
     def score_t(self, hr_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        return self._generate_fake_scores(batch=hr_batch)
+        return self._generate_fake_scores(
+            h=hr_batch[:, 0:1],
+            r=hr_batch[:, 1:2],
+            t=torch.arange(self.num_entities).unsqueeze(dim=0),
+        )
 
     def score_h(self, rt_batch: torch.LongTensor) -> torch.FloatTensor:  # noqa: D102
-        return self._generate_fake_scores(batch=rt_batch)
+        return self._generate_fake_scores(
+            h=torch.arange(self.num_entities).unsqueeze(dim=0),
+            r=rt_batch[:, 0:1],
+            t=rt_batch[:, 1:2],
+        )
 
     def reset_parameters_(self) -> Model:  # noqa: D102
         pass  # Not needed for unittest
