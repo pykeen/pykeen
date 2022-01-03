6e4c091679011ccc97b12c98ea74c169b457a5fc
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-01-03 20:57:58 +0100

    add example to module docstring

--- src/pykeen/models/uncertainty.py
+++ src/pykeen/models/uncertainty.py
@@ -11,6 +11,47 @@ if we run the same prediction method $k$ times, we get $k$ different predictions
 The variance of these predictions can be used as an approximation of uncertainty, where
 larger variance indicates higher uncertainty.
 
+Example:
+
+.. code-block:: python
+
+    from pykeen.pipeline import pipeline
+    from pykeen.models.predict import predict_hrt_uncertain
+
+    # train model
+    # note: as this is an example, the model is only trained for a few epochs, 
+    #       but not until convergence. In practice, you would usually first verify that
+    #       the model is sufficiently good in prediction, before looking at uncertainty scores
+    result = pipeline(dataset="nations", model="ERMLPE")
+
+    # predict triple scores with uncertainty
+    prediction_with_uncertainty = predict_hrt_uncertain(
+        model=result.model,
+        hrt_batch=result.training.mapped_triples[0:8],
+    )
+
+    # use a larger number of samples, to increase quality of uncertainty estimate
+    prediction_with_uncertainty = predict_hrt_uncertain(
+        model=result.model,
+        hrt_batch=result.training.mapped_triples[0:8],
+        num_samples=100,
+    )
+
+    # get most and least uncertain prediction on training set
+    prediction_with_uncertainty = predict_hrt_uncertain(
+        model=result.model,
+        hrt_batch=result.training.mapped_triples,
+        num_samples=100,
+    )
+    df = result.training.tensor_to_df(
+        result.training.mapped_triples,
+        score=prediction_with_uncertainty.score[:, 0],
+        uncertainty=prediction_with_uncertainty.uncertainty[:, 0],
+    )
+    print(df.nlargest(5, columns="uncertainty"))
+    print(df.nsmallest(5, columns="uncertainty"))
+
+
 [gal2016] 
   Dropout as a Bayesian Approximation: Representing Model Uncertainty in Deep Learning
   Yarin Gal, Zoubin Ghahramani
