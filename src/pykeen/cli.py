9a5057dd840c99bc9373545b622adb42257cb1ea
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-01-11 13:58:56 +0100

    Fix interaction formatting

--- src/pykeen/cli.py
+++ src/pykeen/cli.py
@@ -102,12 +102,18 @@ def _get_model_lines(*, link_fmt: Optional[str] = None):
         docdata = getattr(model_cls, "__docdata__", None)
         if docdata is None:
             raise ValueError("All models must have docdata")
-        if link_fmt and interaction_reference:
+        if link_fmt:
             model_reference = _fmt_ref(model_reference, link_fmt)
-            interaction_reference = _fmt_ref(interaction_reference, link_fmt)
         else:
             model_reference = f"`{model_reference}`"
-            interaction_reference = f"`{interaction_reference}`" if interaction_reference is not None else ""
+
+        if interaction_reference:
+            if link_fmt:
+                interaction_reference = _fmt_ref(interaction_reference, link_fmt)
+            else:
+                interaction_reference = f"`{interaction_reference}`"
+        else:
+            interaction_reference = ""
 
         name = docdata.get("name", model_cls.__name__)
         yield name, model_reference, interaction_reference, _citation(docdata)
