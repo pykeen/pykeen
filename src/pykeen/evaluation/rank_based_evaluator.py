122a43d86d945988eb1cc068757c24209ae62f89
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-01-19 10:43:00 +0100

    retry merge

--- src/pykeen/evaluation/rank_based_evaluator.py
+++ src/pykeen/evaluation/rank_based_evaluator.py
@@ -677,14 +677,14 @@ class SampledRankBasedEvaluator(RankBasedEvaluator):
             num_samples=num_negatives,
         )
 
-    def _process_scores(
+    def _update_ranks_(
         self,
-        hrt_batch: MappedTriples,
         true_scores: torch.FloatTensor,
-        scores: torch.FloatTensor,
+        all_scores: torch.FloatTensor,
         side: str,
-        dense_positive_mask: Optional[torch.FloatTensor] = None,
-    ) -> None:
+        hrt_batch: MappedTriples,
+    ) -> None:  # noqa: D102
+        # TODO: do not require to compute all scores beforehand
         columns = ["head", "relation", "tail"]
         indices = torch.as_tensor(
             pd.merge(
@@ -696,38 +696,6 @@ class SampledRankBasedEvaluator(RankBasedEvaluator):
             device=hrt_batch.device,
         )
         samples = self.head_samples if side == SIDE_HEAD else self.tail_samples
-        negative_scores = scores[torch.arange(hrt_batch.shape[0], device=hrt_batch.device), samples[indices]]
-        self._update_ranks_(true_scores=true_scores, all_scores=negative_scores, side=side)
-        self.num_entities = scores.shape[1]
-
-    def process_head_scores_(
-        self,
-        hrt_batch: MappedTriples,
-        true_scores: torch.FloatTensor,
-        scores: torch.FloatTensor,
-        dense_positive_mask: Optional[torch.FloatTensor] = None,
-    ) -> None:
-        # TODO: do not require to compute all scores beforehand
-        self._process_scores(
-            hrt_batch=hrt_batch,
-            true_scores=true_scores,
-            scores=scores,
-            dense_positive_mask=dense_positive_mask,
-            side=SIDE_HEAD,
-        )
-
-    def process_tail_scores_(
-        self,
-        hrt_batch: MappedTriples,
-        true_scores: torch.FloatTensor,
-        scores: torch.FloatTensor,
-        dense_positive_mask: Optional[torch.FloatTensor] = None,
-    ) -> None:
-        # TODO: do not require to compute all scores beforehand
-        self._process_scores(
-            hrt_batch=hrt_batch,
-            true_scores=true_scores,
-            scores=scores,
-            dense_positive_mask=dense_positive_mask,
-            side=SIDE_TAIL,
-        )
+        negative_scores = all_scores[torch.arange(hrt_batch.shape[0], device=hrt_batch.device), samples[indices]]
+        super()._update_ranks_(true_scores=true_scores, all_scores=negative_scores, side=side, hrt_batch=hrt_batch)
+        self.num_entities = all_scores.shape[1]
