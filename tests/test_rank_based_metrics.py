44222f83604ddac2fcb27b6115d441dd742293ce
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-01-26 10:48:24 +0100

    add meta test

--- tests/test_rank_based_metrics.py
+++ tests/test_rank_based_metrics.py
@@ -1,4 +1,6 @@
 """Tests for rank-based metrics."""
+import unittest_templates
+
 from pykeen.evaluation import rank_based_evaluator
 from tests import cases
 
@@ -13,3 +15,10 @@ class InverseArithmeticMeanRankTests(cases.RankBasedMetricTestCase):
     """Tests for inverse arithmetic mean rank."""
 
     cls = rank_based_evaluator.InverseArithmeticMeanRank
+
+
+class RankBasedMetricsTest(unittest_templates.MetaTestCase[rank_based_evaluator.RankBasedMetric]):
+    """Test for test coverage for rank-based metrics."""
+
+    base_cls = rank_based_evaluator.RankBasedMetric
+    base_test = cases.RankBasedMetricTestCase
