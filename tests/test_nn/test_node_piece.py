37e9a142e7d2a740c5c6382e81dc2f662592707c
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-01-28 11:04:58 +0100

    add test for bfs
    trigger ci

--- tests/test_nn/test_node_piece.py
+++ tests/test_nn/test_node_piece.py
@@ -1,4 +1,7 @@
 """Tests for node piece."""
+import numpy
+import numpy.testing
+import scipy.sparse.csgraph
 import unittest_templates
 
 import pykeen.nn.node_piece
@@ -46,6 +49,34 @@ class ScipySparseAnchorSearcherTests(cases.AnchorSearcherTestCase):
     """Tests for anchor search with scipy.sparse."""
 
     cls = pykeen.nn.node_piece.ScipySparseAnchorSearcher
+
+    def test_bfs(self):
+        """Test bfs."""
+        self.instance: pykeen.nn.node_piece.ScipySparseAnchorSearcher
+        k = 2
+        max_iter = 3
+        edge_index = numpy.stack([numpy.arange(self.num_entities - 1), numpy.arange(1, self.num_entities)])
+        adjacency = self.instance.create_adjacency(edge_index=edge_index)
+        anchors = numpy.arange(3)
+        # determine pool using anchor searcher
+        pool = self.instance.bfs(
+            anchors=anchors,
+            adjacency=adjacency,
+            max_iter=max_iter,
+            k=k,
+        )
+        # determine expected pool using shortest path distances via scipy.sparse.csgraph
+        distances = scipy.sparse.csgraph.shortest_path(
+            csgraph=adjacency,
+            directed=False,
+            return_predecessors=False,
+            unweighted=True,
+            indices=anchors,
+        )
+        k_dist = numpy.partition(distances, kth=k, axis=0)[:k, :].T.max(axis=1)
+        exp_pool = ((distances <= k_dist) & (k_dist <= max_iter)).T
+
+        numpy.testing.assert_array_equal(pool, exp_pool)
 
 
 class AnchorSearcherMetaTestCase(unittest_templates.MetaTestCase[pykeen.nn.node_piece.AnchorSearcher]):
