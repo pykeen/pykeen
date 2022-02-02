9a35f4259c6988ce1c82437e20574b9e1272b64b
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-02 14:24:49 +0100

    update classification evaluator

--- src/pykeen/evaluation/classification_evaluator.py
+++ src/pykeen/evaluation/classification_evaluator.py
@@ -3,15 +3,17 @@
 """Implementation of wrapper around sklearn metrics."""
 
 from dataclasses import dataclass, field, fields, make_dataclass
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, MutableMapping, Optional, Tuple
 
 import numpy as np
 import torch
 from dataclasses_json import dataclass_json
 
+from pykeen.constants import TARGET_TO_INDEX
+
 from .evaluator import Evaluator, MetricResults
 from .rexmex_compat import classifier_annotator
-from ..typing import MappedTriples
+from ..typing import MappedTriples, Target
 from ..utils import fix_dataclass_init_docs
 
 __all__ = [
@@ -62,8 +64,8 @@ class ClassificationMetricResults(ClassificationMetricResultsBase):  # type: ign
 class ClassificationEvaluator(Evaluator):
     """An evaluator that uses a classification metrics."""
 
-    all_scores: Dict[Tuple[Any, ...], np.ndarray]
-    all_positives: Dict[Tuple[Any, ...], np.ndarray]
+    all_scores: MutableMapping[Tuple[Target, int, int], np.ndarray]
+    all_positives: MutableMapping[Tuple[Target, int, int], np.ndarray]
 
     def __init__(self, **kwargs):
         super().__init__(
@@ -74,48 +76,29 @@ class ClassificationEvaluator(Evaluator):
         self.all_scores = {}
         self.all_positives = {}
 
-    def _process_scores(
+    def process_scores_(
         self,
-        keys: torch.LongTensor,
+        hrt_batch: MappedTriples,
+        target: Target,
+        true_scores: torch.FloatTensor,
         scores: torch.FloatTensor,
-        positive_mask: torch.FloatTensor,
-        head_side: bool,
+        dense_positive_mask: Optional[torch.FloatTensor] = None,
     ) -> None:
+        if dense_positive_mask is None:
+            raise KeyError("Sklearn evaluators need the positive mask!")
+
         # Transfer to cpu and convert to numpy
         scores = scores.detach().cpu().numpy()
-        positive_mask = positive_mask.detach().cpu().numpy()
-        keys = keys.detach().cpu().numpy()
+        dense_positive_mask = dense_positive_mask.detach().cpu().numpy()
+        remaining = [i for i in range(hrt_batch.shape[1]) if i != TARGET_TO_INDEX[target]]
+        keys = hrt_batch[:, remaining].detach().cpu().numpy()
 
         # Ensure that each key gets counted only once
         for i in range(keys.shape[0]):
             # include head_side flag into key to differentiate between (h, r) and (r, t)
-            key = (head_side,) + tuple(map(int, keys[i]))
+            key = (target,) + tuple(map(int, keys[i]))
             self.all_scores[key] = scores[i]
-            self.all_positives[key] = positive_mask[i]
-
-    def process_tail_scores_(
-        self,
-        hrt_batch: MappedTriples,
-        true_scores: torch.FloatTensor,
-        scores: torch.FloatTensor,
-        dense_positive_mask: Optional[torch.FloatTensor] = None,
-    ) -> None:  # noqa: D102
-        if dense_positive_mask is None:
-            raise KeyError("Sklearn evaluators need the positive mask!")
-
-        self._process_scores(keys=hrt_batch[:, :2], scores=scores, positive_mask=dense_positive_mask, head_side=False)
-
-    def process_head_scores_(
-        self,
-        hrt_batch: MappedTriples,
-        true_scores: torch.FloatTensor,
-        scores: torch.FloatTensor,
-        dense_positive_mask: Optional[torch.FloatTensor] = None,
-    ) -> None:  # noqa: D102
-        if dense_positive_mask is None:
-            raise KeyError("Sklearn evaluators need the positive mask!")
-
-        self._process_scores(keys=hrt_batch[:, 1:], scores=scores, positive_mask=dense_positive_mask, head_side=True)
+            self.all_positives[key] = dense_positive_mask[i]
 
     def finalize(self) -> ClassificationMetricResults:  # noqa: D102
         # Important: The order of the values of an dictionary is not guaranteed. Hence, we need to retrieve scores and
