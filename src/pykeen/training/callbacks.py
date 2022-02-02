1423fb6a8afc2d183c89e18042741c20d4ab6adb
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-02 15:58:54 +0100

    fix mypy
    trigger ci

--- src/pykeen/training/callbacks.py
+++ src/pykeen/training/callbacks.py
@@ -307,7 +307,7 @@ class MultiTrainingCallback(TrainingCallback):
             callback_kwargs = upgrade_to_sequence(callback_kwargs)
         # callback broadcasting
         if len(callback_kwargs) > 1 and len(callbacks) == 1:
-            callbacks = callbacks * len(callback_kwargs)
+            callbacks = list(callbacks) * len(callback_kwargs)
         self.callbacks = [
             callback_resolver.make(callback, kwargs) for callback, kwargs in zip(callbacks, callback_kwargs)
         ]
