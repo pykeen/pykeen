b5ccb53d74b78f035b2b00b7e3b3cdaba74bc1c3
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-04 10:34:02 +0100

    black

--- tests/test_joint_nodepiece_model.py
+++ tests/test_joint_nodepiece_model.py
@@ -1,15 +1,14 @@
+import unittest
+
+import torch
+
 from pykeen.datasets import FB15k237, Nations
 from pykeen.losses import NSSALoss
 from pykeen.models.unimodal import NodePiece
-
-import unittest
-import torch
-
 from pykeen.nn import NodePieceRepresentation, RepresentationModule
 
 
 class TestJointNodePiece(unittest.TestCase):
-
     def test_anchor_tokenizer(self):
         """
         The shape of NodePiece representation should correspond to number of anchors + 1 for AnchorTokenizer
@@ -23,15 +22,15 @@ class TestJointNodePiece(unittest.TestCase):
             triples_factory=dataset.training,
             random_seed=42,
             num_tokens=num_tokens,
-            tokenizers=['anchor', 'relation'],
+            tokenizers=["anchor", "relation"],
             tokenizers_kwargs=[
                 dict(
                     selection="DegreeAnchorSelection",
                     searcher="ScipySparseAnchorSearcher",
                     selection_kwargs=dict(num_anchors=num_anchors),
                 ),
-                dict()
-            ]
+                dict(),
+            ],
         )
 
         assert isinstance(model.entity_representations[0], NodePieceRepresentation)
