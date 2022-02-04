73edddf747987f23c22fa5242a5569363282d7fd
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-04 22:44:02 +0100

    🍸✌️ Mixed tokenization for NodePiece (#770)
    Pour one out for NodePiece 🍸 
    
    Co-authored-by: migalkin <mikhgalkin@gmail.com>
    Co-authored-by: Charles Tapley Hoyt <cthoyt@gmail.com>

--- tests/cases.py
+++ tests/cases.py
@@ -1373,6 +1373,11 @@ class BaseNodePieceTest(ModelTestCase):
     cls = pykeen.models.NodePiece
     create_inverse_triples = True
 
+    def _help_test_cli(self, args):  # noqa: D102
+        if self.instance_kwargs.get("tokenizers_kwargs"):
+            raise SkipTest("No support for tokenizers_kwargs via CLI.")
+        return super()._help_test_cli(args)
+
 
 class RepresentationTestCase(GenericTestCase[RepresentationModule]):
     """Common tests for representation modules."""
@@ -1827,14 +1832,14 @@ class TokenizerTestCase(GenericTestCase[pykeen.nn.node_piece.Tokenizer]):
 
     def test_call(self):
         """Test __call__."""
-        total_num_tokens, tokens = self.instance(
+        vocabulary_size, tokens = self.instance(
             mapped_triples=self.factory.mapped_triples,
             num_tokens=self.num_tokens,
             num_entities=self.factory.num_entities,
             num_relations=self.factory.num_relations,
         )
-        assert isinstance(total_num_tokens, int)
-        assert total_num_tokens > 0
+        assert isinstance(vocabulary_size, int)
+        assert vocabulary_size > 0
         # shape
         assert tokens.shape == (self.factory.num_entities, self.num_tokens)
         # value range
@@ -1842,3 +1847,22 @@ class TokenizerTestCase(GenericTestCase[pykeen.nn.node_piece.Tokenizer]):
         # no repetition, except padding idx
         for row in tokens.tolist():
             self.assertDictEqual({k: v for k, v in Counter(row).items() if k >= 0 and v > 1}, {}, msg="duplicate token")
+
+
+class NodePieceTestCase(RepresentationTestCase):
+    """General test case for node piece representations."""
+
+    cls = pykeen.nn.node_piece.NodePieceRepresentation
+    num_entities: ClassVar[int] = 8
+    num_relations: ClassVar[int] = 7
+    num_triples: ClassVar[int] = 31
+
+    def _pre_instantiation_hook(self, kwargs: MutableMapping[str, Any]) -> MutableMapping[str, Any]:  # noqa: D102
+        kwargs = super()._pre_instantiation_hook(kwargs=kwargs)
+        kwargs["triples_factory"] = generation.generate_triples_factory(
+            num_entities=self.num_entities,
+            num_relations=self.num_relations,
+            num_triples=self.num_triples,
+            create_inverse_triples=False,
+        )
+        return kwargs
