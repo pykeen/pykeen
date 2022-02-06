f347ffb347117751d3ba4bdd9caf4f37b4d3a914
migalkin <mikhgalkin@gmail.com>
2022-02-06 18:20:49 -0500

    added _ConcatMLP to avoid cyclical imports error for InductiveNodePiece

--- src/pykeen/models/inductive/inductive_nodepiece.py
+++ src/pykeen/models/inductive/inductive_nodepiece.py
@@ -3,13 +3,14 @@
 """A wrapper which combines an interaction function with NodePiece entity representations."""
 
 import logging
-from typing import Any, Callable, ClassVar, Mapping, Optional, Sequence
+from typing import Any, Callable, ClassVar, Mapping, Optional, Sequence, Union
 
 import torch
 from class_resolver import Hint, HintOrType
+from torch import nn
 
-from .node_piece import _ConcatMLP
 from ..nbase import ERModel, _prepare_representation_module_list
+# from ..unimodal.node_piece import _ConcatMLP  # commented due to circular imports errors
 from ...constants import DEFAULT_EMBEDDING_HPO_EMBEDDING_DIM_RANGE
 from ...nn import (
     DistMultInteraction,
@@ -26,6 +27,48 @@ __all__ = [
 ]
 
 logger = logging.getLogger(__name__)
+
+class _ConcatMLP(nn.Sequential):
+    """A 2-layer MLP with ReLU activation and dropout applied to the concatenation of token representations.
+
+    This is for conveniently choosing a configuration similar to the paper. For more complex aggregation mechanisms,
+    pass an arbitrary callable instead.
+
+    .. seealso:: https://github.com/migalkin/NodePiece/blob/d731c9990/lp_rp/pykeen105/nodepiece_rotate.py#L57-L65
+    """
+
+    def __init__(
+        self,
+        num_tokens: int,
+        embedding_dim: int,
+        dropout: float = 0.1,
+        ratio: Union[int, float] = 2,
+    ):
+        """
+        Initialize the module.
+
+        :param num_tokens:
+            the number of tokens
+        :param embedding_dim:
+            the embedding dimension for a single token
+        :param dropout:
+            the dropout value on the hidden layer
+        :param ratio:
+            the ratio of the embedding dimension to the hidden layer size.
+        """
+        hidden_dim = int(ratio * embedding_dim)
+        super().__init__(
+            nn.Linear(num_tokens * embedding_dim, hidden_dim),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, embedding_dim),
+        )
+
+    def forward(self, xs: torch.FloatTensor, dim: int) -> torch.FloatTensor:  # noqa: D102
+        # dim is only a parameter to match the signature of torch.mean / torch.sum
+        # this class is not thought to be usable from outside
+        assert dim == -2
+        return super().forward(xs.view(*xs.shape[:-2], -1))
 
 
 class InductiveNodePiece(ERModel):
@@ -121,7 +164,8 @@ class InductiveNodePiece(ERModel):
         )
         entity_representations = NodePieceRepresentation(
             triples_factory=triples_factory,
-            token_representation=relation_representations,
+            tokenizers='RelationTokenizer',
+            token_representations=relation_representations,
             aggregation=aggregation,
             shape=shape,
             num_tokens=num_tokens,
@@ -129,7 +173,8 @@ class InductiveNodePiece(ERModel):
 
         inference_representation = NodePieceRepresentation(
             triples_factory=inference_factory,
-            token_representation=relation_representations,
+            tokenizers='RelationTokenizer',
+            token_representations=relation_representations,
             aggregation=aggregation,
             shape=shape,
             num_tokens=num_tokens,
