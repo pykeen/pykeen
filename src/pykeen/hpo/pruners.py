1ce0afbe6734a611b30b9f033b6e6f362bf49284
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-02-06 19:53:49 +0100

    Remove unused modules
    Trigger CI

--- src/pykeen/hpo/pruners.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""A wrapper for looking up pruners from :mod:`optuna`."""
-
-from class_resolver.contrib.optuna import pruner_resolver
-
-__all__ = [
-    "pruner_resolver",
-]
--- src/pykeen/hpo/samplers.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""A wrapper for looking up samplers from :mod:`optuna`."""
-
-from class_resolver.contrib.optuna import sampler_resolver
-
-__all__ = [
-    "sampler_resolver",
-]
