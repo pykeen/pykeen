ba6ba91553f8c827f07aecf29f6ae3ad48b75507
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-16 19:51:22 +0100

    remove resolved fixme
    trigger ci

--- src/pykeen/datasets/cli.py
+++ src/pykeen/datasets/cli.py
@@ -235,7 +235,6 @@ def expected_metrics(dataset: str, max_triples: Optional[int], log_level: str):
             click.echo("Skip OGB WikiKG")
             continue
         dataset_instance = get_dataset(dataset=dataset_cls)
-        # FIXME
         dataset_name = dataset_resolver.normalize_inst(dataset_instance)
         d = directory.joinpath(dataset_name, "analysis")
         d.mkdir(parents=True, exist_ok=True)
