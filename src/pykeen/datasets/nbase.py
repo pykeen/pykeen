4d85123e87033587327f4ca3cc2746d31cd0a3ee
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-17 15:11:18 +0000

    add caching

--- src/pykeen/datasets/nbase.py
+++ src/pykeen/datasets/nbase.py
@@ -1,14 +1,20 @@
 """Utility classes for constructing datasets."""
 
 
+import base64
 import dataclasses
-from abc import abstractmethod
-from typing import Optional
+import hashlib
+import logging
 import pathlib
+from abc import abstractmethod
+from typing import Any, Mapping, Optional
 
-from class_resolver import OptionalKwargs
+from class_resolver import OptionalKwargs, normalize_string
 
+from ..constants import PYKEEN_DATASETS
 from ..triples import CoreTriplesFactory, TriplesFactory
+
+logger = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass
@@ -20,6 +26,21 @@ class Dataset:
     validation: Optional[CoreTriplesFactory] = None
     metadata: OptionalKwargs = None
 
+    @classmethod
+    def from_directory_binary(cls, directory: pathlib.Path) -> "Dataset":
+        raise NotImplementedError
+
+    def to_directory_binary(self, directory: pathlib.Path) -> None:
+        raise NotImplementedError
+
+
+def _digest_kwargs(dataset_kwargs: Mapping[str, Any]) -> str:
+    digester = hashlib.sha256()
+    for key in sorted(dataset_kwargs.keys()):
+        digester.update(key.encode(encoding="utf8"))
+        digester.update(str(dataset_kwargs[key]).encode(encoding="utf8"))
+    return base64.urlsafe_b64encode(digester.digest()).decode("utf8")[:32]
+
 
 @dataclasses.dataclass
 class DatasetLoader:
@@ -27,11 +48,37 @@ class DatasetLoader:
 
     create_inverse_triples: bool = False
 
-    @abstractmethod
-    def load(self) -> Dataset:
+    def _digest(self) -> str:
+        dataset_kwargs = ...
+        return _digest_kwargs(dataset_kwargs)
+
+    @property
+    def name(self) -> str:
+        """The canonical dataset name."""
+        return normalize_string(self.__class__.__name__, suffix=DatasetLoader.__class__)
+
+    def load(self, force: bool = False) -> Dataset:
         """Load the dataset."""
-        # TODO: add caching here
-        raise NotImplementedError
+        # get canonical cache path
+        path = PYKEEN_DATASETS.joinpath(self.name, "cache", self._digest())
+
+        # try to use cached dataset
+        if path.is_dir() and not force:
+            logger.info(f"Loading cached preprocessed dataset from {path.as_uri()}")
+            return Dataset.from_directory_binary(path)
+
+        # load dataset without cache
+        dataset_instance = self._load()
+
+        # store cache
+        logger.info(f"Caching preprocessed dataset to {path.as_uri()}")
+        dataset_instance.to_directory_binary(path=path)
+
+        return dataset_instance
+
+    @abstractmethod
+    def _load(self) -> Dataset:
+        """Load the dataset without cache."""
 
 
 @dataclasses.dataclass
@@ -44,7 +91,7 @@ class PathDatasetLoader(DatasetLoader):
     validation_path: Optional[pathlib.Path] = None
     load_triples_kwargs: OptionalKwargs = None
 
-    def load(self) -> Dataset:  # noqa: D102
+    def _load(self) -> Dataset:  # noqa: D102
         training = TriplesFactory.from_path(
             path=self.training_path,
             create_inverse_triples=self.create_inverse_triples,
