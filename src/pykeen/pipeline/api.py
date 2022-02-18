2ddc3bdcdda9686581ae3614a99348f5765287e7
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-18 14:10:34 +0100

    result tracker as attribute of the training loop

--- src/pykeen/pipeline/api.py
+++ src/pykeen/pipeline/api.py
@@ -1160,6 +1160,7 @@ def pipeline(  # noqa: C901
         triples_factory=training,
         optimizer=optimizer_instance,
         lr_scheduler=lr_scheduler_instance,
+        result_tracker=_result_tracker,
         **training_loop_kwargs,
     )
     _result_tracker.log_params(
@@ -1227,7 +1228,6 @@ def pipeline(  # noqa: C901
     losses = training_loop_instance.train(
         triples_factory=training,
         stopper=stopper_instance,
-        result_tracker=_result_tracker,
         clear_optimizer=clear_optimizer,
         **training_kwargs,
     )
