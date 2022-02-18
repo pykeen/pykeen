785f51156fd4864ad93210e39d4e4755895fae8f
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-18 14:20:23 +0100

    re-use class-resolver utility

--- src/pykeen/trackers/__init__.py
+++ src/pykeen/trackers/__init__.py
@@ -56,30 +56,7 @@ def resolve_result_trackers(
         as the result trackers
     :returns: A multi-result trackers that offloads to all contained result trackers
     """
-    result_trackers: Sequence[HintType[ResultTracker]]
-    if result_tracker is None:
-        result_trackers = []
-    else:
-        result_trackers = upgrade_to_sequence(result_tracker)
-
-    result_tracker_kwargses: Sequence[Optional[Mapping[str, Any]]]
-    if result_tracker_kwargs is None:
-        result_tracker_kwargses = [None] * len(result_trackers)
-    else:
-        result_tracker_kwargses = upgrade_to_sequence(result_tracker_kwargs)
-
-    if 0 < len(result_tracker_kwargses) and 0 == len(result_trackers):
-        raise ValueError("Kwargs were given but no result trackers")
-    elif 1 == len(result_tracker_kwargses) == 1 and 1 < len(result_trackers):
-        result_tracker_kwargses = list(result_tracker_kwargses) * len(result_trackers)
-    elif len(result_tracker_kwargses) != len(result_trackers):
-        raise ValueError("Mismatch in number number of trackers and kwargs")
-
-    trackers = [
-        tracker_resolver.make(query=_result_tracker, pos_kwargs=_result_tracker_kwargs)
-        for _result_tracker, _result_tracker_kwargs in zip(result_trackers, result_tracker_kwargses)
-    ]
+    trackers = tracker_resolver.make_many(queries=result_tracker, kwargs=result_tracker_kwargs)
     # always add a Python result tracker for storing the configuration
     trackers.append(PythonResultTracker(store_metrics=False))
-
     return MultiResultTracker(trackers=trackers)
