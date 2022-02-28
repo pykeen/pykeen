094ebb0f623c79f684fe4ddfda8a7f9948a30753
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-28 16:52:08 +0100

    update link pred dataset
    trigger ci

--- src/pykeen/evaluation/evaluation_loop.py
+++ src/pykeen/evaluation/evaluation_loop.py
@@ -4,7 +4,8 @@
 
 import itertools
 from abc import abstractmethod
-from typing import Collection, Generic, Optional, TypeVar
+from collections import defaultdict
+from typing import Collection, Generic, Iterable, Mapping, Optional, Tuple, TypeVar
 
 import torch
 from class_resolver import OptionalKwargs
@@ -16,7 +17,7 @@ from .evaluator import Evaluator, MetricResults
 from ..constants import TARGET_TO_INDEX
 from ..models import Model
 from ..triples import CoreTriplesFactory
-from ..typing import LABEL_HEAD, LABEL_TAIL, MappedTriples, Target
+from ..typing import InductiveMode, LABEL_HEAD, LABEL_TAIL, MappedTriples, Target
 
 BatchType = TypeVar("BatchType")
 
@@ -84,34 +85,54 @@ class EvaluationLoop(Generic[BatchType]):
 
 
 class LinkPredictionEvaluationDataset(Dataset):
-    def __init__(self, factory: CoreTriplesFactory) -> None:
+    """A dataset for link prediction evaluation."""
+
+    def __init__(self, factory: CoreTriplesFactory, targets: Collection[Target]) -> None:
         super().__init__()
         self.mapped_triples = factory.mapped_triples
         self.num_triples = factory.num_triples
+        self.targets = list(targets)
+        # TODO: filtering
+
+    @property
+    def num_targets(self) -> int:
+        """Return the number of targets."""
+        return len(self.targets)
 
     def __len__(self) -> int:
-        return self.num_triples
+        return self.num_triples * self.num_targets
 
-    def __getitem__(self, index: int) -> MappedTriples:
-        return self.mapped_triples[index, :]
+    def __getitem__(self, index: int) -> Tuple[Target, MappedTriples]:
+        target = self.targets[index // self.num_triples]
+        return target, self.mapped_triples[index, :]
+
+    @staticmethod
+    def collate(batch: Iterable[Tuple[Target, MappedTriples]]) -> Mapping[Target, MappedTriples]:
+        """Collate batches."""
+        result = defaultdict(list)
+        for target, triple in batch:
+            result[target].append(triple)
+        return {target: torch.stack(triples) for target, triples in result.items()}
 
 
-class LinkPredictionEvaluationLoop(EvaluationLoop[MappedTriples]):
+class LinkPredictionEvaluationLoop(EvaluationLoop[Mapping[Target, MappedTriples]]):
     """Link prediction evaluation loop."""
 
     def __init__(
         self,
         triples_factory: CoreTriplesFactory,
         targets: Collection[Target] = (LABEL_HEAD, LABEL_TAIL),
+        mode: Optional[InductiveMode] = None,
         **kwargs,
     ) -> None:
         super().__init__(dataset=LinkPredictionEvaluationDataset(factory=triples_factory), **kwargs)
         self.targets = targets
+        self.mode = mode
 
-    def process_batch(self, batch: MappedTriples) -> None:  # noqa: D102
-        hrt_batch = batch
-        for target in self.targets:
-            scores = self.model.predict(hrt_batch=hrt_batch, target=target)
+    def process_batch(self, batch: Mapping[Target, MappedTriples]) -> None:  # noqa: D102
+        for target, hrt_batch in batch.items():
+            scores = self.model.predict(hrt_batch=hrt_batch, target=target, mode=self.mode)
+            # TODO: Filtering?
             true_scores = scores[torch.arange(scores.shape[0]), hrt_batch[:, TARGET_TO_INDEX[target]], None]
             self.evaluator.process_scores_(
                 hrt_batch=hrt_batch,
