0ff9923fe6ce7f47ac7af179d647d582de61d4a8
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-03-03 13:59:02 +0100

    fix docstring obsolete newline

--- src/pykeen/nn/representation.py
+++ src/pykeen/nn/representation.py
@@ -439,8 +439,7 @@ class LowRankRepresentation(Representation):
             the initializer for basis weights
         :param kwargs:
             additional keyword based arguments passed to :class:`pykeen.nn.representation.Embedding`, which is used
-            for
-            the base representations.
+            for the base representations.
         """
         super().__init__(max_id=max_id, shape=shape)
         self.bases = Embedding(max_id=num_bases, shape=shape, **kwargs)
