ea3d862bfdb38acff1c160a07fa4b5c203fb850b
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-03-07 14:11:14 +0100

    update some TripleRE parameters

--- src/pykeen/models/unimodal/triple_re.py
+++ src/pykeen/models/unimodal/triple_re.py
@@ -10,13 +10,26 @@ from torch.nn.init import uniform_
 
 from ..nbase import ERModel
 from ...constants import DEFAULT_EMBEDDING_HPO_EMBEDDING_DIM_RANGE
-from ...nn.node_piece import NodePieceRepresentation
+from ...nn import Embedding, representation_resolver
 from ...nn.modules import TripleREInteraction
+from ...nn.node_piece import (
+    AnchorTokenizer,
+    DegreeAnchorSelection,
+    MixtureAnchorSelection,
+    NodePieceRepresentation,
+    PageRankAnchorSelection,
+    RandomAnchorSelection,
+    RelationTokenizer,
+)
+from ...triples import CoreTriplesFactory
 from ...typing import Hint, Initializer, Normalizer
 
 __all__ = [
     "TripleRE",
 ]
+
+
+# cf. https://github.com/LongYu-360/TripleRE-Add-NodePiece/blob/994216dcb1d718318384368dd0135477f852c6a4/TripleRE%2BNodepiece/ogb_wikikg2/model.py#L196-L204
 
 
 class TripleRE(ERModel):
@@ -48,9 +61,12 @@ class TripleRE(ERModel):
 
     def __init__(
         self,
+        triples_factory: CoreTriplesFactory,
         embedding_dim: int = 128,
         p: int = 1,
         power_norm: bool = False,
+        num_relation_tokens: int = 12,
+        num_anchors: int = 20_000,
         entity_initializer: Hint[Initializer] = uniform_,
         entity_initializer_kwargs: Optional[Mapping[str, Any]] = None,
         entity_normalizer: Hint[Normalizer] = functional.normalize,
@@ -74,17 +90,55 @@ class TripleRE(ERModel):
         """
         # TODO: the published code uses NodePiece representations instead
         entity_normalizer_kwargs = ChainMap(entity_normalizer_kwargs or {}, self.default_entity_normalizer_kwargs)
+        relation_mid_representation = representation_resolver.make(
+            Embedding, initializer=relation_initializer, initializer_kwargs=relation_initializer_kwargs
+        )
         super().__init__(
+            triples_factory=triples_factory,
             interaction=TripleREInteraction,
             interaction_kwargs=dict(p=p, power_norm=power_norm),
-            entity_representations=NodePieceRepresentation(
-                embedding_dim=embedding_dim,
-                initializer=entity_initializer,
-                initializer_kwargs=entity_initializer_kwargs,
-                normalizer=entity_normalizer,
-                normalizer_kwargs=entity_normalizer_kwargs,
+            entity_representations=NodePieceRepresentation,
+            entity_representation_kwargs=dict(
+                triples_factory=triples_factory,
+                token_representations=[
+                    relation_mid_representation,  # for relation-tokens
+                    Embedding,  # for anchor nodes
+                ],
+                # tokenizers:
+                # https://github.com/LongYu-360/TripleRE-Add-NodePiece/blob/994216dcb1d718318384368dd0135477f852c6a4/TripleRE%2BNodepiece/run_ogb.py#L273-L292
+                token_representations_kwargs=[
+                    None,
+                    dict(
+                        embedding_dim=embedding_dim,
+                        initializer=entity_initializer,
+                        initializer_kwargs=entity_initializer_kwargs,
+                        normalizer=entity_normalizer,
+                        normalizer_kwargs=entity_normalizer_kwargs,
+                    ),
+                ],
+                tokenizers=[
+                    RelationTokenizer,
+                    # TODO: use anchor distances
+                    AnchorTokenizer,
+                ],
+                tokenizers_kwargs=[
+                    #   https://github.com/LongYu-360/TripleRE-Add-NodePiece/blob/994216dcb1d718318384368dd0135477f852c6a4/TripleRE%2BNodepiece/run_ogb.py#L86-L88
+                    None,
+                    dict(
+                        selection=MixtureAnchorSelection,
+                        selection_kwargs=dict(
+                            selections=[
+                                DegreeAnchorSelection,
+                                PageRankAnchorSelection,
+                                RandomAnchorSelection,
+                            ],
+                            ratios=[0.4, 0.4, 0.2],
+                        ),
+                    ),
+                ],
+                num_tokens=[num_relation_tokens, num_anchors],
             ),
-            relation_representations=[NodePieceRepresentation,NodePieceRepresentation,NodePieceRepresentation],
+            relation_representations=[None, relation_mid_representation, None],
             relation_representations_kwargs=[
                 dict(
                     embedding_dim=embedding_dim,
