dfef5510e6ba79787b65a07424a0dd46874cfe5a
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-03-07 13:32:57 +0100

    add test for SLCWA training instances

--- tests/test_instances.py
+++ tests/test_instances.py
@@ -1,7 +1,7 @@
 """Tests for training instances."""
 from typing import Any, MutableMapping
 
-from pykeen.triples import LCWAInstances
+from pykeen.triples import LCWAInstances, SLCWAInstances
 from tests import cases
 
 
@@ -19,4 +19,15 @@ class LCWAInstancesTestCase(cases.TrainingInstancesTestCase):
         )
         kwargs["pairs"] = other_instance.pairs
         kwargs["compressed"] = other_instance.compressed
+        return kwargs
+
+
+class SLCWAInstancesTestCase(cases.TrainingInstancesTestCase):
+    """Tests for sLCWA training instances."""
+
+    cls = SLCWAInstances
+
+    def _pre_instantiation_hook(self, kwargs: MutableMapping[str, Any]) -> MutableMapping[str, Any]:  # noqa: D102
+        kwargs = super()._pre_instantiation_hook(kwargs=kwargs)
+        kwargs["mapped_triples"] = self.factory.mapped_triples
         return kwargs
