8c3c5118af6b84608ec43f0d641bcb4b4fc77097
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-03-07 13:38:08 +0100

    add test for batched sLCWA instances

--- tests/test_instances.py
+++ tests/test_instances.py
@@ -1,7 +1,12 @@
 """Tests for training instances."""
 from typing import Any, MutableMapping
 
+import torch.utils.data
+import unittest_templates
+
+from pykeen.datasets import Nations
 from pykeen.triples import LCWAInstances, SLCWAInstances
+from pykeen.triples.instances import BatchedSLCWAInstances
 from tests import cases
 
 
@@ -31,3 +36,23 @@ class SLCWAInstancesTestCase(cases.TrainingInstancesTestCase):
         kwargs = super()._pre_instantiation_hook(kwargs=kwargs)
         kwargs["mapped_triples"] = self.factory.mapped_triples
         return kwargs
+
+
+class BatchedSLCWAInstancesTestCase(unittest_templates.GenericTestCase[BatchedSLCWAInstances]):
+    """Tests for batched sLCWA training instances."""
+
+    cls = BatchedSLCWAInstances
+    batch_size = 2
+    kwargs = dict(
+        batch_size=batch_size,
+    )
+
+    def _pre_instantiation_hook(self, kwargs: MutableMapping[str, Any]) -> MutableMapping[str, Any]:  # noqa: D102
+        self.factory = Nations().training
+        kwargs["mapped_triples"] = self.factory.mapped_triples
+        return kwargs
+
+    def test_data_loader(self):
+        """Test data loader."""
+        for batch in torch.utils.data.DataLoader(dataset=self.instance, batch_size=None):
+            assert batch is not None
