68510702cf378565f4a9858187840fca2efad8b8
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-03-13 20:29:42 +0100

    Add missing imports

--- src/pykeen/datasets/__init__.py
+++ src/pykeen/datasets/__init__.py
@@ -48,7 +48,7 @@ from .umls import UMLS
 from .utils import get_dataset
 from .wd50k import WD50KT
 from .wikidata5m import Wikidata5M
-from .wk3l import MTransEDataset, WK3l15k
+from .wk3l import MTransEDataset, WK3l15k, WK3l120k, CN3l
 from .wordnet import WN18, WN18RR
 from .yago import YAGO310
 
@@ -68,12 +68,14 @@ __all__ = [
     "CoDExSmall",
     "CoDExMedium",
     "CoDExLarge",
+    "CN3l",
     "OGBBioKG",
     "OGBWikiKG2",
     "UMLS",
     "FB15k",
     "FB15k237",
     "WK3l15k",
+    "WK3l120k",
     "WN18",
     "WN18RR",
     "YAGO310",
