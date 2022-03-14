fix numeric variance for adjusted metrics

src/pykeen/metrics/ranking.py
@@ -163,7 +163,7 @@ class RankBasedMetric(Metric):
         if generator is None:
             generator = np.random.default_rng()
         ranks = generator.integers(low=1, high=num_candidates + 1, size=(num_samples, *num_candidates.shape))
-        return np.apply_along_axis(self, 0, ranks).var().item()
+        return np.apply_along_axis(self, 0, ranks, num_candidates=num_candidates).var().item()
 
     def variance(
         self,