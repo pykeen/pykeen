fix variance of H@k

src/pykeen/metrics/ranking.py
@@ -1002,8 +1002,8 @@ class HitsAtK(RankBasedMetric):
     .. math::
 
         \mathbb{V}[Hits@k] = \mathbb{V}\left[\frac{1}{n} \sum \limits_{i=1}^{n} \mathbb{I}[r_i \leq k]\right]
-                           = \frac{1}{n} \sum \limits_{i=1}^{n} \mathbb{V}\left[\mathbb{I}[r_i \leq k]\right]
-                           = \frac{1}{n} \sum \limits_{i=1}^{n} p_i(1 - p_i)
+                           = \frac{1}{n^2} \sum \limits_{i=1}^{n} \mathbb{V}\left[\mathbb{I}[r_i \leq k]\right]
+                           = \frac{1}{n^2} \sum \limits_{i=1}^{n} p_i(1 - p_i)
     ---
     description: The relative frequency of ranks not larger than a given k.
     link: https://pykeen.readthedocs.io/en/stable/tutorial/understanding_evaluation.html#hits-k
@@ -1045,7 +1045,7 @@ class HitsAtK(RankBasedMetric):
     ) -> float:  # noqa:D102
         num_candidates = np.asanyarray(num_candidates, dtype=float)
         p = np.minimum(self.k / num_candidates, 1.0)
-        return (p * (1.0 - p)).mean().item()
+        return (p * (1.0 - p)).mean().item() / num_candidates.size
 
 
 @parse_docdata