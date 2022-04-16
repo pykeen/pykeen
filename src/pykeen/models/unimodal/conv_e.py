36a2d38adbcc630fcd247f3d83a5b1a038c785ef
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-04-16 19:09:15 +0200

    update ConvE to ERModel

--- src/pykeen/models/unimodal/conv_e.py
+++ src/pykeen/models/unimodal/conv_e.py
@@ -3,22 +3,18 @@
 """Implementation of ConvE."""
 
 import logging
-import sys
 from typing import Any, ClassVar, Mapping, Optional, Type
 
 import torch
 from torch import nn
-from torch.nn import functional as F  # noqa: N812
 
-from ..base import EntityRelationEmbeddingModel
+from ..nbase import ERModel
 from ...constants import DEFAULT_DROPOUT_HPO_RANGE
 from ...losses import BCEAfterSigmoidLoss, Loss
-from ...nn import representation_resolver
 from ...nn.init import xavier_normal_
-from ...nn.modules import _calculate_missing_shape_information
+from ...nn.modules import ConvEInteraction
 from ...triples import CoreTriplesFactory
 from ...typing import Hint, Initializer
-from ...utils import is_cudnn_error
 
 __all__ = [
     "ConvE",
@@ -27,7 +23,7 @@ __all__ = [
 logger = logging.getLogger(__name__)
 
 
-class ConvE(EntityRelationEmbeddingModel):
+class ConvE(ERModel):
     r"""An implementation of ConvE from [dettmers2018]_.
 
     ConvE  is a CNN-based approach. For each triple $(h,r,t)$, the input to ConvE is a matrix
@@ -152,242 +148,35 @@ class ConvE(EntityRelationEmbeddingModel):
 
         super().__init__(
             triples_factory=triples_factory,
-            entity_representations_kwargs=dict(
-                shape=embedding_dim,
-                initializer=entity_initializer,
+            interaction=ConvEInteraction,
+            interaction_kwargs=dict(
+                input_channels=input_channels,
+                output_channels=output_channels,
+                embedding_height=embedding_height,
+                embedding_width=embedding_width,
+                kernel_height=kernel_height,
+                kernel_width=kernel_width,
+                input_dropout=input_dropout,
+                output_dropout=output_dropout,
+                feature_map_dropout=feature_map_dropout,
+                embedding_dim=embedding_dim,
+                apply_batch_normalization=apply_batch_normalization,
             ),
+            entity_representations_kwargs=[
+                # entity embedding
+                dict(
+                    shape=embedding_dim,
+                    initializer=entity_initializer,
+                ),
+                # ConvE uses one bias for each entity
+                dict(
+                    shape=tuple(),
+                    initializer=nn.init.zeros_,
+                ),
+            ],
             relation_representations_kwargs=dict(
                 shape=embedding_dim,
                 initializer=relation_initializer,
             ),
             **kwargs,
         )
-
-        # ConvE uses one bias for each entity
-        self.bias_term = representation_resolver.make(
-            query=None,
-            max_id=self.num_entities,
-            shape=(1,),  # TODO: switch to scalar?
-            initializer=nn.init.zeros_,
-        )
-
-        # Automatic calculation of remaining dimensions
-        logger.info(f"Resolving {input_channels} * {embedding_width} * {embedding_height} = {embedding_dim}.")
-        if embedding_dim is None:
-            embedding_dim = input_channels * embedding_width * embedding_height
-
-        # Parameter need to fulfil:
-        #   input_channels * embedding_height * embedding_width = embedding_dim
-        input_channels, embedding_width, embedding_height = _calculate_missing_shape_information(
-            embedding_dim=embedding_dim,
-            input_channels=input_channels,
-            width=embedding_width,
-            height=embedding_height,
-        )
-        logger.info(f"Resolved to {input_channels} * {embedding_width} * {embedding_height} = {embedding_dim}.")
-        self.embedding_height = embedding_height
-        self.embedding_width = embedding_width
-        self.input_channels = input_channels
-
-        if self.input_channels * self.embedding_height * self.embedding_width != self.embedding_dim:
-            raise ValueError(
-                f"Product of input channels ({self.input_channels}), height ({self.embedding_height}), and width "
-                f"({self.embedding_width}) does not equal target embedding dimension ({self.embedding_dim})",
-            )
-
-        self.inp_drop = nn.Dropout(input_dropout)
-        self.hidden_drop = nn.Dropout(output_dropout)
-        self.feature_map_drop = nn.Dropout2d(feature_map_dropout)
-
-        self.conv1 = torch.nn.Conv2d(
-            in_channels=self.input_channels,
-            out_channels=output_channels,
-            kernel_size=(kernel_height, kernel_width),
-            stride=1,
-            padding=0,
-            bias=True,
-        )
-
-        self.apply_batch_normalization = apply_batch_normalization
-        if self.apply_batch_normalization:
-            self.bn0 = nn.BatchNorm2d(self.input_channels)
-            self.bn1 = nn.BatchNorm2d(output_channels)
-            self.bn2 = nn.BatchNorm1d(self.embedding_dim)
-        else:
-            self.bn0 = None
-            self.bn1 = None
-            self.bn2 = None
-        num_in_features = (
-            output_channels
-            * (2 * self.embedding_height - kernel_height + 1)
-            * (self.embedding_width - kernel_width + 1)
-        )
-        self.fc = nn.Linear(num_in_features, self.embedding_dim)
-
-    def _reset_parameters_(self):  # noqa: D102
-        super()._reset_parameters_()
-
-        self.bias_term.reset_parameters()
-
-        # weights
-        for module in [
-            self.conv1,
-            self.bn0,
-            self.bn1,
-            self.bn2,
-            self.fc,
-        ]:
-            if module is None:
-                continue
-            module.reset_parameters()
-
-    def _convolve_entity_relation(self, h: torch.LongTensor, r: torch.LongTensor) -> torch.FloatTensor:
-        """Perform the main calculations of the ConvE model."""
-        batch_size = h.shape[0]
-
-        # batch_size, num_input_channels, 2*height, width
-        x = torch.cat([h, r], dim=2)
-
-        try:
-            # batch_size, num_input_channels, 2*height, width
-            if self.bn0 is not None:
-                x = self.bn0(x)
-
-            # batch_size, num_input_channels, 2*height, width
-            x = self.inp_drop(x)
-            # (N,C_out,H_out,W_out)
-            x = self.conv1(x)
-
-            if self.bn1 is not None:
-                x = self.bn1(x)
-            x = F.relu(x)
-            x = self.feature_map_drop(x)
-            # batch_size, num_output_channels * (2 * height - kernel_height + 1) * (width - kernel_width + 1)
-            x = x.view(batch_size, -1)
-            x = self.fc(x)
-            x = self.hidden_drop(x)
-
-            if self.bn2 is not None:
-                x = self.bn2(x)
-            x = F.relu(x)
-        except RuntimeError as e:
-            if not is_cudnn_error(e):
-                raise e
-            logger.warning(
-                "\nThis code crash might have been caused by a CUDA bug, see "
-                "https://github.com/allenai/allennlp/issues/2888, "
-                "which causes the code to crash during evaluation mode.\n"
-                "To avoid this error, the batch size has to be reduced.\n"
-                f"The original error message: \n{e.args[0]}",
-            )
-            sys.exit(1)
-
-        return x
-
-    def score_hrt(self, hrt_batch: torch.LongTensor, **kwargs) -> torch.FloatTensor:  # noqa: D102
-        h = self.entity_embeddings(indices=hrt_batch[:, 0]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-        r = self.relation_embeddings(indices=hrt_batch[:, 1]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-        t = self.entity_embeddings(indices=hrt_batch[:, 2])
-
-        # Embedding Regularization
-        self.regularize_if_necessary(h, r, t)
-
-        x = self._convolve_entity_relation(h, r)
-
-        # For efficient calculation, each of the convolved [h, r] rows has only to be multiplied with one t row
-        x = (x.view(-1, self.embedding_dim) * t).sum(dim=1, keepdim=True)
-
-        """
-        In ConvE the bias term add the end is added for each tail item. In the sLCWA training approach we only have
-        one tail item for each head and relation. Accordingly the relevant bias for each tail item and triple has to be
-        looked up.
-        """
-        x = x + self.bias_term(indices=hrt_batch[:, 2])
-        # The application of the sigmoid during training is automatically handled by the default loss.
-
-        return x
-
-    def score_t(self, hr_batch: torch.LongTensor, **kwargs) -> torch.FloatTensor:  # noqa: D102
-        h = self.entity_embeddings(indices=hr_batch[:, 0]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-        r = self.relation_embeddings(indices=hr_batch[:, 1]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-        t = self.entity_embeddings(indices=None).transpose(1, 0)
-
-        # Embedding Regularization
-        self.regularize_if_necessary(h, r, t)
-
-        x = self._convolve_entity_relation(h, r)
-
-        x = x @ t
-        x = x + self.bias_term(indices=None).t()
-        # The application of the sigmoid during training is automatically handled by the default loss.
-
-        return x
-
-    def score_h(self, rt_batch: torch.LongTensor, **kwargs) -> torch.FloatTensor:  # noqa: D102
-        rt_batch_size = rt_batch.shape[0]
-        h = self.entity_embeddings(indices=None)
-        r = self.relation_embeddings(indices=rt_batch[:, 0]).view(
-            -1,
-            self.input_channels,
-            self.embedding_height,
-            self.embedding_width,
-        )
-        t = self.entity_embeddings(indices=rt_batch[:, 1])
-
-        # Embedding Regularization
-        self.regularize_if_necessary(h, r, t)
-
-        """
-        Every head has to be convolved with every relation in the rt_batch. Hence we repeat the
-        relation _num_entities_ times and the head _rt_batch_size_ times.
-        """
-        r = r.repeat(h.shape[0], 1, 1, 1)
-        # Code to repeat each item successively instead of the entire tensor
-        h = (
-            h.unsqueeze(1)
-            .repeat(1, rt_batch_size, 1)
-            .view(
-                -1,
-                self.input_channels,
-                self.embedding_height,
-                self.embedding_width,
-            )
-        )
-
-        x = self._convolve_entity_relation(h, r)
-
-        """
-        For efficient computation, each convolved [h, r] pair has only to be multiplied with the corresponding t
-        embedding found in the rt_batch with [r, t] pairs.
-        """
-        x = (x.view(self.num_entities, rt_batch_size, self.embedding_dim) * t[None, :, :]).sum(2).transpose(1, 0)
-
-        """
-        In ConvE the bias term at the end is added for each tail item. In the score_h function, each row holds
-        the same tail for many different heads, meaning that these items have to be looked up for each tail of each row
-        and only then can be added correctly.
-        """
-        x = x + self.bias_term(indices=rt_batch[:, 1])
-        # The application of the sigmoid during training is automatically handled by the default loss.
-
-        return x
