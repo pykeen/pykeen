d46f1d7935ea2cff82ec09800ad518c75fb79afe
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-05-02 13:21:54 +0200

    add backwards compatiblity to rotate_interaction

--- src/pykeen/nn/functional.py
+++ src/pykeen/nn/functional.py
@@ -9,7 +9,7 @@ as well as broadcasting and thus naturally support slicing and 1:n scoring.
 from __future__ import annotations
 
 import functools
-from typing import Optional, Sequence, Tuple, Union
+from typing import Iterable, Optional, Sequence, Tuple, Union
 
 import numpy
 import torch
@@ -597,6 +597,16 @@ def rescal_interaction(
     return torch.einsum("...d,...de,...e->...", h, r, t)
 
 
+def _ensure_complex(*xs: torch.Tensor) -> Iterable[torch.Tensor]:
+    for x in xs:
+        if x.is_complex():
+            yield x
+            continue
+        if x.shape[-1] != 2:
+            x = x.view(*x.shape[:-1], -1, 2)
+        yield torch.view_as_complex(x)
+
+
 def rotate_interaction(
     h: torch.FloatTensor,
     r: torch.FloatTensor,
@@ -617,6 +627,8 @@ def rotate_interaction(
     :return: shape: batch_dims
         The scores.
     """
+    # ensure complex
+    h, r, t = _ensure_complex(h, r, t)
     if estimate_cost_of_sequence(h.shape, r.shape) < estimate_cost_of_sequence(r.shape, t.shape):
         # r expresses a rotation in complex plane.
         # rotate head by relation (=Hadamard product in complex space)
