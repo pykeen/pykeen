c34d4016bc979acb17b88ba3a20d0caa93427bf5
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-05-02 13:12:24 +0200

    🪛📁 Fix directory path normalization (#890)
    * fix directory path normalization
    * extract and use utility
    
    Co-authored-by: Charles Tapley Hoyt <cthoyt@gmail.com>
    Co-authored-by: PyKEEN_bot <pykeen2019@gmail.com>

--- src/pykeen/utils.py
+++ src/pykeen/utils.py
@@ -29,6 +29,7 @@ from typing import (
     Optional,
     Sequence,
     Set,
+    TextIO,
     Tuple,
     Type,
     TypeVar,
@@ -111,6 +112,7 @@ __all__ = [
     "triple_tensor_to_set",
     "is_triple_tensor_subset",
     "logcumsumexp",
+    "normalize_path",
 ]
 
 logger = logging.getLogger(__name__)
@@ -1319,6 +1321,61 @@ def logcumsumexp(a: np.ndarray) -> np.ndarray:
     out = np.log(s)
     out += a_max
     return out
+
+
+PathType = Union[str, pathlib.Path, TextIO]
+
+
+def normalize_path(
+    path: Optional[PathType],
+    *other: Union[str, pathlib.Path],
+    mkdir: bool = False,
+    is_file: bool = False,
+    default: Optional[PathType] = None,
+) -> pathlib.Path:
+    """
+    Normalize a path.
+
+    :param path:
+        the path in either of the valid forms.
+    :param other:
+        additional parts to join to the path
+    :param mkdir:
+        whether to ensure that the path refers to an existing directory by creating it if necessary
+    :param is_file:
+        whether the path is intended to be a file - only relevant for creating directories
+    :param default:
+        the default to use if path is None
+
+    :raises TypeError:
+        if `path` is of unsuitable type
+    :raises ValueError:
+        if `path` and `default` are both `None`
+
+    :return:
+        the absolute and resolved path
+    """
+    if path is None:
+        if default is None:
+            raise ValueError("If no default is provided, path cannot be None.")
+        path = default
+    if isinstance(path, TextIO):
+        path = path.name
+    if isinstance(path, str):
+        path = pathlib.Path(path)
+    if not isinstance(path, pathlib.Path):
+        raise TypeError(f"path is invalid type: {type(path)}")
+    if other:
+        path = path.joinpath(*other)
+    # resolve path to make sure it is an absolute path
+    path = path.expanduser().resolve()
+    # ensure directory exists
+    if mkdir:
+        directory = path
+        if is_file:
+            directory = path.parent
+        directory.mkdir(exist_ok=True, parents=True)
+    return path
 
 
 if __name__ == "__main__":
