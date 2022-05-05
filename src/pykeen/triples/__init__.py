fbc1e9853081f24eb36339565fd74eb9ff659a5d
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-05-05 13:45:37 +0200

    export

--- src/pykeen/triples/__init__.py
+++ src/pykeen/triples/__init__.py
@@ -3,13 +3,14 @@
 """Classes for creating and storing training data from triples."""
 
 from .instances import Instances, LCWAInstances, SLCWAInstances
-from .triples_factory import CoreTriplesFactory, RelationInverter, TriplesFactory, relation_inverter
+from .triples_factory import CoreTriplesFactory, RelationInverter, TriplesFactory, relation_inverter, DatasetInfo
 from .triples_numeric_literals_factory import TriplesNumericLiteralsFactory
 
 __all__ = [
     "Instances",
     "LCWAInstances",
     "SLCWAInstances",
+    "DatasetInfo",
     "CoreTriplesFactory",
     "RelationInverter",
     "relation_inverter",
