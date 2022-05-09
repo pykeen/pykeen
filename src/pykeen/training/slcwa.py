3f2467bab6c9c3e22f80b287123ba2bda56edf13
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-05-09 10:08:53 +0200

    extract static sLCWA

--- src/pykeen/training/slcwa.py
+++ src/pykeen/training/slcwa.py
@@ -10,9 +10,12 @@ from class_resolver import HintOrType, OptionalKwargs
 from torch.utils.data import DataLoader
 
 from .training_loop import TrainingLoop
+from ..losses import Loss
+from ..models.base import Model
 from ..sampling import NegativeSampler
 from ..triples import CoreTriplesFactory
 from ..triples.instances import SLCWABatch, SLCWASampleType
+from ..typing import InductiveMode
 
 __all__ = [
     "SLCWATrainingLoop",
@@ -76,15 +79,17 @@ class SLCWATrainingLoop(TrainingLoop[SLCWASampleType, SLCWABatch]):
     def _get_batch_size(batch: SLCWABatch) -> int:  # noqa: D102
         return batch[0].shape[0]
 
-    # docstr-coverage: inherited
-    def _process_batch(
-        self,
+    @staticmethod
+    def _process_batch_static(
+        model: Model,
+        loss: Loss,
+        mode: InductiveMode,
         batch: SLCWABatch,
         start: int,
         stop: int,
         label_smoothing: float = 0.0,
         slice_size: Optional[int] = None,
-    ) -> torch.FloatTensor:  # noqa: D102
+    ) -> torch.FloatTensor:
         # Slicing is not possible in sLCWA training loops
         if slice_size is not None:
             raise AttributeError("Slicing is not possible for sLCWA training loops.")
@@ -93,7 +98,7 @@ class SLCWATrainingLoop(TrainingLoop[SLCWASampleType, SLCWABatch]):
         positive_batch, negative_batch, positive_filter = batch
 
         # send to device
-        positive_batch = positive_batch[start:stop].to(device=self.device)
+        positive_batch = positive_batch[start:stop].to(device=model.device)
         negative_batch = negative_batch[start:stop]
         if positive_filter is not None:
             positive_filter = positive_filter[start:stop]
@@ -104,22 +109,33 @@ class SLCWATrainingLoop(TrainingLoop[SLCWASampleType, SLCWABatch]):
 
         # Ensure they reside on the device (should hold already for most simple negative samplers, e.g.
         # BasicNegativeSampler, BernoulliNegativeSampler
-        negative_batch = negative_batch.to(self.device)
+        negative_batch = negative_batch.to(model.device)
 
         # Compute negative and positive scores
-        positive_scores = self.model.score_hrt(positive_batch, mode=self.mode)
-        negative_scores = self.model.score_hrt(negative_batch, mode=self.mode).view(*negative_score_shape)
+        positive_scores = model.score_hrt(positive_batch, mode=mode)
+        negative_scores = model.score_hrt(negative_batch, mode=mode).view(*negative_score_shape)
 
         return (
-            self.loss.process_slcwa_scores(
+            loss.process_slcwa_scores(
                 positive_scores=positive_scores,
                 negative_scores=negative_scores,
                 label_smoothing=label_smoothing,
                 batch_filter=positive_filter,
-                num_entities=self.model._get_entity_len(mode=self.mode),
+                num_entities=model._get_entity_len(mode=mode),
             )
-            + self.model.collect_regularization_term()
+            + model.collect_regularization_term()
         )
+
+    # docstr-coverage: inherited
+    def _process_batch(
+        self,
+        batch: SLCWABatch,
+        start: int,
+        stop: int,
+        label_smoothing: float = 0.0,
+        slice_size: Optional[int] = None,
+    ) -> torch.FloatTensor:  # noqa: D102
+        return self._process_batch_static()
 
     # docstr-coverage: inherited
     def _slice_size_search(
