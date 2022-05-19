8628f1927390bf91ff1b0067201371b28b6096f4
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-05-19 16:29:32 +0200

    parametrize test
    trigger ci

--- tests/test_lightning.py
+++ tests/test_lightning.py
@@ -1,62 +1,49 @@
 """Tests for training with PyTorch Lightning."""
+import itertools
 import unittest
-from typing import Any, ClassVar, Mapping
 
-import unittest_templates
+import pytest
+
+from pykeen.models import model_resolver
 
 try:
     import pytorch_lightning
 
-    from pykeen.contrib.lightning import LCWALitModule, LitModule, SLCWALitModule
+    from pykeen.contrib.lightning import lit_module_resolver
+
+    LIT_MODULES = lit_module_resolver.lookup_dict.keys()
 except ImportError:
     pytorch_lightning = None
-    LitModule = LCWALitModule = SLCWALitModule = None
+    lit_module_resolver = None
+    LIT_MODULES = []
+
+INTERACTIONS = model_resolver.lookup_dict.keys()
 
 
 @unittest.skipIf(
     pytorch_lightning is None, reason="PyTorch Lightning tests require `pytorch-lightning` to be installed"
 )
-class LitModuleTestCase(unittest_templates.GenericTestCase[LitModule]):
-    """A base test for PyTorch Lightning training."""
-
-    kwargs: ClassVar[Mapping[str, Any]] = dict(
-        # dataset
+# test combinations of models with training loops
+@pytest.mark.parametrize(("model", "training_loop"), itertools.product(INTERACTIONS, LIT_MODULES))
+def test_lit_training(model, training_loop):
+    """Test training models with PyTorch Lightning."""
+    lit = lit_module_resolver.make(
+        training_loop,
+        model=model,
+        # use a small configuration for testing
+        # TODO: this does not properly work for all models
         dataset="nations",
-        dataset_kwargs=None,
-        mode=None,
-        # model
-        model="distmult",  # TODO: we may want to train more than one interaction function
-        # choose a small embedding dim for fast tests
-        model_kwargs=dict(embedding_dim=16),
+        model_kwargs=dict(embedding_dim=8),
         batch_size=8,
-        learning_rate=1.0e-03,
-        label_smoothing=0.0,
-        # optimizer
-        optimizer="adam",
-        optimizer_kwargs=None,
     )
-
-    def test_training(self):
-        """Test training."""
-        pytorch_lightning.Trainer(
-            # automatically choose accelerator
-            accelerator="auto",
-            # defaults to TensorBoard; explicitly disabled here
-            logger=False,
-            # disable checkpointing
-            enable_checkpointing=False,
-            # fast run
-            max_epochs=2,
-        ).fit(model=self.instance)
-
-
-class LCWALitModuleTestCase(LitModuleTestCase):
-    """Tests for training with LCWA."""
-
-    cls = LCWALitModule
-
-
-class SLCWALitModuleTestCase(LitModuleTestCase):
-    """Tests for training with LCWA."""
-
-    cls = SLCWALitModule
+    trainer = pytorch_lightning.Trainer(
+        # automatically choose accelerator
+        accelerator="auto",
+        # defaults to TensorBoard; explicitly disabled here
+        logger=False,
+        # disable checkpointing
+        enable_checkpointing=False,
+        # fast run
+        max_epochs=2,
+    )
+    trainer.fit(model=lit)
