182cf0a62b84e5608883c9385fb2833867ce0873
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-29 12:59:57 +0200

    Lint
    Trigger CI

--- src/pykeen/datasets/__init__.py
+++ src/pykeen/datasets/__init__.py
@@ -38,6 +38,7 @@ from .dbpedia import DBpedia50
 from .drkg import DRKG
 from .ea import CN3l, EADataset, MTransEDataset, OpenEA, WK3l15k, WK3l120k
 from .freebase import FB15k, FB15k237
+from .globi import Globi
 from .hetionet import Hetionet
 from .kinships import Kinships
 from .literal_base import NumericPathDataset
@@ -50,7 +51,6 @@ from .umls import UMLS
 from .utils import get_dataset
 from .wd50k import WD50KT
 from .wikidata5m import Wikidata5M
-from .globi import Globi
 from .wordnet import WN18, WN18RR
 from .yago import YAGO310
 
