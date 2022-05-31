7d5e2b081ab328c6a1a7afec20a30eb6350185a7
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-05-31 10:26:11 +0200

    extend get_csr_matrix functionality

--- src/pykeen/models/baseline/utils.py
+++ src/pykeen/models/baseline/utils.py
@@ -23,13 +23,33 @@ def get_csr_matrix(
     row_indices: numpy.ndarray,
     col_indices: numpy.ndarray,
     shape: Tuple[int, int],
+    dtype: numpy.dtype = numpy.float32,
+    normalize: bool = True,
 ) -> scipy.sparse.csr_matrix:
-    """Create a sparse matrix, for the given non-zero locations."""
+    """
+    Create a sparse matrix, with ones for the given non-zero locations.
+
+    :param row_indices: shape: (nnz,)
+        the non-zero row indices
+    :param col_indices: shape: (nnz,)
+        the non-zero column indices
+    :param shape:
+        the matrix' shape
+    :param dtype:
+        the data type to use
+    :param normalize:
+        whether to perform row-wise l1 normalization
+
+    :return: shape: shape
+        a sparse csr matrix
+    """
     # create sparse matrix of absolute counts
     matrix = scipy.sparse.coo_matrix(
-        (numpy.ones(row_indices.shape, dtype=numpy.float32), (row_indices, col_indices)),
+        (numpy.ones(row_indices.shape, dtype=dtype), (row_indices, col_indices)),
         shape=shape,
     ).tocsr()
+    if not normalize:
+        return matrix
     # normalize to relative counts
     return sklearn_normalize(matrix, norm="l1")
 
