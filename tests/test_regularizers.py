ac406b5d5f2a40e49cdefc3fad11e4c86741b2e6
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-06-01 09:44:40 +0200

    chaining

--- tests/test_regularizers.py
+++ tests/test_regularizers.py
@@ -128,7 +128,7 @@ class OrthogonalityRegularizerTest(cases.RegularizerTestCase):
 
     def _expected_updated_term(self, inputs: Sequence[torch.FloatTensor]) -> torch.FloatTensor:
         assert len(inputs) == 2
-        return (functional.cosine_similarity(*inputs).pow(2) - self.instance_kwargs["epsilon"]).relu().sum()
+        return functional.cosine_similarity(*inputs).pow(2).subtract(self.instance_kwargs["epsilon"]).relu().sum()
 
     # docstr-coverage: inherited
     def test_forward(self) -> None:  # noqa: D102
