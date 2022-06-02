3477ceace30b73fbcaae4fb4e9aa05c19c9daa0f
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-06-02 14:08:15 +0200

    fix imports

--- src/pykeen/stoppers/early_stopping.py
+++ src/pykeen/stoppers/early_stopping.py
@@ -4,11 +4,12 @@
 
 import dataclasses
 import logging
-from dataclasses import dataclass
 import pathlib
 import tempfile
+from dataclasses import dataclass
 from typing import Any, Callable, List, Mapping, Optional, Union
 from uuid import uuid4
+
 import torch
 
 from .stopper import Stopper
