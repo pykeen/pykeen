cb59159de734b09d76fdb2e320c6bf4e88f165c2
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-06-08 11:49:32 +0200

    add tests

--- tests/test_nn/test_combination.py
+++ tests/test_nn/test_combination.py
@@ -11,6 +11,30 @@ class ConcatCombinationTest(cases.CombinationTestCase):
     cls = pykeen.nn.combination.ConcatCombination
 
 
+class ConcatAggregationCombinationTest(cases.CombinationTestCase):
+    """Test for concat + aggregation combination."""
+
+    cls = pykeen.nn.combination.ConcatAggregationCombination
+
+
+class ConcatProjectionCombinationTest(cases.CombinationTestCase):
+    """Test for concat + projection combination."""
+
+    cls = pykeen.nn.combination.ConcatProjectionCombination
+
+
+class ComplexSeparatedCombinationTest(cases.CombinationTestCase):
+    """Test for complex literal combination."""
+
+    cls = pykeen.nn.combination.ComplexSeparatedCombination
+
+
+class GatedCombinationTest(cases.CombinationTestCase):
+    """Test for gated combination."""
+
+    cls = pykeen.nn.combination.GatedCombination
+
+
 class CombinationMetaTestCase(unittest_templates.MetaTestCase[pykeen.nn.combination.Combination]):
     """Test for tests for combinations."""
 
