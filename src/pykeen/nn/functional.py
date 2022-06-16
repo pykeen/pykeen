fix dimension broadcasting

src/pykeen/nn/functional.py
@@ -250,8 +250,8 @@ def convkb_interaction(
     # here, kernel_size = (1, 3), C_in = 1, C_out = num_filters
     # => (num_filters, 1, 1, 3)
     x = tensor_sum(
-        conv.bias,
-        *(torch.einsum("...d,f->...df", x, w) for (x, w) in zip((h, r, t), conv.weight[:, 0, 0, :].unbind(dim=-1))),
+        conv.bias.unsqueeze(dim=-1),
+        *(torch.einsum("...d,f->...fd", x, w) for (x, w) in zip((h, r, t), conv.weight[:, 0, 0, :].unbind(dim=-1))),
     )
     x = activation(x)
 