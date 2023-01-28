6bc89ca94b099416317891a2f53f084a8a36eee9
Max Berrendorf <max.berrendorf@gmail.com>
2023-01-28 12:36:32 +0100

    forward additional_filter_triples

--- src/pykeen/training/callbacks.py
+++ src/pykeen/training/callbacks.py
@@ -52,7 +52,7 @@ to implement a gradient clipping callback:
 """
 
 import pathlib
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Union
 
 from class_resolver import ClassResolver, HintOrType, OptionalKwargs
 from torch import optim
@@ -279,6 +279,7 @@ class EvaluationLoopTrainingCallback(TrainingCallback):
         prefix: Optional[str] = None,
         evaluator: HintOrType[Evaluator] = None,
         evaluator_kwargs: OptionalKwargs = None,
+        additional_filter_triples: Optional[OneOrSequence[Union[MappedTriples, CoreTriplesFactory]]] = None,
         **kwargs,
     ):
         """
@@ -294,6 +295,8 @@ class EvaluationLoopTrainingCallback(TrainingCallback):
             the evaluator, or a hint thereof
         :param evaluator_kwargs:
             additional keyword-based parameters used for the evaluation instantiation
+        :param additional_filter_triples:
+            additional filter triples to use for creating the filter
         :param kwargs:
             additional keyword-based parameters passed to :meth:`EvaluationLoop.evaluate`
         """
@@ -306,6 +309,7 @@ class EvaluationLoopTrainingCallback(TrainingCallback):
         # lazy init
         self._evaluation_loop = None
         self.kwargs = kwargs
+        self.additional_filter_triples = additional_filter_triples
 
     @property
     def evaluation_loop(self):
@@ -315,6 +319,7 @@ class EvaluationLoopTrainingCallback(TrainingCallback):
                 triples_factory=self.factory,
                 evaluator=self.evaluator,
                 model=self.model,
+                additional_filter_triples=self.additional_filter_triples,
             )
         return self._evaluation_loop
 
