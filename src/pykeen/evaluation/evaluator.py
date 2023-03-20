f1f142b115e3ef57337383c381c05405c1c3abba
Max Berrendorf <max.berrendorf@gmail.com>
2023-03-20 19:37:36 +0100

    replace method by torch built-in

--- src/pykeen/evaluation/evaluator.py
+++ src/pykeen/evaluation/evaluator.py
@@ -659,8 +659,6 @@ def evaluate(
         # This should be a reasonable default size that works on most setups while being faster than batch_size=1
         batch_size = 32
         logger.info(f"No evaluation batch_size provided. Setting batch_size to '{batch_size}'.")
-    batches = cast(Iterable[np.ndarray], split_list_in_batches_iter(input_list=mapped_triples, batch_size=batch_size))
-
     # Show progressbar
     num_triples = mapped_triples.shape[0]
 
@@ -680,7 +678,7 @@ def evaluate(
         _tqdm_kwargs.update(tqdm_kwargs)
     with optional_context_manager(use_tqdm, tqdm(**_tqdm_kwargs)) as progress_bar, torch.inference_mode():
         # batch-wise processing
-        for batch in batches:
+        for batch in mapped_triples.split(split_size=batch_size):
             batch_size = batch.shape[0]
             relation_filter = None
             for target in targets:
