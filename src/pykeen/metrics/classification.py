0e4c62ce9548c7eaefc67bde1f13e2e5138a09be
Max Berrendorf <max.berrendorf@gmail.com>
2023-03-26 12:25:39 +0200

    remove aps from old module

--- src/pykeen/metrics/classification.py
+++ src/pykeen/metrics/classification.py
@@ -242,12 +242,6 @@ classifier_annotator.higher(
     link="https://en.wikipedia.org/wiki/F1_score",
 )
 classifier_annotator.higher(
-    rmc.average_precision_score,
-    name="Average Precision",
-    description="A summary statistic over the precision-recall curve",
-    link="https://scikit-learn.org/stable/modules/generated/sklearn.metrics.average_precision_score.html",
-)
-classifier_annotator.higher(
     rmc.matthews_correlation_coefficient,
     binarize=True,
     lower=-1.0,
