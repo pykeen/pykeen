c10f24370fa4ead2e3acba8535b924679c0d64c0
Max Berrendorf <max.berrendorf@gmail.com>
2023-03-30 22:39:53 +0200

    assertions for mypy?

--- src/pykeen/triples/triples_factory.py
+++ src/pykeen/triples/triples_factory.py
@@ -89,6 +89,8 @@ def create_entity_mapping(
     if triples is None:
         if heads is None or tails is None:
             raise ValueError("If no triples are provided, both, heads and tails must be provided.")
+        assert heads is not None
+        assert tails is not None
     else:
         heads, tails = triples[:, 0], triples[:, 2]
 
