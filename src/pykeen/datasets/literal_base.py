9639070418fc34443b5ce506009ac03f8beb5e7f
Antonis Klironomos <antonisklironomos@gmail.com>
2023-06-19 19:10:14 +0200

    Fix init arguments of NumericPathDataset class

--- src/pykeen/datasets/literal_base.py
+++ src/pykeen/datasets/literal_base.py
@@ -3,6 +3,7 @@
 """Base classes for literal datasets."""
 
 import pathlib
+from re import Pattern
 from typing import TextIO, Union
 
 from class_resolver import Hint, OptionalKwargs
@@ -29,10 +30,10 @@ class NumericPathDataset(LazyDataset):
         literals_path: Union[str, pathlib.Path, TextIO],
         eager: bool = False,
         create_inverse_triples: bool = False,
-        numeric_triples_preprocessing: Hint[NdArrayInOutCallable] = None,
-        numeric_triples_preprocessing_kwargs: OptionalKwargs = None,
-        numeric_literals_preprocessing: Hint[NdArrayInOutCallable] = None,
-        numeric_literals_preprocessing_kwargs: OptionalKwargs = None,
+        relation_regex: Union[Pattern, str, None] = None,
+        min_occurrence: int = 0,
+        literal_matrix_preprocessing: Hint[NdArrayInOutCallable] = None,
+        literal_matrix_preprocessing_kwargs: OptionalKwargs = None,
     ) -> None:
         """Initialize the dataset.
 
@@ -42,12 +43,11 @@ class NumericPathDataset(LazyDataset):
         :param literals_path: Path to the literals triples file or literal triples file
         :param eager: Should the data be loaded eagerly? Defaults to false.
         :param create_inverse_triples: Should inverse triples be created? Defaults to false.
-        :param numeric_triples_preprocessing: function for preprocessing numeric attributive triples, defaults to None
-               e.g. ..utils.filter_triples_by_relations() can be used or a custom function to add/remove/edit triples
-        :param numeric_triples_preprocessing_kwargs: args to pass to the above preprocessing function, defaults to None
-        :param numeric_literals_preprocessing: function for preprocessing numeric literals, defaults to None
+        :param relation_regex: an optional filter-regex for attribute relations
+        :param min_occurrence: a minimum number of occurrence to be considered for filtering attribute relations
+        :param literal_matrix_preprocessing: function for preprocessing numeric literals, defaults to None
                e.g. ..utils.minmax_normalize() can be used or a custom function to modify literals
-        :param numeric_literals_preprocessing_kwargs: args to pass to the above preprocessing function, defaults to None
+        :param literal_matrix_preprocessing_kwargs: args to pass to the above preprocessing function, defaults to None
         """
         self.training_path = training_path
         self.testing_path = testing_path
@@ -55,10 +55,10 @@ class NumericPathDataset(LazyDataset):
         self.literals_path = literals_path
 
         self._create_inverse_triples = create_inverse_triples
-        self.numeric_triples_preprocessing = numeric_triples_preprocessing
-        self.numeric_triples_preprocessing_kwargs = numeric_triples_preprocessing_kwargs
-        self.numeric_literals_preprocessing = numeric_literals_preprocessing
-        self.numeric_literals_preprocessing_kwargs = numeric_literals_preprocessing_kwargs
+        self.relation_regex = relation_regex
+        self.min_occurrence = min_occurrence
+        self.literal_matrix_preprocessing = literal_matrix_preprocessing
+        self.literal_matrix_preprocessing_kwargs = literal_matrix_preprocessing_kwargs
 
         if eager:
             self._load()
@@ -69,20 +69,20 @@ class NumericPathDataset(LazyDataset):
             path=self.training_path,
             path_to_numeric_triples=self.literals_path,
             create_inverse_triples=self._create_inverse_triples,
-            numeric_triples_preprocessing=self.numeric_triples_preprocessing,
-            numeric_triples_preprocessing_kwargs=self.numeric_triples_preprocessing_kwargs,
-            numeric_literals_preprocessing=self.numeric_literals_preprocessing,
-            numeric_literals_preprocessing_kwargs=self.numeric_literals_preprocessing_kwargs,
+            relation_regex=self.relation_regex,
+            min_occurrence=self.min_occurrence,
+            literal_matrix_preprocessing=self.literal_matrix_preprocessing,
+            literal_matrix_preprocessing_kwargs=self.literal_matrix_preprocessing_kwargs,
         )
         self._testing = self.triples_factory_cls.from_path(
             path=self.testing_path,
             path_to_numeric_triples=self.literals_path,
             entity_to_id=self._training.entity_to_id,  # share entity index with training
             relation_to_id=self._training.relation_to_id,  # share relation index with training
-            numeric_triples_preprocessing=self.numeric_triples_preprocessing,
-            numeric_triples_preprocessing_kwargs=self.numeric_triples_preprocessing_kwargs,
-            numeric_literals_preprocessing=self.numeric_literals_preprocessing,
-            numeric_literals_preprocessing_kwargs=self.numeric_literals_preprocessing_kwargs,
+            relation_regex=self.relation_regex,
+            min_occurrence=self.min_occurrence,
+            literal_matrix_preprocessing=self.literal_matrix_preprocessing,
+            literal_matrix_preprocessing_kwargs=self.literal_matrix_preprocessing_kwargs,
         )
 
     def _load_validation(self) -> None:
@@ -94,10 +94,10 @@ class NumericPathDataset(LazyDataset):
             path_to_numeric_triples=self.literals_path,
             entity_to_id=self._training.entity_to_id,  # share entity index with training
             relation_to_id=self._training.relation_to_id,  # share relation index with training
-            numeric_triples_preprocessing=self.numeric_triples_preprocessing,
-            numeric_triples_preprocessing_kwargs=self.numeric_triples_preprocessing_kwargs,
-            numeric_literals_preprocessing=self.numeric_literals_preprocessing,
-            numeric_literals_preprocessing_kwargs=self.numeric_literals_preprocessing_kwargs,
+            relation_regex=self.relation_regex,
+            min_occurrence=self.min_occurrence,
+            literal_matrix_preprocessing=self.literal_matrix_preprocessing,
+            literal_matrix_preprocessing_kwargs=self.literal_matrix_preprocessing_kwargs,
         )
 
     def __repr__(self) -> str:  # noqa: D105
