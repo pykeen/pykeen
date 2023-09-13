ef2e6cb42ad3bc57034e5aefc5ef9d92c3ab8e01
Max Berrendorf <max.berrendorf@gmail.com>
2023-09-13 12:28:46 +0200

    fix score shape

--- src/pykeen/predict.py
+++ src/pykeen/predict.py
@@ -1029,7 +1029,7 @@ def predict_all(
         logger.warning(
             "Not providing k to `predict_all` entails huge memory requirements for reasonably-sized knowledge graphs.",
         )
-        consumer = AllScoreConsumer(num_entities=num_entities, num_relations=model.num_relations)
+        consumer = AllScoreConsumer(num_entities=num_entities, num_relations=model.num_real_relations)
     else:
         consumer = TopKScoreConsumer(k=k, device=model.device)
     dataset = AllPredictionDataset(num_entities=num_entities, num_relations=model.num_real_relations, target=target)
