a541a3b581c5f57c5e36061fd43515b536bb3938
Max Berrendorf <max.berrendorf@gmail.com>
2023-09-23 16:42:16 +0200

    🏗️🌊 Support resolving non-rank-based metrics (#1237)
    * [x] convert classification metrics into classes (parallel to
    rank-based metrics)
    * [x] add side/prediction target support for classification metrics
    * [x] introduce `MetricKey` base class, with `ClassificationMetricKey`
    and `RankBasedMetricKey`; those implement the conversion of strings into
    fully qualified metric keys (i.e., inferring defaults, and resolving
    synonyms and spelling variants)
    * [x] annotate base `Evaluator` with generic metric key type - this
    allows static code analysis to check whether we use the correct key with
    the correct evaluator / evaluation results.
    
    Fix #1235
    
    ---------
    
    Co-authored-by: Charles Tapley Hoyt <cthoyt@gmail.com>

--- src/pykeen/hpo/hpo.py
+++ src/pykeen/hpo/hpo.py
@@ -23,7 +23,6 @@ from ..constants import USER_DEFINED_CODE
 from ..datasets import dataset_resolver, has_dataset
 from ..datasets.base import Dataset
 from ..evaluation import Evaluator, evaluator_resolver
-from ..evaluation.ranking_metric_lookup import MetricKey
 from ..losses import Loss, loss_resolver
 from ..lr_schedulers import LRScheduler, lr_scheduler_resolver, lr_schedulers_hpo_defaults
 from ..models import Model, model_resolver
@@ -796,12 +795,12 @@ def hpo_pipeline(
         raise ValueError("can not use early stopping while optimizing epochs")
 
     # 8. Evaluation
-    evaluator_cls: Type[Evaluator] = evaluator_resolver.lookup(evaluator)
+    evaluator_cls = evaluator_resolver.lookup(evaluator)
     study.set_user_attr("evaluator", evaluator_cls.get_normalized_name())
     logger.info(f"Using evaluator: {evaluator_cls}")
-    metric = MetricKey.normalize(metric)
-    study.set_user_attr("metric", metric)
-    logger.info(f"Attempting to {direction} {metric}")
+    resolved_metric = evaluator_cls.metric_result_cls.key_to_string(metric)
+    study.set_user_attr("metric", resolved_metric)
+    logger.info(f"Attempting to {direction} {resolved_metric}")
     study.set_user_attr("filter_validation_when_testing", filter_validation_when_testing)
     logger.info("Filter validation triples when testing: %s", filter_validation_when_testing)
 
@@ -858,7 +857,7 @@ def hpo_pipeline(
         result_tracker=result_tracker,
         result_tracker_kwargs=result_tracker_kwargs,
         # Optuna Misc.
-        metric=metric,
+        metric=resolved_metric,
         save_model_directory=save_model_directory,
         # Pipeline Misc.
         device=device,
