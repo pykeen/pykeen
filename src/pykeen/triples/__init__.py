4d1e9431274f9de4aef0c341bd300eda3c0394ed
Max Berrendorf <max.berrendorf@gmail.com>
2024-02-27 21:49:09 +0100

    remove trailing whitespcae

--- src/pykeen/triples/__init__.py
+++ src/pykeen/triples/__init__.py
@@ -9,8 +9,8 @@ on them, most of PyKEEN assumes that the set of entities has been that the set o
 transformed so that they are identified by integers from $[0, \ldots, E)$, where $E$ is the number of
 entities. A similar assumption is made for relations with their indices of $[0, \ldots, R]$.
 
-This module includes classes and methods for loading and transforming triples from other formats 
-into the index-based format, as well as advanced methods for creating leakage-free training and 
+This module includes classes and methods for loading and transforming triples from other formats
+into the index-based format, as well as advanced methods for creating leakage-free training and
 test splits and analyzing data distribution.
 
 Basic Handling
@@ -22,17 +22,17 @@ relations, as well as information about the use of inverse relations (which arti
 the number of relations).
 
 To store information about triples, there is the :class:`CoreTriplesFactory`. It extends
-:class:`KGInfo` by additionally storing a set of index-based triples, in the form of a 
-3-column matrix. It also allows to store arbitrary metadata in the form of a 
+:class:`KGInfo` by additionally storing a set of index-based triples, in the form of a
+3-column matrix. It also allows to store arbitrary metadata in the form of a
 (JSON-compatible) dictionary. It also adds support for serialization, i.e. saving and loading
 to a file, as well as filter operations and utility methods to create dataframes for
 further (external) processing.
 
 Finally, there is :class:`TriplesFactory`, which adds mapping of string-based entity and relation
-names to IDs. This class also provides rich factory methods that allow creating mappings from 
+names to IDs. This class also provides rich factory methods that allow creating mappings from
 string-based triples alone, loading triples from sufficiently similar external file formats such
 as TSV or CSV, and converting back and forth between label-based and index-based formats.
-It also extends serialization to ensure that the string-to-index mappings are included along 
+It also extends serialization to ensure that the string-to-index mappings are included along
 with the files.
 
 Splitting
@@ -66,7 +66,6 @@ Analysis
 We also provide methods for analyzing knowledge graphs.
 These include simple statistics such as the number of entities or relations (in :mod:`pykeen.triples.stats`),
 as well as advanced analysis of relational patterns (:mod:`pykeen.triples.analysis`).
-
 """
 
 from .instances import Instances, LCWAInstances, SLCWAInstances
