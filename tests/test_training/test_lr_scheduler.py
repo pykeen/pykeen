b3284ebf6e33cc85d20bd82f1a995da020681ede
Max Berrendorf <max.berrendorf@gmail.com>
2024-05-04 17:25:18 +0200

    add smoketest

---
+++ tests/test_training/test_lr_scheduler.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+import pytest
+from class_resolver import HintOrType
+from torch.optim import lr_scheduler
+
+from pykeen.pipeline import pipeline
+
+
+@pytest.mark.parametrize("cls, kwargs", [("CosineAnnealingWarmRestarts", {"T_0": 10})])
+def test_lr_scheduler(cls: HintOrType[lr_scheduler.LRScheduler], kwargs: dict[str, Any] | None):
+    """Smoke-test for training with learning rate schedule."""
+    pipeline(
+        dataset="nations",
+        model="mure",
+        model_kwargs=dict(embedding_dim=2),
+        training_kwargs=dict(num_epochs=1),
+        lr_scheduler=cls,
+        lr_scheduler_kwargs=kwargs,
+    )
