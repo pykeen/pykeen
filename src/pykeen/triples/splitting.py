3f808dd564a3d0fc098cbdf4da39523300f5ba7c
Max Berrendorf <max.berrendorf@gmail.com>
2024-08-01 22:34:05 +0200

    update docstr

--- src/pykeen/triples/splitting.py
+++ src/pykeen/triples/splitting.py
@@ -482,7 +482,8 @@ def split(
         it does not necessarily have to move all of them, but it might be significantly slower since it moves one
         triple at a time.
     :param method:
-        The name of the method to use, cf. :data:`splitter_resolver`. Defaults to "coverage".
+        The name of the method to use, cf. :data:`splitter_resolver`. Defaults to "coverage", i.e.,
+        :class:`CoverageSplitter`.
 
     :return:
         A partition of triples, which are split (approximately) according to the ratios.
