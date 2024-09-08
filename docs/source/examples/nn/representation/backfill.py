a9421b0aa3e8adb58e685919f25f149d2a9ef4de
Max Berrendorf <max.berrendorf@gmail.com>
2024-09-08 10:46:31 +0200

    remove #%%

--- docs/source/examples/nn/representation/backfill.py
+++ docs/source/examples/nn/representation/backfill.py
@@ -6,10 +6,8 @@ from pykeen.datasets import get_dataset
 from pykeen.nn import BackfillRepresentation, Embedding, init
 from pykeen.pipeline import pipeline
 
-# %%
 dataset = get_dataset(dataset="nations")
 
-# %%
 # we start by creating the representation for those entities where we have pre-trained features
 # here we simulate this for a set of Asian countries
 embedding_dim = 32
@@ -18,23 +16,19 @@ pre_trained_embeddings = torch.rand(len(known_ids), embedding_dim)
 initializer = init.PretrainedInitializer(tensor=pre_trained_embeddings)
 base_repr = Embedding(max_id=len(known_ids), shape=(embedding_dim,), initializer=initializer, trainable=False)
 
-# %%
 # Next, we directly create representations for the remaining ones using the backfill representation.
 # To do this, we need to create an iterable (e.g., a set) of all of the entity IDs that are in the base
 # representation. Then, the assignments to the base representation and an auxillary representation are
 # automatically generated for the base class.
 entity_repr = BackfillRepresentation(base_ids=known_ids, max_id=dataset.num_entities, base=base_repr)
 
-# %%
 # We assume that we do not have any pre-trained information for relations here for simplicity and train
 # them from scratch.
 relation_repr = Embedding(max_id=dataset.num_relations, shape=(embedding_dim,))
 
-# %%
 # The combined representation can now be used as any other representation, e.g., to train a model with
-# :class:`pykeen.nn.modules.DistMultInteraction` interaction:
+# distmult interaction:
 result = pipeline(
-    # model=ERModel,
     dataset=dataset,
     interaction="distmult",
     dimensions=dict(d=embedding_dim),
