5e402f30b1fc0f3223062642c6e28312ead8ac69
Max Berrendorf <max.berrendorf@gmail.com>
2024-09-14 22:40:41 +0200

    update ERMLP docstr

--- src/pykeen/models/unimodal/ermlp.py
+++ src/pykeen/models/unimodal/ermlp.py
@@ -18,18 +18,11 @@ __all__ = [
 class ERMLP(ERModel):
     r"""An implementation of ERMLP from [dong2014]_.
 
-    ERMLP is a multi-layer perceptron based approach that uses a single hidden layer and represents entities and
-    relations as vectors. In the input-layer, for each triple the embeddings of head, relation, and tail are
-    concatenated and passed to the hidden layer. The output-layer consists of a single neuron that computes the
-    plausibility score of the triple:
+    This model represents both entities and relations as $d$-dimensional vectors stored in an
+    :class:`~pykeen.nn.representation.Embedding` matrix.
+    The representations are then passed to the :class:`~pykeen.nn.modules.ERMLPInteraction` function to obtain
+    scores.
 
-    .. math::
-
-        f(h,r,t) = \textbf{w}^{T} g(\textbf{W} [\textbf{h}; \textbf{r}; \textbf{t}]),
-
-    where $\textbf{W} \in \mathbb{R}^{k \times 3d}$ represents the weight matrix of the hidden layer,
-    $\textbf{w} \in \mathbb{R}^{k}$, the weights of the output layer, and $g$ denotes an activation function such
-    as the hyperbolic tangent.
     ---
     name: ER-MLP
     citation:
