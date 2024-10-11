e01601293128a5f1e40d0c9ea352beadb3c0e293
Max Berrendorf <max.berrendorf@gmail.com>
2024-10-11 20:44:01 +0200

    improve docstr formatting in code

--- src/pykeen/models/unimodal/pair_re.py
+++ src/pykeen/models/unimodal/pair_re.py
@@ -71,15 +71,19 @@ class PairRE(ERModel):
         r"""Initialize the model.
 
         :param embedding_dim: The entity embedding dimension $d$.
+
         :param p: The $l_p$ norm.
         :param power_norm: Should the power norm be used?
+
         :param entity_initializer: Entity initializer function. Defaults to :func:`torch.nn.init.uniform_`
         :param entity_initializer_kwargs: Keyword arguments to be used when calling the entity initializer
         :param entity_normalizer: Entity normalizer function. Defaults to :func:`torch.nn.functional.normalize`
         :param entity_normalizer_kwargs: Keyword arguments to be used when calling the entity normalizer
+
         :param relation_initializer: Relation initializer function. Defaults to :func:`torch.nn.init.uniform_`
         :param relation_initializer_kwargs: Keyword arguments to be used when calling the relation initializer
-        :param kwargs: Remaining keyword arguments passed through to :class:`pykeen.models.ERModel`.
+
+        :param kwargs: Remaining keyword arguments passed through to :class:`~pykeen.models.ERModel`.
         """
         entity_normalizer_kwargs = _resolve_kwargs(
             kwargs=entity_normalizer_kwargs,
