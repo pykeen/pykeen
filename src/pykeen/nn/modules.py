1a212a85ca7832ad67c9683e760b7d1d95b42437
Max Berrendorf <max.berrendorf@gmail.com>
2024-10-23 22:21:25 +0200

    expose

--- src/pykeen/nn/modules.py
+++ src/pykeen/nn/modules.py
@@ -67,6 +67,7 @@ __all__ = [
     "NormBasedInteraction",
     # Adapter classes
     "MonotonicAffineTransformationInteraction",
+    "ClampedInteraction",
     # Concrete Classes
     "AutoSFInteraction",
     "BoxEInteraction",
