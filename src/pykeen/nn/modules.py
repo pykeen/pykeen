309ecd0de8b4558667936ac478cf25cb1a841de1
Max Berrendorf <max.berrendorf@gmail.com>
2025-01-08 20:38:56 +0100

    remove unused import

--- src/pykeen/nn/modules.py
+++ src/pykeen/nn/modules.py
@@ -8,7 +8,7 @@ import logging
 import math
 from abc import ABC, abstractmethod
 from collections import Counter
-from collections.abc import Collection, Iterable, Mapping, MutableMapping, Sequence
+from collections.abc import Collection, Iterable, Mapping, Sequence
 from operator import itemgetter
 from typing import Any, ClassVar, Generic, Optional, Union, cast, overload
 
