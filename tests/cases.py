403d508d3903d5c7253c2ba9526678872b9fbf5a
Max Berrendorf <max.berrendorf@gmail.com>
2025-02-06 00:09:05 +0100

    tox -e format

--- tests/cases.py
+++ tests/cases.py
@@ -1271,7 +1271,7 @@ class ModelTestCase(unittest_templates.GenericTestCase[Model]):
             msg=f"""
 Command
 =======
-$ pykeen train {self.cls.__name__.lower()} {' '.join(map(str, args))}
+$ pykeen train {self.cls.__name__.lower()} {" ".join(map(str, args))}
 
 Output
 ======
@@ -1283,7 +1283,7 @@ Exception
 
 Traceback
 =========
-{''.join(traceback.format_tb(result.exc_info[2]))}
+{"".join(traceback.format_tb(result.exc_info[2]))}
             """,
         )
 
