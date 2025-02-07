d2b1d5a5a5bc992e8d8a5877e3b77f084ff04255
Max Berrendorf <max.berrendorf@gmail.com>
2025-02-07 17:58:41 +0100

    update example

--- docs/source/examples/tutorial/inductive_lp/fully_inductive_dataset.py
+++ docs/source/examples/tutorial/inductive_lp/fully_inductive_dataset.py
@@ -2,36 +2,26 @@
 
 import logging
 
-import torch
-
 from pykeen.datasets import get_dataset
 from pykeen.datasets.inductive.base import EagerInductiveDataset
-from pykeen.triples.splitting import split_fully_inductive
-from pykeen.triples.triples_factory import TriplesFactory
 
 logging.basicConfig(level=logging.INFO)
 
 # we use all of CodexSmall's data as source graph
 dataset = get_dataset(dataset="CodexSmall")
-mapped_triples = torch.cat([tf.mapped_triples for tf in dataset.factory_dict.values()])
+dataset.summarize()
+tf_all = dataset.training.merge(dataset.validation, dataset.testing)
 
 # create a fully inductive split with two evaluation parts (validation & test)
-training, inference, validation, test = split_fully_inductive(
-    mapped_triples=mapped_triples,
-    entity_split_train_ratio=0.5,
-    evaluation_triples_ratios=(0.8, 0.1),
-    random_state=42,
+tf_training, tf_inference, tf_testing, tf_validation = tf_all.split_fully_inductive(
+    entity_split_train_ratio=0.5, evaluation_triples_ratios=(0.8, 0.1), random_state=42
 )
-tf_training, tf_inference, tf_validation, tf_testing = (
-    TriplesFactory(
-        mapped_triples=mapped_triples, entity_to_id=dataset.entity_to_id, relation_to_id=dataset.relation_to_id
-    )
-    for mapped_triples in (training, inference, validation, test)
-)
+
 inductive_dataset = EagerInductiveDataset(
     transductive_training=tf_training,
     inductive_inference=tf_inference,
     inductive_testing=tf_testing,
     inductive_validation=tf_validation,
+    create_inverse_triples=False,
 )
-inductive_dataset.summarize()
+inductive_dataset.summarize(show_examples=False)
