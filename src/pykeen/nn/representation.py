37b6e71045445eaf2a40517911ff5d913850707d
Dwayne Thomson <48846485+chartsNgraphs@users.noreply.github.com>
2025-02-07 13:37:21 -0500

    Update src/pykeen/nn/representation.py
    Co-authored-by: Max Berrendorf <max.berrendorf@gmail.com>

--- src/pykeen/nn/representation.py
+++ src/pykeen/nn/representation.py
@@ -1573,7 +1573,7 @@ class BackfillRepresentation(PartitionRepresentation):
 
         # If the maximum id is not greater than the maximum base id, the backfill representation creation will fail at a later stage.
         if max_id <= max(base_ids):
-            raise ValueError(f"max_id={max_id}: Value must be greater than the largest base_id ({max(base_ids)})")
+            raise ValueError(f"{max_id=}: Value must be greater than the largest base_id ({max(base_ids)=})")
 
         # import here to avoid cyclic import
         from . import representation_resolver
