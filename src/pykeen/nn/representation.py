76597d98b5515c84e73b3a7cc8ae39a7c40f5022
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-02-15 17:17:07 +0100

    Update representation.py

--- src/pykeen/nn/representation.py
+++ src/pykeen/nn/representation.py
@@ -805,10 +805,16 @@ class CompGCNLayer(nn.Module):
         # prepare for inverse relations
         edge_type = 2 * edge_type
         # update entity representations: mean over self-loops / forward edges / backward edges
+
+        # FIXME do these need to get passed the relation inverter? or how should
+        #  this math get updated
+        inverse_edge_index = edge_index.flip(0)
+        inverse_edge_type = edge_type + 1
+
         x_e = (
             self.composition(x_e, self.self_loop) @ self.w_loop
             + self.message(x_e=x_e, x_r=x_r, edge_index=edge_index, edge_type=edge_type, weight=self.w_fwd)
-            + self.message(x_e=x_e, x_r=x_r, edge_index=edge_index.flip(0), edge_type=edge_type + 1, weight=self.w_bwd)
+            + self.message(x_e=x_e, x_r=x_r, edge_index=inverse_edge_index, edge_type=inverse_edge_type, weight=self.w_bwd)
         ) / 3
 
         if self.bias:
