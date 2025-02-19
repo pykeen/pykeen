2f9a11369030a1306384a923a94783f7ddb535d7
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-02-19 13:42:20 +0100

    Update naming and avoid pandas warning

--- docs/source/examples/nn/representation/low_rank_mixture.py
+++ docs/source/examples/nn/representation/low_rank_mixture.py
@@ -14,7 +14,7 @@ dataset = get_dataset(dataset="CoDExSmall", dataset_kwargs=dict(create_inverse_t
 # set up relation representations as a mixture (~soft clustering) with 5 components
 embedding_dim = 32
 num_components = 5
-r = LowRankRepresentation(
+relation_representation = LowRankRepresentation(
     max_id=dataset.num_relations,
     shape=embedding_dim,
     num_bases=num_components,
@@ -25,25 +25,27 @@ model = ERModel[FloatTensor, FloatTensor, FloatTensor](
     triples_factory=dataset.training,
     interaction="distmult",
     entity_representations_kwargs=dict(shape=embedding_dim),
-    relation_representations=r,
+    relation_representations=relation_representation,
 )
 result = pipeline(dataset=dataset, model=model, training_kwargs=dict(num_epochs=20))
 
 # keys are Wikidata IDs, which are the "labels" in CoDEx, and values
 # are the concatenation of the Wikidata label + description
-relation_to_text = WikidataTextCache().get_texts_dict(dataset.relation_to_id)
+wikidata_id_to_label = WikidataTextCache().get_texts_dict(dataset.relation_to_id)
 
 # use the mixture weights
-weights = r.weight().detach().cpu().numpy()
-_data = []
-for label, relation_index in dataset.relation_to_id.items():
-    text = relation_to_text[label]
-    for c, w in enumerate(weights[relation_index]):
-        _data.append((relation_index, label, text, c, w))
-df = pandas.DataFrame(data=_data, columns=["relation_index", "wikidata-id", "text", "component_index", "weight"])
+relation_weights = relation_representation.weight().detach().cpu().numpy()
+rows = [
+    (relation_index, wikidata_id, wikidata_id_to_label[wikidata_id], component, weight)
+    for wikidata_id, relation_index in dataset.relation_to_id.items()
+    for component, weight in enumerate(relation_weights[relation_index])
+]
+df = pandas.DataFrame(data=rows, columns=["relation_index", "wikidata-id", "text", "component_index", "weight"])
 
 
 # For each component, look at the relations that are most assigned to it
 print(
-    df.groupby(by="component_index").apply(lambda g: g.nlargest(3, columns="weight"))[["wikidata-id", "text", "weight"]]
+    df.groupby(by="component_index").apply(lambda g: g.nlargest(3, columns="weight"), include_groups=False)[
+        ["wikidata-id", "text", "weight"]
+    ]
 )
