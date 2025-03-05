a5caae4d8b6c8eecdeb2acb5e158f566598811e3
Alain Villagrasa <92668599+Str1ien@users.noreply.github.com>
2025-03-05 20:36:19 +0100

    Update docs/source/examples/first_steps/using_learned_embeddings.py
    Co-authored-by: Max Berrendorf <max.berrendorf@gmail.com>

--- docs/source/examples/first_steps/using_learned_embeddings.py
+++ docs/source/examples/first_steps/using_learned_embeddings.py
@@ -41,7 +41,7 @@ entity_embedding_tensor = entity_embeddings(indices=torch.as_tensor([1, 3]))
 # %%
 # get embeddings for some entity labels
 entity_ids = triples_factory.entities_to_ids(["china", "egypt"])
-entity_embedding_tensor = entity_embeddings(indices=torch.as_tensor([ent_id for ent_id in entity_ids]))
+entity_embedding_tensor = entity_embeddings(indices=torch.as_tensor(entity_ids))
 
 # %%
 # detach tensor, move to cpu, and convert to numpy
