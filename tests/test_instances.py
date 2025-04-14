1f3cac420f3c752116e88150ba2f311f4b65e5fc
Max Berrendorf <max.berrendorf@gmail.com>
2025-04-14 21:16:01 +0200

    update tests

--- tests/test_instances.py
+++ tests/test_instances.py
@@ -3,8 +3,10 @@
 from collections.abc import MutableMapping
 from typing import Any
 
+import numpy
+
 from pykeen.triples import LCWAInstances, SLCWAInstances
-from pykeen.triples.instances import BatchedSLCWAInstances, SubGraphSLCWAInstances
+from pykeen.triples.instances import BatchedSLCWAInstances, LCWABatch, SLCWABatch, SubGraphSLCWAInstances
 from tests import cases
 
 
@@ -20,8 +22,21 @@ class LCWAInstancesTestCase(cases.TrainingInstancesTestCase):
         kwargs["compressed"] = other_instance.compressed
         return kwargs
 
-    def _get_expected_length(self) -> int:
-        return self.factory.mapped_triples[:, :2].unique(dim=0).shape[0]
+    def test_getitem(self) -> None:
+        """Test item access."""
+        self.instance: LCWAInstances
+        item = self.instance[0]
+        assert isinstance(item, dict)
+        assert {"pairs", "target"}.issubset(item.keys())
+        assert item["pairs"].shape == (2,)
+        assert item["target"].shape == (self.factory.num_entities,)
+
+    def test_construction(self) -> None:
+        """Test proper construction."""
+        self.instance: LCWAInstances
+        # unique pairs
+        assert len(self.instance.pairs) == len(numpy.unique(self.instance.pairs, axis=0))
+        # TODO: we could check whether can recover all mapped_triples from pairs + targets
 
 
 class SLCWAInstancesTestCase(cases.TrainingInstancesTestCase):
@@ -34,8 +49,17 @@ class SLCWAInstancesTestCase(cases.TrainingInstancesTestCase):
         kwargs["mapped_triples"] = self.factory.mapped_triples
         return kwargs
 
-    def _get_expected_length(self) -> int:
-        return self.factory.mapped_triples.shape[0]
+    def test_getitem(self) -> None:
+        """Test item access."""
+        self.instance: SLCWAInstances
+        item = self.instance[0]
+        assert {"positives", "negatives"}.issubset(item.keys())
+        assert item["positives"].shape == (3,)
+        assert item["negatives"].shape == (self.instance.sampler.num_negs_per_pos, 3)
+        if "pos_weights" in item:
+            assert item["pos_weights"].shape == item["positives"].shape
+        if "neg_weights" in item:
+            assert item["neg_weights"].shape == item["negatives"].shape
 
 
 class BatchedSLCWAInstancesTestCase(cases.BatchSLCWATrainingInstancesTestCase):
