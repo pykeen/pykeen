828659233b3405680bf1c7efe275af4fad304bce
Max Berrendorf <max.berrendorf@gmail.com>
2025-04-18 21:46:03 +0200

    add example

---
+++ docs/source/examples/training/weights.py
@@ -0,0 +1,11 @@
+"""Training with sample weights."""
+
+from pykeen.datasets.utils import get_dataset
+from pykeen.pipeline import pipeline
+from pykeen.triples.weights import RelationSampleWeighter
+
+dataset = get_dataset(dataset="CodexSmall")
+sample_weighter = RelationSampleWeighter.inverse_relation_frequency(mapped_triples=dataset.training.mapped_triples)
+result = pipeline(
+    dataset=dataset, model="MuRE", loss="BCEWithLogits", training_loop_kwargs=dict(sample_weighter=sample_weighter)
+)
