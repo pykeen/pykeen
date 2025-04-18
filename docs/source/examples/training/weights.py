bedd7e6491d0b5334ad6a473333ceebd7a035926
Max Berrendorf <max.berrendorf@gmail.com>
2025-04-18 21:56:27 +0200

    update example to LCWA

--- docs/source/examples/training/weights.py
+++ docs/source/examples/training/weights.py
@@ -7,5 +7,9 @@ from pykeen.triples.weights import RelationSampleWeighter
 dataset = get_dataset(dataset="CodexSmall")
 sample_weighter = RelationSampleWeighter.inverse_relation_frequency(mapped_triples=dataset.training.mapped_triples)
 result = pipeline(
-    dataset=dataset, model="MuRE", loss="BCEWithLogits", training_loop_kwargs=dict(sample_weighter=sample_weighter)
+    dataset=dataset,
+    model="MuRE",
+    loss="BCEWithLogits",
+    training_loop_kwargs=dict(sample_weighter=sample_weighter),
+    training_loop="lcwa",
 )
