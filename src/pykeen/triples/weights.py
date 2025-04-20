eab00479b336fd664f4519ab71e901313b9384a4
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-04-20 10:56:18 +0200

    Update weights.py

--- src/pykeen/triples/weights.py
+++ src/pykeen/triples/weights.py
@@ -1,4 +1,18 @@
-"""Sample weights."""
+"""Loss weighting for triples.
+
+In the current implementation, these weights are loss weights, i.e. they influence how much a given triple is weighted
+in the loss function. Thus, they are primarily a tool to shape your optimization criterion, e.g. to focus more or less
+on certain types of triples, e.g. because they are not that important to you, or you want to counteract imbalances, etc.
+
+They are not:
+
+1. (static) weights for message passing - we already support those, cf. :class:`pykeen.nn.weighting.EdgeWeighting`
+2. weights in the sense of how reliable a particular triple is. If you have a source of uncertainty about triples, you
+   might want to set lower loss weights for uncertain triples (in the sense of "it does not matter so much if the model
+   reproduces an uncertain label"). Another alternative would be to use softer labels for them (i.e., try to predict the
+   uncertain label directly), or more advanced ways of incorporating uncertainty.
+3. more general qualifiers on the triples, e.g., ``(km, multiple_of, m)`` could have a qualifier ``(factor, 10)`` on it
+"""
 
 from abc import ABC, abstractmethod
 
@@ -16,23 +30,7 @@ __all__ = [
 
 
 class LossWeighter(ABC):
-    """Determine loss weights for triples.
-
-    In the current implementation, these weights are loss weights, i.e. they influence how much a given triple is
-    weighted in the loss function. Thus, they are primarily a tool to shape your optimization criterion, e.g. to focus
-    more or less on certain types of triples, e.g. because they are not that important to you, or you want to counteract
-    imbalances, etc.
-
-    They are not:
-
-    1. (static) weights for message passing - we already support those, cf. :class:`pykeen.nn.weighting.EdgeWeighting`
-    2. weights in the sense of how reliable a particular triple is. If you have a source of uncertainty about triples,
-       you might want to set lower loss weights for uncertain triples (in the sense of "it does not matter so much if
-       the model reproduces an uncertain label"). Another alternative would be to use softer labels for them (i.e., try
-       to predict the uncertain label directly), or more advanced ways of incorporating uncertainty.
-    3. more general qualifiers on the triples, e.g., ``(km, multiple_of, m)`` could have a qualifier ``(factor, 10)`` on
-       it
-    """
+    """Determine loss weights for triples."""
 
     @abstractmethod
     def __call__(self, h: LongTensor | None, r: LongTensor | None, t: LongTensor | None) -> FloatTensor:
@@ -78,11 +76,12 @@ class RelationLossWeighter(LossWeighter):
 
     @classmethod
     def inverse_relation_frequency(cls, mapped_triples: MappedTriples) -> Self:
-        """Create a relation weighter with inverse relation frequencies."""
+        """Create a loss weighter with inverse relation frequencies."""
         uniq, counts = mapped_triples[:, COLUMN_RELATION].unique(return_counts=True)
         if not torch.equal(uniq, torch.arange(len(uniq))):
             raise ValueError(f"Non contiguous relation ids: {uniq=}")
         return cls(weights=torch.reciprocal(counts))
 
 
+#: A resolver for loss weighters
 loss_weighter_resolver: ClassResolver[LossWeighter] = ClassResolver.from_subclasses(base=LossWeighter)
