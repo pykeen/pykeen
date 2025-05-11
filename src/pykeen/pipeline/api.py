aeae8ca2593622cbe996603ba0755d2ce5588ca3
Max Berrendorf <max.berrendorf@gmail.com>
2025-05-11 20:09:20 +0200

    remove todo

--- src/pykeen/pipeline/api.py
+++ src/pykeen/pipeline/api.py
@@ -1209,7 +1209,6 @@ def _handle_evaluation(
     filter_validation_when_testing: bool = True,
     use_tqdm: bool | None = None,
 ) -> tuple[MetricResults, float]:
-    # TODO: use LCWAEvaluationLoop
     if use_testing_data:
         evaluation_factory = testing
     elif validation is None:
