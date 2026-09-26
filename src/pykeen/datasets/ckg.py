60a51ff36fd9f58f39b65c29b4144a4935ecec56
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-26 20:09:14 +0200

    Simplify CKG ensure function

--- src/pykeen/datasets/ckg.py
+++ src/pykeen/datasets/ckg.py
@@ -1,6 +1,7 @@
 """Clinical Knowledge Graph."""
 
 import tarfile
+import tempfile
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
@@ -55,7 +56,6 @@ class CKG(TabbedDataset):
         cache_root_ = self._help_cache(cache_root)
         source = CKGSimpleSource(
             path=cache_root_.joinpath("preloaded.tsv.gz"),
-            raw_path=cache_root_.joinpath("data.tar.gz"),
             url=URL,
             force=force,
         )
@@ -66,38 +66,36 @@ class CKG(TabbedDataset):
 class CKGSimpleSource(RemoteSimpleSource):
     """A simple source for CKG."""
 
-    raw_path: Path | None = None  # TODO how to make this non-default?
-
     def ensure(self) -> None:
         """Download and process the CKG."""
-        if self.raw_path is None:
-            raise ValueError
         if self.path.is_file() and not self.force:
             return
-        download(
-            url=self.url,
-            path=self.raw_path,
-            force=self.force,
-            **(self.download_kwargs or {}),
-        )
         dfs: list[pd.DataFrame] = []
-        with tarfile.TarFile.open(self.raw_path) as tar_file:
-            if tar_file is None:
-                raise ValueError
-            for tarinfo in tar_file:
-                if not tarinfo.name.startswith("data/imports/") or not tarinfo.name.endswith(".tsv"):
-                    continue
-                path = Path(tarinfo.name)
-                if path.name.startswith("."):
-                    continue
+        with tempfile.TemporaryDirectory() as directory:
+            raw_path = Path(directory).joinpath("data.tar.gz")
+            download(
+                url=self.url,
+                path=raw_path,
+                force=self.force,
+                **(self.download_kwargs or {}),
+            )
+            with tarfile.TarFile.open(raw_path) as tar_file:
+                if tar_file is None:
+                    raise ValueError
+                for tarinfo in tar_file:
+                    if not tarinfo.name.startswith("data/imports/") or not tarinfo.name.endswith(".tsv"):
+                        continue
+                    path = Path(tarinfo.name)
+                    if path.name.startswith("."):
+                        continue
 
-                _inner_file = tar_file.extractfile(tarinfo)
-                if _inner_file is None:
-                    raise ValueError(f"Unable to open inner file: {tarinfo}")
-                with _inner_file as file:
-                    df = pd.read_csv(file, usecols=COLUMNS, sep="\t", dtype=str)
-                    df = df[COLUMNS]
-                    dfs.append(df)
+                    _inner_file = tar_file.extractfile(tarinfo)
+                    if _inner_file is None:
+                        raise ValueError(f"Unable to open inner file: {tarinfo}")
+                    with _inner_file as file:
+                        df = pd.read_csv(file, usecols=COLUMNS, sep="\t", dtype=str)
+                        df = df[COLUMNS]
+                        dfs.append(df)
         pd.concat(dfs).to_csv(self.path, sep="\t", index=False)
 
 
