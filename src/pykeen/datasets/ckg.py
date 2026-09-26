31bbe82192929601688dbf1874d8eddfe6de94fc
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-26 18:59:54 +0200

    Update ckg.py

--- src/pykeen/datasets/ckg.py
+++ src/pykeen/datasets/ckg.py
@@ -1,23 +1,26 @@
 """Clinical Knowledge Graph."""
 
 import tarfile
-from collections.abc import Iterable
+from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Any
 
 import click
 import pandas as pd
 from docdata import parse_docdata
 from more_click import verbose_option
-from pystow.utils import download
+from pystow.utils import DownloadKwargs, download
 
 from .base import TabbedDataset
+from .source import SimpleSource, RemoteSimpleSource
 from ..typing import TorchRandomHint
 
 __all__ = [
     "CKG",
 ]
 
-URL = "https://md-datasets-public-files-prod.s3.eu-west-1.amazonaws.com/d1e8d3df-2342-468a-91a9-97a981a479ad"
+URL_OLD = "https://md-datasets-public-files-prod.s3.eu-west-1.amazonaws.com/d1e8d3df-2342-468a-91a9-97a981a479ad"
+URL = "https://prod-dcd-datasets-public-files-eu-west-1.s3.eu-west-1.amazonaws.com/d1e8d3df-2342-468a-91a9-97a981a479ad"
 COLUMNS = ["START_ID", "TYPE", "END_ID"]
 
 
@@ -43,45 +46,55 @@ class CKG(TabbedDataset):
     """
 
     def __init__(
-        self,
-        random_state: TorchRandomHint = 0,
-        **kwargs,
-    ):
+        self, random_state: TorchRandomHint = 0, force: bool = False, **kwargs: Any
+    ) -> None:
         """Initialize the `CKG <https://github.com/MannLabs/CKG>`_ dataset from [santos2020]_.
 
         :param random_state: The random seed to use in splitting the dataset. Defaults to 0.
         :param kwargs: keyword arguments passed to :class:`~pykeen.datasets.base.TabbedDataset`.
         """
-        super().__init__(
-            random_state=random_state,
-            **kwargs,
+        super().__init__(random_state=random_state, **kwargs)
+        self.source = CKGSimpleSource(
+            path=self.cache_root.joinpath("preloaded.tsv.gz"),
+            raw_path=self.cache_root / "data.tar.gz",
+            url=URL,
+            force=force,
         )
 
-    @property
-    def preloaded_path(self) -> Path:
-        """The path of the concatenated dataframe, which is cached across runs."""
-        return self.cache_root.joinpath("preloaded.tsv.gz")
-
     def _get_path(self) -> Path | None:
-        return self.preloaded_path
+        return self.source.path
 
     def _get_df(self) -> pd.DataFrame:
-        path = self.preloaded_path
-        if path.exists():
-            return pd.read_csv(path, sep="\t", dtype=str)
-        df = pd.concat(self._iterate_dataframes())
-        df.to_csv(path, sep="\t", index=False)
-        return df
+        with self.source.open() as file:
+            return pd.read_csv(file, sep="\t", dtype=str)
 
-    def _iterate_dataframes(self) -> Iterable[pd.DataFrame]:
-        archive_path = self.cache_root / "data.tar.gz"
-        # note: pystow removes a partially downloaded archive on failure, so an interrupted download is retried
-        download(url=URL, path=archive_path, force=False)
-        with tarfile.TarFile.open(archive_path) as tar_file:
+
+@dataclass
+class CKGSimpleSource(RemoteSimpleSource):
+    """A simple source for CKG."""
+
+    raw_path: Path | None = None # TODO how to make this non-default?
+
+    def ensure(self) -> None:
+        """Download and process the CKG."""
+        if self.raw_path is None:
+            raise ValueError
+        if self.path.is_file() and not self.force:
+            return
+        download(
+            url=self.url,
+            path=self.raw_path,
+            force=self.force,
+            **(self.download_kwargs or {}),
+        )
+        dfs: list[pd.DataFrame] = []
+        with tarfile.TarFile.open(self.raw_path) as tar_file:
             if tar_file is None:
                 raise ValueError
             for tarinfo in tar_file:
-                if not tarinfo.name.startswith("data/imports/") or not tarinfo.name.endswith(".tsv"):
+                if not tarinfo.name.startswith(
+                    "data/imports/"
+                ) or not tarinfo.name.endswith(".tsv"):
                     continue
                 path = Path(tarinfo.name)
                 if path.name.startswith("."):
@@ -93,7 +106,8 @@ class CKG(TabbedDataset):
                 with _inner_file as file:
                     df = pd.read_csv(file, usecols=COLUMNS, sep="\t", dtype=str)
                     df = df[COLUMNS]
-                    yield df
+                    dfs.append(df)
+        pd.concat(dfs).to_csv(self.path, sep="\t", index=False)
 
 
 @click.command()
